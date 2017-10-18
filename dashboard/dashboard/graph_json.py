1ed31d4835f92d83c4fc4af99ab5479abf16b024
Simon <simonhatch@chromium.org>
2017-10-18 17:56:19 -0400

    WIP - Testing addSeriesGroup removal.
    Demo:
    
    https://dev-simonhatch-1ddd6778-dot-chromeperf.appspot.com/report?sid=5628cd6205c3822e07dc543248dbf926ce2250bf75e4345d0e63157e87bfe95b
    https://dev-simonhatch-1ddd6778-dot-chromeperf.appspot.com/group_report?keys=agxzfmNocm9tZXBlcmZyFAsSB0Fub21hbHkYgIDg66_ItwkM
    
    Bug: catapult:#
    Change-Id: I2402d97a8d5de0bd1bc8f798f4fc66dc6063c4c7

--- dashboard/dashboard/graph_json.py
+++ dashboard/dashboard/graph_json.py
@@ -82,17 +82,10 @@ class GraphJsonHandler(request_handler.RequestHandler):
       logging.error('Invalid JSON string for graphs')
       return None
 
-    test_path_dict = graphs.get('test_path_dict')
     test_path_list = graphs.get('test_path_list')
     is_selected = graphs.get('is_selected')
 
-    if test_path_dict and test_path_list:
-      logging.error(
-          'Only one of test_path_dict and test_path_list may be specified')
-      return None
-    elif test_path_dict:
-      test_paths = _ResolveTestPathDict(test_path_dict, is_selected)
-    elif test_path_list:
+    if test_path_list:
       test_paths = test_path_list
     else:
       logging.error(
@@ -109,18 +102,6 @@ class GraphJsonHandler(request_handler.RequestHandler):
         'end_rev': _PositiveIntOrNone(graphs.get('end_rev')),
     }
     return arguments
-
-
-def _ResolveTestPathDict(test_path_dict, is_selected):
-  # TODO(eakuefner): These are old-style test path dicts which means that []
-  # doesn't mean 'no tests' but rather 'all tests'. Remove this hack.
-  if is_selected:
-    for test, selected in test_path_dict.iteritems():
-      if selected == []:
-        test_path_dict[test] = 'all'
-
-  return list_tests.GetTestsForTestPathDict(
-      test_path_dict, bool(is_selected))['tests']
 
 
 def GetGraphJson(
