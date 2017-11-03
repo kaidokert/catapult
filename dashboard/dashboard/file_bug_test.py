5e4e1bf33032737389435af9f9006185d2311677
Annie Sullivan <sullivan@chromium.org>
2017-11-03 16:34:44 -0400

    If there is only a single commit in the range for all alerts, auto-assign author.
    Bug: catapult:#3698
    Change-Id: I074d5b981d087a79caa6883f428bdf0f448e7318

--- dashboard/dashboard/file_bug_test.py
+++ dashboard/dashboard/file_bug_test.py
@@ -17,11 +17,13 @@ from dashboard import mock_oauth2_decorator
 # pylint: enable=unused-import
 
 from dashboard import file_bug
+from dashboard.common import namespaced_stored_object
 from dashboard.common import testing_common
 from dashboard.common import utils
 from dashboard.models import anomaly
 from dashboard.models import bug_label_patterns
 from dashboard.models import sheriff
+from dashboard.services import crrev_service
 
 
 class MockIssueTrackerService(object):
@@ -88,12 +90,14 @@ class FileBugTest(testing_common.TestCase):
     test_key2 = utils.TestKey(test_path2)
     anomaly_key1 = self._AddAnomaly(111995, 112005, test_key1, sheriff_key)
     anomaly_key2 = self._AddAnomaly(112000, 112010, test_key2, sheriff_key)
+    anomaly_key3 = self._AddAnomaly(112015, 112015, test_key2, sheriff_key)
     rows_1 = testing_common.AddRows(test_path1, [112005])
     rows_2 = testing_common.AddRows(test_path2, [112010])
+    rows_2 = testing_common.AddRows(test_path2, [112015])
     if is_chromium:
       rows_1[0].r_commit_pos = 112005
       rows_2[0].r_commit_pos = 112010
-    return (anomaly_key1, anomaly_key2)
+    return (anomaly_key1, anomaly_key2, anomaly_key3)
 
   def _AddSampleClankAlerts(self):
     """Adds sample data and returns a dict of rev to anomaly key.
@@ -188,16 +192,20 @@ class FileBugTest(testing_common.TestCase):
   @mock.patch.object(
       file_bug.auto_bisect, 'StartNewBisectForBug',
       mock.MagicMock(return_value={'issue_id': 123, 'issue_url': 'foo.com'}))
-  def _PostSampleBug(self, is_chromium=True, is_clankium=False):
+  def _PostSampleBug(
+      self, is_chromium=True, is_clankium=False, is_single_rev=False):
     if is_clankium:
       alert_keys = self._AddSampleClankAlerts()
     else:
       alert_keys = self._AddSampleAlerts(is_chromium)
+    if is_single_rev:
+      alert_keys = alert_keys[2].urlsafe()
+    else:
+      alert_keys = '%s,%s' % (alert_keys[0].urlsafe(), alert_keys[1].urlsafe())
     response = self.testapp.post(
         '/file_bug',
         [
-            ('keys', '%s,%s' % (alert_keys[0].urlsafe(),
-                                alert_keys[1].urlsafe())),
+            ('keys', alert_keys),
             ('summary', 's'),
             ('description', 'd\n'),
             ('finish', 'true'),
@@ -239,6 +247,42 @@ class FileBugTest(testing_common.TestCase):
     self.assertIn('https://chromeperf.appspot.com/group_report?sid=', comment)
     self.assertIn(
         '\n\n\nBot(s) for this bug\'s original alert(s):\n\nlinux', comment)
+
+  @mock.patch.object(utils, 'ServiceAccountHttp', mock.MagicMock())
+  @mock.patch.object(
+      file_bug, '_GetAllCurrentVersionsFromOmahaProxy',
+      mock.MagicMock(return_value=[]))
+  @mock.patch.object(
+      crrev_service, 'GetNumbering',
+      mock.MagicMock(return_value={
+          'git_sha': '852ba7672ce02911e9f8f2a22363283adc80940e'}))
+  @mock.patch('dashboard.services.gitiles_service.CommitInfo',
+              mock.MagicMock(return_value={
+                  'author': {'email': 'foo@bar.com'},
+                  'message': 'My first commit!'}))
+  def testGet_WithFinish_CreatesBugSingleRevOwner(self):
+    # When a POST request is sent with keys specified and with the finish
+    # parameter given, an issue will be created using the issue tracker
+    # API, and the anomalies will be updated, and a response page will
+    # be sent which indicates success.
+    namespaced_stored_object.Set(
+        'repositories',
+        {"chromium": {
+            "repository_url": "https://chromium.googlesource.com/chromium/src"
+        }})
+    self.service.bug_id = 277761
+    response = self._PostSampleBug(is_single_rev=True)
+
+    # The response page should have a bug number.
+    self.assertIn('277761', response.body)
+
+    # Three HTTP requests are made when filing a bug with owner; test third
+    # request for owner hame.
+    comment = self.service.add_comment_args[1]
+    self.assertIn(
+        'Assigning to foo@bar.com because this is the only CL in range',
+        comment)
+    self.assertIn('My first commit', comment)
 
   @mock.patch.object(utils, 'ServiceAccountHttp', mock.MagicMock())
   @mock.patch.object(
