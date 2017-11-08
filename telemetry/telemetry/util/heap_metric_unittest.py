c80ad24542935d9d967b2c59827c2f70083187c8
benshayden <benjhayden@chromium.org>
2017-11-08 12:15:34 -0800

    Add a telemetry utility to measure heap usage.
    Histograms use too much memory in telemetry. Before fixing that problem, 
    let's measure it.
    
    This CL adds a utility HeapMetric() function to measure memory usage.
    
    HeapMetric() takes a root object such as a PageTestResults or
    HistogramSet, and measures how much memory it consumes by recursively
    descending through its properties. HeapMetric returns a HistogramSet
    containing one Histogram for each user-defined class encountered,
    containing one sample per instance of each class.
    
    This is based on a Python recipe by Raymond Hettinger:
    http://code.activestate.com/recipes/577504
    
    Usage:
    heap_metric.HeapMetric(histograms, label='TOT', html_filename='heap.html')
    
    Demo: http://www/~benjhayden/754453.html?s=sum&c=1&d=&a=
    This results file contains measurements of heap usage of the HistogramSet
    after running the following command on a linux desktop:
    ./tools/perf/run_benchmark system_health.memory_desktop \
    --story-filter=blank --browser=stable --pageset-repeat=1
    
    As you can see, the 14,949 HistogramBins take up 5.9MB, 80% of the total
    HistogramSet's 7.4MB.
    
    Next steps: 
    Copy HistogramBins on-write:
    https://chromium-review.googlesource.com/c/catapult/+/754119
    Use __slots__:
    https://chromium-review.googlesource.com/c/catapult/+/754634
    
    https://github.com/catapult-project/catapult/issues/3856
    Bug: chromium:780477
    Bug: catapult:#3856
    Change-Id: Id579e5f0f104b79cef3d8444c16b6a8e9d2d80d2

---
+++ telemetry/telemetry/util/heap_metric_unittest.py
@@ -0,0 +1,46 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from telemetry.util import heap_metric
+
+from tracing.value import histogram
+from tracing.value import histogram_set
+
+
+class HeapMetricUnitTest(unittest.TestCase):
+
+  def testHeapMetric(self):
+    test_data = histogram_set.HistogramSet()
+    for i in xrange(10):
+      test_hist = histogram.Histogram('test', 'n%')
+      test_hist.AddSample(i / 10.0)
+      test_data.AddHistogram(test_hist)
+
+    results = heap_metric.HeapMetric(test_data)
+
+    set_size_hist = results.GetHistogramNamed('heap:HistogramSet')
+    self.assertEquals(set_size_hist.num_values, 1)
+    self.assertEquals(set_size_hist.sum, 32627)
+
+    hist_size_hist = results.GetHistogramNamed('heap:Histogram')
+    self.assertEquals(hist_size_hist.num_values, 10)
+    self.assertEquals(hist_size_hist.sum, 29825)
+
+    bin_size_hist = results.GetHistogramNamed('heap:HistogramBin')
+    self.assertEquals(bin_size_hist.num_values, 32)
+    self.assertEquals(bin_size_hist.sum, 6696)
+
+    diag_map_size_hist = results.GetHistogramNamed('heap:DiagnosticMap')
+    self.assertEquals(diag_map_size_hist.num_values, 10)
+    self.assertEquals(diag_map_size_hist.sum, 2824)
+
+    range_size_hist = results.GetHistogramNamed('heap:Range')
+    self.assertEquals(range_size_hist.num_values, 22)
+    self.assertEquals(range_size_hist.sum, 2088)
+
+    stats_size_hist = results.GetHistogramNamed('heap:RunningStatistics')
+    self.assertEquals(stats_size_hist.num_values, 10)
+    self.assertEquals(stats_size_hist.sum, 1784)
