28e33839580c7fa5ab1d94fbd65b2a4e38b6bdb4
benshayden <benjhayden@chromium.org>
2017-11-07 16:36:38 -0800

    Add a telemetry utility to measure heap usage.
    Histograms use too much memory in telemetry. Before fixing that problem, 
    let's measure it.
    
    This CL adds a utility HeapMetric() function to measure memory usage.
    
    HeapMetric() takes a root object such as a PageTestResults or
    HistogramSet, and measures how much memory it consumes by recursively
    descending through its properties. HeapMetric returns a HistogramSet
    containing one Histogram for each user-defined class encountered,
    containing one sample per instance of each class.
    
    This is based on a Python recipe by Raymond Hettinger:
    http://code.activestate.com/recipes/577504
    
    Usage:
    heap_metric.HeapMetric(histograms, label='TOT', html_filename='heap.html')
    
    Demo: http://www/~benjhayden/754453.html?s=sum&c=1&d=&a=
    This results file contains measurements of heap usage of the HistogramSet
    after running the following command on a linux desktop:
    ./tools/perf/run_benchmark system_health.memory_desktop \
    --story-filter=blank --browser=stable --pageset-repeat=1
    
    As you can see, the 14,949 HistogramBins take up 5.9MB, 80% of the total
    HistogramSet's 7.4MB.
    
    Next steps: 
    Copy HistogramBins on-write:
    https://chromium-review.googlesource.com/c/catapult/+/754119
    Use __slots__:
    https://chromium-review.googlesource.com/c/catapult/+/754634
    
    https://github.com/catapult-project/catapult/issues/3856
    Bug: chromium:780477
    Bug: catapult:#3856
    Change-Id: Id579e5f0f104b79cef3d8444c16b6a8e9d2d80d2

---
+++ telemetry/telemetry/util/heap_metric.py
@@ -0,0 +1,118 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import codecs
+import collections
+import sys
+import time
+
+from tracing.value import histogram
+from tracing.value import histogram_set
+from tracing.value.diagnostics import reserved_infos
+from tracing_build import vulcanize_histograms_viewer
+
+
+def _IsUserDefinedInstance(obj):
+  return str(type(obj)).startswith('<class ')
+
+
+class _HeapMetric(object):
+  __slots__ = '_diagnostics_callback', '_histograms', '_seen'
+
+  def __init__(self, diagnostics_callback):
+    self._diagnostics_callback = diagnostics_callback
+    self._histograms = None
+    self._seen = set()
+
+  def Run(self, root):
+    self._histograms = histogram_set.HistogramSet()
+    total_hist = self._GetOrCreateHistogram('heap')
+    total_hist.diagnostics['breakdown'] = histogram.RelatedNameMap()
+    total_breakdown = histogram.Breakdown()
+    total_size = self._Recurse(
+        root, total_hist.diagnostics['breakdown'], total_breakdown)
+    other_size = total_size - sum(subsize for _, subsize in total_breakdown)
+
+    if other_size:
+      total_breakdown.Set('other', other_size)
+    total_hist.AddSample(total_size, dict(breakdown=total_breakdown))
+
+    self._histograms.AddSharedDiagnostic(
+        reserved_infos.TRACE_START.name,
+        histogram.DateRange(time.time() * 1000))
+    return self._histograms
+
+  def _GetOrCreateHistogram(self, name):
+    hs = self._histograms.GetHistogramsNamed(name)
+    if len(hs) > 1:
+      raise Exception('Too many Histograms named %s' % name)
+
+    if len(hs) == 1:
+      return hs[0]
+
+    hist = histogram.Histogram(name, 'sizeInBytes_smallerIsBetter')
+    hist.CustomizeSummaryOptions(dict(std=False, min=False, max=False))
+    self._histograms.AddHistogram(hist)
+    return hist
+
+  def _Recurse(self, obj, parent_related_names, parent_breakdown):
+    if id(obj) in self._seen:
+      return 0
+    self._seen.add(id(obj))
+
+    size = sys.getsizeof(obj)
+
+    related_names = parent_related_names
+    breakdown = parent_breakdown
+    hist = None
+    if _IsUserDefinedInstance(obj):
+      type_name = type(obj).__name__
+      hist = self._GetOrCreateHistogram('heap:' + type_name)
+      related_names = histogram.RelatedNameMap()
+      hist.diagnostics['breakdown'] = related_names
+      breakdown = histogram.Breakdown()
+
+    if isinstance(obj, dict):
+      for objkey, objvalue in obj.iteritems():
+        size += self._Recurse(objkey, related_names, breakdown)
+        size += self._Recurse(objvalue, related_names, breakdown)
+    elif isinstance(obj, (tuple, list, set, frozenset, collections.deque)):
+      # Can't use collections.Iterable because strings are iterable, but
+      # sys.getsizeof() already handles strings, we don't need to iterate over
+      # them.
+      for elem in obj:
+        size += self._Recurse(elem, related_names, breakdown)
+    elif hasattr(obj.__class__, '__slots__'):
+      for slot in obj.__class__.__slots__:
+        if hasattr(obj, slot):
+          size += self._Recurse(getattr(obj, slot), related_names, breakdown)
+    elif hasattr(obj, '__dict__'):
+      size += self._Recurse(obj.__dict__, related_names, breakdown)
+
+    if hist is None:
+      return size
+
+    parent_related_names.Set(type_name, hist.name)
+    parent_breakdown.Set(type_name, parent_breakdown.Get(type_name) + size)
+    breakdown.Set('other', size - sum(subsize for _, subsize in breakdown))
+    sample_diagnostics = {'breakdown': breakdown}
+    if self._diagnostics_callback:
+      sample_diagnostics.update(self._diagnostics_callback(obj))
+    hist.AddSample(size, sample_diagnostics)
+    return size
+
+
+def HeapMetric(root, label=None, html_filename=None, diagnostics_callback=None):
+  histograms = _HeapMetric(diagnostics_callback).Run(root)
+
+  if label:
+    histograms.AddSharedDiagnostic(
+        reserved_infos.LABELS.name, histogram.GenericSet([label]))
+
+  if html_filename:
+    output_stream = codecs.open(html_filename, mode='ar+', encoding='utf-8')
+    vulcanize_histograms_viewer.VulcanizeAndRenderHistogramsViewer(
+        histograms.AsDicts(), output_stream)
+
+  return histograms
