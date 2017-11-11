ae40a56da79b2fc7a6ba49ae5965016f096bc5d5
benshayden <benjhayden@chromium.org>
2017-11-11 00:36:06 -0800

    Translate HistogramGrouping to python.
    Change-Id: Iedbacc6a84faf2ce7ca18c024dac8cdb0f23c654

---
+++ tracing/tracing/value/histogram_grouping_unittest.py
@@ -0,0 +1,24 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from tracing.value import histogram_grouping
+
+class HistogramGroupingUnittest(unittest.TestCase):
+
+  def testBooleanTags(self):
+    histogram_grouping
+
+  def testKeyValueTags(self):
+    pass
+
+  def testDisplayLabel(self):
+    pass
+
+  def testGenericSet(self):
+    pass
+
+  def testDateRange(self):
+    pass
