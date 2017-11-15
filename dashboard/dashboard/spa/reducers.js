adafadac2bfef5b32742b6459a6f80ea1fd9fa68
benshayden <benjhayden@chromium.org>
2017-11-15 11:17:50 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/reducers.js
@@ -0,0 +1,39 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+
+(function() {
+  window.REDUCERS = (state, action) => {
+    if (state === undefined) return DEFAULT_STATE;
+    if (!REDUCERS[action.type]) return state;
+    return REDUCERS[action.type](state, action);
+  };
+
+  const NEW_CHART_SECTION_STATE = {
+    type: 'chart',
+  };
+
+  REDUCERS.addChartSection = (state, action) => {
+    return Object.assign({}, state, {
+      sections: state.sections.concat([
+        Object.assign({}, NEW_CHART_SECTION_STATE),
+      ]),
+    });
+  };
+
+  const NEW_ALERTS_SECTION_STATE = {
+    type: 'alerts',
+    rows: [],
+  };
+
+  REDUCERS.addAlertsSection = (state, action) => {
+    return Object.assign({}, state, {
+      sections: state.sections.concat([
+        Object.assign({}, NEW_ALERTS_SECTION_STATE),
+      ]),
+    });
+  };
+})();
