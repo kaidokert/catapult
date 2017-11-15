6227e1dbe5de12252ea8195c24d01f99b8b3725a
benshayden <benjhayden@chromium.org>
2017-11-15 15:57:23 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/action_creators.js
@@ -0,0 +1,39 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+
+(function() {
+  // Action creators MAY have side-effects.
+  // Action creators MUST return an object with type containing a string name of a
+  // reducer.
+  // Action creators MAY take any number of parameters.
+  // Action creators SHOULD NOT have the same name as any reducer in order to avoid
+  // confusiong, but technically COULD.
+
+  const ACTION_CREATORS = {};
+
+  ACTION_CREATORS.newAlertsSection = () => {
+    return {type: 'addAlertsSection'};
+  };
+
+  ACTION_CREATORS.newChartSection = () => {
+    return {type: 'addChartsSection'};
+  };
+
+  ACTION_CREATORS.newReleasingSection = () => {
+    return {type: 'addReleasingSection'};
+  };
+
+  ACTION_CREATORS.loadAlerts = (sectionId, sheriff) => {
+    return {
+      type: 'addAlerts',
+      sectionId,
+      sheriff,
+    };
+  };
+
+  window.ACTION_CREATORS = ACTION_CREATORS;
+})();
