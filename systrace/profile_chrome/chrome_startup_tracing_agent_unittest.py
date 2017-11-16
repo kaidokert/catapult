40c5ad8ed929b4c1b60d3d499976a019697d921e
Xi Han <hanxi@google.com>
2017-11-16 11:23:25 -0500

    Startup tracing for WebAPKs
    In this CL, we introduce "--external-app-package" in adb_profile_chrome_startup.py to
    customize the package name for the launching Intent. Therefore,
    we can launch a URL specified by "--url" in a WebAPK.
    
    If installing a WebAPK from mobile.twitter.com, a sample script could be:
    build/android/adb_profile_chrome_startup  --browser build --url=https://mobile.twitter.com/notifications --external-app-package=org.chromium.webapk.ace0b15a6ce931426
    Bug:catapult:#4039
    Change-Id: Ib6254d71204daeccee2728c2e661b818c31d762a

--- systrace/profile_chrome/chrome_startup_tracing_agent_unittest.py
+++ systrace/profile_chrome/chrome_startup_tracing_agent_unittest.py
@@ -17,7 +17,7 @@ class ChromeAgentTest(agents_unittest.BaseAgentTest):
   @decorators.Disabled
   def testTracing(self):
     agent = chrome_startup_tracing_agent.ChromeStartupTracingAgent(
-        self.device, self.package_info, False, 'https://www.google.com')
+        self.device, self.package_info, '', False, 'https://www.google.com')
 
     try:
       agent.StartAgentTracing(None)
