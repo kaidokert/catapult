22e880fb5618333cb1c3545b910a740a8e40bbf9
benshayden <benjhayden@chromium.org>
2017-11-17 12:19:38 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/dist/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/reducers.js
@@ -0,0 +1,343 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+
+(function() {
+  // Reducers MUST NOT have side effects.
+  // Reducers MUST NOT modify state.
+  // Reducers MUST return a new object.
+  // Reducers MAY copy properties from state.
+
+  const REDUCERS = new Map();
+
+  const DEFAULT_STATE = {
+    sections: [],
+  };
+
+  const NEW_SECTION_STATES = new Map();
+
+  NEW_SECTION_STATES.set('chart', {
+    isLoading: false,
+    onlyChart: false,
+    testSuiteDescription: 'test suite description',
+    histograms: undefined,
+    testPathComponents: [
+      {
+        placeholder: 'Test suite',
+        canAggregate: true,
+        isFocused: true,
+        value: '',
+        options: [
+          'system_health.common_desktop',
+          'system_health.common_mobile',
+          'system_health.memory_desktop',
+          'system_health.memory_mobile',
+        ],
+      },
+      {
+        placeholder: 'Bot',
+        canAggregate: true,
+        isFocused: false,
+        value: '',
+        options: [
+          'nexus5X',
+          'nexus5',
+          'mac',
+          'win',
+          'linux',
+        ],
+      },
+      {
+        placeholder: 'Measurement',
+        canAggregate: true,
+        isFocused: false,
+        value: '',
+        options: [
+          'PSS',
+          'power',
+          'TTFMP',
+          'TTI',
+        ],
+      },
+      {
+        placeholder: 'Stories',
+        canAggregate: true,
+        isFocused: false,
+        value: '',
+        options: [
+          'load:news:cnn',
+          'load:news:nytimes',
+          'load:news:qq',
+        ],
+      },
+      {
+        placeholder: 'Statistics',
+        canAggregate: false,
+        value: 'avg',
+        isFocused: false,
+        options: [
+          'avg',
+          'std',
+          'median',
+          '90%',
+          '95%',
+          '99%',
+        ],
+      },
+    ],
+  });
+
+  NEW_SECTION_STATES.set('alerts', {
+    sheriffOrBug: '',
+    isMenuFocused: true,
+    rows: [
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+    ],
+    areRowsPlaceholders: true,
+    isLoading: false,
+    showingImprovements: false,
+    showingTriaged: false,
+    sheriffList: [
+      "ARC Perf Sheriff",
+      "Angle Perf Sheriff",
+      "Binary Size Sheriff",
+      "Blink Memory Mobile Sheriff",
+      "Chrome OS Graphics Perf Sheriff",
+      "Chrome OS Installer Perf Sheriff",
+      "Chrome OS Perf Sheriff",
+      "Chrome Perf Accessibility Sheriff",
+      "Chromium Perf AV Sheriff",
+      "Chromium Perf Sheriff",
+      "Chromium Perf Sheriff - Sub-series",
+      "CloudView Perf Sheriff",
+      "Cronet Perf Sheriff",
+      "Jochen",
+      "Mojo Perf Sheriff",
+      "NaCl Perf Sheriff",
+      "Network Service Sheriff",
+      "OWP Storage Perf Sheriff",
+      "Oilpan Perf Sheriff",
+      "Pica Sheriff",
+      "Power Perf Sheriff",
+      "Service Worker Perf Sheriff",
+      "Tracing Perftests Sheriff",
+      "V8 Memory Perf Sheriff",
+      "V8 Perf Sheriff",
+      "WebView Perf Sheriff",
+    ],
+  });
+
+  NEW_SECTION_STATES.set('releasing', {
+    isLoading: false,
+  });
+
+  function assign(obj, delta) {
+    return Object.assign({}, obj, delta);
+  }
+
+  function assignInArray(arr, index, delta) {
+    return arr.slice(0, index).concat([
+      assign(arr[index], delta),
+    ]).concat(arr.slice(index + 1));
+  }
+
+  function assignSection(state, sectionId, delta) {
+    return assign(state, {
+      sections: assignInArray(state.sections, sectionId, delta),
+    });
+  }
+
+  REDUCERS.set('appendSection', (state, action) => {
+    return assign(state, {
+      sections: state.sections.concat([
+        assign({type: action.section}, NEW_SECTION_STATES.get(action.section)),
+      ]),
+    });
+  });
+
+  REDUCERS.set('closeSection', (state, action) => {
+    return assign(state, {
+      sections: state.sections.slice(0, action.sectionId).concat(
+        state.sections.slice(action.sectionId + 1)),
+    });
+  });
+
+  REDUCERS.set('receiveAlerts', (state, action) => {
+    return assignSection(state, action.sectionId, {
+      areRowsPlaceholders: false,
+      isMenuFocused: false,
+      rows: action.rows,
+      summary: action.summary,
+    });
+  });
+
+  REDUCERS.set('toggleAlertGroupExpanded', (state, action) => {
+    const oldRows = state.sections[action.sectionId].rows;
+    const oldRow = oldRows[action.rowIndex];
+
+    const newRow = assign(oldRow, {
+      isGroupExpanded: !oldRow.isGroupExpanded,
+    });
+
+    let newRows = oldRows.slice(0, action.rowIndex);
+    newRows.push(newRow);
+
+    if (newRow.isGroupExpanded) {
+      newRows = newRows.concat(newRow.subRows);
+      newRows = newRows.concat(oldRows.slice(action.rowIndex + 1));
+    } else {
+      newRows = newRows.concat(oldRows.slice(
+        action.rowIndex + 1 + newRow.subRows.length));
+    }
+
+    return assignSection(state, action.sectionId, {
+      rows: newRows,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {String} action.value
+   */
+  REDUCERS.set('selectTestPathComponent', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    const oldComponent = oldComponents[action.componentIndex];
+    let newComponents = assignInArray(oldComponents, action.componentIndex, {
+      value: action.value,
+      isFocused: false,
+    });
+    if ((1 + action.componentIndex) < newComponents.length) {
+      newComponents = assignInArray(newComponents, 1 + action.componentIndex, {
+        isFocused: true,
+      });
+    }
+
+    return assignSection(state, action.sectionId, {
+      testPathComponents: newComponents,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {Boolean} action.isFocused
+   */
+  REDUCERS.set('testPathFocus', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    return assignSection(state, action.sectionId, {
+      testPathComponents: assignInArray(oldComponents, action.componentIndex, {
+        isFocused: action.isFocused,
+      }),
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {String} action.value
+   */
+  REDUCERS.set('testPathFocus', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    return assignSection(state, action.sectionId, {
+      testPathComponents: assignInArray(oldComponents, action.componentIndex, {
+        value: action.value,
+      }),
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Boolean} action.isFocused
+   */
+  REDUCERS.set('focusAlertsMenu', (state, action) => {
+    return assignSection(state, action.sectionId, {
+      isMenuFocused: action.isFocused,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {String} action.sheriffOrBug
+   */
+  REDUCERS.set('alertsMenuKeydown', (state, action) => {
+    return assignSection(state, action.sectionId, {
+      sheriffOrBug: action.sheriffOrBug,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   */
+  REDUCERS.set('alertsMenuClear', (state, action) => {
+    return assignSection(state, action.sectionId, {
+      sheriffOrBug: '',
+      isMenuFocused: true,
+    });
+  });
+
+  function rootReducer(state, action) {
+    if (state === undefined) return DEFAULT_STATE;
+    if (!REDUCERS.has(action.type)) return state;
+    return REDUCERS.get(action.type)(state, action);
+  };
+
+  function sectionProperties(configs) {
+    const properties = {sectionId: Number};
+    for (const [name, config] of Object.entries(configs)) {
+      properties[name] = Object.assign(config, {
+        statePath(state) {
+          if (!state.sections[this.sectionId]) return undefined;
+          return state.sections[this.sectionId][name];
+        }
+      });
+    }
+    return properties;
+  }
+
+  window.sectionProperties = sectionProperties;
+  window.rootReducerForTesting = rootReducer;
+  window.ReduxMixin = PolymerRedux(Redux.createStore(rootReducer, DEFAULT_STATE));
+})();
