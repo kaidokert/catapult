3342e25cab6cca8e7d597b3a461838819adb5cfb
benshayden <benjhayden@chromium.org>
2017-11-18 23:30:17 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/chromeperf-app-reducers.js
@@ -0,0 +1,201 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+
+tr.exportTo('cp', () => {
+  const NEW_SECTION_STATES = new Map();
+
+  NEW_SECTION_STATES.set('chart', {
+    isLoading: false,
+    onlyChart: false,
+    testSuiteDescription: 'test suite description',
+    histograms: undefined,
+    testPathComponents: [
+      {
+        placeholder: 'Test suite',
+        canAggregate: true,
+        isFocused: true,
+        value: '',
+        options: [
+          'system_health.common_desktop',
+          'system_health.common_mobile',
+          'system_health.memory_desktop',
+          'system_health.memory_mobile',
+        ],
+      },
+      {
+        placeholder: 'Bot',
+        canAggregate: true,
+        isFocused: false,
+        value: '',
+        options: [
+          'nexus5X',
+          'nexus5',
+          'mac',
+          'win',
+          'linux',
+        ],
+      },
+      {
+        placeholder: 'Measurement',
+        canAggregate: false,
+        isFocused: false,
+        value: '',
+        options: [
+          'PSS',
+          'power',
+          'TTFMP',
+          'TTI',
+        ],
+      },
+      {
+        placeholder: 'Stories',
+        canAggregate: true,
+        isFocused: false,
+        value: '',
+        options: [
+          'load:news:cnn',
+          'load:news:nytimes',
+          'load:news:qq',
+        ],
+      },
+      {
+        placeholder: 'Statistics',
+        canAggregate: false,
+        value: 'avg',
+        isFocused: false,
+        options: [
+          'avg',
+          'std',
+          'median',
+          '90%',
+          '95%',
+          '99%',
+        ],
+      },
+    ],
+  });
+
+  NEW_SECTION_STATES.set('alerts', {
+    sheriffOrBug: '',
+    isMenuFocused: true,
+    rows: [
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+    ],
+    areRowsPlaceholders: true,
+    isLoading: false,
+    showingImprovements: false,
+    showingTriaged: false,
+    sheriffList: [
+      "ARC Perf Sheriff",
+      "Angle Perf Sheriff",
+      "Binary Size Sheriff",
+      "Blink Memory Mobile Sheriff",
+      "Chrome OS Graphics Perf Sheriff",
+      "Chrome OS Installer Perf Sheriff",
+      "Chrome OS Perf Sheriff",
+      "Chrome Perf Accessibility Sheriff",
+      "Chromium Perf AV Sheriff",
+      "Chromium Perf Sheriff",
+      "Chromium Perf Sheriff - Sub-series",
+      "CloudView Perf Sheriff",
+      "Cronet Perf Sheriff",
+      "Jochen",
+      "Mojo Perf Sheriff",
+      "NaCl Perf Sheriff",
+      "Network Service Sheriff",
+      "OWP Storage Perf Sheriff",
+      "Oilpan Perf Sheriff",
+      "Pica Sheriff",
+      "Power Perf Sheriff",
+      "Service Worker Perf Sheriff",
+      "Tracing Perftests Sheriff",
+      "V8 Memory Perf Sheriff",
+      "V8 Perf Sheriff",
+      "WebView Perf Sheriff",
+    ],
+  });
+
+  NEW_SECTION_STATES.set('releasing', {
+    isLoading: false,
+    isMenuFocused: true,
+    report: '',
+    reportOptions: [
+      'Chromeperf Public',
+      'Loading',
+      'Input',
+      'Memory',
+      'Battery',
+    ],
+    milestone: 64,
+    isPreviousMilestone: true,
+    isNextMilestone: false,
+  });
+
+  cp.REDUCERS.set('newSection', (state, action) => {
+    const newSection = cp.assign(
+      {type: action.section},
+      NEW_SECTION_STATES.get(action.section));
+    if (state.containsDefaultSection) {
+      return cp.assign(state, {
+        sections: [newSection],
+        containsDefaultSection: false,
+      });
+    }
+    return cp.assign(state, {
+      sections: state.sections.concat([newSection]),
+    });
+  });
+
+  cp.REDUCERS.set('closeSection', (state, action) => {
+    return cp.assign(state, {
+      sections: state.sections.slice(0, action.sectionId).concat(
+        state.sections.slice(action.sectionId + 1)),
+    });
+  });
+
+  return {
+  };
+});
