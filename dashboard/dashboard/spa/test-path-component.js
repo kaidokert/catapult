825b7107ba784d2418a778f8541207dc90fece91
benshayden <benjhayden@chromium.org>
2017-11-22 11:39:58 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/test-path-component.js
@@ -0,0 +1,151 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+
+tr.exportTo('cp', () => {
+  function makeProperties(configs) {
+    const properties = {
+      sectionId: Number,
+      componentIndex: Number,
+    };
+    for (const [name, config] of Object.entries(configs)) {
+      properties[name] = Object.assign(config, {
+        statePath(state) {
+          if (!state.sections[this.sectionId]) return undefined;
+          if (!state.sections[this.sectionId].testPathComponents[
+              this.componentIndex]) {
+            return undefined;
+          }
+          return state.sections[this.sectionId].testPathComponents[
+              this.componentIndex][name];
+        }
+      });
+    }
+    return properties;
+  }
+
+  class TestPathComponent extends cp.Element {
+    static get is() { return 'test-path-component'; }
+
+    static get properties() {
+      return makeProperties({
+        placeholder: {type: String},
+        value: {type: String},
+        options: {type: Array},
+        isFocused: {type: Boolean},
+        canAggregate: {type: Boolean},
+        isAggregated: {type: Boolean},
+      });
+    }
+
+    multipleValues_(/* TODO values */) {
+      return false;
+      // return values && values.length > 1;
+    }
+
+    onFocus_(e) {
+      this.dispatch({
+        type: 'testPathFocus',
+        sectionId: this.sectionId,
+        componentIndex: this.componentIndex,
+        isFocused: true,
+      });
+    }
+
+    onBlur_(e) {
+      this.dispatch({
+        type: 'testPathFocus',
+        sectionId: this.sectionId,
+        componentIndex: this.componentIndex,
+        isFocused: false,
+      });
+    }
+
+    onKeydown_(e) {
+      this.dispatch({
+        type: 'testPathKeydown',
+        sectionId: this.sectionId,
+        componentIndex: this.componentIndex,
+        value: e.detail.value,
+      });
+    }
+
+    onSelect_(e) {
+      if (e.detail.value !== this.value) {
+        this.dispatch({
+          type: 'selectTestPathComponent',
+          sectionId: this.sectionId,
+          componentIndex: this.componentIndex,
+          value: e.detail.value,
+        });
+      }
+    }
+
+    onAggregateChange_(e) {
+      this.dispatch({
+        type: 'testPathAggregate',
+        sectionId: this.sectionId,
+        componentIndex: this.componentIndex,
+        value: this.$.aggregate.checked,
+      });
+    }
+  }
+  customElements.define(TestPathComponent.is, TestPathComponent);
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {String} action.value
+   */
+  cp.REDUCERS.set('selectTestPathComponent', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    const oldComponent = oldComponents[action.componentIndex];
+    let newComponents = cp.assignInArray(oldComponents, action.componentIndex, {
+      value: action.value,
+      isFocused: false,
+    });
+    if ((1 + action.componentIndex) < newComponents.length) {
+      newComponents = cp.assignInArray(newComponents, 1 + action.componentIndex, {
+        isFocused: true,
+      });
+    }
+
+    return cp.assignSection(state, action.sectionId, {
+      testPathComponents: newComponents,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {Boolean} action.isFocused
+   */
+  cp.REDUCERS.set('testPathFocus', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    return cp.assignSection(state, action.sectionId, {
+      testPathComponents: cp.assignInArray(oldComponents, action.componentIndex, {
+        isFocused: action.isFocused,
+      }),
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {String} action.value
+   */
+  cp.REDUCERS.set('testPathKeydown', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    return cp.assignSection(state, action.sectionId, {
+      testPathComponents: cp.assignInArray(oldComponents, action.componentIndex, {
+        value: action.value,
+      }),
+    });
+  });
+
+  return {
+  };
+});
