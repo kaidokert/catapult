78da6ac1f532c72edab1b6c034d4cd69ecdf8b66
benshayden <benjhayden@chromium.org>
2017-11-22 12:08:39 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/releasing-section.js
@@ -0,0 +1,397 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+
+tr.exportTo('cp', () => {
+  class ReleasingSection extends cp.Element {
+    static get is() { return 'releasing-section'; }
+
+    static get properties() {
+      return Object.assign(cp.sectionProperties({
+        isLoading: {type: Boolean},
+        report: {type: String},
+        isMenuFocused: {type: Boolean},
+        reportOptions: {type: Array},
+        milestone: {type: Number},
+        isPreviousMilestone: {type: Boolean},
+        isNextMilestone: {type: Boolean},
+        tables: {type: Array},
+      }), {
+        previousMilestone: {
+          type: Number,
+          computed: 'plus_(milestone, -1)',
+        },
+
+        nextMilestone: {
+          type: Number,
+          computed: 'plus_(milestone, 1)',
+        },
+      });
+    }
+
+    async ready() {
+      super.ready();
+      this.dispatch(cp.updateSectionWidth(this));
+    }
+
+    plus_(x, y) {
+      return x + y;
+    }
+
+    closeSection_() {
+      this.dispatch(cp.closeSection(this.sectionId));
+    }
+
+    onMenuFocus_() {
+      this.dispatch(focusMenu(this.sectionId, true));
+    }
+
+    onMenuBlur_() {
+      this.dispatch(focusMenu(this.sectionId, false));
+    }
+
+    onMenuKeydown_(e) {
+      this.dispatch(keydownMenu(this.sectionId, e.detail.value));
+    }
+
+    onMenuClear_() {
+      this.dispatch(clearMenu(this.sectionId));
+    }
+
+    onMenuSelect_() {
+      this.dispatch(selectReport(this.sectionId, this.$.menu.value));
+    }
+
+    previousMilestone_() {
+      this.dispatch(selectMilestone(this.sectionId, this.milestone - 1));
+    }
+
+    nextMilestone_() {
+      this.dispatch(selectMilestone(this.sectionId, this.milestone + 1));
+    }
+  }
+  customElements.define(ReleasingSection.is, ReleasingSection);
+
+  const focusMenu = (sectionId, isMenuFocused) =>
+    async (dispatch, getState) => {
+      dispatch({
+        type: 'releasing-section.focusMenu',
+        sectionId,
+        isMenuFocused,
+      });
+    };
+
+  const keydownMenu = (sectionId, report) => async (dispatch, getState) => {
+    dispatch({
+      type: 'releasing-section.keydownMenu',
+      sectionId,
+      report,
+    });
+  };
+
+  const clearMenu = sectionId => async (dispatch, getState) => {
+    dispatch({
+      type: 'releasing-section.clearMenu',
+      sectionId,
+    });
+  };
+
+  const selectReport = (sectionId, report) => async (dispatch, getState) => {
+    dispatch({
+      type: 'releasing-section.selectReport',
+      sectionId,
+      report,
+    });
+  };
+
+  const selectMilestone = (sectionId, milestone) =>
+    async (dispatch, getState) => {
+      dispatch({
+        type: 'releasingMilestone',
+        sectionId,
+        milestone,
+      });
+    };
+
+  const DUMMY_RELEASING_SECTION = {
+    type: 'releasing',
+    report: 'Chromeperf Public',
+    isLoading: false,
+    isMenuFocused: false,
+    reportOptions: [
+      'Chromeperf Public',
+      'Loading',
+      'Input',
+      'Memory',
+      'Battery',
+    ],
+    milestone: 64,
+    isPreviousMilestone: true,
+    isNextMilestone: false,
+    tables: [
+      {
+        title: 'health-plan-clankium-phone',
+        currentVersion: '517411-73a',
+        referenceVersion: '508578-c23',
+        rows: [
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            evenCategory: false,
+            category: 'Foreground',
+            href: '#',
+            name: 'Java Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            evenCategory: true,
+            category: 'Background',
+            href: '#',
+            name: 'Java Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+        ],
+      },
+      {
+        title: 'health-plan-clankium-low-end-phone',
+        currentVersion: '517411-73a',
+        referenceVersion: '508578-c23',
+        rows: [
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            evenCategory: false,
+            category: 'Foreground',
+            href: '#',
+            name: 'Java Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            evenCategory: true,
+            category: 'Background',
+            href: '#',
+            name: 'Java Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit: tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+        ],
+      },
+    ],
+  };
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Boolean} action.isMenuFocused
+   */
+  cp.REDUCERS.set('releasing-section.focusMenu', (state, action) => {
+    return cp.assignSection(state, action.sectionId, {
+      isMenuFocused: action.isMenuFocused,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {String} action.report
+   */
+  cp.REDUCERS.set('releasing-section.selectReport', (state, action) => {
+    return cp.assignSection(state, action.sectionId, DUMMY_RELEASING_SECTION);
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   */
+  cp.REDUCERS.set('releasing-section.clearMenu', (state, action) => {
+    return cp.assignSection(state, action.sectionId, {
+      report: '',
+      isMenuFocused: true,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {String} action.report
+   */
+  cp.REDUCERS.set('releasing-section.keydownMenu', (state, action) => {
+    return cp.assignSection(state, action.sectionId, {
+      report: action.report,
+      isMenuFocused: true,
+    });
+  });
+
+  return {
+  };
+});
