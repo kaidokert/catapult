f731abf97f660008de934d7c209767368c3a45a3
benshayden <benjhayden@chromium.org>
2017-11-23 12:21:30 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/chart-section.js
@@ -0,0 +1,170 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class ChartSection extends cp.Element {
+    static get is() { return 'chart-section'; }
+
+    static get properties() {
+      return cp.sectionProperties({
+        isLoading: {type: Boolean},
+        histograms: {type: tr.v.HistogramSet},
+        onlyChart: {type: Boolean},
+        minimapLayout: {type: Object},
+        chartLayout: {type: Object},
+        testPathComponents: {type: Array},
+        testSuiteDescription: {type: String},
+      });
+    }
+
+    async ready() {
+      super.ready();
+      this.dispatch(cp.ChromeperfApp.updateSectionWidth(this));
+    }
+
+    closeSection_() {
+      this.dispatch(cp.ChromeperfApp.closeSection(this.sectionId));
+    }
+
+    toggleChartOnly_() {
+      this.dispatch(ChartSection.toggleChartOnly(this.sectionId));
+    }
+
+    static toggleChartOnly(sectionId) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'chart-section.toggleChartOnly',
+          sectionId: sectionId,
+        });
+      };
+    }
+
+    static loadTimeseries(sectionId) {
+      return async (dispatch, getState) => {
+        console.log('loadTimeseries', sectionId);
+        // TODO fetch rows and histograms from backend
+        // TODO cache
+        // TODO set/clear isLoading
+        // TODO colors
+        // TODO LineChart.layout()
+
+        const state = getState();
+        const maxYAxisTickWidth = 60;
+        const textHeight = 20;
+        const minimapHeight = 60;
+        const chartHeight = 200;
+
+        const minimapXAxisTicks = [
+          'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+          '2018', 'Feb', 'Mar', 'Apr', 'May',
+        ].map((text, index, texts) => ({
+          text,
+          x: maxYAxisTickWidth + (state.sectionWidth - maxYAxisTickWidth) * index / texts.length,
+          y: minimapHeight - 5,
+        }));
+
+        const minimapSequences = [{
+          path: 'M' + maxYAxisTickWidth + ',20',
+          color: 'blue',
+          data: [{x: maxYAxisTickWidth, y: 20}],
+          dotColor: 'blue',
+        }];
+        for (let i = 10; i < (state.sectionWidth - maxYAxisTickWidth); i += 10) {
+          const datum = {
+            x: maxYAxisTickWidth + i,
+            y: parseInt(Math.random() * (minimapHeight - textHeight)),
+          };
+          minimapSequences[0].data.push(datum);
+          minimapSequences[0].path += ' L' + datum.x + ',' + datum.y;
+        }
+
+        const minimapAntiBrushes = [];
+
+        const chartSequences = [{
+          path: 'M' + maxYAxisTickWidth + ',100',
+          color: 'blue',
+          data: [{x: maxYAxisTickWidth, y: 100}],
+          dotColor: 'blue',
+        }];
+        for (let i = 10; i < (state.sectionWidth - maxYAxisTickWidth); i += 10) {
+          const datum = {
+            x: maxYAxisTickWidth + i,
+            y: parseInt(Math.random() * (chartHeight - textHeight)),
+          };
+          chartSequences[0].data.push(datum);
+          chartSequences[0].path += ' L' + datum.x + ',' + datum.y;
+        }
+
+        const chartXAxisTicks = [
+          'Dec', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+          '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
+          '25', '26', '27', '28', '29', '30', '31', '2018', '2', '3',
+        ].map((text, index, texts) => ({
+          text,
+          x: maxYAxisTickWidth + (state.sectionWidth - maxYAxisTickWidth) * index / texts.length,
+          y: chartHeight - 5,
+        }));
+
+        const chartYAxisTicks = [
+          '9MB', '8MB', '7MB', '6MB', '5MB', '4MB', '3MB', '2MB', '1MB',
+        ].map((text, index, texts) => ({
+          text,
+          x: 0,
+          y: (chartHeight - textHeight) * index / texts.length,
+        }));
+
+        const chartAntiBrushes = [];
+
+        dispatch({
+          type: 'chart-section.layoutChart',
+          sectionId,
+          minimapLayout: {
+            width: state.sectionWidth,
+            height: minimapHeight,
+            left: maxYAxisTickWidth,
+            right: state.sectionWidth,
+            top: 0,
+            dotRadius: 6,
+            bottom: minimapHeight - textHeight,
+            sequences: minimapSequences,
+            xAxisTicks: minimapXAxisTicks,
+            showXAxisTickLines: true,
+            fhowYAxisTickLines: false,
+            antiBrushes: minimapAntiBrushes,
+          },
+          chartLayout: {
+            width: state.sectionWidth,
+            height: chartHeight,
+            left: maxYAxisTickWidth,
+            right: state.sectionWidth,
+            top: 0,
+            dotRadius: 6,
+            showYAxisTickLines: true,
+            showXAxisTickLines: true,
+            bottom: chartHeight - textHeight,
+            sequences: chartSequences,
+            yAxisTicks: chartYAxisTicks,
+            xAxisTicks: chartXAxisTicks,
+            antiBrushes: chartAntiBrushes,
+          },
+        });
+      };
+    }
+  }
+  customElements.define(ChartSection.is, ChartSection);
+
+  cp.REDUCERS.set('chart-section.layoutChart', (state, action) => {
+    return cp.assignSection(state, action.sectionId, {
+      minimapLayout: action.minimapLayout,
+      chartLayout: action.chartLayout,
+    });
+  });
+
+  return {
+    ChartSection,
+  };
+});
+
