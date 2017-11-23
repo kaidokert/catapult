aad5e24f9e3359ca3259286ca4c4ab7f70c18955
benshayden <benjhayden@chromium.org>
2017-11-23 12:42:27 -0800

    Chromeperf dashboard redesign SPA Views
    https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/chart-section.js
@@ -0,0 +1,202 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class ChartSection extends cp.Element {
+    static get is() { return 'chart-section'; }
+
+    static get properties() {
+      return cp.sectionProperties({
+        isLoading: {type: Boolean},
+        histograms: {type: tr.v.HistogramSet},
+        onlyChart: {type: Boolean},
+        minimapLayout: {type: Object},
+        chartLayout: {type: Object},
+        testPathComponents: {type: Array},
+        testSuiteDescription: {type: String},
+      });
+    }
+
+    async ready() {
+      super.ready();
+      this.dispatch(cp.ChromeperfApp.updateSectionWidth(this));
+    }
+
+    closeSection_() {
+      this.dispatch(cp.ChromeperfApp.closeSection(this.sectionId));
+    }
+
+    toggleChartOnly_() {
+      this.dispatch(ChartSection.toggleChartOnly(this.sectionId));
+    }
+
+    static toggleChartOnly(sectionId) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'chart-section.toggleChartOnly',
+          sectionId: sectionId,
+        });
+      };
+    }
+
+    static loadTimeseries(sectionId) {
+      return async (dispatch, getState) => {
+        console.log('loadTimeseries', sectionId);
+        // TODO fetch rows and histograms from backend
+        // TODO cache
+        // TODO set/clear isLoading
+        // TODO colors
+        // TODO LineChart.layout()
+
+        const state = getState();
+        const maxYAxisTickWidth = 30;
+        const textHeight = 20;
+        const minimapHeight = 60;
+        const chartHeight = 200;
+
+        const minimapXAxisTicks = [
+          'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+          '2018', 'Feb', 'Mar', 'Apr', 'May',
+        ].map((text, index, texts) => ({
+          text,
+          x: maxYAxisTickWidth + (state.sectionWidth - maxYAxisTickWidth) * index / texts.length,
+          y: minimapHeight - 5,
+        }));
+
+        const minimapSequences = [{
+          path: 'M' + maxYAxisTickWidth + ',20',
+          color: 'blue',
+          data: [{x: maxYAxisTickWidth, y: 20}],
+          dotColor: 'blue',
+        }];
+        for (let i = 10; i < (state.sectionWidth - maxYAxisTickWidth); i += 10) {
+          const datum = {
+            x: maxYAxisTickWidth + i,
+            y: 20 + parseInt(Math.random() * (minimapHeight - textHeight - 20)),
+          };
+          minimapSequences[0].data.push(datum);
+          minimapSequences[0].path += ' L' + datum.x + ',' + datum.y;
+        }
+
+        const minimapAntiBrushes = [
+          {
+            x: maxYAxisTickWidth,
+            width: parseInt(state.sectionWidth * 0.66),
+            leftHandle: false,
+            rightHandle: true,
+            rightHandleX: maxYAxisTickWidth + parseInt(state.sectionWidth * 0.66) - 5,
+          },
+          {
+            x: parseInt(state.sectionWidth * 0.86),
+            width: parseInt(state.sectionWidth * 0.14),
+            leftHandle: true,
+            rightHandle: false,
+            leftHandleX: parseInt(state.sectionWidth * 0.86) - 5,
+          },
+        ];
+
+        const chartSequences = [{
+          path: 'M' + maxYAxisTickWidth + ',100',
+          color: 'blue',
+          data: [{x: maxYAxisTickWidth, y: 100}],
+          dotColor: 'blue',
+        }];
+        for (let i = 10; i < (state.sectionWidth - maxYAxisTickWidth); i += 10) {
+          const datum = {
+            x: maxYAxisTickWidth + i,
+            y: 20 + parseInt(Math.random() * (chartHeight - textHeight - 20)),
+          };
+          chartSequences[0].data.push(datum);
+          chartSequences[0].path += ' L' + datum.x + ',' + datum.y;
+        }
+
+        const chartXAxisTicks = [
+          'Dec', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+          '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
+          '25', '26', '27', '28', '29', '30', '31', '2018', '2', '3',
+        ].map((text, index, texts) => ({
+          text,
+          x: maxYAxisTickWidth + (state.sectionWidth - maxYAxisTickWidth) * index / texts.length,
+          y: chartHeight - 5,
+        }));
+
+        const chartYAxisTicks = [
+          '9MB', '8MB', '7MB', '6MB', '5MB', '4MB', '3MB', '2MB', '1MB',
+        ].map((text, index, texts) => ({
+          text,
+          x: 0,
+          y: 20 + (chartHeight - textHeight - 20) * index / texts.length,
+        }));
+
+        const chartAntiBrushes = [
+          {
+            x: maxYAxisTickWidth,
+            width: parseInt(state.sectionWidth * 0.66),
+            leftHandle: false,
+            rightHandle: true,
+            rightHandleX: maxYAxisTickWidth + parseInt(state.sectionWidth * 0.66) - 5,
+          },
+          {
+            x: parseInt(state.sectionWidth * 0.86),
+            width: parseInt(state.sectionWidth * 0.14),
+            leftHandleX: parseInt(state.sectionWidth * 0.86) - 5,
+            leftHandle: true,
+            rightHandle: false,
+          },
+        ];
+
+        dispatch({
+          type: 'chart-section.layoutChart',
+          sectionId,
+          minimapLayout: {
+            width: state.sectionWidth,
+            height: minimapHeight,
+            left: maxYAxisTickWidth,
+            right: state.sectionWidth,
+            top: 20,
+            brushHandleTop: 0,
+            dotRadius: 6,
+            bottom: minimapHeight - textHeight,
+            sequences: minimapSequences,
+            xAxisTicks: minimapXAxisTicks,
+            showXAxisTickLines: true,
+            fhowYAxisTickLines: false,
+            antiBrushes: minimapAntiBrushes,
+          },
+          chartLayout: {
+            width: state.sectionWidth,
+            height: chartHeight,
+            left: maxYAxisTickWidth,
+            right: state.sectionWidth,
+            top: 20,
+            brushHandleTop: 0,
+            dotRadius: 6,
+            showYAxisTickLines: true,
+            showXAxisTickLines: true,
+            bottom: chartHeight - textHeight,
+            sequences: chartSequences,
+            yAxisTicks: chartYAxisTicks,
+            xAxisTicks: chartXAxisTicks,
+            antiBrushes: chartAntiBrushes,
+          },
+        });
+      };
+    }
+  }
+  customElements.define(ChartSection.is, ChartSection);
+
+  cp.REDUCERS.set('chart-section.layoutChart', (state, action) => {
+    return cp.assignSection(state, action.sectionId, {
+      minimapLayout: action.minimapLayout,
+      chartLayout: action.chartLayout,
+    });
+  });
+
+  return {
+    ChartSection,
+  };
+});
+
---
+++ dashboard/dashboard/spa/test-path-component.js
@@ -0,0 +1,173 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  function makeProperties(configs) {
+    const properties = {
+      sectionId: Number,
+      componentIndex: Number,
+    };
+    for (const [name, config] of Object.entries(configs)) {
+      properties[name] = Object.assign(config, {
+        statePath(state) {
+          if (!state.sections[this.sectionId]) return undefined;
+          if (!state.sections[this.sectionId].testPathComponents[
+              this.componentIndex]) {
+            return undefined;
+          }
+          return state.sections[this.sectionId].testPathComponents[
+              this.componentIndex][name];
+        }
+      });
+    }
+    return properties;
+  }
+
+  class TestPathComponent extends cp.Element {
+    static get is() { return 'test-path-component'; }
+
+    static get properties() {
+      return makeProperties({
+        placeholder: {type: String},
+        value: {type: String},
+        options: {type: Array},
+        isFocused: {type: Boolean},
+        canAggregate: {type: Boolean},
+        isAggregated: {type: Boolean},
+      });
+    }
+
+    onFocus_(e) {
+      this.dispatch(TestPathComponent.focus(
+        this.sectionId, this.componentIndex, true));
+    }
+
+    static focus(sectionId, componentIndex, isFocused) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'test-path-component.focus',
+          sectionId,
+          componentIndex,
+          isFocused,
+        });
+      };
+    }
+
+    onBlur_(e) {
+      this.dispatch(TestPathComponent.focus(
+        this.sectionId, this.componentIndex, false));
+    }
+
+    onKeydown_(e) {
+      this.dispatch(TestPathComponent.keydown(
+        this.sectionId, this.componentIndex, e.detail.value));
+    }
+
+    static keydown(sectionId, componentIndex, value) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'test-path-component.keydown',
+          sectionId: sectionId,
+          componentIndex: componentIndex,
+          value: value,
+        });
+      };
+    }
+
+    onSelect_(e) {
+      this.dispatch(TestPathComponent.select(
+        this.sectionId, this.componentIndex, e.detail.value));
+    }
+
+    static select(sectionId, componentIndex, value) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'test-path-component.select',
+          sectionId,
+          componentIndex,
+          value,
+        });
+
+        // If the first 3 components are filled, then load the timeseries.
+        const components = getState().sections[sectionId].testPathComponents;
+        if (components[0].value && components[1].value && components[2].value) {
+          dispatch(cp.ChartSection.loadTimeseries(sectionId));
+        }
+      };
+    }
+
+    onAggregateChange_(e) {
+      this.dispatch(TestPathComponent.aggregate(
+        this.sectionId, this.componentIndex, this.$.aggregate.checked));
+    }
+
+    static aggregate(sectionId, componentIndex, isAggregated) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'test-path-component.aggregate',
+          sectionId: this.sectionId,
+          componentIndex: this.componentIndex,
+          value: this.$.aggregate.checked,
+        });
+      };
+    }
+  }
+  customElements.define(TestPathComponent.is, TestPathComponent);
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {String} action.value
+   */
+  cp.REDUCERS.set('test-path-component.select', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    const oldComponent = oldComponents[action.componentIndex];
+    let newComponents = cp.assignInArray(oldComponents, action.componentIndex, {
+      value: action.value,
+      isFocused: false,
+    });
+    if ((1 + action.componentIndex) < (newComponents.length - 1)) {
+      newComponents = cp.assignInArray(newComponents, 1 + action.componentIndex, {
+        isFocused: true,
+      });
+    }
+
+    return cp.assignSection(state, action.sectionId, {
+      testPathComponents: newComponents,
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {Boolean} action.isFocused
+   */
+  cp.REDUCERS.set('test-path-component.focus', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    return cp.assignSection(state, action.sectionId, {
+      testPathComponents: cp.assignInArray(oldComponents, action.componentIndex, {
+        isFocused: action.isFocused,
+      }),
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   * @param {Number} action.componentIndex
+   * @param {String} action.value
+   */
+  cp.REDUCERS.set('test-path-component.keydown', (state, action) => {
+    const oldComponents = state.sections[action.sectionId].testPathComponents;
+    return cp.assignSection(state, action.sectionId, {
+      testPathComponents: cp.assignInArray(oldComponents, action.componentIndex, {
+        value: action.value,
+      }),
+    });
+  });
+
+  return {
+  };
+});
