894fd6f0b9a3c5149fd69121da426ef9442cdd7e
benshayden <benjhayden@chromium.org>
2017-11-29 00:01:14 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/chromeperf-app.js
@@ -0,0 +1,298 @@
+/*
+Copyright 2017 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
+'use strict';
+
+tr.exportTo('cp', () => {
+  class ChromeperfApp extends cp.Element {
+    static get is() { return 'chromeperf-app'; }
+
+    static get properties() {
+      return {
+        sections: {
+          type: Array,
+          statePath: 'sections',
+        },
+      };
+    }
+
+    ready() {
+      super.ready();
+      this.onResize_ = this.onResize_.bind(this);
+      window.addEventListener('resize', this.onResize_);
+    }
+
+    onResize_(e) {
+      this.dispatch(ChromeperfApp.updateSectionWidth(
+        this.shadowRoot.querySelector(
+          'chart-section, alerts-section, releasing-section')));
+    }
+
+    newAlertsSection_() {
+      this.dispatch(ChromeperfApp.appendSection('alerts'));
+    }
+
+    newChartSection_() {
+      this.dispatch(ChromeperfApp.appendSection('chart'));
+    }
+
+    newReleasingSection_() {
+      this.dispatch(ChromeperfApp.appendSection('releasing'));
+    }
+
+    showFabs_() {
+      // TODO for touchscreens
+    }
+
+    static updateSectionWidth(section) {
+      return async (dispatch, getState) => {
+        const rect = await cp.measureElement(section);
+        if (Math.abs(rect.width - getState().sectionWidth) < 1) return;
+        dispatch({
+          type: 'chromeperf-app.updateSectionWidth',
+          sectionWidth: rect.width,
+        });
+      };
+    }
+
+    static appendSection(sectionType) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'chromeperf-app.newSection',
+          sectionType,
+        });
+      };
+    }
+
+    static closeSection(sectionId) {
+      return  async (dispatch, getState) => {
+        dispatch({
+          type: 'chromeperf-app.closeSection',
+          sectionId,
+        });
+      };
+    }
+  }
+  customElements.define(ChromeperfApp.is, ChromeperfApp);
+
+  const NEW_SECTION_STATES = new Map();
+
+  NEW_SECTION_STATES.set('chart', {
+    isLoading: false,
+    onlyChart: false,
+    chartLayout: false,
+    minimapLayout: false,
+    testSuiteDescription: 'test suite description',
+    histograms: undefined,
+    testPathComponents: [
+      {
+        placeholder: 'Test suite',
+        canAggregate: true,
+        isFocused: true,
+        inputValue: '',
+        selectedOptions: [],
+        multipleSelectedOptions: false,
+        options: [
+          'system_health.common_desktop',
+          'system_health.common_mobile',
+          'system_health.memory_desktop',
+          'system_health.memory_mobile',
+        ],
+      },
+      {
+        placeholder: 'Bot',
+        canAggregate: true,
+        isFocused: false,
+        inputValue: '',
+        selectedOptions: [],
+        multipleSelectedOptions: false,
+        options: [
+          'nexus5X',
+          'nexus5',
+          'mac',
+          'win',
+          'linux',
+        ],
+      },
+      {
+        placeholder: 'Measurement',
+        canAggregate: false,
+        isFocused: false,
+        inputValue: '',
+        selectedOptions: [],
+        multipleSelectedOptions: false,
+        options: [
+          'PSS',
+          'power',
+          'TTFMP',
+          'TTI',
+        ],
+      },
+      {
+        placeholder: 'Stories',
+        canAggregate: true,
+        isFocused: false,
+        inputValue: '',
+        selectedOptions: [],
+        multipleSelectedOptions: false,
+        options: [
+          'load:news:cnn',
+          'load:news:nytimes',
+          'load:news:qq',
+        ],
+      },
+      {
+        placeholder: 'Statistics',
+        canAggregate: false,
+        inputValue: 'avg',
+        selectedOptions: ['avg'],
+        multipleSelectedOptions: false,
+        isFocused: false,
+        options: [
+          'avg',
+          'std',
+          'median',
+          '90%',
+          '95%',
+          '99%',
+        ],
+      },
+    ],
+  });
+
+  NEW_SECTION_STATES.set('alerts', {
+    sheriffOrBug: '',
+    isMenuFocused: true,
+    anyAlertsSelected: false,
+    rows: [
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+      {
+        revisions: '-----',
+        bot: '-----',
+        testSuite: '-----',
+        testParts: ['-----', '', '', '', ''],
+        delta: '-----',
+        deltaPct: '-----',
+      },
+    ],
+    areRowsPlaceholders: true,
+    isLoading: false,
+    showingImprovements: false,
+    showingTriaged: false,
+    sheriffList: [
+      "ARC Perf Sheriff",
+      "Angle Perf Sheriff",
+      "Binary Size Sheriff",
+      "Blink Memory Mobile Sheriff",
+      "Chrome OS Graphics Perf Sheriff",
+      "Chrome OS Installer Perf Sheriff",
+      "Chrome OS Perf Sheriff",
+      "Chrome Perf Accessibility Sheriff",
+      "Chromium Perf AV Sheriff",
+      "Chromium Perf Sheriff",
+      "Chromium Perf Sheriff - Sub-series",
+      "CloudView Perf Sheriff",
+      "Cronet Perf Sheriff",
+      "Jochen",
+      "Mojo Perf Sheriff",
+      "NaCl Perf Sheriff",
+      "Network Service Sheriff",
+      "OWP Storage Perf Sheriff",
+      "Oilpan Perf Sheriff",
+      "Pica Sheriff",
+      "Power Perf Sheriff",
+      "Service Worker Perf Sheriff",
+      "Tracing Perftests Sheriff",
+      "V8 Memory Perf Sheriff",
+      "V8 Perf Sheriff",
+      "WebView Perf Sheriff",
+    ],
+  });
+
+  NEW_SECTION_STATES.set('releasing', {
+    isLoading: false,
+    isMenuFocused: true,
+    report: '',
+    reportOptions: [
+      'Chromeperf Public',
+      'Loading',
+      'Input',
+      'Memory',
+      'Battery',
+    ],
+    milestone: 64,
+    isPreviousMilestone: false,
+    isNextMilestone: false,
+    anyAlerts: false,
+  });
+
+  /**
+   * @param {String} action.sectionType
+   */
+  cp.REDUCERS.set('chromeperf-app.newSection', (state, action) => {
+    if (state.sections.length >= 3) throw 'WTF';
+    const newSection = cp.assign(
+      {type: action.sectionType},
+      NEW_SECTION_STATES.get(action.sectionType));
+    if (state.containsDefaultSection) {
+      return cp.assign(state, {
+        sections: [newSection],
+        containsDefaultSection: false,
+      });
+    }
+    return cp.assign(state, {
+      sections: state.sections.concat([newSection]),
+    });
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   */
+  cp.REDUCERS.set('chromeperf-app.closeSection', (state, action) => {
+    return cp.assign(state, {
+      sections: state.sections.slice(0, action.sectionId).concat(
+        state.sections.slice(action.sectionId + 1)),
+    });
+  });
+
+  cp.REDUCERS.set('chromeperf-app.updateSectionWidth', (state, action) => {
+    return cp.assign(state, {sectionWidth: action.sectionWidth});
+  });
+
+  return {
+    ChromeperfApp,
+  };
+});
