57ccd27f81a05fa3b7e79a91400c3f3d25a20510
benshayden <benjhayden@chromium.org>
2017-12-05 15:20:31 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/chromeperf-app.js
@@ -0,0 +1,166 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+tr.exportTo('cp', () => {
+  class ChromeperfApp extends cp.Element {
+    static get is() { return 'chromeperf-app'; }
+
+    static get properties() {
+      return {
+        sections: {
+          type: Array,
+          statePath(state) {
+            return state.sectionIds.map(id => state.sectionsById[id]);
+          },
+        },
+        hasClosedSection: {
+          type: Boolean,
+          statePath(state) {
+            return state.closedSectionId !== undefined;
+          },
+        },
+        closedSectionType: {
+          type: String,
+          statePath(state) {
+            if (!state.closedSectionId) return '';
+            return state.sectionsById[state.closedSectionId].type.split('-')[0];
+          },
+        },
+      };
+    }
+
+    newAlertsSection_() {
+      this.dispatch(ChromeperfApp.appendSection('alerts-section'));
+    }
+
+    newChartSection_() {
+      this.dispatch(ChromeperfApp.appendSection('chart-section'));
+    }
+
+    newReleasingSection_() {
+      this.dispatch(ChromeperfApp.appendSection('releasing-section'));
+    }
+
+    showFabs_() {
+      // TODO for touchscreens
+    }
+
+    reopenClosedSection_() {
+      this.dispatch(ChromeperfApp.reopenClosedSection());
+    }
+
+    static reopenClosedSection() {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'chromeperf-app.reopenClosedSection',
+        });
+      };
+    }
+
+    static appendSection(sectionType) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'chromeperf-app.newSection',
+          sectionType,
+        });
+      };
+    }
+
+    static closeSection(sectionId) {
+      return async (dispatch, getState) => {
+        dispatch({
+          type: 'chromeperf-app.closeSection',
+          sectionId,
+        });
+
+        await tr.b.timeout(3000);
+        if (getState().closedSectionId !== sectionId) return;
+        dispatch({
+          type: 'chromeperf-app.forgetClosedSection',
+        });
+      };
+    }
+
+    static clearAllFocused(oldSectionsById) {
+      const newSectionsById = {};
+      for (const [id, section] of Object.entries(oldSectionsById)) {
+        newSectionsById[id] =
+          SECTION_CLASSES_BY_TYPE.get(section.type).clearAllFocused(section);
+      }
+      return newSectionsById;
+    }
+  }
+  customElements.define(ChromeperfApp.is, ChromeperfApp);
+
+  const SECTION_CLASSES_BY_TYPE = new Map([
+    cp.ChartSection,
+    cp.AlertsSection,
+    cp.ReleasingSection,
+  ].map(cls => [cls.is, cls]));
+
+  /**
+   * @param {String} action.sectionType
+   */
+  cp.REDUCERS.set('chromeperf-app.newSection', (state, action) => {
+    const newSection = {
+      type: action.sectionType,
+      id: tr.b.GUID.allocateSimple(),
+      ...SECTION_CLASSES_BY_TYPE.get(action.sectionType).NEW_STATE,
+    };
+    const sectionsById = {...state.sectionsById};
+    sectionsById[newSection.id] = newSection;
+
+    if (state.containsDefaultSection) {
+      state = {
+        ...state,
+        sectionIds: [newSection.id],
+        sectionsById,
+        containsDefaultSection: false,
+      };
+      return state;
+    }
+    return {
+      ...state,
+      sectionsById,
+      sectionIds: state.sectionIds.concat([newSection.id]),
+    };
+  });
+
+  /**
+   * @param {Number} action.sectionId
+   */
+  cp.REDUCERS.set('chromeperf-app.closeSection', (state, action) => {
+    const sectionIdIndex = state.sectionIds.indexOf(action.sectionId);
+    return {
+      ...state,
+      sectionIds: state.sectionIds.slice(0, sectionIdIndex).concat(
+          state.sectionIds.slice(sectionIdIndex + 1)),
+      closedSectionId: action.sectionId,
+    };
+  });
+
+  cp.REDUCERS.set('chromeperf-app.forgetClosedSection', (state, action) => {
+    const sectionsById = {...state.sectionsById};
+    delete sectionsById[state.closedSectionId];
+    return {
+      ...state,
+      sectionsById,
+      closedSectionId: undefined,
+    };
+  });
+
+  cp.REDUCERS.set('chromeperf-app.reopenClosedSection', (state, action) => {
+    return {
+      ...state,
+      sectionIds: state.sectionIds.concat([state.closedSectionId]),
+      closedSectionId: undefined,
+    };
+  });
+
+  return {
+    ChromeperfApp,
+  };
+});
