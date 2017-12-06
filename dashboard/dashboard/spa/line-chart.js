730b4f05b605c72ed8764a780125704d890246b5
benshayden <benjhayden@chromium.org>
2017-12-06 14:45:35 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/line-chart.js
@@ -0,0 +1,102 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  PolymerSvgTemplate('line-chart');
+  class LineChart extends Polymer.Element {
+    static get is() { return 'line-chart'; }
+
+    static get properties() {
+      return {
+        layout: {
+          type: Object,
+          value: {
+            width: 100,
+            height: 100,
+            left: 0,
+            right: 100,
+            top: 0,
+            bottom: 100,
+            dotRadius: 6,
+            brushHandleTop: 50,
+            sequences: [],
+            yAxisLabel: '',
+            yAxisTicks: [],
+            xAxisTicks: [],
+            showYAxisTickLines: true,
+            showXAxisTickLines: true,
+            antiBrushes: [],
+          },
+        },
+      };
+    }
+
+    path_(sequence) {
+      let path = '';
+      for (const datum of sequence.data) {
+        const prefix = path ? ' L' : 'M';
+        path += `${prefix}${datum.x},${datum.y}`;
+      }
+      return path;
+    }
+
+    pct_(x) {
+      return x + '%';
+    }
+
+    /**
+     * Non-Polymer-Redux Usage:
+     *   this.$.line_chart.layout = await LineChart.layout(options);
+     * Polymer-Redux usage:
+     *   - call layout() in an action creator,
+     *   - then() dispatch an action containing the layout object,
+     *   - data-bind the layout object to a <line-chart> element.
+     */
+    static async layout(options) {
+      // TODO Promise.all(ticks.map(tick => cp.measureText(tick, {})))
+    }
+
+    onDotClick_(event) {
+      this.dispatchEvent(new CustomEvent('dot-click', {
+        detail: event.model,
+      }));
+    }
+
+    onDotMouseOver_(event) {
+      this.dispatchEvent(new CustomEvent('dot-mouseover', {
+        detail: event.model,
+      }));
+    }
+
+    onDotMouseOut_(event) {
+      this.dispatchEvent(new CustomEvent('dot-mouseout', {
+        detail: event.model,
+      }));
+    }
+
+    onBrushHandleMouseDown_(event) {
+      this.dispatchEvent(new CustomEvent('brush-handle-down', {
+        detail: event.model,
+      }));
+    }
+
+    onBrushHandleMouseMove_(event) {
+      this.dispatchEvent(new CustomEvent('brush-handle-move', {
+        detail: event.model,
+      }));
+    }
+
+    onBrushHandleMouseUp_(event) {
+      this.dispatchEvent(new CustomEvent('brush-handle-up', {
+        detail: event.model,
+      }));
+    }
+  }
+  customElements.define(LineChart.is, LineChart);
+
+  return {
+    LineChart,
+  };
+});
