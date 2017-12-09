7b30c87f6b8e1bcdb2292e2ef3d91ebce337b1d5
benshayden <benjhayden@chromium.org>
2017-12-08 16:06:36 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/state.js
@@ -0,0 +1,425 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+tr.exportTo('cp', () => {
+  // Reducers MUST NOT have side effects.
+  // Reducers MUST NOT modify state.
+  // Reducers MUST return a new object.
+  // Reducers MAY copy properties from state.
+
+  // Action creators MUST return an object with type containing a string name of
+  // a reducer.
+  // Action creators SHOULD contain all async code in the app.
+  // Action creators MAY have side-effects.
+  // Action creators MAY take any number of parameters.
+
+  // TODO request this onload? templatize in a request handler?
+  const DEFAULT_SECTION = {
+    type: 'releasing-section',
+    id: -1,
+    source: {
+      isFocused: false,
+      inputValue: 'Chromeperf Public',
+      selectedOptions: ['Chromeperf Public'],
+      options: [
+        'Chromeperf Public',
+        'Loading',
+        'Input',
+        'Memory',
+        'Battery',
+      ],
+    },
+    isLoading: false,
+    isOwner: Math.random() < 0.5,
+    milestone: 64,
+    isPreviousMilestone: true,
+    isNextMilestone: false,
+    tables: [
+      {
+        title: 'health-plan-clankium-phone',
+        currentVersion: '517411-73a',
+        referenceVersion: '508578-c23',
+        rows: [
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            category: 'Foreground',
+            href: '#',
+            name: 'Java Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            category: 'Background',
+            href: '#',
+            name: 'Java Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+        ],
+      },
+      {
+        title: 'health-plan-clankium-low-end-phone',
+        currentVersion: '517411-73a',
+        referenceVersion: '508578-c23',
+        rows: [
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            category: 'Foreground',
+            href: '#',
+            name: 'Java Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: true,
+            rowCount: 4,
+            category: 'Background',
+            href: '#',
+            name: 'Java Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Native Heap',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Ashmem',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+          {
+            isFirstInCategory: false,
+            href: '#',
+            name: 'Overall PSS',
+            testPath: [['system_health.memory_mobile'], ['nexus5'], ['PSS']],
+            currentValue: 2,
+            unit: tr.b.Unit.byName.sizeInBytes_smallerIsBetter,
+            referenceValue: 1,
+            percentDeltaValue: 1,
+            percentDeltaUnit:
+              tr.b.Unit.byName.normalizedPercentageDelta_smallerIsBetter,
+            deltaValue: 1,
+            deltaUnit: tr.b.Unit.byName.sizeInBytesDelta_smallerIsBetter,
+          },
+        ],
+      },
+    ],
+  };
+
+  // Maps from string "action type" to synchronous
+  // function(!Object state, !Object action):!Object state.
+  const REDUCERS = new Map();
+
+  const DEFAULT_STATE = {
+    sectionIds: [DEFAULT_SECTION.id],
+    sectionsById: {
+      [DEFAULT_SECTION.id]: DEFAULT_SECTION,
+    },
+    containsDefaultSection: true,
+    closedSectionId: undefined,
+    isExplainingFab: true,
+  };
+
+  // Forwards (state, action) to the registered reducer function in the REDUCERS
+  // Map.
+  function rootReducer(state, action) {
+    if (state === undefined) return DEFAULT_STATE;
+    if (!REDUCERS.has(action.type)) return state;
+    return REDUCERS.get(action.type)(state, action);
+  }
+
+  // This is all that is needed from redux-thunk to enable asynchronous action
+  // creators.
+  // https://tur-nr.github.io/polymer-redux/docs#async-actions
+  const THUNK = ({dispatch, getState}) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState);
+    }
+    return next(action);
+  };
+
+  const STORE = Redux.createStore(
+      rootReducer, DEFAULT_STATE, Redux.applyMiddleware(THUNK));
+
+  const ReduxMixin = PolymerRedux(STORE);
+
+  /*
+   * This is a base class for elements in this app that mixes together
+   * Polymer.Element, Polymer-Redux, and some functions to help data-bindings in
+   * elements perform minimal computation without computed properties.
+   */
+  class Element extends ReduxMixin(Polymer.Element) {
+    _add() {
+      let sum = arguments[0];
+      for (const arg of Array.from(arguments).slice(1)) {
+        sum += arg;
+      }
+      return sum;
+    }
+
+    _eq() {
+      const test = arguments[0];
+      for (const arg of Array.from(arguments).slice(1)) {
+        if (arg !== test) return false;
+      }
+      return true;
+    }
+
+    _len(seq) {
+      if (seq === undefined) return 0;
+      if (seq === null) return 0;
+      if (seq instanceof Array) return seq.length;
+      if (seq instanceof Map || seq instanceof Set) return seq.size;
+      if (seq instanceof tr.v.HistogramSet) return seq.length;
+      return Object.keys(seq).length;
+    }
+
+    _empty(seq) {
+      return this._len(seq) === 0;
+    }
+  }
+
+  /**
+   * This function helps reducers create a new Array based on an existing one
+   * without modifying it.
+   *
+   * @param {!Array} arr
+   * @param {Number} index
+   * @param {!Object} delta
+   * @returns {!Array}
+   */
+  function assignInArray(arr, index, delta) {
+    return arr.slice(0, index).concat([
+      {...arr[index], ...delta},
+    ]).concat(arr.slice(index + 1));
+  }
+
+  /**
+   * This function helps Section elements define their properties with
+   * statePath.
+   *
+   * @param {!Object} configs
+   * @returns {!Object}
+   */
+  function sectionProperties(configs) {
+    const properties = {sectionId: Number};
+    for (const [name, config] of Object.entries(configs)) {
+      properties[name] = {
+        ...config,
+        statePath(state) {
+          if (!state.sectionsById[this.sectionId]) return undefined;
+          return state.sectionsById[this.sectionId][name];
+        },
+      };
+    }
+    return properties;
+  }
+
+  /**
+   * This function helps Section elements register reducers that only modify
+   * their own state.
+   * @param {string} actionType
+   * @param {!Function(state, action, section):state} reducer
+   */
+  function sectionReducer(actionType, reducer) {
+    REDUCERS.set(actionType, (state, action) => {
+      const section = state.sectionsById[action.sectionId];
+      const delta = reducer(state, action, section);
+      if (typeof action.sectionId !== 'number') {
+        throw new Error(`Invalid sectionId ${action.sectionId}`);
+      }
+      const sectionsById = {...state.sectionsById};
+      sectionsById[action.sectionId] = {...section, ...delta};
+      return {...state, sectionsById};
+    });
+  }
+
+  return {
+    Element,
+    REDUCERS,
+    assignInArray,
+    sectionProperties,
+    sectionReducer,
+  };
+});
