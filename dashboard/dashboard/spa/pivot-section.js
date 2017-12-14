5448402d6c62356dd7cfb90f193e19bdc094b646
benshayden <benjhayden@chromium.org>
2017-12-13 22:51:50 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/pivot-section.js
@@ -0,0 +1,62 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class PivotSection extends cp.Element {
+    static get is() { return 'pivot-section'; }
+
+    static get properties() {
+      return cp.sectionProperties({
+        benchmark: {type: Object},
+        bot: {type: Object},
+        revisions: {type: Object},
+        histograms: {type: Object},
+      });
+    }
+
+    closeSection_() {
+      this.dispatch(cp.ChromeperfApp.closeSection(this.sectionId));
+    }
+
+    static clearAllFocused(sectionState) {
+      return {
+        ...sectionState,
+        benchmark: {
+          ...sectionState.benchmark,
+          isFocused: false,
+        },
+        bot: {
+          ...sectionState.bot,
+          isFocused: false,
+        },
+        revisions: {
+          ...sectionState.revisions,
+          isFocused: false,
+        },
+      };
+    }
+
+    static updateLocation(sectionState) {
+      return async (dispatch, getState) => {
+        const queryParams = {};
+        // eslint-disable-next-line no-console
+        console.log('TODO distill', sectionState);
+        dispatch(cp.ChromeperfApp.updateRoute('pivot', queryParams));
+      };
+    }
+
+    static restoreFromQueryParams(sectionId, queryParams) {
+      return async (dispatch, getState) => {
+        // eslint-disable-next-line no-console
+        console.log('TODO restore from', queryParams);
+      };
+    }
+  }
+  customElements.define(PivotSection.is, PivotSection);
+
+  return {
+    PivotSection,
+  };
+});
