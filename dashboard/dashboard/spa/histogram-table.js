4e8cbfb235a14b809b5ff0f8017c968328fd7f00
benshayden <benjhayden@chromium.org>
2017-12-23 22:52:51 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/histogram-table.js
@@ -0,0 +1,56 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class HistogramTable extends Polymer.GestureEventListeners(cp.Element) {
+    static get is() { return 'histogram-table'; }
+
+    static get reduxProperties() {
+      return {
+        columns: {type: Array},
+        rows: {type: Array},
+        sortColumn: {type: String},
+        sortDescending: {type: Boolean},
+        commonDiagnostics: {type: Array},
+      };
+    }
+
+    visibleRows_(rows) {
+      return HistogramTable.visibleRows(rows, 0);
+    }
+
+    static visibleRows(groupedRows, depth) {
+      const rows = [];
+      for (const row of groupedRows) {
+        row.depth = depth;
+        rows.push(row);
+        if (row.isExpanded) {
+          rows.push.apply(rows, HistogramTable.visibleRows(
+              row.subRows, depth + 1));
+        }
+      }
+      return rows;
+    }
+
+    getCommonDiagnostic_(diagnosticName, columnName) {
+      return HistogramTable.getCommonDiagnostic(
+          diagnosticName, columnName, this.rows);
+    }
+
+    static getCommonDiagnostic(diagnosticName, columnName, rows) {
+      return rows[0].columns[columnName].diagnostics.get(diagnosticName);
+    }
+
+    sort_(event) {
+      // eslint-disable-next-line no-console
+      console.log('TODO sort', event);
+    }
+  }
+  cp.Element.register(HistogramTable);
+
+  return {
+    HistogramTable,
+  };
+});
