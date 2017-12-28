2b47d6305a821feef600c649c969dc512cfa7935
benshayden <benjhayden@chromium.org>
2017-12-28 15:27:49 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/expand-button.js
@@ -0,0 +1,47 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class ExpandButton extends Polymer.GestureEventListeners(cp.ElementBase) {
+    static get is() { return 'expand-button'; }
+
+    static get properties() {
+      return {
+        opened: {
+          type: Boolean,
+          value: false,
+        },
+        horizontal: {
+          type: Boolean,
+          value: false,
+        },
+        after: {
+          type: Boolean,
+          value: false,
+        },
+      };
+    }
+
+    toggle_(event) {
+      this.dispatchEvent(new CustomEvent('toggle', {
+        bubbles: true,
+        composed: true,
+        detail: {opened: !this.opened},
+      }));
+    }
+
+    getIcon_(opened) {
+      if (this.after) opened = !opened;
+      if (horizontal) return (opened ? 'chevron-left' : 'chevron-right');
+      return (opened ? 'expand-less' : 'expand-more');
+    }
+  }
+
+  cp.ElementBase.register(ExpandButton);
+
+  return {
+    ExpandButton,
+  };
+});
