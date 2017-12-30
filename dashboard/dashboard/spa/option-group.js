b9123fe8fdc645771d24bb2d571db0a03ecec1e7
benshayden <benjhayden@chromium.org>
2017-12-30 13:45:08 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/option-group.js
@@ -0,0 +1,107 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class OptionGroup extends cp.ElementBase {
+    static get is() { return 'option-group'; }
+
+    static get properties() {
+      return {
+        ...cp.ElementBase.statePathProperties('statePath', {
+          options: {
+            type: Array,
+            value: [],
+          },
+          selectedOptions: {
+            type: Array,
+            value: [],
+          },
+        }),
+        isRoot: {
+          type: Boolean,
+          value: false,
+        },
+      };
+    }
+
+    countDescendents_(children) {
+      return OptionGroup.countDescendents(children);
+    }
+
+    static countDescendents(children) {
+      let count = 0;
+      for (const option of children) {
+        if (option.children) {
+          count += OptionGroup.countDescendents(option.children);
+        } else {
+          count += 1;
+        }
+      }
+      return count;
+    }
+
+    isSelected_(option, selectedOptions) {
+      if (!option || !selectedOptions) return false;
+      return selectedOptions.includes(this.value_(option));
+    }
+
+    label_(option) {
+      return option.label || this.value_(option);
+    }
+
+    value_(option) {
+      return option.value || option;
+    }
+
+    indentRow_(option) {
+      if (option.children) return false;
+      return !this.isRoot || this.getAnyGroups_(this.options);
+    }
+
+    getAnyGroups_(options) {
+      if (!options) return false;
+      for (const option of options) {
+        if (option.children) return true;
+      }
+      return false;
+    }
+
+    onSelect_(e) {
+      const selectedOptions = Array.from(this.selectedOptions);
+      if (e.target.checked) {
+        selectedOptions.push(this.value_(e.model.option));
+      } else {
+        selectedOptions.splice(
+            selectedOptions.indexOf(this.value_(e.model.option)), 1);
+      }
+      this.dispatchEvent(new CustomEvent('option-select', {
+        bubbles: true,
+        composed: true,
+        detail: {selectedOptions},
+      }));
+    }
+
+    toggleGroupExpanded_(e) {
+      this.dispatch('toggleGroupExpanded', this.statePath, e.model.optionIndex);
+    }
+  }
+
+  OptionGroup.actions = {
+    toggleGroupExpanded: (statePath, optionIndex) =>
+      async (dispatch, getState) => {
+        const optionStatePath = statePath.concat(['options', optionIndex]);
+        const option = cp.ElementBase.getStateAtPath(
+            getState(), optionStatePath);
+        dispatch(cp.ElementBase.actions.updateObjectAtPath(
+            optionStatePath, {isExpanded: !option.isExpanded}));
+      },
+  };
+
+  cp.ElementBase.register(OptionGroup);
+
+  return {
+    OptionGroup,
+  };
+});
