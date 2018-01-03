a0a88d15dc6c87100987787fd7e9c08028c2cd5e
Ethan Kuefner <eakuefner@chromium.org>
2018-01-03 10:56:16 -0800

    [TBMv2] Switch to dynamically importing diagnostics in Python
    Right now, since all diagnostic subclasses are contained in histogram.py, it's
    enough for all_diagnostics to maintain a map from typenames to classes.
    However, when we move diagnostic subclasses into their own files, we need to
    avoid a circular import problem as follows:
    
    * `diagnostic` must import `all_diagnostics` to look up classes for
      deserialization
    * `all_diagnostics` must import all known diagnostic modules, to include them
      in the dict.
    * Diagnostic subclasses must import `diagnostic` for subclassing.
    
    To work around this, we change the map stored in all_diagnostics to map from
    diagnostic typenames to the modules that contain them, so we can dynamically
    import each module.
    
    To assist users, we encapsulate this functionality in a new function
    `all_diagnostics.GetDiagnosticClassForName(name)`, which can be used
    equivalently of the old all_diagnostics.DIAGNOSTICS_BY_NAME.
    
    This CL unblocks moving Diagnostic subclasses out of histogram.py.
    
    Bug: catapult:#3613
    Change-Id: Ib08bffc8a2b8d7523152227b9600943aa1056d35

--- tracing/tracing/value/diagnostics/all_diagnostics.py
+++ tracing/tracing/value/diagnostics/all_diagnostics.py
@@ -2,5 +2,33 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import importlib
+import sys
 
-DIAGNOSTICS_BY_NAME = {}
+
+MODULES_BY_DIAGNOSTIC_NAME = {
+    'Breakdown': 'histogram',
+    'GenericSet': 'histogram',
+    'UnmergeableDiagnosticSet': 'histogram',
+    'RelatedEventSet': 'histogram',
+    'DateRange': 'histogram',
+    'TagMap': 'histogram',
+    'RelatedHistogramBreakdown': 'histogram',
+    'RelatedHistogramMap': 'histogram',
+    'RelatedNameMap': 'histogram',
+}
+
+
+_CLASSES_BY_NAME = {}
+
+
+def GetDiagnosticClassForName(name):
+  if name in _CLASSES_BY_NAME:
+    return _CLASSES_BY_NAME[name]
+
+  module_name = 'tracing.value.%s' % MODULES_BY_DIAGNOSTIC_NAME[name]
+  importlib.import_module(module_name)
+
+  cls = getattr(sys.modules[module_name], name)
+  _CLASSES_BY_NAME[name] = cls
+  return cls
