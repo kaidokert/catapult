1cc5679f1c654a7fb12deab49f1b225301ff747c
benshayden <benjhayden@chromium.org>
2018-01-03 16:14:28 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/chart-placeholder.js
@@ -0,0 +1,16 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class ChartPlaceholder extends cp.ElementBase {
+    static get is() { return 'chart-placeholder'; }
+  }
+
+  cp.ElementBase.register(ChartPlaceholder);
+
+  return {
+    ChartPlaceholder,
+  };
+});
