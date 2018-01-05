cc6e8ff1f06efef92778764528c6152e2aad8cfa
benshayden <benjhayden@chromium.org>
2018-01-05 09:35:47 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/test-path-component.js
@@ -0,0 +1,107 @@
+/* Copyright 2017 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class TestPathComponent extends cp.ElementBase {
+    static get is() { return 'test-path-component'; }
+
+    static get properties() {
+      return cp.ElementBase.statePathProperties('statePath', {
+        canAggregate: {type: Boolean},
+        isAggregated: {type: Boolean},
+        tags: {type: Object},
+        selectedOptions: {type: Array},
+      });
+    }
+
+    isMultiple_(ary) {
+      return ary.length > 1;
+    }
+
+    onKeydown_(e) {
+      this.dispatch('keydown', this.statePath, e.detail.value);
+    }
+
+    onClear_(e) {
+      this.dispatch('clear', this.statePath);
+    }
+
+    onSelect_(e) {
+      // Allow this event to bubble up.
+      cp.ElementBase.measureInputLatency('test-path-component', 'select', e);
+      this.dispatch('select', this.statePath, this.selectedOptions);
+    }
+
+    onAggregateChange_(e) {
+      this.dispatch('aggregate', this.statePath, this.$.aggregate.checked);
+      this.dispatchEvent(new CustomEvent('aggregate'));
+    }
+
+    onToggleTagGroupExpanded_(e) {
+      e.cancelBubble = true;
+      this.dispatch('toggleTagGroupExpanded', this.statePath, e.detail.path);
+    }
+
+    onToggleGroupExpanded_(e) {
+      this.dispatch('toggleGroupExpanded',
+          this.statePath, e.detail.path);
+    }
+  }
+
+  TestPathComponent.actions = {
+    keydown: (statePath, inputValue) => async (dispatch, getState) => {
+      dispatch(cp.ElementBase.actions.updateObjectAtPath(
+          statePath, {inputValue}));
+    },
+
+    clear: statePath => async (dispatch, getState) => {
+      dispatch(cp.ElementBase.actions.updateObjectAtPath(statePath, {
+        inputValue: '',
+        selectedOptions: [],
+      }));
+      dispatch(cp.DropdownInput.actions.focus(statePath));
+    },
+
+    aggregate: (statePath, isAggregated) => async (dispatch, getState) => {
+      dispatch(cp.ElementBase.actions.updateObjectAtPath(statePath, {
+        isAggregated,
+      }));
+    },
+
+    select: (statePath, selectedOptions) =>
+      async (dispatch, getState) => {
+        if (selectedOptions === undefined) return;
+        dispatch({
+          type: 'test-path-component.select',
+          statePath,
+          selectedOptions,
+        });
+      },
+  };
+
+  TestPathComponent.reducers = {
+    select: cp.ElementBase.statePathReducer((state, action) => {
+      let inputValue = state.inputValue;
+      if (action.selectedOptions.length === 0) {
+        inputValue = '';
+      } else if (action.selectedOptions.length === 1) {
+        inputValue = action.selectedOptions[0];
+      } else if (action.selectedOptions.length > 1) {
+        inputValue = `[${action.selectedOptions.length} selected]`;
+      }
+      return {
+        ...state,
+        inputValue,
+        selectedOptions: action.selectedOptions,
+      };
+    }),
+  };
+
+  cp.ElementBase.register(TestPathComponent);
+
+  return {
+    TestPathComponent,
+  };
+});
