aa5296a50e6a07a91c11110773f337167779f286
Achuith Bhandarkar <achuith@chromium.org>
2018-01-17 14:28:28 -0800

    [oobe]: Use --oobe-skip-to-login instead of Oobe.skipToLoginForTesting.
    BUG=chromium:753870
    TEST=testGaiaLogin
    
    Change-Id: Ia5a0514431b4f010802fccce97608f6d2fedf2a9

--- telemetry/telemetry/internal/backends/chrome/cros_browser_backend.py
+++ telemetry/telemetry/internal/backends/chrome/cros_browser_backend.py
@@ -94,10 +94,11 @@ class CrOSBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
         vmodule
     ])
 
+    if self.browser_options.gaia_login:
+      args.append('--oobe-skip-to-login')
     # Disable GAIA services unless we're using GAIA login, or if there's an
     # explicit request for it.
-    if (self.browser_options.disable_gaia_services and
-        not self.browser_options.gaia_login):
+    elif self.browser_options.disable_gaia_services:
       args.append('--disable-gaia-services')
 
     trace_config_file = (self.platform_backend.tracing_controller_backend
