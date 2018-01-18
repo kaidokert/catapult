1b0fcd2b2f336f5c5c1fb429a1d27b2ca82e7ec9
Dave Tu <dtu@chromium.org>
2018-01-18 14:55:28 -0800

    [dashboard] Add optional caching to the services.request module.
    Bug: catapult:#4114
    Change-Id: Iab92f1a5f09a1a0a682add4c27777e8352957d2c

--- dashboard/dashboard/services/request.py
+++ dashboard/dashboard/services/request.py
@@ -7,6 +7,8 @@ import json
 import socket
 import urllib
 
+from google.appengine.api import memcache
+
 from dashboard.common import utils
 
 
@@ -25,7 +27,7 @@ def RequestJson(*args, **kwargs):
   return json.loads(content)
 
 
-def Request(url, method='GET', body=None, **parameters):
+def Request(url, method='GET', body=None, use_cache=False, **parameters):
   """Fetch a URL while authenticated as the service account.
 
   Args:
@@ -40,6 +42,9 @@ def Request(url, method='GET', body=None, **parameters):
     httplib.HTTPException: The request or response is malformed, or there is a
         network or server error, or the HTTP status code is not 2xx.
   """
+  if use_cache and body:
+    raise NotImplementedError('Caching not supported with request body.')
+
   if parameters:
     # URL-encode the parameters.
     for key, value in parameters.iteritems():
@@ -55,11 +60,21 @@ def Request(url, method='GET', body=None, **parameters):
     kwargs['body'] = json.dumps(body)
     kwargs['headers'] = {'Content-Type': 'application/json'}
 
+  if use_cache:
+    content = memcache.get(key=url)
+    if content:
+      return content
+
   try:
-    return _RequestAndProcessHttpErrors(url, **kwargs)
+    content = _RequestAndProcessHttpErrors(url, **kwargs)
   except (httplib.HTTPException, socket.error):
     # Retry once.
-    return _RequestAndProcessHttpErrors(url, **kwargs)
+    content = _RequestAndProcessHttpErrors(url, **kwargs)
+
+  if use_cache:
+    memcache.set(key=url, value=content)
+
+  return content
 
 
 def _RequestAndProcessHttpErrors(*args, **kwargs):
