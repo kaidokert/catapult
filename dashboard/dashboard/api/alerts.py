9bcf972c2c023985c9343d82498b1c2f403cbe58
benshayden <benjhayden@chromium.org>
2018-01-24 11:05:57 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

--- dashboard/dashboard/api/alerts.py
+++ dashboard/dashboard/api/alerts.py
@@ -1,19 +1,83 @@
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 import datetime
+import httplib2
 
 from google.appengine.ext import ndb
 
+from dashboard import alerts
+from dashboard import file_bug
+from dashboard import group_report
+from dashboard.api import api_auth
 from dashboard.api import api_request_handler
 from dashboard.common import request_handler
+from dashboard.common import utils
+from dashboard.models import alert_group
 from dashboard.models import anomaly
-from dashboard import alerts
-from dashboard import group_report
+from dashboard.services import issue_tracker_service
 
 
 class AlertsHandler(api_request_handler.ApiRequestHandler):
   """API handler for various alert requests."""
+
+  def _AuthorizedHttp(self):
+    # TODO(benjhayden): Use this instead of ServiceAccountHttp in order to use
+    # the user's account. That will require changing the google-signin's
+    # client-id in chromeperf-app.html to a client-id that is whitelisted by the
+    # issue tracker service, which will require either adding
+    # v2spa-dot-chromeperf.appspot.com to the list of domains for an existing
+    # client id, or else launching v2spa to chromeperf.appspot.com.
+    http = httplib2.Http()
+    orig_request = http.request
+    def NewRequest(uri, method='GET', body=None, headers=None,
+                   redirections=httplib2.DEFAULT_MAX_REDIRECTS,
+                   connection_type=None):
+      headers = dict(headers or {})
+      headers['Authorization'] = self.request.headers.get('Authorization')
+      return orig_request(uri, method, body, headers, redirections,
+                          connection_type)
+    http.request = NewRequest
+    return http
+
+  def _FileBug(self):
+    if not api_auth.GetCurrentUser().email().endswith('@chromium.org'):
+      # TODO(benjhayden): Use IsValidSheriffUser() instead.
+      raise api_request_handler.BadRequestError(
+          'Only chromium.org accounts may file bugs')
+    owner = self.request.get('owner')
+    cc = self.request.get('cc')
+    if owner and not owner.endswith('@chromium.org'):
+      raise api_request_handler.BadRequestError(
+          'Owner email address must end with @chromium.org')
+    summary = self.request.get('summary')
+    description = self.request.get('description')
+    labels = self.request.get_all('label')
+    components = self.request.get_all('component')
+    keys = self.request.get_all('key')
+    http = utils.ServiceAccountHttp()  # TODO use self._AuthorizedHttp()
+    return file_bug.FileBug(
+        http, keys, summary, description, labels, components, owner, cc)
+
+  def _RecentBugs(self):
+    if not api_auth.GetCurrentUser().email().endswith('@chromium.org'):
+      # TODO(benjhayden): Use IsValidSheriffUser() instead.
+      raise api_request_handler.BadRequestError(
+          'Only chromium.org accounts may query recent bugs')
+    http = utils.ServiceAccountHttp()  # TODO use self._AuthorizedHttp()
+    issue_tracker = issue_tracker_service.IssueTrackerService(http)
+    response = issue_tracker.List(
+        q='opened-after:today-5', label='Type-Bug-Regression,Performance',
+        sort='-id')
+    return {'bugs': response.get('items', [])}
+
+  def _ExistingBug(self):
+    keys = self.request.get_all('key')
+    bug_id = int(self.request.get('bug_id'))
+    alert_entities = ndb.get_multi([ndb.Key(urlsafe=k) for k in keys])
+    alert_group.ModifyAlertsAndAssociatedGroups(alert_entities, bug_id=bug_id)
+    return {}
 
   def AuthorizedPost(self, *args):
     """Returns alert data in response to API requests.
@@ -38,6 +102,12 @@ class AlertsHandler(api_request_handler.ApiRequestHandler):
       elif list_type.startswith('rev'):
         rev = list_type.replace('rev/', '')
         alert_list = group_report.GetAlertsAroundRevision(rev)
+      elif list_type == 'new_bug':
+        return self._FileBug()
+      elif list_type == 'recent_bugs':
+        return self._RecentBugs()
+      elif list_type == 'existing_bug':
+        return self._ExistingBug()
       elif list_type.startswith('history'):
         try:
           days = int(list_type.replace('history/', ''))
