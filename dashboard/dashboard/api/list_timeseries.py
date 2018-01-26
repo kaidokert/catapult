13d58d14f7ce45148104377af892f1ed8599ee71
Ethan Kuefner <eakuefner@chromium.org>
2018-01-26 10:14:09 -0800

    [Dashboard] API mods for histogram test hierarchy investigation
    This CL contains some modifcations that we'll deploy so we can inspect
    the correctness of timeseries hierarchies uploaded by the histogram
    pipeline using Colab.
    
    Change-Id: I010c48023b09ee358cc09f079a59abcbd72582ec

--- dashboard/dashboard/api/list_timeseries.py
+++ dashboard/dashboard/api/list_timeseries.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
+
 from google.appengine.ext import ndb
 
 from dashboard.api import api_request_handler
@@ -22,12 +24,15 @@ class ListTimeseriesHandler(api_request_handler.ApiRequestHandler):
       JSON list of monitored timeseries for the benchmark, see README.md.
     """
     benchmark = args[0]
-    sheriff_name = self.request.get('sheriff', 'Chromium Perf Sheriff')
-    sheriff = ndb.Key('Sheriff', sheriff_name)
+    only_monitored = self.request.get('only_monitored') != '0'
     query = graph_data.TestMetadata.query()
-    query = query.filter(graph_data.TestMetadata.suite_name == benchmark)
-    query = query.filter(graph_data.TestMetadata.has_rows == True)
-    query = query.filter(graph_data.TestMetadata.deprecated == False)
-    query = query.filter(graph_data.TestMetadata.sheriff == sheriff)
+    query = query.filter(
+        graph_data.TestMetadata.parent_test == utils.TestKey(benchmark))
+    if only_monitored:
+      sheriff_name = self.request.get('sheriff', 'Chromium Perf Sheriff')
+      sheriff = ndb.Key('Sheriff', sheriff_name)
+      query = query.filter(graph_data.TestMetadata.sheriff == sheriff)
+    else:
+      logging.info('Listing tests for %s regardless of monitoring' % benchmark)
     keys = query.fetch(keys_only=True)
     return [utils.TestPath(key) for key in keys]
