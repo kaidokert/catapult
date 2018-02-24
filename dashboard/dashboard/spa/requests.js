a5b4e226653c277d88cb790e6876ab39932bef1f
benshayden <benjhayden@chromium.org>
2018-02-24 14:50:42 -0800

    Chromeperf dashboard redesign SPA Views
    Design doc: https://docs.google.com/document/d/1tL-q3sxcuw1bKTmdpvP86XD1kh5eP0EtdGuWCgXttQI/edit
    
    Screenshot: http://www/~benjhayden/770251.png
    
    How to play along:
    git cl patch 770251
    bower cache clean
    pushd third_party/polymer2
    bower install
    popd
    pushd dashboard
    ln -sf ../third_party/apiclient/apiclient .
    ln -sf ../third_party/d3/d3.min.js .
    ln -sf ../devil/devil .
    ln -sf ../tracing/third_party/gl-matrix/dist/gl-matrix-min.js .
    ln -sf ../third_party/apiclient/googleapiclient .
    ln -sf ../third_party/httplib2/httplib2 .
    ln -sf ../tracing/third_party/mannwhitneyu .
    ln -sf ../third_party/oauth2client/oauth2client .
    ln -sf ../third_party/polymer .
    ln -sf ../third_party/polymer2 .
    ln -sf ../third_party/redux/redux.min.js .
    ln -sf ../third_party/six/six.py .
    ln -sf ../tracing/tracing .
    ln -sf ../tracing/tracing_project.py .
    ln -sf ../third_party/uritemplate/uritemplate .
    dev_appserver.py app.yaml &
    google-chrome localhost:8080/spa &
    popd
    
    Change-Id: I096963c9807288e75b11a7d93f80a67025e427cd

---
+++ dashboard/dashboard/spa/requests.js
@@ -0,0 +1,143 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class RequestBase {
+    constructor(options) {
+      this.method = 'GET';
+      this.headers = new Headers(options.headers);
+      this.body = undefined;
+
+      this.abortController = options.abortController;
+      if (!this.abortController && window.AbortController) {
+        this.abortController = new window.AbortController();
+      }
+      this.signal = undefined;
+      if (this.abortController) {
+        this.signal = this.abortController.signal;
+      }
+    }
+
+    abort() {
+      if (!this.abortController) return;
+      this.abortController.abort();
+    }
+
+    get url() {
+      throw new Error('not implemented');
+    }
+
+    get localhostResponse() {
+      return {};
+    }
+
+    async fetch() {
+      if (location.hostname === 'localhost') {
+        return this.postProcess(this.localhostResponse);
+      }
+
+      const response = await fetch(this.url, {
+        body: this.body,
+        headers: this.headers,
+        method: this.method,
+        signal: this.signal,
+      });
+      return this.postProcess(await response.json());
+    }
+
+    postProcess(json) {
+      return json;
+    }
+  }
+
+  class NewBugRequest extends RequestBase {
+    constructor(options) {
+      super(options);
+      this.method = 'POST';
+      this.body = new FormData();
+      for (const key of options.alertKeys) this.body.append('key', key);
+      for (const label of options.labels) this.body.append('label', label);
+      for (const component of options.components) {
+        this.body.append('component', component);
+      }
+      this.body.set('summary', options.summary);
+      this.body.set('description', options.description);
+      this.body.set('owner', options.owner);
+      this.body.set('cc', options.cc);
+    }
+
+    get url() {
+      return '/api/alerts/new_bug';
+    }
+
+    get localhostResponse() {
+      return {bug_id: 123456789};
+    }
+
+    postProcess(json) {
+      return json.bug_id;
+    }
+  }
+
+  class ExistingBugRequest extends RequestBase {
+    constructor(options) {
+      super(options);
+      this.method = 'POST';
+      this.body = new FormData();
+      for (const key of options.alertKeys) this.body.append('key', key);
+      this.body.set('bug_id', options.bugId);
+    }
+
+    get url() {
+      return '/api/alerts/existing_bug';
+    }
+  }
+
+  class AlertsRequest extends RequestBase {
+    constructor(options) {
+      super(options);
+      this.headers.set('Content-type', 'application/x-www-form-urlencoded');
+      this.method = 'POST';
+      this.body = new URLSearchParams();
+      for (const [key, value] of Object.entries(options.body)) {
+        if (value === undefined) continue;
+        this.body.set(key, value);
+      }
+    }
+
+    get url() {
+      // TODO /api/alerts
+      return '/alerts';
+    }
+
+    get localhostResponse() {
+      // TODO options
+      return {
+        anomaly_list: cp.dummyAlerts(
+            Boolean(options.improvements), Boolean(options.triaged)),
+        recent_bugs: cp.dummyRecentBugs(),
+      };
+    }
+
+    postProcess(json) {
+      if (json.error) {
+        // eslint-disable-next-line no-console
+        console.error('Error fetching alerts', err);
+        return {
+          anomaly_list: [],
+          recent_bugs: [],
+        };
+      }
+      return json;
+    }
+  }
+
+  return {
+    AlertsRequest,
+    ExistingBugRequest,
+    NewBugRequest,
+    RequestBase,
+  };
+});
