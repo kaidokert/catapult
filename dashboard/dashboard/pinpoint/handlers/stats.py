07eee90441e81e0192aa5b1ed8ee8242cc5caaab
Simon <simonhatch@chromium.org>
2018-03-02 16:15:23 -0500

    .
    .
    
    
    .
    
    
    tmp
    
    Bug: catapult:#
    Change-Id: I5847138674ec95c4f67b4cc7a8ac0e0820f2cc10

--- dashboard/dashboard/pinpoint/handlers/stats.py
+++ dashboard/dashboard/pinpoint/handlers/stats.py
@@ -10,7 +10,7 @@ import webapp2
 from dashboard.pinpoint.models import job as job_module
 
 
-_MAX_JOBS_TO_FETCH = 1000
+_MAX_JOBS_TO_FETCH = 500
 
 
 # TODO: Generalize the Jobs handler to allow the user to choose what fields to
@@ -25,15 +25,29 @@ class Stats(webapp2.RequestHandler):
 
 def _GetJobs():
   query = job_module.Job.query().order(-job_module.Job.created)
-  jobs = query.fetch(limit=_MAX_JOBS_TO_FETCH)
 
   job_infos = []
-  for job in jobs:
-    job_infos.append({
-        'created': job.created.isoformat(),
-        # TODO: Don't access JobState outside of the Job object.
-        'differences': len(list(job.state.Differences())),
-        'status': job.status,
-    })
+
+  start_cursor = None
+  for _ in xrange(10):
+    jobs, start_cursor, more = query.fetch_page(
+        _MAX_JOBS_TO_FETCH / 10, start_cursor=start_cursor)
+
+    for job in jobs:
+      if not job.auto_explore:
+        continue
+      job_infos.append({
+          'job_id': job.job_id,
+          'created': job.created.isoformat(),
+          # TODO: Don't access JobState outside of the Job object.
+          'differences': len(list(job.state.Differences())),
+          'status': job.status,
+          'bug_id': job.bug_id,
+          'exception': job.exception,
+          'arguments': job.arguments
+      })
+    jobs = None
+    if not more:
+      break
 
   return job_infos
