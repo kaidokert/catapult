0ca8bfc050543c5a45043e2aff035f07cb8a4ff8
Simon <simonhatch@chromium.org>
2018-03-06 20:06:10 -0500

    Pinpoint - Have results2 generated on the backend.
    Previously, we tried to download all the histograms for a job and then
    render out the vulcanized view. This would fail on large datasets with
    memory issues. Additionally, it always tried to render from scratch.
    
    This restructures the endpoint to generate it in a task on the task
    queue (which has a much higher timeout limit). We don't try to download
    all the histograms at once, instead we stream them in one by one as
    we render out the viewer, which is streamed out to cloud storage.
    
    Bug: catapult:#4168
    Change-Id: Ide282d20abd8e050f35e9d83632833ecee651378

--- dashboard/dashboard/pinpoint/handlers/results2_test.py
+++ dashboard/dashboard/pinpoint/handlers/results2_test.py
@@ -2,12 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
 import mock
-import unittest
 
 import webapp2
 import webtest
 
+from dashboard.common import testing_common
 from dashboard.pinpoint.handlers import results2
 
 
@@ -53,9 +54,11 @@ _JOB_MISSING_EXECUTIONS = {
 }
 
 
-class _Results2Test(unittest.TestCase):
+class _Results2Test(testing_common.TestCase):
 
   def setUp(self):
+    super(_Results2Test, self).setUp()
+
     app = webapp2.WSGIApplication([
         webapp2.Route(r'/results2/<job_id>', results2.Results2),
     ])
@@ -72,49 +75,91 @@ class _Results2Test(unittest.TestCase):
     self._job_from_id.return_value = job
 
 
-class FailureTest(_Results2Test):
+@mock.patch.object(results2.cloudstorage, 'listbucket')
+class Results2TestGetTest(_Results2Test):
 
-  def testGet_InvalidJob(self):
+  def testGet_InvalidJob(self, mock_cloudstorage):
+    mock_cloudstorage.return_value = []
+
     self._SetJob(None)
     response = self.testapp.get('/results2/123', status=400)
     self.assertIn('Error', response.body)
 
-  def testGet_JobHasNoTestQuest(self):
-    self._SetJob(_JobStub({'quests': []}))
-    response = self.testapp.get('/results2/123', status=400)
-    self.assertIn('No Test quest', response.body)
+  def testGet_Cached_ReturnsResult(self, mock_cloudstorage):
+    mock_cloudstorage.return_value = ['foo']
+
+    self._SetJob(None)
+    response = self.testapp.get('/results2/456')
+    result = json.loads(response.body)
+    self.assertEqual('complete', result['status'])
+    self.assertEqual(
+        'https://storage.cloud.google.com/chromeperf.appspot.com/results2/'
+        '456.html',
+        result['url'])
+
+  def testGet_Uncached_SchedulesGenerate(self, mock_cloudstorage):
+    mock_cloudstorage.return_value = []
+
+    self._SetJob(_JobStub(_JOB_WITH_DIFFERENCES))
+    response = self.testapp.get('/results2/789')
+    result = json.loads(response.body)
+    self.assertEqual('pending', result['status'])
+
+
+@mock.patch.object(results2, 'open', mock.mock_open(read_data='fake_viewer'),
+                   create=True)
+class PostTest(_Results2Test):
+
+  @mock.patch.object(results2, '_FetchHistogramsDataFromJobData',
+                     mock.MagicMock(return_value=['a', 'b']))
+  @mock.patch.object(results2, '_GcsFileStream', mock.MagicMock())
+  @mock.patch.object(results2.render_histograms_viewer,
+                     'RenderHistogramsViewer')
+  def testPost_Renders(self, mock_render):
+    self._SetJob(_JobStub(_JOB_NO_DIFFERENCES))
+    self.testapp.post('/results2/123')
+
+    mock_render.assert_called_with(
+        ['a', 'b'], mock.ANY, reset_results=True, vulcanized_html='fake_viewer')
+
+    results = results2.JobCachedResults2.query().fetch()
+    self.assertEqual(1, len(results))
+
+  @mock.patch.object(results2.read_value, '_RetrieveOutputJson',
+                     mock.MagicMock(return_value=['a']))
+  @mock.patch.object(results2, '_GcsFileStream', mock.MagicMock())
+  def testPost_InvalidJob(self):
+    self._SetJob(None)
+    response = self.testapp.post('/results2/123')
+    self.assertIn('Error', response.body)
 
 
 @mock.patch.object(results2.read_value, '_RetrieveOutputJson',
                    mock.MagicMock(return_value=['a']))
-@mock.patch.object(results2, 'open', mock.mock_open(read_data='fake_viewer'),
-                   create=True)
-@mock.patch.object(results2.render_histograms_viewer, 'RenderHistogramsViewer')
-class SuccessTest(_Results2Test):
-
-  def testGet_NoDifferences(self, mock_render):
+class FetchHistogramsTest(_Results2Test):
+  def testGet_WithNoDifferences(self):
     self._SetJob(_JobStub(_JOB_NO_DIFFERENCES))
-    self.testapp.get('/results2/123')
-    mock_render.assert_called_with(
-        ['a', 'a', 'a', 'a'], mock.ANY, vulcanized_html='fake_viewer')
+    fetch = results2._FetchHistogramsDataFromJobData('123')
+    self.assertEqual(['a', 'a', 'a', 'a'], [f for f in fetch])
 
-  def testGet_WithDifferences(self, mock_render):
+  def testGet_WithDifferences(self):
     self._SetJob(_JobStub(_JOB_WITH_DIFFERENCES))
-    self.testapp.get('/results2/123')
-    mock_render.assert_called_with(
-        ['a', 'a', 'a'], mock.ANY, vulcanized_html='fake_viewer')
+    fetch = results2._FetchHistogramsDataFromJobData('123')
+    self.assertEqual(['a', 'a', 'a'], [f for f in fetch])
 
-  def testGet_MissingExecutions(self, mock_render):
+  def testGet_MissingExecutions(self):
     self._SetJob(_JobStub(_JOB_MISSING_EXECUTIONS))
-    self.testapp.get('/results2/123')
-    mock_render.assert_called_with(
-        ['a', 'a'], mock.ANY, vulcanized_html='fake_viewer')
+    fetch = results2._FetchHistogramsDataFromJobData('123')
+    self.assertEqual(['a', 'a'], [f for f in fetch])
 
 
 class _JobStub(object):
 
   def __init__(self, job_dict):
     self._job_dict = job_dict
+
+  def ScheduleResults2Generation(self):
+    return 'pending'
 
   def AsDict(self, options=None):
     del options
