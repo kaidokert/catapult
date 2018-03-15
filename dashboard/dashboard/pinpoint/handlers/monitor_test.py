28d4e3290f3ed2d96a74395da97d7ebab9884d83
Dave Tu <dtu@chromium.org>
2018-03-15 11:50:29 -0700

    [pinpoint] Add Job monitoring cron.
    The Job monitoring cron loops through every Job with an outstanding task.
    If the Job was updated more than 10 minutes ago, it assumes it is stale, and
    kicks off a new task.
    
    Bug: catapult:#4248
    Change-Id: I2a54e83ec884ae4e082813cfde36caa11889255e

---
+++ dashboard/dashboard/pinpoint/handlers/monitor_test.py
@@ -0,0 +1,46 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import datetime
+import webapp2
+import webtest
+
+from dashboard.common import testing_common
+from dashboard.pinpoint.handlers import monitor
+from dashboard.pinpoint.models import job as job_module
+
+
+class MonitorTest(testing_common.TestCase):
+
+  def setUp(self):
+    super(MonitorTest, self).setUp()
+
+    app = webapp2.WSGIApplication([
+        webapp2.Route(r'/api/monitor', monitor.Monitor),
+    ])
+    self.testapp = webtest.TestApp(app)
+
+  def testCompletedJob(self):
+    job = job_module.Job.New({}, [], False)
+    job.put()
+
+    self.testapp.get('/api/monitor', status=200)
+    self.assertIsNone(job.task)
+
+  def testFreshJob(self):
+    job = job_module.Job.New({}, [], False)
+    job.task = 'task'
+    job.put()
+
+    self.testapp.get('/api/monitor', status=200)
+    self.assertEqual(job.task, 'task')
+
+  def testStaleJob(self):
+    job = job_module.Job.New({}, [], False)
+    job.task = 'task'
+    job.updated = datetime.datetime.now() - datetime.timedelta(minutes=11)
+    job.put()
+
+    self.testapp.get('/api/monitor', status=200)
+    self.assertNotEqual(job.task, 'task')
