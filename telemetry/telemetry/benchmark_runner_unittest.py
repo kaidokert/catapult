6ef1ca868a69c341996b3bff51d571ea0fb72e1f
Ting-Yuan Huang <laszio@chromium.org>
2018-03-19 12:02:41 -0700

    [Telemetry] Support outputing json in benchmark_runner
    The default is unchanged and still in a human readable format.
    
    Bug: chromium:819969
    Change-Id: Id511540395eefbbf4e324d8657cfc51d12c69e69

--- telemetry/telemetry/benchmark_runner_unittest.py
+++ telemetry/telemetry/benchmark_runner_unittest.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
 import os
 import tempfile
 import unittest
@@ -70,6 +71,35 @@ class BenchmarkRunnerUnittest(unittest.TestCase):
                                           self._stream)
 
       self.assertEquals(expected_printed_stream, self._stream.output_data)
+
+    finally:
+      os.remove(expectations_file.name)
+
+  def testPrintBenchmarkListInJSON(self):
+    expected_out = json.dumps([
+        {'name': BenchmarkFoo.Name(),
+         'description': BenchmarkFoo.Description(),
+         'enabled': True},
+        {'name': BenchmarkBar.Name(),
+         'description': BenchmarkBar.Description(),
+         'enabled': False}], indent=4, sort_keys=True, separators=(',', ': '))
+
+    expectations_file_contents = (
+        '# tags: All\n'
+        'crbug.com/123 [ All ] BenchmarkBar/* [ Skip ]\n'
+    )
+
+    expectations_file = tempfile.NamedTemporaryFile(bufsize=0, delete=False)
+    try:
+      expectations_file.write(expectations_file_contents)
+      expectations_file.close()
+      benchmark_runner.PrintBenchmarkList([BenchmarkFoo, BenchmarkBar],
+                                          self._mock_possible_browser,
+                                          expectations_file.name,
+                                          self._stream,
+                                          'json')
+
+      self.assertEquals(expected_out, self._stream.output_data)
 
     finally:
       os.remove(expectations_file.name)
