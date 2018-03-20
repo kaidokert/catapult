ad9a3c200a965809aab62aae7df67f4a37614f4a
Ting-Yuan Huang <laszio@chromium.org>
2018-03-20 15:07:30 -0700

    TEST; DO NOT REVIEW
    Bug: chromium:819969
    Change-Id: Ia7ecd9e19201add47096735263fdeb91f51d2b75

--- telemetry/telemetry/benchmark_runner_unittest.py
+++ telemetry/telemetry/benchmark_runner_unittest.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
 import os
 import tempfile
 import unittest
@@ -36,6 +37,8 @@ class BenchmarkRunnerUnittest(unittest.TestCase):
     self._mock_possible_browser.browser_type = 'TestBrowser'
 
   def testPrintBenchmarkListWithNoDisabledBenchmark(self):
+    print '@@@', dir(self._stream), '@@@'
+    print '@@@', self._stream.__class__, '@@@'
     expected_printed_stream = (
         'Available benchmarks for TestBrowser are:\n'
         '  BenchmarkBar Benchmark bar for testing.\n'
@@ -47,6 +50,8 @@ class BenchmarkRunnerUnittest(unittest.TestCase):
     self.assertEquals(expected_printed_stream, self._stream.output_data)
 
   def testPrintBenchmarkListWithOneDisabledBenchmark(self):
+    print '@@@', dir(self._stream), '@@@'
+    print '@@@', self._stream.__class__, '@@@'
     expected_printed_stream = (
         'Available benchmarks for TestBrowser are:\n'
         '  BenchmarkFoo Benchmark foo for testing.\n'
@@ -68,6 +73,36 @@ class BenchmarkRunnerUnittest(unittest.TestCase):
                                           self._mock_possible_browser,
                                           expectations_file.name,
                                           self._stream)
+
+      self.assertEquals(expected_printed_stream, self._stream.output_data)
+
+    finally:
+      os.remove(expectations_file.name)
+
+  def testPrintBenchmarkListInJSON(self):
+    expected_printed_stream = json.dumps([
+        {'name': BenchmarkFoo.Name(),
+         'description': BenchmarkFoo.Description(),
+         'enabled': True},
+        {'name': BenchmarkBar.Name(),
+         'description': BenchmarkBar.Description(),
+         'enabled': False}], indent=4, sort_keys=True, separators=(',', ': '))
+
+    expectations_file_contents = (
+        '# tags: All\n'
+        'crbug.com/123 [ All ] BenchmarkBar/* [ Skip ]\n'
+    )
+
+    expectations_file = tempfile.NamedTemporaryFile(bufsize=0, delete=False)
+    try:
+      expectations_file.write(expectations_file_contents)
+      expectations_file.close()
+      print '@@@', dir(self._stream), '@@@'
+      print '@@@', self._stream.__class__, '@@@'
+      benchmark_runner.PrintBenchmarkList([BenchmarkFoo, BenchmarkBar],
+                                          self._mock_possible_browser,
+                                          expectations_file.name,
+                                          self._stream, as_json=True)
 
       self.assertEquals(expected_printed_stream, self._stream.output_data)
 
