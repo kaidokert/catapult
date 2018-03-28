13c6e1ca7b5ae8d86300f90cb7b0425a148c6965
Stefan Zager <szager@chromium.org>
2018-03-28 12:31:21 -0700

    Add DeviceUtils.GetPackageArchitecture
    Change-Id: Id5e27a8ad7521ac3fcec81644163847b26f7d12d

--- devil/devil/android/device_utils.py
+++ devil/devil/android/device_utils.py
@@ -649,6 +649,22 @@ class DeviceUtils(object):
         'Version name for %s not found on dumpsys output' % package, str(self))
 
   @decorators.WithTimeoutAndRetriesFromInstance()
+  def GetPackageArchitecture(self, package, timeout=None, retries=None):
+    """Get the architecture of a package installed on the device.
+
+    Args:
+      package: Name of the package.
+
+    Returns:
+      A string with the architecture, or None if the package is missing.
+    """
+    lines = self._GetDumpsysOutput('package %s' % package, 'primaryCpuAbi')
+    if lines:
+      _, _, package_arch = lines[-1].partition('=')
+      return package_arch.strip()
+    return None
+
+  @decorators.WithTimeoutAndRetriesFromInstance()
   def GetApplicationDataDirectory(self, package, timeout=None, retries=None):
     """Get the data directory on the device for the given package.
 
@@ -2351,6 +2367,28 @@ class DeviceUtils(object):
         continue
       processes.append(ProcessInfo(**row))
     return processes
+
+  def _GetDumpsysOutput(self, category, pattern=None):
+    """Runs |dumpsys| command on the device and returns its output,
+
+    This private method implements support for filtering the output by a given
+    |pattern|, but does not do any output parsing.
+    """
+    try:
+      cmd = 'dumpsys %s' % category
+      if pattern:
+        return self._RunPipedShellCommand(
+            '%s | grep -F %s' % (cmd, cmd_helper.SingleQuote(pattern)))
+      else:
+        return self.RunShellCommand(
+            cmd.split(), check_return=True, large_output=True)
+    except device_errors.AdbShellCommandFailedError as e:
+      if e.status and isinstance(e.status, list) and not e.status[0]:
+        # If dumpsys succeeded but grep failed, there were no lines matching
+        # the given pattern.
+        return []
+      else:
+        raise
 
   # TODO(#4103): Remove after migrating clients to ListProcesses.
   @decorators.WithTimeoutAndRetriesFromInstance()
