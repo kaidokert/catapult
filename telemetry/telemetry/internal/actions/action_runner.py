0cd5a4a44e877e6947e58d1296068472ab7d9119
Ulan Degenbaev <ulan@chromium.org>
2018-04-04 20:34:18 +0200

    Metric experiment: GC + Wait + 2GC
    Change-Id: I0e4d2596ff1e147597612bc691b4232689a6c003

--- telemetry/telemetry/internal/actions/action_runner.py
+++ telemetry/telemetry/internal/actions/action_runner.py
@@ -41,7 +41,7 @@ _MEMORY_DUMP_WAIT_TIME = 3
 # Time to wait in seconds after forcing garbage collection to allow its
 # effects to propagate. Experimentally determined on an Android One device
 # that Java Heap garbage collection can take ~5 seconds to complete.
-_GARBAGE_COLLECTION_WAIT_TIME = 6
+_GARBAGE_COLLECTION_PROPAGATION_TIME = 6
 
 
 class ActionRunner(object):
@@ -752,13 +752,20 @@ class ActionRunner(object):
     - JavaScript on the current renderer.
     - System caches (on supported platforms).
     """
-    if self._tab.browser.supports_java_heap_garbage_collection:
-      self._tab.browser.ForceJavaHeapGarbageCollection()
+    # 1) Perform V8 and Blink garbage collection. This may free java wrappers.
     self._tab.CollectGarbage()
+    if self._tab.browser.supports_java_heap_garbage_collection:
+      # 2) Perform Java garbage collection
+      self._tab.browser.ForceJavaHeapGarbageCollection()
     if self._tab.browser.platform.SupportFlushEntireSystemCache():
       self._tab.browser.platform.FlushEntireSystemCache()
-    self.Wait(_GARBAGE_COLLECTION_WAIT_TIME)
-
+    self.Wait(_GARBAGE_COLLECTION_PROPAGATION_TIME)
+    # 3) Re-do V8 and Blink garbage collection to free garbage allocated
+    # while waiting.
+    self._tab.CollectGarbage()
+    # 4) Finally, finish with V8 and Blink garbage collection because some
+    # objects require V8 GC => Blink GC => V8 GC roundtrip.
+    self._tab.CollectGarbage()
 
   def SimulateMemoryPressureNotification(self, pressure_level):
     """Simulate memory pressure notification.
