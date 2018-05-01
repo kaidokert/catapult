b81a81f623cb6dfef54541d6813d48c3cc6cb313
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-05-01 16:19:43 +0100

    [soundwave] Store timeseries data in database
    Adds tables.timeseries.DataFrameFromJson to convert dashboard API
    reponses into a dataframe to store in the local database.
    
    This CL makes all of soundwave data now available through SQLite.
    
    Bug: catapult:#4382
    Change-Id: I97e15a06ad9b303ff543256a0fe1d3a5c65c52b1

--- experimental/soundwave/soundwave/pandas_sqlite_test.py
+++ experimental/soundwave/soundwave/pandas_sqlite_test.py
@@ -10,39 +10,44 @@ from soundwave import pandas_sqlite
 
 
 class TestPandasSQLite(unittest.TestCase):
-  def testInsertOrReplaceRecords_newTable(self):
-    columns = ('bug_id', 'summary', 'status')
-    df1 = pandas.DataFrame.from_records(
+  COLUMNS = ('bug_id', 'summary', 'status')
+  INDEX = COLUMNS[0]
+
+  def _ExampleDf1(self):
+    return pandas.DataFrame.from_records(
         [(123, 'Some bug', 'Started'), (456, 'Another bug', 'Assigned')],
-        columns=columns, index=columns[0])
+        columns=self.COLUMNS, index=self.INDEX)
+
+  def _ExampleDf2(self):
+    return pandas.DataFrame.from_records(
+        [(123, 'Some bug', 'Fixed'), (789, 'A new bug', 'Untriaged')],
+        columns=self.COLUMNS, index=self.INDEX)
+
+  def testInsertOrReplaceRecords_newTable(self):
+    df1 = self._ExampleDf1()
     conn = sqlite3.connect(':memory:')
     try:
       # Write new table to database, read back and check they are equal.
       pandas_sqlite.InsertOrReplaceRecords(df1, 'bugs', conn)
-      df = pandas.read_sql('SELECT * FROM bugs', conn, index_col=columns[0])
+      df = pandas.read_sql('SELECT * FROM bugs', conn, index_col=self.INDEX)
       self.assertTrue(df.equals(df1))
     finally:
       conn.close()
 
   def testInsertOrReplaceRecords_existingTable(self):
-    columns = ('bug_id', 'summary', 'status')
-    df1 = pandas.DataFrame.from_records(
-        [(123, 'Some bug', 'Started'), (456, 'Another bug', 'Assigned')],
-        columns=columns, index=columns[0])
-    df2 = pandas.DataFrame.from_records(
-        [(123, 'Some bug', 'Fixed'), (789, 'A new bug', 'Untriaged')],
-        columns=columns, index=columns[0])
+    df1 = self._ExampleDf1()
+    df2 = self._ExampleDf2()
     conn = sqlite3.connect(':memory:')
     try:
       # Write first data frame to database.
       pandas_sqlite.InsertOrReplaceRecords(df1, 'bugs', conn)
-      df = pandas.read_sql('SELECT * FROM bugs', conn, index_col=columns[0])
+      df = pandas.read_sql('SELECT * FROM bugs', conn, index_col=self.INDEX)
       self.assertEqual(len(df), 2)
       self.assertEqual(df.loc[123]['status'], 'Started')
 
       # Write second data frame to database.
       pandas_sqlite.InsertOrReplaceRecords(df2, 'bugs', conn)
-      df = pandas.read_sql('SELECT * FROM bugs', conn, index_col=columns[0])
+      df = pandas.read_sql('SELECT * FROM bugs', conn, index_col=self.INDEX)
       self.assertEqual(len(df), 3)  # Only one extra record added.
       self.assertEqual(df.loc[123]['status'], 'Fixed')  # Bug is now fixed.
       self.assertItemsEqual(df.index, (123, 456, 789))
