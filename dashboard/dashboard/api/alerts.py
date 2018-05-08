dfc557090fa6cfbe2283a614866fb39aea452cf2
benshayden <benjhayden@chromium.org>
2018-05-08 15:07:18 -0700

    Chromeperf dashboard V2 single page app NOT FOR REVIEW
    https://v2spa-dot-chromeperf.appspot.com/spa
    Change-Id: Id566b7c0ee6174c76537d1410a293f9f4d37cc46

--- dashboard/dashboard/api/alerts.py
+++ dashboard/dashboard/api/alerts.py
@@ -1,19 +1,83 @@
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 import datetime
+import httplib2
+import logging
 
 from google.appengine.ext import ndb
 
+from dashboard import alerts
+from dashboard import file_bug
+from dashboard import group_report
 from dashboard.api import api_request_handler
 from dashboard.common import request_handler
+from dashboard.common import utils
+from dashboard.models import alert_group
 from dashboard.models import anomaly
-from dashboard import alerts
-from dashboard import group_report
+from dashboard.services import issue_tracker_service
 
 
 class AlertsHandler(api_request_handler.ApiRequestHandler):
   """API handler for various alert requests."""
+
+  def _AuthorizedHttp(self):
+    # TODO(benjhayden): Use this instead of ServiceAccountHttp in order to use
+    # the user's account. That will require changing the google-signin's
+    # client-id in chromeperf-app.html to a client-id that is whitelisted by the
+    # issue tracker service, which will require either adding
+    # v2spa-dot-chromeperf.appspot.com to the list of domains for an existing
+    # client id, or else launching v2spa to chromeperf.appspot.com.
+    http = httplib2.Http()
+    orig_request = http.request
+    def NewRequest(uri, method='GET', body=None, headers=None,
+                   redirections=httplib2.DEFAULT_MAX_REDIRECTS,
+                   connection_type=None):
+      headers = dict(headers or {})
+      headers['Authorization'] = self.request.headers.get('Authorization')
+      return orig_request(uri, method, body, headers, redirections,
+                          connection_type)
+    http.request = NewRequest
+    return http
+
+  def _FileBug(self):
+    if not utils.IsValidSheriffUser():
+      raise api_request_handler.BadRequestError(
+          'Only chromium.org accounts may file bugs')
+
+    owner = self.request.get('owner')
+    cc = self.request.get('cc')
+    if owner and not owner.endswith('@chromium.org'):
+      raise api_request_handler.BadRequestError(
+          'Owner email address must end with @chromium.org')
+
+    summary = self.request.get('summary')
+    description = self.request.get('description')
+    labels = self.request.get_all('label')
+    components = self.request.get_all('component')
+    keys = self.request.get_all('key')
+    http = utils.ServiceAccountHttp()  # TODO use self._AuthorizedHttp()
+    return file_bug.FileBug(
+        http, keys, summary, description, labels, components, owner, cc)
+
+  def _RecentBugs(self):
+    if not utils.IsValidSheriffUser():
+      raise api_request_handler.BadRequestError(
+          'Only chromium.org accounts may query recent bugs')
+    http = utils.ServiceAccountHttp()  # TODO use self._AuthorizedHttp()
+    issue_tracker = issue_tracker_service.IssueTrackerService(http)
+    response = issue_tracker.List(
+        q='opened-after:today-5', label='Type-Bug-Regression,Performance',
+        sort='-id')
+    return {'bugs': response.get('items', [])}
+
+  def _ExistingBug(self):
+    keys = self.request.get_all('key')
+    bug_id = int(self.request.get('bug_id'))
+    alert_entities = ndb.get_multi([ndb.Key(urlsafe=k) for k in keys])
+    alert_group.ModifyAlertsAndAssociatedGroups(alert_entities, bug_id=bug_id)
+    return {}
 
   def AuthorizedPost(self, *args):
     """Returns alert data in response to API requests.
@@ -38,26 +102,50 @@ class AlertsHandler(api_request_handler.ApiRequestHandler):
       elif list_type.startswith('rev'):
         rev = list_type.replace('rev/', '')
         alert_list = group_report.GetAlertsAroundRevision(rev)
+      elif list_type == 'new_bug':
+        return self._FileBug()
+      elif list_type == 'recent_bugs':
+        return self._RecentBugs()
+      elif list_type == 'existing_bug':
+        return self._ExistingBug()
       elif list_type.startswith('history'):
+        query = anomaly.Anomaly.query()
+
+        sheriff_name = self.request.get('sheriff', 'Chromium Perf Sheriff')
+        if sheriff_name:
+          sheriff_key = ndb.Key('Sheriff', sheriff_name)
+          sheriff = sheriff_key.get()
+          if sheriff:
+            logging.info('sheriff %s', sheriff_name)
+            query = query.filter(anomaly.Anomaly.sheriff == sheriff_key)
+
         try:
           days = int(list_type.replace('history/', ''))
         except ValueError:
           days = 7
-        cutoff = datetime.datetime.now() - datetime.timedelta(days=days)
-        sheriff_name = self.request.get('sheriff', 'Chromium Perf Sheriff')
-        sheriff_key = ndb.Key('Sheriff', sheriff_name)
-        sheriff = sheriff_key.get()
-        if not sheriff:
-          raise api_request_handler.BadRequestError(
-              'Invalid sheriff %s' % sheriff_name)
+        if days > 0:
+          logging.info('days %d', days)
+          cutoff = datetime.datetime.now() - datetime.timedelta(days=days)
+          query = query.filter(anomaly.Anomaly.timestamp > cutoff)
+
+        include_triaged = bool(self.request.get('triaged'))
+        if not include_triaged:
+          logging.info('untriaged')
+          query = query.filter(anomaly.Anomaly.bug_id == None)
+
+        include_recovered = bool(self.request.get('recovered'))
+        if not include_recovered:
+          logging.info('unrecovered')
+          query = query.filter(anomaly.Anomaly.recovered == False)
+
         include_improvements = bool(self.request.get('improvements'))
-        filter_for_benchmark = self.request.get('benchmark')
-        query = anomaly.Anomaly.query(anomaly.Anomaly.sheriff == sheriff_key)
-        query = query.filter(anomaly.Anomaly.timestamp > cutoff)
         if not include_improvements:
-          query = query.filter(
-              anomaly.Anomaly.is_improvement == False)
+          logging.info('regressions')
+          query = query.filter(anomaly.Anomaly.is_improvement == False)
+
+        filter_for_benchmark = self.request.get('benchmark')
         if filter_for_benchmark:
+          logging.info('benchmark %s', filter_for_benchmark)
           query = query.filter(
               anomaly.Anomaly.benchmark_name == filter_for_benchmark)
 
