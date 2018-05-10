aedf5e1fa6fc871016a8bd016ccc8fafc6905283
Annie Sullivan <sullivan@chromium.org>
2018-05-10 13:16:47 -0400

    Implement gerrit_service.PostChangeComment
    This will allow perf dashboard an pinpoint to use the service account
    to post comments to gerrit code reviews. Example use cases:
    * Pinpoint updates CL to inform that try job results are in
    * Perf dashboard updates CL to inform that it has caused a regression
    
    Bug: catapult:#4356
    Change-Id: I61d129441f5a9b816a71ebc92c1dbf1832c2698e

--- dashboard/dashboard/services/gerrit_service.py
+++ dashboard/dashboard/services/gerrit_service.py
@@ -7,9 +7,26 @@
 API doc: https://gerrit-review.googlesource.com/Documentation/rest-api.html
 """
 
+import json
+import logging
+
+from dashboard.common import utils
 from dashboard.services import request
 
+
+OAUTH_SCOPE_GERRIT = 'https://www.googleapis.com/auth/gerritcodereview'
 
 def GetChange(server_url, change_id, fields=None):
   url = '%s/changes/%s' % (server_url, change_id)
   return request.RequestJson(url, o=fields)
+
+def PostChangeComment(server_url, change_id, comment):
+  url = '%s/changes/%s/current/review' % (server_url, change_id)
+  content = request.Request(
+      url,
+      method='POST',
+      body=json.dumps({'message': comment}),
+      use_cache=False,
+      use_auth=True,
+      scope=[OAUTH_SCOPE_GERRIT, utils.EMAIL_SCOPE])
+  logging.Log(content)
