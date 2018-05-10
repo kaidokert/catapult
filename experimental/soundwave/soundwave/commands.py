5e65ec0e7a1df6d68df30a25da316879a4ee9ec7
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-05-10 13:38:40 +0100

    [OBSOLETE] First try implementing paralellism
    Change-Id: I21931767f1fa0af23d39198a2209bfdcf30029ae

--- experimental/soundwave/soundwave/commands.py
+++ experimental/soundwave/soundwave/commands.py
@@ -1,9 +1,12 @@
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+from __future__ import division
 
+import multiprocessing
 import pandas  # pylint: disable=import-error
 import sqlite3
+import sys
 
 from soundwave import dashboard_api
 from soundwave import pandas_sqlite
@@ -45,6 +48,23 @@ def _SkipTestPathsInCache(con, test_paths):
       yield test_path
 
 
+def _TimeseriesWorker(test_path):
+  try:
+    api = _TimeseriesWorker.api
+    con = _TimeseriesWorker.con
+    data = api.GetTimeseries(test_path, days=_TimeseriesWorker.days)
+    timeseries = tables.timeseries.DataFrameFromJson(data)
+    pandas_sqlite.InsertOrReplaceRecords(timeseries, 'timeseries', con)
+  except KeyboardInterrupt:
+    pass
+
+
+def _InitTimeseriesWorker(args):
+  _TimeseriesWorker.days = args.days
+  _TimeseriesWorker.api = api = dashboard_api.PerfDashboardCommunicator(args)
+  _TimeseriesWorker.con = sqlite3.connect(args.database_file)
+
+
 def FetchTimeseriesData(args):
   def _MatchesAllFilters(test_path):
     return all(f in test_path for f in args.filters)
@@ -60,13 +80,25 @@ def FetchTimeseriesData(args):
 
     if args.use_cache and tables.timeseries.HasTable(con):
       test_paths = list(_SkipTestPathsInCache(con, test_paths))
-      num_skipped = num_found - len(test_paths)
+      num_found, prev_num_found = len(test_paths), num_found
+      num_skipped = prev_num_found - num_found
       if num_skipped:
         print '(skipping %d test paths already in the database)' % num_skipped
-
-    for test_path in test_paths:
-      data = api.GetTimeseries(test_path, days=args.days)
-      timeseries = tables.timeseries.DataFrameFromJson(data)
-      pandas_sqlite.InsertOrReplaceRecords(timeseries, 'timeseries', con)
   finally:
     con.close()
+
+  test_paths = test_paths[:100]
+  num_found = len(test_paths)
+
+  time_started = pandas.Timestamp.utcnow()
+  sys.stdout.write('Fetching %d timeseries: ' % num_found)
+  sys.stdout.flush()
+  pool = multiprocessing.Pool(
+      processes=10, initializer=_InitTimeseriesWorker, initargs=(args, ))
+  for _ in pool.imap_unordered(_TimeseriesWorker, test_paths):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+  sys.stdout.write('\n')
+  time_finished = pandas.Timestamp.utcnow()
+  test_paths_per_sec = num_found / (time_finished - time_started).seconds
+  print '[Speed: %.1f test paths per second]' % test_paths_per_sec
