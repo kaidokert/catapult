88e868ac3412e62be9b589c27820a4fe33892c94
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-05-10 16:58:55 +0100

    [soundwave] Fetch timeseries data in parallel!
    Use multiprocessing to fetch multiple timeseries concurrently.
    
    Bug: catapult:#4349
    Change-Id: I33eddf98f03ff9ccdc5ac193d9e7bfab8fd687ea

--- experimental/soundwave/soundwave/commands.py
+++ experimental/soundwave/soundwave/commands.py
@@ -1,9 +1,12 @@
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+from __future__ import division
 
+import multiprocessing
 import pandas  # pylint: disable=import-error
 import sqlite3
+import sys
 
 from soundwave import dashboard_api
 from soundwave import pandas_sqlite
@@ -45,6 +48,38 @@ def _SkipTestPathsInCache(con, test_paths):
       yield test_path
 
 
+def _FetchTimeseriesWorkerInit(args):
+  api = dashboard_api.PerfDashboardCommunicator(args)
+  con = sqlite3.connect(args.database_file, timeout=10)
+
+  def Process(test_path):
+    data = api.GetTimeseries(test_path, days=args.days)
+    timeseries = tables.timeseries.DataFrameFromJson(data)
+    pandas_sqlite.InsertOrReplaceRecords(con, 'timeseries', timeseries)
+
+  _FetchTimeseriesWorker.Process = Process
+
+
+def _FetchTimeseriesWorker(test_path):
+  try:
+    _FetchTimeseriesWorker.Process(test_path)
+  except KeyboardInterrupt:
+    pass
+
+
+def _ProgressIndicator(label, iterable):
+  time_started = pandas.Timestamp.utcnow()
+  sys.stdout.write(label)
+  sys.stdout.flush()
+  for _ in iterable:
+    sys.stdout.write('.')
+    sys.stdout.flush()
+  sys.stdout.write('\n')
+  sys.stdout.flush()
+  time_finished = pandas.Timestamp.utcnow()
+  return (time_finished - time_started).total_seconds()
+
+
 def FetchTimeseriesData(args):
   def _MatchesAllFilters(test_path):
     return all(f in test_path for f in args.filters)
@@ -64,10 +99,13 @@ def FetchTimeseriesData(args):
       num_skipped = num_found - len(test_paths)
       if num_skipped:
         print '(skipping %d test paths already in the database)' % num_skipped
-
-    for test_path in test_paths:
-      data = api.GetTimeseries(test_path, days=args.days)
-      timeseries = tables.timeseries.DataFrameFromJson(data)
-      pandas_sqlite.InsertOrReplaceRecords(con, 'timeseries', timeseries)
   finally:
     con.close()
+
+  pool = multiprocessing.Pool(
+      processes=args.processes,
+      initializer=_FetchTimeseriesWorkerInit, initargs=(args,))
+  total_seconds = _ProgressIndicator(
+      'Fetching %d timeseries: ' % num_found,
+      pool.imap_unordered(_FetchTimeseriesWorker, test_paths))
+  print '[%.1f test paths per second]' % (len(test_paths) / total_seconds)
