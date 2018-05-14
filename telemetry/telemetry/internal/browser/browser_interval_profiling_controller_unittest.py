477336bb905c77d7d7d31e3b75763c094a9cd1c8
Stefan Zager <szager@chromium.org>
2018-05-14 12:25:52 -0700

    Add linux support for interval profiling
    R=nednguyen@google.com,charliea@chromium.org
    
    Change-Id: I4e24a60a91b85a40eea6a3e636a82e4d70e24089

--- telemetry/telemetry/internal/browser/browser_interval_profiling_controller_unittest.py
+++ telemetry/telemetry/internal/browser/browser_interval_profiling_controller_unittest.py
@@ -30,6 +30,11 @@ class FakeLinuxPlatformBackend(object):
     return 'linux'
 
 
+class FakeWindowsPlatformBackend(object):
+  def GetOSName(self):
+    return 'windows'
+
+
 class FakePossibleBrowser(object):
   def __init__(self, platform_backend):
     self._platform_backend = platform_backend
@@ -37,10 +42,11 @@ class FakePossibleBrowser(object):
 
 class BrowserIntervalProfilingControllerTest(unittest.TestCase):
   def _RunTest(
-      self, possible_browser, periods, constructor_count, sample_count):
-    with mock.patch(
-        'telemetry.internal.browser.browser_interval_profiling_controller'
-        '._AndroidController') as android_controller_mock:
+      self, possible_browser, periods, expected_platform, expected_samples):
+    with mock.patch.multiple(
+        'telemetry.internal.browser.browser_interval_profiling_controller',
+        _AndroidController=mock.DEFAULT,
+        _LinuxController=mock.DEFAULT) as mock_classes:
       # 80 characters FTW
       profiling_mod = browser_interval_profiling_controller
       controller = profiling_mod.BrowserIntervalProfilingController(
@@ -51,29 +57,37 @@ class BrowserIntervalProfilingControllerTest(unittest.TestCase):
         pass
 
       # Verify that the appropriate platform controller was instantiated.
-      self.assertEqual(android_controller_mock.call_count, constructor_count)
+      self.assertEqual(mock_classes['_AndroidController'].call_count,
+                       1 if expected_platform == 'android' else 0)
+      self.assertEqual(mock_classes['_LinuxController'].call_count,
+                       1 if expected_platform == 'linux' else 0)
 
       # Verify that samples were collected the expected number of times.
       if controller._platform_controller:
         self.assertEqual(
             controller._platform_controller.SamplePeriod.call_count,
-            sample_count)
+            expected_samples)
       else:
-        self.assertEqual(0, sample_count)
+        self.assertEqual(expected_samples, 0)
 
   def testSupportedAndroid(self):
     possible_browser = FakePossibleBrowser(
         FakeAndroidPlatformBackend(version_codes.OREO))
-    self._RunTest(possible_browser, ['period1'], 1, 1)
-    self._RunTest(possible_browser, [], 0, 0)
+    self._RunTest(possible_browser, ['period1'], 'android', 1)
+    self._RunTest(possible_browser, [], None, 0)
 
   def testUnsupportedAndroid(self):
     possible_browser = FakePossibleBrowser(
         FakeAndroidPlatformBackend(version_codes.KITKAT))
-    self._RunTest(possible_browser, ['period1'], 0, 0)
-    self._RunTest(possible_browser, [], 0, 0)
+    self._RunTest(possible_browser, ['period1'], None, 0)
+    self._RunTest(possible_browser, [], None, 0)
+
+  def testSupportedDesktop(self):
+    possible_browser = FakePossibleBrowser(FakeLinuxPlatformBackend())
+    self._RunTest(possible_browser, ['period1', 'period2'], 'linux', 2)
+    self._RunTest(possible_browser, [], None, 0)
 
   def testUnsupportedDesktop(self):
-    possible_browser = FakePossibleBrowser(FakeLinuxPlatformBackend())
-    self._RunTest(possible_browser, ['period1', 'period2'], 0, 0)
-    self._RunTest(possible_browser, [], 0, 0)
+    possible_browser = FakePossibleBrowser(FakeWindowsPlatformBackend())
+    self._RunTest(possible_browser, ['period1', 'period2'], None, 0)
+    self._RunTest(possible_browser, [], None, 0)
