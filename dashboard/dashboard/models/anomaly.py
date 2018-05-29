769008953f1477b63730c5275b7f51fc82d18123
benshayden <benjhayden@chromium.org>
2018-05-29 16:09:50 -0700

    Add an index over all Anomaly properties and a method to query it.
    https://dev-benjhayden-1cad5f3b-dot-chromeperf.appspot.com/_ah/dev_console/interactive
    
    BUG=catapult:#4479
    
    Change-Id: Iea0191eaedcd52b5e87e820a2789f72bb2ce3be5

--- dashboard/dashboard/models/anomaly.py
+++ dashboard/dashboard/models/anomaly.py
@@ -19,6 +19,8 @@ FREAKIN_HUGE = 'zero-to-nonzero'
 # direction of UP or DOWN, but a test's improvement direction can be UNKNOWN.
 UP, DOWN, UNKNOWN = (0, 1, 4)
 
+INVALID_BUG_ID = -1000
+
 
 class Anomaly(internal_only_model.InternalOnlyModel):
   """Represents a change-point or step found in the data series for a test.
@@ -34,6 +36,7 @@ class Anomaly(internal_only_model.InternalOnlyModel):
 
   # Note: -1 denotes an invalid alert and -2 an ignored alert.
   # By default, this is None, which denotes a non-triaged alert.
+  # Do not set this to INVALID_BUG_ID; that would break GetAll().
   bug_id = ndb.IntegerProperty(indexed=True)
 
   # The sheriff rotation that should handle this alert.
@@ -125,6 +128,196 @@ class Anomaly(internal_only_model.InternalOnlyModel):
     if self.median_before_anomaly < self.median_after_anomaly:
       return UP
     return DOWN
+
+  @classmethod
+  @ndb.tasklet
+  def GetAll(cls,
+             internal_only=None,
+             bug_id=INVALID_BUG_ID,
+             sheriff=None,
+             is_improvement=None,
+             recovered=None,
+             test=None,
+             master_name=None,
+             bot_name=None,
+             benchmark_name=None,
+             inequality_property_name=None,
+             min_start_revision=None,
+             max_start_revision=None,
+             min_end_revision=None,
+             max_end_revision=None,
+             min_timestamp=None,
+             max_timestamp=None,
+             limit=None):
+    """Query for entities generally and efficiently.
+
+    Users should be able to filter by any and all properties generally.
+    There are enough properties that it is too cumbersome to maintain sufficient
+    indexes to support every possible style of query. A single index over all
+    properties is used instead, which requires all queries to filter by all
+    properties. This classmethod encapsulates that complex process of specifying
+    all properties correctly.
+    """
+    # The query must filter by all properties in order to hit the single index.
+    # If the caller does not specify a property, then "filter" such that that
+    # property != None, which should match all entities.
+    query = cls.query()
+
+    if internal_only is not None:
+      query = query.filter(cls.internal_only == internal_only)
+    if bug_id != INVALID_BUG_ID:
+      query = query.filter(cls.bug_id == bug_id)
+    if sheriff is not None:
+      query = query.filter(cls.sheriff == sheriff)
+    if test is not None:
+      query = query.filter(cls.test == test)
+    if master_name is not None:
+      query = query.filter(cls.master_name == master_name)
+    if bot_name is not None:
+      query = query.filter(cls.bot_name == bot_name)
+    if benchmark_name is not None:
+      query = query.filter(cls.benchmark_name == benchmark_name)
+    if is_improvement is not None:
+      query = query.filter(cls.is_improvement == is_improvement)
+    if recovered is not None:
+      query = query.filter(cls.recovered == recovered)
+
+    # If callers set inequality_property_name without actually specifying a
+    # corresponding inequality filter, then reset the inequality_property_name
+    # and compute it automatically as if it were not specified.
+    if inequality_property_name == 'start_revision':
+      if min_start_revision is None and max_start_revision is None:
+        inequality_property_name = None
+    elif inequality_property_name == 'end_revision':
+      if min_end_revision is None and max_end_revision is None:
+        inequality_property_name = None
+    elif inequality_property_name == 'timestamp':
+      if min_timestamp is None and max_timestamp is None:
+        inequality_property_name = None
+
+    if inequality_property_name is None:
+      # Callers may set any subset of the inequality filters without specifying
+      # inequality_property_name, so automatically select one of the inequality
+      # filters.
+      if min_start_revision is not None or max_start_revision is not None:
+        inequality_property_name = 'start_revision'
+      elif min_end_revision is not None or max_end_revision is not None:
+        inequality_property_name = 'end_revision'
+      elif min_timestamp is not None or max_timestamp is not None:
+        inequality_property_name = 'timestamp'
+
+    query = cls._InequalityFilters(
+        query, inequality_property_name, min_start_revision, max_start_revision,
+        min_end_revision, max_end_revision, min_timestamp, max_timestamp)
+
+    # All properties must be involved in the query in order to hit the single
+    # index. There can only be one inequality filter, but arbitrarily many sort
+    # orders.
+    query = query.order(cls.internal_only)
+    query = query.order(cls.bug_id)
+    query = query.order(cls.sheriff)
+    query = query.order(cls.is_improvement)
+    query = query.order(cls.recovered)
+    query = query.order(cls.test)
+    query = query.order(cls.master_name)
+    query = query.order(cls.bot_name)
+    query = query.order(cls.benchmark_name)
+
+    results = yield query.fetch_async(limit=limit)
+    results = cls._PostFilter(
+        results, inequality_property_name, min_start_revision,
+        max_start_revision, min_end_revision, max_end_revision, min_timestamp,
+        max_timestamp)
+    raise ndb.Return(results)
+
+  @classmethod
+  def _InequalityFilters(cls, query, inequality_property_name,
+                         min_start_revision, max_start_revision,
+                         min_end_revision, max_end_revision,
+                         min_timestamp, max_timestamp):
+    if inequality_property_name == 'start_revision':
+      query = query.order(-cls.start_revision)
+      query = query.order(-cls.end_revision)
+      query = query.order(-cls.timestamp)
+      if min_start_revision is not None:
+        query = query.filter(cls.start_revision >= min_start_revision)
+      if max_start_revision is not None:
+        query = query.filter(cls.start_revision <= max_start_revision)
+    elif inequality_property_name == 'end_revision':
+      query = query.order(-cls.end_revision)
+      query = query.order(-cls.timestamp)
+      query = query.order(-cls.start_revision)
+      if min_end_revision is not None:
+        query = query.filter(cls.end_revision >= min_end_revision)
+      if max_end_revision is not None:
+        query = query.filter(cls.end_revision <= max_end_revision)
+    elif inequality_property_name == 'timestamp':
+      query = query.order(-cls.timestamp)
+      query = query.order(-cls.start_revision)
+      query = query.order(-cls.end_revision)
+      if min_timestamp is not None:
+        query = query.filter(cls.timestamp >= min_timestamp)
+      if max_timestamp is not None:
+        query = query.filter(cls.timestamp <= max_timestamp)
+    else:
+      query = query.order(-cls.timestamp)
+      query = query.order(-cls.start_revision)
+      query = query.order(-cls.end_revision)
+    return query
+
+  @staticmethod
+  def _PostFilter(
+      results, inequality_property_name, min_start_revision, max_start_revision,
+      min_end_revision, max_end_revision, min_timestamp, max_timestamp):
+    postfilter = False
+    if inequality_property_name == 'start_revision':
+      postfilter = (
+          min_end_revision is not None or
+          max_end_revision is not None or
+          min_timestamp is not None or
+          max_timestamp is not None)
+    elif inequality_property_name == 'end_revision':
+      postfilter = (
+          min_start_revision is not None or
+          max_start_revision is not None or
+          min_timestamp is not None or
+          max_timestamp is not None)
+    elif inequality_property_name == 'timestamp':
+      postfilter = (
+          min_start_revision is not None or
+          max_start_revision is not None or
+          min_end_revision is not None or
+          max_end_revision is not None)
+
+    if not postfilter:
+      return results
+
+    # The NDB query can only use inequality filters for a single property, so
+    # handle any other inequality filters after fetching results.
+    filtered_results = []
+    for result in results:
+      # Cache locality requires applying all filters to each result at a time.
+      if inequality_property_name != 'start_revision':
+        if (min_start_revision is not None and
+            result.start_revision < min_start_revision):
+          continue
+        if (max_start_revision is not None and
+            result.start_revision > max_start_revision):
+          continue
+      if inequality_property_name != 'end_revision':
+        if (min_end_revision is not None and
+            result.end_revision < min_end_revision):
+          continue
+        if (max_end_revision is not None and
+            result.end_revision > max_end_revision):
+          continue
+      if inequality_property_name != 'timestamp':
+        if min_timestamp is not None and result.timestamp < min_timestamp:
+          continue
+        if max_timestamp is not None and result.timestamp > max_timestamp:
+          continue
+      filtered_results.append(result)
+    return filtered_results
 
   def GetDisplayPercentChanged(self):
     """Gets a string showing the percent change."""
