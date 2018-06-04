67bb5140191909515a24a2841588fc933f261764
Dave Tu <dtu@chromium.org>
2018-06-04 01:53:03 -0700

    [pinpoint] Add testapp to the global TestCase.
    With a global TestApp class, we can set up all the testapp routes at
    once in the same place. This reduces boilerplate code in the handler
    unit tests, and covers the dispatcher code in the unit tests.
    
    Bug: catapult:#4495
    Change-Id: Ib095feef5b8f0d3b2948542ae5e06676b4e7149e

--- dashboard/dashboard/pinpoint/test.py
+++ dashboard/dashboard/pinpoint/test.py
@@ -5,9 +5,12 @@
 import os
 import unittest
 
+import webtest
+
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
+from dashboard.pinpoint import dispatcher
 from dashboard.pinpoint.models.change import repository
 
 
@@ -15,21 +18,38 @@ CATAPULT_URL = 'https://chromium.googlesource.com/catapult'
 CHROMIUM_URL = 'https://chromium.googlesource.com/chromium/src'
 
 
+_ROOT_PATH = os.path.join(os.path.dirname(__file__), '..', '..')
+
+
 class TestCase(unittest.TestCase):
 
   def setUp(self):
+    self._SetUpTestbed()
+    self._SetUpTestApp()
+    self._PopulateData()
+
+  def _SetUpTestbed(self):
     self.testbed = testbed.Testbed()
     self.testbed.activate()
+    self.addCleanup(self.testbed.deactivate)
+
     self.testbed.init_datastore_v3_stub()
     self.testbed.init_memcache_stub()
-    ndb.get_context().clear_cache()
+    self.testbed.init_taskqueue_stub(root_path=_ROOT_PATH)
     self.testbed.init_user_stub()
-    root_path = os.path.join(os.path.dirname(__file__), '..', '..')
-    self.testbed.init_taskqueue_stub(root_path=root_path)
 
+    ndb.get_context().clear_cache()
+
+  def _SetUpTestApp(self):
+    self.testapp = webtest.TestApp(dispatcher.APP)
+    self.testapp.extra_environ.update({'REMOTE_ADDR': 'remote_ip'})
+
+  def _PopulateData(self):
+    # Add repository mappings.
     repository.Repository(id='catapult', urls=[CATAPULT_URL]).put()
     repository.Repository(id='chromium', urls=[CHROMIUM_URL]).put()
     repository.Repository(id='another_repo', urls=['https://another/url']).put()
 
-  def tearDown(self):
-    self.testbed.deactivate()
+  def GetTaskQueueTasks(self, task_queue_name):
+    task_queue = self.testbed.get_stub(testbed.TASKQUEUE_SERVICE_NAME)
+    return task_queue.GetTasks(task_queue_name)
