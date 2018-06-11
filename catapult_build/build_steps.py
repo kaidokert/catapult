19a220ca2b56d970d0063494afc0f4b5fdde544e
Nghia Nguyen <nednguyen@google.com>
2018-06-11 18:37:35 -0400

    Add typ test to Catapult CQ
    Change-Id: I17dcf9090c4e7b02b3e996fe59edb342d6edc38b
    
    BUG:chromium:851498
    
    TBR=sullivan@chromium.org
    
    Change-Id: I17dcf9090c4e7b02b3e996fe59edb342d6edc38b

--- catapult_build/build_steps.py
+++ catapult_build/build_steps.py
@@ -179,6 +179,12 @@ _CATAPULT_TESTS = [
         'disabled': ['android'],
     },
     {
+        'name': 'Typ unittest',
+        'path': 'third_party/typ/run',
+        'additional_args': ['tests'],
+        'disabled': ['android'],
+    },
+    {
         'name': 'Vinn Tests',
         'path': 'third_party/vinn/bin/run_tests',
         'disabled': ['android'],
