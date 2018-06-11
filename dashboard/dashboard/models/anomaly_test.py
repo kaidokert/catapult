4b4a72b5780a1b2985cc8abeeae4d06c562f2136
benshayden <benjhayden@chromium.org>
2018-06-11 12:31:20 -0700

    Refactor Anomaly queries.
    Currently, there are lots of places that query Anomalies.
    This CL refactors all of them to use a single tasklet based on the new
    /api/alerts handler.
    
    Bug: catapult:#4479
    Change-Id: I5d8c8158595837342e697d21c407792effcf3d0f

--- dashboard/dashboard/models/anomaly_test.py
+++ dashboard/dashboard/models/anomaly_test.py
@@ -14,29 +14,6 @@ from dashboard.models import anomaly
 class AnomalyTest(testing_common.TestCase):
   """Test case for some functions in anomaly."""
 
-  def testGetBotNamesFromAnomalies_EmptyList_ReturnsEmptySet(self):
-    self.assertEqual(set(), anomaly.GetBotNamesFromAlerts([]))
-
-  def testGetBotNamesFromAnomalies_RemovesDuplicates(self):
-    testing_common.AddTests(
-        ['SuperGPU'], ['Bot1'], {'foo': {'bar': {}}})
-    anomaly.Anomaly(test=utils.TestKey('SuperGPU/Bot1/foo/bar')).put()
-    anomaly.Anomaly(test=utils.TestKey('SuperGPU/Bot1/foo/bar')).put()
-    anomalies = anomaly.Anomaly.query().fetch()
-    bot_names = anomaly.GetBotNamesFromAlerts(anomalies)
-    self.assertEqual(2, len(anomalies))
-    self.assertEqual(1, len(bot_names))
-
-  def testGetBotNamesFromAnomalies_ReturnsBotNames(self):
-    testing_common.AddTests(
-        ['SuperGPU'], ['Bot1', 'Bot2', 'Bot3'], {'foo': {'bar': {}}})
-    anomaly.Anomaly(test=utils.TestKey('SuperGPU/Bot1/foo/bar')).put()
-    anomaly.Anomaly(test=utils.TestKey('SuperGPU/Bot2/foo/bar')).put()
-    anomaly.Anomaly(test=utils.TestKey('SuperGPU/Bot3/foo/bar')).put()
-    anomalies = anomaly.Anomaly.query().fetch()
-    bot_names = anomaly.GetBotNamesFromAlerts(anomalies)
-    self.assertEqual({'Bot1', 'Bot2', 'Bot3'}, bot_names)
-
   def testGetTestMetadataKey_Test(self):
     a = anomaly.Anomaly(
         test=ndb.Key('Master', 'm', 'Bot', 'b', 'Test', 't', 'Test', 't'))
@@ -68,14 +45,16 @@ class AnomalyTest(testing_common.TestCase):
     anomaly.Anomaly(id="new_1", test=new_style_key1).put()
     anomaly.Anomaly(id="new_2", test=new_style_key2).put()
     anomaly.Anomaly(id="new_2a", test=new_style_key2).put()
-    key1_alerts = anomaly.Anomaly.GetAlertsForTest(new_style_key1)
+    key1_alerts, _, _ = anomaly.Anomaly.QueryAsync(
+        test=new_style_key1).get_result()
     self.assertEqual(
         ['new_1', 'old_1', 'old_1a'], [a.key.id() for a in key1_alerts])
-    key2_alerts = anomaly.Anomaly.GetAlertsForTest(old_style_key2)
+    key2_alerts, _, _ = anomaly.Anomaly.QueryAsync(
+        test=old_style_key2).get_result()
     self.assertEqual(
         ['new_2', 'new_2a', 'old_2'], [a.key.id() for a in key2_alerts])
-    key2_alerts_limit = anomaly.Anomaly.GetAlertsForTest(
-        old_style_key2, limit=2)
+    key2_alerts_limit, _, _ = anomaly.Anomaly.QueryAsync(
+        test=old_style_key2, limit=2).get_result()
     self.assertEqual(
         ['new_2', 'new_2a'], [a.key.id() for a in key2_alerts_limit])
 
