269016736438384cc8df89edab530ba36a754a33
benshayden <benjhayden@chromium.org>
2018-06-13 11:26:39 -0700

    Update benchmark_health_report.dashboard_api to use the new /api/alerts.
    Bug: catapult:#4479
    Change-Id: Ieada1c09ec44c808cfaf14d7fc299436956ba53a

--- experimental/benchmark_health_report/dashboard_api.py
+++ experimental/benchmark_health_report/dashboard_api.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import datetime
 import httplib2
 import json
 import re
@@ -13,11 +14,64 @@ from oauth2client import service_account  # pylint: disable=no-name-in-module
 _PATH_TO_JSON_KEYFILE = 'PATH/TO/keyfile.json'
 
 def GetAlerts(num_days, benchmark=None):
-  url = '/api/alerts/history/%d' % num_days
-  postdata = 'improvements=true'
-  if benchmark is not None:
-    postdata += '&benchmark=%s' % benchmark
-  return _MakeDashboardApiRequest(url, postdata)['anomalies']
+  min_timestamp = datetime.datetime.now() - datetime.timedelta(days=num_days)
+  return GetAlerts2(
+      min_timestamp=min_timestamp, test_suite=benchmark)['anomalies']
+
+def GetAlerts2(
+    bot=None,
+    bug_id=None,
+    is_improvement=None,
+    key=None,
+    limit=None,
+    master=None,
+    max_end_revision=None,
+    max_start_revision=None,
+    max_timestamp=None,
+    min_end_revision=None,
+    min_start_revision=None,
+    min_timestamp=None,
+    recovered=None,
+    sheriff=None,
+    start_cursor=None,
+    test=None,
+    test_suite=None):
+  postdata = []
+  if bot:
+    postdata.append('bot=' + bot)
+  if bug_id is not None:
+    postdata.append('bug_id=' + bug_id)
+  if is_improvement:
+    postdata.append('is_improvement=' + is_improvement)
+  if key:
+    postdata.append('key=' + key)
+  if limit:
+    postdata.append('limit=' + limit)
+  if master:
+    postdata.append('master=' + master)
+  if max_start_revision:
+    postdata.append('max_start_revision=' + max_start_revision)
+  if min_start_revision:
+    postdata.append('min_start_revision=' + min_start_revision)
+  if max_end_revision:
+    postdata.append('max_end_revision=' + max_end_revision)
+  if min_end_revision:
+    postdata.append('min_end_revision=' + min_end_revision)
+  if max_timestamp:
+    postdata.append('max_timestamp=' + max_timestamp.isoformat())
+  if min_timestamp:
+    postdata.append('min_timestamp=' + min_timestamp.isoformat())
+  if recovered:
+    postdata.append('recovered=' + recovered)
+  if sheriff:
+    postdata.append('sheriff=' + sheriff)
+  if start_cursor:
+    postdata.append('cursor=' + start_cursor)
+  if test:
+    postdata.append('test=' + test)
+  if test_suite:
+    postdata.append('test_suite=' + test_suite)
+  return _MakeDashboardApiRequest('/api/alerts', '&'.join(postdata))
 
 def GetBug(bug_id):
   url = '/api/bugs/%d' % bug_id
