baac25b67a1998d37e9498b96693a0c3fa035fef
Ben Hayden <benjhayden@chromium.org>
2018-07-02 10:42:08 -0700

    __debugging__
    Change-Id: I09aba56ecc024df489eee9f660f63ccbbf3b6ab0

--- dashboard/dashboard/common/request_handler.py
+++ dashboard/dashboard/common/request_handler.py
@@ -37,8 +37,12 @@ class RequestHandler(webapp2.RequestHandler):
           values.
       status: int. HTTP status code.
     """
+    logging.error('file=%r', template_file)
+    logging.error('values=%r', template_values)
+    logging.error('status=%r', status)
     self.response.set_status(status)
     template = JINJA2_ENVIRONMENT.get_template(template_file)
+    logging.error('template=%r', template)
     self.GetDynamicVariables(template_values)
     self.response.out.write(template.render(template_values))
 
