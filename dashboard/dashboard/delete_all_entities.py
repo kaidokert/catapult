d72390fdcdaa0b390914a1b3c91d907232802057
benshayden <benjhayden@chromium.org>
2018-07-05 15:44:11 -0700

    Add /delete_all_entities
    There are currently many entities in the datastore whose models have been or can
    be removed from the source.
    Attempts to access these entities raises KindError, which are occasionally seen
    in the logs.
    This CL adds a handler that deletes all entities of a given kind.
    It can be called from dev console, optionally using taskqueue:
    from google.appengine.api import taskqueue
    taskqueue.add(url='/delete_all_entities', params={'kind':'Foo'}, queue_name='default')
    
    Bug: chromium:851127
    Change-Id: I01c467cb0264ff0eadf36218c960ddf4ebaf1807

---
+++ dashboard/dashboard/delete_all_entities.py
@@ -0,0 +1,46 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import time
+
+from google.appengine.ext import ndb
+
+from dashboard.common import request_handler
+
+
+class DeleteAllEntitiesHandler(request_handler.RequestHandler):
+
+  def post(self):
+    DeleteAllEntities(self.request.get('kind'))
+
+
+def DeleteAllEntities(kind, limit=5000, deadline_seconds=540):
+  """DELETES ALL ENTITIES OF KIND |kind|.
+
+  This function is intended to be called in the dev console.
+
+  Args:
+    kind: String name of model.
+    limit: Number of keys to fetch in each page.
+    deadline_seconds: Number of seconds to stop after.
+
+  Returns:
+    more: True if there might be more entities that could not be deleted within
+    |deadline_seconds|.
+  """
+  if not kind:
+    # Query(kind='') would delete the entire datastore.
+    raise ValueError('"kind" must be set to a model name')
+  more = True
+  cursor = None
+  deadline = time.time() + deadline_seconds
+  while more and time.time() < deadline:
+    query = ndb.Query(kind=kind)
+    # Let's keep things synchronous for simplicity.
+    keys, cursor, more = query.fetch_page(
+        limit, keys_only=True, start_cursor=cursor)
+    logging.info('Fetched %d keys; more=%r', len(keys), more)
+    ndb.delete_multi(keys)
+  return more
