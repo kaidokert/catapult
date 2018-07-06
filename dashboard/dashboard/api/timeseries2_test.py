d8127be046ea15c51665216b8a00a93da1a2d02e
benshayden <benjhayden@chromium.org>
2018-07-06 14:00:14 -0700

    Add /api/timeseries2
    This endpoint accepts a Descriptor, revision range, and columns, and
    collates TestMetadata, Row, Anomaly, Histogram, and SparseDiagnostic
    data.
    
    Bug: catapult:#4461
    
    Change-Id: I1b5da3da2f83d3f4ad8fb77480afa631d5b193ad

---
+++ dashboard/dashboard/api/timeseries2_test.py
@@ -0,0 +1,18 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from dashboard.api import timeseries2
+from dashboard.common import testing_common
+
+
+class Timeseries2Test(testing_common.TestCase):
+
+  def setUp(self):
+    super(Timeseries2Test, self).setUp()
+
+  def testCollate(self):
+    pass # TODO
+
+  def testRange(self):
+    pass # TODO
