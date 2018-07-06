2b69ff55de53ab352aabbaa2a20da57e1839ce22
benshayden <benjhayden@chromium.org>
2018-07-06 11:50:11 -0700

    Add /delete_all_entities
    There are currently many entities in the datastore whose models have been or can
    be removed from the source.
    Attempts to access these entities raises KindError, which are occasionally seen
    in the logs.
    This CL adds a handler that deletes all entities of a given kind.
    It can be called from dev console, optionally using taskqueue:
    from google.appengine.api import taskqueue
    taskqueue.add(url='/delete_all_entities', params={'kind':'Foo'}, queue_name='default')
    
    Bug: chromium:851127
    Change-Id: I01c467cb0264ff0eadf36218c960ddf4ebaf1807

---
+++ dashboard/dashboard/delete_all_entities.py
@@ -0,0 +1,37 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+
+from google.appengine.ext import deferred
+from google.appengine.ext import ndb
+
+from dashboard.common import request_handler
+
+
+QUERY_PAGE_LIMIT = 1000
+
+
+class DeleteAllEntitiesHandler(request_handler.RequestHandler):
+
+  def post(self):
+    DeleteAllEntities(self.request.get('kind'))
+
+
+def DeleteAllEntities(kind):
+  """DELETES ALL ENTITIES OF KIND |kind|.
+
+  Args:
+    kind: Required string name of model.
+  """
+  if not kind:
+    # Query(kind='') would delete the entire datastore.
+    raise ValueError('"kind" cannot be empty')
+
+  keys, _, more = ndb.Query(kind=kind).fetch_page(
+      QUERY_PAGE_LIMIT, keys_only=True)
+  logging.info('Fetched %d keys; more=%r', len(keys), more)
+  ndb.delete_multi(keys)
+  if more:
+    deferred.defer(DeleteAllEntities, kind)
