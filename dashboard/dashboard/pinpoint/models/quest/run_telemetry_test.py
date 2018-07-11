f781dfb2f3b5c2dbf25d4dfe31c55cb1c3f0f345
Dave Tu <dtu@chromium.org>
2018-07-10 22:54:57 -0700

    [pinpoint] Add --also-run-disabled-tests flag to Telemetry runs.
    Sometimes we encounter tests that fail on the perf waterfall, but run
    fine in Pinpoint. Users may also want to run functional bisects on
    tests that are disabled.
    
    Bug: catapult:#4514
    Change-Id: Iee1452ecee285b80f3ea012d63dfb1b1ba9a9c0d

--- dashboard/dashboard/pinpoint/models/quest/run_telemetry_test.py
+++ dashboard/dashboard/pinpoint/models/quest/run_telemetry_test.py
@@ -10,7 +10,8 @@ from dashboard.pinpoint.models.quest import run_test
 
 
 _DEFAULT_EXTRA_ARGS = [
-    '-v', '--upload-results', '--output-format', 'histograms']
+    '-v', '--upload-results', '--output-format', 'histograms',
+    '--also-run-disabled-tests']
 
 
 class RunTelemetryTest(run_test.RunTest):
