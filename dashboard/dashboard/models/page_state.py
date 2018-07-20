69bf0d559ff6bb90484807c289b97f64e8478bfb
Simon <simonhatch@chromium.org>
2018-07-20 17:15:36 -0400

    Dashboard - Add a pagestate migration function
    Intended for use from dev_console, mostly to migrate the bots for the
    OBBS migration. I don't intend for this to stick around for long,
    either just as a temporary bit of code or even just as a dev deploy.
    
    Bug: chromium:865999
    Change-Id: I0c08c7300e749708b0217730f75da167a4cd3451

--- dashboard/dashboard/models/page_state.py
+++ dashboard/dashboard/models/page_state.py
@@ -6,11 +6,51 @@
 
 This is used for storing front-end configuration.
 """
+import json
 
+from google.appengine.ext import deferred
 from google.appengine.ext import ndb
+
+
+QUERY_PAGE_LIMIT = 1000
 
 
 class PageState(ndb.Model):
   """An entity with a single blob value where id is a hash value."""
 
   value = ndb.BlobProperty(indexed=False)
+
+
+def _FixPageStatePaths(ps, src_path, dst_path):
+  try:
+    r = json.loads(ps.value)
+
+    changed = False
+    for c in r['charts']:
+      if type(c) == list:
+        for row in c:
+          if src_path in row[0]:
+            row[0] = row[0].replace(src_path, dst_path)
+            changed = True
+      elif type(c) == dict:
+        for row in c['seriesGroups']:
+          print ''
+          print row
+          if src_path in row[0]:
+            row[0] = row[0].replace(src_path, dst_path)
+            changed = True
+    if changed:
+      ps.value = json.dumps(r)
+      ps.put()
+
+  except Exception:  # pylint: disable=broad-except
+    return
+
+def _MigratePageStates(src_path, dst_path):
+  entities, _, more = PageState.Query().fetch_page(QUERY_PAGE_LIMIT)
+
+  for e in entities:
+    _FixPageStatePaths(e, src_path, dst_path)
+
+  if more:
+    deferred.defer(_MigratePageStates, src_path, dst_path)
