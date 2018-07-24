ee8e2fb405a338dcef99bf15e733e82244cb720b
Simon <simonhatch@chromium.org>
2018-07-24 12:45:09 -0400

    Dashboard - Add a pagestate migration function
    Intended for use from dev_console, mostly to migrate the bots for the
    OBBS migration. I don't intend for this to stick around for long,
    either just as a temporary bit of code or even just as a dev deploy.
    
    Bug: chromium:865999
    Change-Id: I0c08c7300e749708b0217730f75da167a4cd3451

--- dashboard/dashboard/models/page_state.py
+++ dashboard/dashboard/models/page_state.py
@@ -6,11 +6,55 @@
 
 This is used for storing front-end configuration.
 """
+import json
 
+from google.appengine.datastore.datastore_query import Cursor
+from google.appengine.ext import deferred
 from google.appengine.ext import ndb
+
+
+QUERY_PAGE_LIMIT = 1000
 
 
 class PageState(ndb.Model):
   """An entity with a single blob value where id is a hash value."""
 
   value = ndb.BlobProperty(indexed=False)
+
+
+def _FixPageStatePaths(ps, src_path, dst_path):
+  try:
+    r = json.loads(ps.value)
+
+    changed = False
+    for c in r['charts']:
+      if isinstance(c, list):
+        for row in c:
+          if src_path in row[0]:
+            row[0] = row[0].replace(src_path, dst_path)
+            changed = True
+      elif isinstance(c, dict):
+        for row in c['seriesGroups']:
+          if src_path in row[0]:
+            row[0] = row[0].replace(src_path, dst_path)
+            changed = True
+    if changed:
+      ps.value = json.dumps(r)
+      ps.put()
+
+  except Exception:  # pylint: disable=broad-except
+    return
+
+def _MigratePageStatesBots(src_path, dst_path, query_cursor=None):
+  """Intended for dev_console usage only in the short term and then removed."""
+  if query_cursor:
+    query_cursor = Cursor(urlsafe=query_cursor)
+  entities, next_cursor, more = PageState.query(
+      start_cursor=query_cursor).fetch_page(QUERY_PAGE_LIMIT)
+
+  if more:
+    deferred.defer(
+        _MigratePageStatesBots, src_path, dst_path, next_cursor.urlsafe())
+
+  for e in entities:
+    _FixPageStatePaths(e, src_path, dst_path)
