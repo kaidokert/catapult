aa6728ec7be43b9d4f38481751678b2cc99c8d14
Simon <simonhatch@chromium.org>
2018-07-25 17:13:05 -0400

    DO NOT LAND - TEST CQ
    Bug: catapult:#
    Change-Id: I2178f227b6e7476fe61bab3ca1c56c40dfd96411

--- common/py_utils/py_utils/cloud_storage.py
+++ common/py_utils/py_utils/cloud_storage.py
@@ -138,6 +138,16 @@ def _RunCommand(args):
   elif _IsRunningOnSwarming():
     gsutil_env = os.environ.copy()
 
+  def _pretty(e):
+    for k, v in e.iteritems():
+      print '  - %s -> %s' % (k, v)
+
+  print 'TEST'
+  print _pretty(os.environ, indent=2)
+  print
+  print 'gsutil_env'
+  print _pretty(gsutil_env, indent=2)
+
   if os.name == 'nt':
     # If Windows, prepend python. Python scripts aren't directly executable.
     args = [sys.executable, _GSUTIL_PATH] + args
@@ -150,6 +160,9 @@ def _RunCommand(args):
     raise CloudStorageIODisabled(
         "Environment variable DISABLE_CLOUD_STORAGE_IO is set to 1. "
         'Command %s is not allowed to run' % args)
+
+  print
+  print args
 
   gsutil = subprocess.Popen(args, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE, env=gsutil_env)
