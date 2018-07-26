cae9a2767ad09ffe8feeef9dc08ccfa95ebe42d9
benshayden <benjhayden@chromium.org>
2018-07-26 15:50:17 -0700

    Support bot aliases in Descriptor and ReportQuery.
    This CL makes Descriptor.ToTestPathsAsync detect when its bot has aliases,
    and generate a separate test path for each bot alias.
    /api/timeseries2 and /api/alerts need no modifications to support bot
    aliases because they already support multiple test paths.
    ReportQuery requires slight modification to support multiple test paths.
    timeseries2_test.py was occasionally flaking with an error about
    multiple inequality filters (revision and timestamp), so this CL also
    fixes that flake by restructuring _FilterRowQuery().
    
    Bug: catapult:#4461
    Change-Id: I60a28091b7f63f90f2bfc57b71e6eddbb2fb3742

--- dashboard/dashboard/pinpoint/models/bot_configurations.py
+++ dashboard/dashboard/pinpoint/models/bot_configurations.py
@@ -4,22 +4,57 @@
 
 import string
 
+from google.appengine.ext import ndb
+
 from dashboard.common import namespaced_stored_object
 
 
-_BOT_CONFIGURATIONS = 'bot_configurations'
+BOT_CONFIGURATIONS_KEY = 'bot_configurations'
 
 
 def Get(name):
-  configurations = namespaced_stored_object.Get(_BOT_CONFIGURATIONS)
+  configurations = namespaced_stored_object.Get(BOT_CONFIGURATIONS_KEY)
   configuration = configurations[name]
   if 'alias' in configuration:
     return configurations[configuration['alias']]
   return configuration
 
 
+@ndb.tasklet
+def AliasesesAsync():
+  aliaseses = []
+  configurations = yield namespaced_stored_object.GetAsync(
+      BOT_CONFIGURATIONS_KEY)
+  if not configurations:
+    raise ndb.Return(aliaseses)
+
+  for name, configuration in configurations.iteritems():
+    if 'alias' in configuration:
+      aliaseses.append([name, configuration['alias']])
+
+  # Merge intersecting aliases. If 'a' is aka 'b' and 'b' is aka 'c' then 'a' is
+  # aka 'c'.
+  for sort_index in range(2):
+    aliaseses.sort(key=lambda pair, si=sort_index: pair[si])
+    i = 1
+    while i < len(aliaseses):
+      prev_aliases = aliaseses[i - 1]
+      cur_aliases = aliaseses[i]
+      if not any(alias in cur_aliases for alias in prev_aliases):
+        i += 1
+        continue
+      # Merge cur_aliases into prev_aliases and remove cur_aliases from
+      # aliaseses.
+      for alias in cur_aliases:
+        if alias not in prev_aliases:
+          prev_aliases.append(alias)
+      aliaseses.pop(i)
+
+  raise ndb.Return(aliaseses)
+
+
 def List():
-  bot_configurations = namespaced_stored_object.Get(_BOT_CONFIGURATIONS)
+  bot_configurations = namespaced_stored_object.Get(BOT_CONFIGURATIONS_KEY)
   canonical_names = [name for name, value in bot_configurations.iteritems()
                      if 'alias' not in value]
   return sorted(canonical_names, key=string.lower)
