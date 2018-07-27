88af915c701fc65654539f800d498e608d04d6d4
benshayden <benjhayden@chromium.org>
2018-07-26 23:15:55 -0700

    Fix descriptors for huge test suites.
    Some test suites are so huge that, not only can their keys not fit in 1GB RAM,
    but it also takes longer than 10 minutes to fetch and process them.
    Also, sometimes the iterator runs for a few minutes and then throws an
    inscrutable BadRequestError 'invalid handle'.
    This CL runs the iterator for as long as possible, then defers another task to
    pick up where it left off.
    I can remove some of the logging if this works well enough.
    
    See logs for version dev-benjhayden-1151612
    rendering.mobile took just over 2 hours.
    
    Bug: catapult:#4461
    Change-Id: I13abde02764e12e42e3fbc5a107f5acb1252dd63

--- dashboard/dashboard/update_test_suite_descriptors.py
+++ dashboard/dashboard/update_test_suite_descriptors.py
@@ -3,8 +3,11 @@
 # found in the LICENSE file.
 
 import logging
+import time
 
+from google.appengine.ext import db
 from google.appengine.ext import deferred
+from google.appengine.ext import ndb
 
 from dashboard import update_test_suites
 from dashboard.common import datastore_hooks
@@ -47,19 +50,39 @@ def ScheduleUpdateDescriptor(test_suite, namespace):
   deferred.defer(_UpdateDescriptor, test_suite, namespace)
 
 
-def _UpdateDescriptor(test_suite, namespace):
-  logging.info('%s %s', test_suite, namespace)
-  # This function always runs in the taskqueue as an anonymous user.
-  if namespace == datastore_hooks.INTERNAL:
-    datastore_hooks.SetPrivilegedRequest()
+@ndb.tasklet
+def _DescribeTestKey(key, measurements, bots, cases):
+  desc = yield descriptor.Descriptor.FromTestPathAsync(utils.TestPath(key))
+  bots.add(desc.bot)
+  if desc.measurement:
+    measurements.add(desc.measurement)
+  if desc.test_case:
+    cases.add(desc.test_case)
 
+
+def _IsRunning(future):
+  if future.done():
+    future.check_success()
+    return False
+  return True
+
+
+def _BufferFuture(running_futures, future, buffer_size=1000):
+  running_futures.append(future)
+  if len(running_futures) < buffer_size:
+    return
+
+  running_futures[:] = [f for f in running_futures if _IsRunning(f)]
+
+  while len(running_futures) > (buffer_size / 2):
+    done_future = ndb.Future.wait_any(running_futures)
+    done_future.check_success()
+    running_futures.remove(done_future)
+
+
+def _QueryTestSuite(test_suite):
   test_path = descriptor.Descriptor(
       test_suite=test_suite, bot='place:holder').ToTestPathsSync()[0].split('/')
-
-  measurements = set()
-  bots = set()
-  cases = set()
-  # TODO(4549) Tagmaps.
 
   query = graph_data.TestMetadata.query()
   query = query.filter(graph_data.TestMetadata.suite_name == test_path[2])
@@ -69,19 +92,61 @@ def _UpdateDescriptor(test_suite, namespace):
         graph_data.TestMetadata.test_part1_name == test_path[3])
   query = query.filter(graph_data.TestMetadata.deprecated == False)
   query = query.filter(graph_data.TestMetadata.has_rows == True)
+  return query
 
-  # Use an iterator because some test suites have more keys than can fit in
-  # memory.
-  for key in query.iter(keys_only=True):
-    desc = descriptor.Descriptor.FromTestPathSync(utils.TestPath(key))
-    bots.add(desc.bot)
-    if desc.measurement:
-      measurements.add(desc.measurement)
-    if desc.test_case:
-      cases.add(desc.test_case)
 
-  logging.info('%d measurements, %d bots, %d cases',
+DEADLINE_SECONDS = 60 * 9.5
+
+
+def _UpdateDescriptor(test_suite, namespace, start_cursor=None,
+                      measurements=(), bots=(), cases=()):
+  logging.info('%s %s %d %d %d', test_suite, namespace,
                len(measurements), len(bots), len(cases))
+
+  # This function always runs in the taskqueue as an anonymous user.
+  if namespace == datastore_hooks.INTERNAL:
+    datastore_hooks.SetPrivilegedRequest()
+
+  start = time.time()
+  deadline = start + DEADLINE_SECONDS
+  key_count = 0
+  describe_futures = []
+  measurements = set(measurements)
+  bots = set(bots)
+  cases = set(cases)
+  # TODO(4549) Tagmaps.
+
+  # Some test suites have more keys than can fit in memory or can be processed
+  # in 10 minutes, so use an iterator.
+  query_iter = _QueryTestSuite(test_suite).iter(
+      keys_only=True, produce_cursors=True, start_cursor=start_cursor,
+      use_cache=False, use_memcache=False)
+
+  try:
+    for key in query_iter:
+      key_count += 1
+      _BufferFuture(describe_futures, _DescribeTestKey(
+          key, measurements, bots, cases))
+      if time.time() > deadline:
+        break
+  except db.BadRequestError:
+    # Sometimes the iterator runs for a while then throws this. Try, try again.
+    logging.warn('BadRequestError caught')
+
+  ndb.Future.wait_all(describe_futures)
+  for future in describe_futures:
+    future.check_success()
+
+  logging.info('%d keys, %d measurements, %d bots, %d cases',
+               key_count, len(measurements), len(bots), len(cases))
+  logging.info('per_key:wall=%f', round((time.time() - start) / key_count))
+
+  if query_iter.probably_has_next():
+    logging.info('continuing')
+    deferred.defer(_UpdateDescriptor, test_suite, namespace,
+                   query_iter.cursor_before(), measurements, bots, cases)
+    return
+
   desc = {
       'measurements': list(sorted(measurements)),
       'bots': list(sorted(bots)),
