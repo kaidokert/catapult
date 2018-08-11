822b44e1a5972ac1e996104d39bf50ef4e5918f4
Dirk Pranke <dpranke@chromium.org>
2018-08-10 17:31:44 -0700

    Integrate test expectation-parsing code into TYP.
    As noted in earlier CLs on this bug, this adds support for
    the "tagged test list" variant of bit.ly/chromium-test-list-format
    to TYP.
    
    Programmatically, this is done by adding support for a single file
    argument (-X/--expectations-file) and a list of
    conditions (-x/--conditions), both specified as
    args to the runner like most other parameters.
    
    The "conditions" must be the subset of tags specified in the
    file that should be applied to the tests in this run. For example,
    if the tags are [ Debug Release ] and [ Mac Win Linux ], then
    you might specify `-x Mac -x Debug` for a debug Mac build.
    
    Bug: chromium:835690
    Change-Id: I165964e4f4f6403f01669500d0ea90a99e2e8b81

--- third_party/typ/typ/arg_parser.py
+++ third_party/typ/typ/arg_parser.py
@@ -144,6 +144,16 @@ class ArgumentParser(argparse.ArgumentParser):
             self.add_argument('-v', '--verbose', action='count', default=0,
                               help=('Prints more stuff (can specify multiple '
                                     'times for more output).'))
+            self.add_argument('-x', '--expectations-tag',
+                              dest='expectation_tags',
+                              nargs='*', default=[],
+                              help=('test expectations tag (can specify '
+                                    'multiple times'))
+            self.add_argument('-X', '--expectations-file',
+                              dest='expectations_files',
+                              nargs='*', default=[],
+                              help=('test expectations file (can specify '
+                                    'multiple times'))
             self.add_argument('--passthrough', action='store_true',
                               default=False,
                               help='Prints all output while running.')
