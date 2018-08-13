bc33bfe122ca130078282bd7fd589ba64521b41b
Ben Hayden <benjhayden@chromium.org>
2018-08-13 10:01:00 -0700

    Finish removing paper elements This reduces vulcanized size by >20KB
    Change-Id: I44365a218c1efb10fc90081f3c56edcb838d81e4

---
+++ dashboard/dashboard/spa/cp-tab.js
@@ -0,0 +1,21 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class CpTab extends cp.ElementBase {
+  }
+
+  CpTab.properties = {
+    name: {type: String},
+    checked: {type: Boolean},
+    disabled: {type: Boolean},
+  };
+
+  cp.ElementBase.register(CpTab);
+
+  return {
+    CpTab,
+  };
+});
