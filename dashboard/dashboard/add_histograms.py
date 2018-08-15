f9db81df919d27e6e8f578ed381aa24b72e7e8c4
benshayden <benjhayden@chromium.org>
2018-08-15 13:58:53 -0700

    Remove bot_whitelist.
    There are several Bots whose internal_only doesn't match bot_whitelist. They are
    all deprecated, so they don't need to be fixed, but having multiple sources of
    the same information that aren't automatically updated can cause confusion.
    
    This CL removes bot_whitelist so that the Bot entities are the sole source of truth
    for internal_only.
    
    This is split from https://chromium-review.googlesource.com/c/catapult/+/1153585
    
    Change-Id: Id98f74962fc37adc795cab297159d9d0c7b89937

--- dashboard/dashboard/add_histograms.py
+++ dashboard/dashboard/add_histograms.py
@@ -13,13 +13,12 @@ from google.appengine.api import taskqueue
 from google.appengine.ext import ndb
 
 from dashboard import add_point
-from dashboard import add_point_queue
 from dashboard.api import api_request_handler
 from dashboard.common import datastore_hooks
 from dashboard.common import histogram_helpers
-from dashboard.common import stored_object
 from dashboard.common import timing
 from dashboard.common import utils
+from dashboard.models import graph_data
 from dashboard.models import histogram
 from tracing.value import histogram_set
 from tracing.value.diagnostics import diagnostic
@@ -103,9 +102,6 @@ def ProcessHistogramSet(histogram_dicts):
     raise api_request_handler.BadRequestError(
         'HistogramSet JSON much be a list of dicts')
 
-  bot_whitelist_future = stored_object.GetAsync(
-      add_point_queue.BOT_WHITELIST_KEY)
-
   histograms = histogram_set.HistogramSet()
 
   with timing.WallTimeLogger('hs.ImportDicts'):
@@ -153,8 +149,7 @@ def ProcessHistogramSet(histogram_dicts):
 
     revision = ComputeRevision(histograms)
 
-    bot_whitelist = bot_whitelist_future.get_result()
-    internal_only = add_point_queue.BotInternalOnly(bot, bot_whitelist)
+    internal_only = graph_data.Bot.GetInternalOnlySync(master, bot)
 
   # We'll skip the histogram-level sparse diagnostics because we need to
   # handle those with the histograms, below, so that we can properly assign
