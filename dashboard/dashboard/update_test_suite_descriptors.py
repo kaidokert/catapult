76602c6cb853a4c6bd490cad93da6ed3c46ac02e
benshayden <benjhayden@chromium.org>
2018-08-15 20:30:56 -0700

    Plumb test case tag maps via test suite descriptors.
    Bug: catapult:#4549
    
    Change-Id: I260bf3cd613c651c6fe8abb34aa8f48689238082

--- dashboard/dashboard/update_test_suite_descriptors.py
+++ dashboard/dashboard/update_test_suite_descriptors.py
@@ -3,10 +3,12 @@
 # found in the LICENSE file.
 
 import logging
+import sys
 import time
 
 from google.appengine.ext import db
 from google.appengine.ext import deferred
+from google.appengine.ext import ndb
 
 from dashboard import update_test_suites
 from dashboard.common import datastore_hooks
@@ -16,6 +18,8 @@ from dashboard.common import request_handler
 from dashboard.common import stored_object
 from dashboard.common import utils
 from dashboard.models import graph_data
+from dashboard.models import histogram
+from tracing.value.diagnostics import reserved_infos
 
 
 def CacheKey(test_suite):
@@ -64,6 +68,29 @@ def _QueryTestSuite(test_suite):
   return query
 
 
+def _QueryCaseTags(test_suite, bots):
+  test_paths = set()
+  for bot in bots:
+    desc = descriptor.Descriptor(test_suite=test_suite, bot=bot)
+    for test_path in desc.ToTestPathsSync():
+      test_paths.add(test_path)
+
+  futures = []
+  for test_path in test_paths:
+    query = histogram.SparseDiagnostic.query(
+        histogram.SparseDiagnostic.end_revision == sys.maxint,
+        histogram.SparseDiagnostic.name == reserved_infos.TAG_MAP.name,
+        histogram.SparseDiagnostic.test == utils.TestKey(test_path))
+    futures.append(query.fetch_async())
+
+  diagnostics = ndb.Future.wait_all(futures)
+  case_tags = {}
+  for diagnostic in diagnostics:
+    print diagnostic
+
+  return {tag: list(sorted(cases)) for tag, cases in case_tags}
+
+
 DEADLINE_SECONDS = 60 * 9.5
 
 
@@ -82,7 +109,6 @@ def _UpdateDescriptor(test_suite, namespace, start_cursor=None,
   measurements = set(measurements)
   bots = set(bots)
   cases = set(cases)
-  # TODO(4549) Tagmaps.
 
   # Some test suites have more keys than can fit in memory or can be processed
   # in 10 minutes, so use an iterator instead of a page limit.
@@ -120,6 +146,7 @@ def _UpdateDescriptor(test_suite, namespace, start_cursor=None,
       'measurements': list(sorted(measurements)),
       'bots': list(sorted(bots)),
       'cases': list(sorted(cases)),
+      'caseTags': _QueryCaseTags(test_suite, bots),
   }
   key = namespaced_stored_object.NamespaceKey(
       CacheKey(test_suite), namespace)
