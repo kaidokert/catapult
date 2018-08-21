fa7c4a89dfc1e656bb182b819d230ebd293198c7
Anthony Alridge <anthonyalridge@google.com>
2018-08-21 15:11:09 +0100

    Fix bug in key selectors.
    This fixes a bug related to using d3 selectors. In particular,
    sometimes keys which are used as class names contain
    spaces which means they are registered as multiple
    separate classes and so cannot later be retrieved with a
    selector based on the original name.
    This cl changes the encoding used to a simpler implementation to
    avoid bugs related to selectors in the future.
    
    Bug: chromium:866423
    Change-Id: I5a8476cb4705b2c9916342495334273a9e519ec8

---
+++ experimental/v8_tools/graph_utils_test.js
@@ -0,0 +1,19 @@
+'use strict';
+describe('GraphUtils', function() {
+  describe('getClassName', function() {
+    it('should create new entries by default', function() {
+      const getClassName = GraphUtils.createClassNameSuffixGenerator();
+      chai.expect(getClassName('never seen')).to.equal('0');
+    });
+    it('should return same class name for same key', function() {
+      const getClassName = GraphUtils.createClassNameSuffixGenerator();
+      chai.expect(getClassName('key')).to.equal('0');
+      chai.expect(getClassName('key')).to.equal('0');
+    });
+    it('should return different class names for different keys', function() {
+      const getClassName = GraphUtils.createClassNameSuffixGenerator();
+      chai.expect(getClassName('key one'))
+          .to.not.equal(getClassName('key two'));
+    });
+  });
+});
