ad5249c5cce37b24290f98a40e22fff6cafaac1f
Sam Balana <sbalana@google.com>
2018-08-23 15:05:21 -0700

    Use IndexedDB to cache timeseries data.
    The Chrome performance dashboard takes a considerable time fetching
    timeseries data from the backend. It is fair to assume that users
    will be viewing the same chart several times to monitor regressions
    and improvements. By storing timeseries data in browser persistent
    storage, users will not have to wait for the data they previously
    viewed.
    
    Demo: https://sbalana-dot-chromeperf.appspot.com
    
    Bug: chromium:855252
    Change-Id: I53fa5e008041d07571401b5ef3da0a0946ca0095

---
+++ dashboard/dashboard/spa/service-worker-listener.js
@@ -0,0 +1,50 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  // ServiceWorkerListener creates a communication channel between the
+  // application and the service worker. Use as an asynchronous iterator.
+  // See https://jakearchibald.com/2017/async-iterators-and-generators/
+  class ServiceWorkerListener {
+    constructor(url) {
+      this.channel = new BroadcastChannel(url);
+    }
+    [Symbol.asyncIterator]() {
+      return this;
+    }
+    next() {
+      return new Promise((resolve, reject) => {
+        if (navigator.serviceWorker.controller === null) {
+          // The request is force refresh (Shift + refresh) or there is no
+          // active service worker.
+          resolve({ done: true });
+        }
+
+        const handler = event => {
+          this.channel.removeEventListener('message', handler);
+          const {type, payload} = event.data;
+
+          switch (type) {
+            case 'RESULTS':
+              resolve({ done: false, value: payload });
+              return;
+            case 'DONE':
+              this.channel.close();
+              resolve({ done: true });
+              return;
+            default:
+              reject(new Error(`Unknown Service Worker message type: ${type}`));
+          }
+        };
+
+        this.channel.addEventListener('message', handler);
+      });
+    }
+  }
+
+  return {
+    ServiceWorkerListener,
+  };
+});
