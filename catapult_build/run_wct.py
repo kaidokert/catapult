b0bc243f57bd5374371f6e075625aa79351bb3ac
benshayden <benjhayden@chromium.org>
2018-08-31 09:13:12 -0700

    dashboard/bin/run_wct_tests
    The Chromeperf Dashboard V2 prototype currently uses Polymer2.
    Catapult's UI test framework only supports Polymer1.
    This CL adds web-component-tester to third_party,
    and a script to run V2SPA tests with Polymer2.
    run_wct_tests uses cipd to install wct. Another CL could provide wct
    via the recipe:
    https://chromium-review.googlesource.com/c/chromium/tools/build/+/1183891
    
    Bug: chromium:877660
    Change-Id: If7f0bf5c76b635711b96bd83fda8f099b9c30f7d

---
+++ catapult_build/run_wct.py
@@ -0,0 +1,28 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+from catapult_build import run_dev_server_tests
+from py_utils import binary_manager
+from py_utils import dependency_util
+
+
+def RunWct(base_dir, dep_dirs, chrome_channel='stable'):
+  wct_bin = os.environ.get('WCT', 'wct')
+  chrome_bin = run_dev_server_tests.GetLocalChromePath(None)
+  if not chrome_bin:
+    chrome_manager = binary_manager.BinaryManager([
+        run_dev_server_tests.CHROME_BINARIES_CONFIG])
+    arch, os_name = dependency_util.GetOSAndArchForCurrentDesktopPlatform()
+    chrome_bin = chrome_manager.FetchPathWithVersion(
+        'chrome_%s' % chrome_channel, arch, os_name)[0]
+  command = [wct_bin, '-chrome', chrome_bin, '-base', base_dir]
+  for dep in dep_dirs:
+    command += ['-dep', dep]
+  print command
+  # If 'wct' is not found, install it from cipd and add it to your path:
+  # cipd install -root ~/Downloads/cipd infra/testing/wct/linux-amd64 prod
+  # export PATH=~/Downloads/cipd:$PATH
+  return subprocess.call(command)
