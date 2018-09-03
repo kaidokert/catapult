a1f5343ee3a8ace6bbe4af47b5aac72e819d21b9
benshayden <benjhayden@chromium.org>
2018-09-02 21:14:08 -0700

    dashboard/bin/run_wct_tests
    The Chromeperf Dashboard V2 prototype currently uses Polymer2.
    Catapult's UI test framework only supports Polymer1.
    This CL adds web-component-tester to third_party,
    and a script to run V2SPA tests with Polymer2.
    run_wct_tests uses cipd to install wct. Another CL could provide wct
    via the recipe:
    https://chromium-review.googlesource.com/c/chromium/tools/build/+/1183891
    
    Bug: chromium:877660
    Change-Id: If7f0bf5c76b635711b96bd83fda8f099b9c30f7d

---
+++ catapult_build/run_wct.py
@@ -0,0 +1,49 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import os
+import shutil
+import subprocess
+import tempfile
+
+from catapult_build import run_dev_server_tests
+from py_utils import binary_manager
+from py_utils import dependency_util
+
+
+def RunWct(base_dir, dep_dirs, chrome_channel='stable'):
+  wct_bin = os.path.join(base_dir, 'bin/wct')#os.environ.get('WCT', 'wct')
+  if os.system('which %s > /dev/null' % wct_bin):
+    print 'FATAL ERROR: wct not found. Install it and add it to your path:'
+    print 'cipd install -root ~/cipd infra/testing/wct/linux-amd64 prod'
+    print 'export PATH=~/cipd:$PATH'
+    return 1
+
+  chrome_bin = run_dev_server_tests.GetLocalChromePath(None)
+  if not chrome_bin:
+    chrome_manager = binary_manager.BinaryManager([
+        run_dev_server_tests.CHROME_BINARIES_CONFIG])
+    arch, os_name = dependency_util.GetOSAndArchForCurrentDesktopPlatform()
+    chrome_bin = chrome_manager.FetchPathWithVersion(
+        'chrome_%s' % chrome_channel, arch, os_name)[0]
+
+  user_data_dir = tempfile.mkdtemp()
+
+  command = [wct_bin]
+  command += ['-chrome', chrome_bin]
+  command += ['-dir', user_data_dir]
+  command += ['-base', base_dir]
+  #command += ['-persist']
+  for dep in dep_dirs:
+    command += ['-dep', dep]
+  print command
+  try:
+    return subprocess.call(command)
+  finally:
+    try:
+      shutil.rmtree(user_data_dir)
+    except OSError as e:
+      logging.error('Error cleaning up temp dir %r: %s',
+                    user_data_dir, e)
