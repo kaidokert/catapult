be2c88807983977429f306ccaa2e1e19dc8ce9e3
benshayden <benjhayden@chromium.org>
2018-09-04 15:29:09 -0700

    dashboard/bin/run_wct_tests
    The Chromeperf Dashboard V2 prototype currently uses Polymer2.
    Catapult's UI test framework only supports Polymer1.
    This CL adds web-component-tester to third_party,
    and a script to run V2SPA tests with Polymer2.
    run_wct_tests uses cipd to install wct. Another CL could provide wct
    via the recipe:
    https://chromium-review.googlesource.com/c/chromium/tools/build/+/1183891
    
    Bug: chromium:877660
    Change-Id: If7f0bf5c76b635711b96bd83fda8f099b9c30f7d

--- dashboard/dashboard_build/dashboard_dev_server_config.py
+++ dashboard/dashboard_build/dashboard_dev_server_config.py
@@ -19,8 +19,10 @@ class TestListHandler(webapp2.RequestHandler):
 
   def get(self, *args, **kwargs):  # pylint: disable=unused-argument
     project = dashboard_project.DashboardProject()
+    # Exclude .../spa/... because those are run by run_wct_tests.
     test_relpaths = ['/' + _RelPathToUnixPath(x)
-                     for x in project.FindAllTestModuleRelPaths()]
+                     for x in project.FindAllTestModuleRelPaths()
+                     if '/spa/' not in x]
 
     tests = {'test_relpaths': test_relpaths}
     tests_as_json = json.dumps(tests)
