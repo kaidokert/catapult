4386dc5505196afa5ce181911789761cfc4f1806
benshayden <benjhayden@chromium.org>
2018-09-08 21:53:45 -0700

    Add session id cache for v2spa
    The Chromeperf dashboard (both V1 and V2) store session state in the
    backend, addressed by hash. V2SPA duplicates the hash function in the
    frontend so it doesn't need to wait for the backend, and remembers which
    session id hashes are already stored to reduce API requests.
    
    Bug: catapult:#4461
    Change-Id: I6c34263e6e7a370d30491d5010f5cfd0896e6869

---
+++ dashboard/dashboard/spa/request-base.js
@@ -0,0 +1,149 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class RequestBase {
+    constructor(options) {
+      this.promise_ = undefined;
+
+      this.method_ = 'GET';
+      this.headers_ = new Headers(options.headers);
+      this.body_ = undefined;
+    }
+
+    get response() {
+      // Don't call fetch_ before the subclass constructor finishes.
+      if (!this.promise_) this.promise_ = this.fetch_();
+      return this.promise_;
+    }
+
+    async addAuthorizationHeaders_() {
+      const headers = await cp.authorizationHeaders();
+      for (const [name, value] of headers) {
+        this.headers_.set(name, value);
+      }
+    }
+
+    async fetch_() {
+      await this.addAuthorizationHeaders_();
+
+      if (window.IS_DEBUG) {
+        // Simulate network latency in order to test loading state e.g. progress
+        // bars.
+        await cp.timeout(1000);
+        return this.postProcess_(await this.localhostResponse_());
+      }
+
+      const mark = tr.b.Timing.mark('fetch', this.constructor.name);
+      const response = await fetch(this.url_, {
+        body: this.body_,
+        headers: this.headers_,
+        method: this.method_,
+        signal: this.signal_,
+      });
+      mark.end();
+      return this.postProcess_(await response.json());
+    }
+
+    get url_() {
+      throw new Error('subclasses must override get url_()');
+    }
+
+    async localhostResponse_() {
+      return {};
+    }
+
+    postProcess_(json) {
+      return json;
+    }
+  }
+
+  class CacheBase {
+    constructor(options, dispatch, getState) {
+      this.options_ = options;
+      this.dispatch_ = dispatch;
+      this.getState_ = getState;
+      this.rootState_ = this.getState_();
+      this.cacheKey_ = undefined; // will be computed in read()
+    }
+
+    get cacheStatePath_() {
+      // Subclasses may override this to return a statePath. read() will ensure
+      // that the statePath exists.
+    }
+
+    get defaultCacheState_() {
+      // Subclasses may override this to return a different default cache state.
+    }
+
+    computeCacheKey_() {
+      // Subclasses must override this to return a unique string per request.
+      throw new Error('subclasses must override computeCacheKey_');
+    }
+
+    get isInCache_() {
+      throw new Error('subclasses must override isInCache_()');
+    }
+
+    createRequest_() {
+      // Subclasses must override this to return an instatiation of a class
+      // extending from cp.RequestBase for creating an outgoing HTTP request.
+      throw new Error('subclasses must override createRequest_()');
+    }
+
+    async fetch_() {
+      const request = this.createRequest_();
+      const completion = (async() => {
+        const response = await request.response;
+        this.onFinishRequest_(response);
+        return response;
+      })();
+      this.onStartRequest_(request, completion);
+      return await completion;
+    }
+
+    onStartRequest_(request, completion) {
+      // Subclasses may override this to store request or request.response in
+      // the redux store.
+    }
+
+    onFinishRequest_(response) {
+      // Subclasses may override this to store response in the redux store.
+    }
+
+    ensureCacheState_() {
+      this.dispatch_(Redux.ENSURE(
+          this.cacheStatePath_, this.defaultCacheState_));
+      this.rootState_ = this.getState_();
+    }
+
+    // Usage:
+    // class FooCache extends CacheBase { ... }
+    // const ReadFoo = options => async(dispatch, getState) =>
+    //   await new FooCache(options, dispatch, getState).read();
+    // const foo = await dispatch(ReadFoo(options))
+    async read() {
+      this.ensureCacheState_();
+      this.cacheKey_ = this.computeCacheKey_();
+      if (this.cacheKey_ instanceof Promise) {
+        // Some caches need to use async APIs to compute their cacheKey_,
+        // some caches need read() to call onStartRequest_() before the first
+        // await.
+        this.cacheKey_ = await this.cacheKey_;
+      }
+
+      if (this.isInCache_) {
+        return await this.readFromCache_();
+      }
+
+      return await this.fetch_();
+    }
+  }
+
+  return {
+    CacheBase,
+    RequestBase,
+  };
+});
