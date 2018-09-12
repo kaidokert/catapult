2e90f2b49fbaaf564dde438f3b3d7ccf50f23850
benshayden <benjhayden@chromium.org>
2018-09-11 23:11:27 -0700

    Investigate revisions
    Change-Id: I33744fad26db0e0cedaea1bd1afed34a238e0bbb

---
+++ dashboard/dashboard/revs.py
@@ -0,0 +1,26 @@
+import logging
+from google.appengine.ext import deferred
+from dashboard.models.graph_data import TestMetadata, Row
+from dashboard.common import utils
+
+def findro(bs):
+  test = TestMetadata.query(
+    TestMetadata.master_name==bs.master_name,
+    TestMetadata.bot_name==bs.bot_name,
+    TestMetadata.suite_name==bs.suite_name,
+    TestMetadata.deprecated==False,
+    TestMetadata.has_rows==True).get()
+  if not test: return
+  return Row.query(Row.parent_test==utils.OldStyleTestKey(test.key)).order(-Row.revision).get()
+
+def revs(cursor=None):
+  query = TestMetadata.query(TestMetadata.test_part1_name=='')
+  botsuites, next_cursor, more = query.fetch_page(1000, start_cursor=cursor)
+  for bs in botsuites:
+    r = findro(bs)
+    if r is None: continue
+    nrs = {n: v for n, v in r.to_dict().iteritems() if n.startswith('r_') and isinstance(v, (int,long))}
+    if not nrs: continue
+    logging.info('%s %d %r', utils.TestMetadataKey(r.parent_test).id(), r.revision, nrs)
+  if next_cursor and more:
+    deferred.defer(revs, next_cursor)
