b1056fd4e1b423c662bb2956a9dbda888ba7eefb
benshayden <benjhayden@chromium.org>
2018-09-17 16:01:56 -0700

    Add column-head for v2spa
    This component indicates table column sort order.
    
    Demo: https://v2spa-dot-chromeperf.appspot.com/#sheriff=Chromium_Perf_Sheriff
    
    Bug: catapult:#4461
    Change-Id: I67dac3c18e643218e7340c1f0fcbd26adee1e24a

---
+++ dashboard/dashboard/spa/column-head.js
@@ -0,0 +1,39 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class ColumnHead extends Polymer.Element {
+    static get is() { return 'column-head'; }
+
+    getIcon_(disabled, name, sortColumn, sortDescending) {
+      if (disabled || (name !== sortColumn)) return '';
+      return sortDescending ? 'cp:arrow-downward' : 'cp:arrow-upward';
+    }
+  }
+
+  ColumnHead.properties = {
+    disabled: {
+      type: Boolean,
+      reflectToAttribute: true,
+      value: false,
+    },
+    name: {
+      type: String,
+      value: '',
+    },
+    sortColumn: {
+      type: String,
+      value: '',
+    },
+    sortDescending: {
+      type: Boolean,
+      value: false,
+    },
+  };
+
+  customElements.define(ColumnHead.is, ColumnHead);
+
+  return {ColumnHead};
+});
