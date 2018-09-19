64aed761062118d9ea152747890dc051ebeab728
Sergiy Byelozyorov <sergiyb@chromium.org>
2018-09-19 17:27:04 +0200

    Added parameter to keep output logs on test timeout
    This is a follow-up after https://crrev.com/c/1217424.
    
    This will add a flag that will start using a 5% timeout for the internal adb
    command allowing the logs to be collected by a mechanism implemented in
    adb_wrapper rather than being lost due to thread being preempted by the thread
    watchdog. We implement this behind a flag to ensure that this does not break any
    tests that are already close to the timeout, but eventually may choose to remove
    the flag and apply this logic for all users of the API.
    
    R=jbudorick@chromium.org
    
    Bug: chromium:872257, chromium:891314
    Change-Id: I1d754f227befb843d1cbe41abba5fa96dc175c08

--- devil/devil/android/decorators.py
+++ devil/devil/android/decorators.py
@@ -79,7 +79,7 @@ def WithTimeoutAndRetries(f):
   return _TimeoutRetryWrapper(f, get_timeout, get_retries)
 
 
-def WithTimeoutAndConditionalRetries(retry_if_func):
+def WithTimeoutAndConditionalRetries(retry_if_func, pass_values=False):
   """Returns a decorator that handles timeouts and, in some cases, retries.
 
   'timeout' and 'retries' kwargs must be passed to the function.
@@ -87,6 +87,8 @@ def WithTimeoutAndConditionalRetries(retry_if_func):
   Args:
     retry_if_func: A unary callable that takes an exception and returns
       whether failures should be retried.
+    pass_values: If True, passes the `timeout` and 'retries' kwargs through to
+      the wrapped function.
   Returns:
     The actual decorator.
   """
@@ -94,7 +96,8 @@ def WithTimeoutAndConditionalRetries(retry_if_func):
     get_timeout = lambda *a, **kw: kw['timeout']
     get_retries = lambda *a, **kw: kw['retries']
     return _TimeoutRetryWrapper(
-        f, get_timeout, get_retries, retry_if_func=retry_if_func)
+        f, get_timeout, get_retries, retry_if_func=retry_if_func,
+        pass_values=pass_values)
   return decorator
 
 
