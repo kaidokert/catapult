7a266623fbc8c9b3467104c1f1dc8010c6597e1f
Ben Hayden <benjhayden@chromium.org>
2018-09-21 10:35:33 -0700

    Rely solely on the service worker cache!
    Change-Id: I09c041b870392a30b846a0a9fcd6897dff2aebfd

--- dashboard/dashboard/api/describe.py
+++ dashboard/dashboard/api/describe.py
@@ -4,18 +4,10 @@
 
 from dashboard import update_test_suite_descriptors
 from dashboard.api import api_request_handler
-from dashboard.common import timing
-from dashboard.common import utils
-
-
-CACHE_SECONDS = 60 * 60 * 20
 
 
 class DescribeHandler(api_request_handler.ApiRequestHandler):
   """API handler for describing test suites."""
-
-  def get(self, *args):
-    self.post(*args)
 
   def _AllowAnonymous(self):
     return True
@@ -24,9 +16,4 @@ class DescribeHandler(api_request_handler.ApiRequestHandler):
     return self.UnprivilegedPost(*args)
 
   def UnprivilegedPost(self, *args):
-    # TODO(benjhayden): Replace IsInternalUser with descriptor['any_internal'].
-    self.response.headers['Cache-Control'] = '%s, max-age=%d' % (
-        'private' if utils.IsInternalUser() else 'public', CACHE_SECONDS)
-    with timing.WallTimeLogger('Describe'), timing.CpuTimeLogger('Describe'):
-      return update_test_suite_descriptors.FetchCachedTestSuiteDescriptor(
-          args[0])
+    return update_test_suite_descriptors.FetchCachedTestSuiteDescriptor(args[0])
