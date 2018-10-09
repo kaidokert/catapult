06d04a574cdd33a11b6cad6747cb2c7536e2ef45
benshayden <benjhayden@chromium.org>
2018-10-09 12:11:55 -0700

    Add Request classes for v2spa
    V2SPA makes several different kinds of API requests.
    RequestBase provides common functionality to allow subclasses to
    encapsulate those requests.
    
    Bug: catapult:#4461
    Change-Id: I011aacaeee0fe7a92d01dd46199451efdcf8867a

---
+++ dashboard/dashboard/spa/alerts-request.js
@@ -0,0 +1,32 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class AlertsRequest extends cp.RequestBase {
+    constructor(options) {
+      super(options);
+      this.method_ = 'POST';
+      this.body_ = new FormData();
+      for (const [key, value] of Object.entries(options.body)) {
+        this.body_.set(key, value);
+      }
+    }
+
+    get url_() {
+      return '/api/alerts';
+    }
+
+    postProcess_(json) {
+      if (json.error) {
+        // eslint-disable-next-line no-console
+        console.error('Error fetching alerts', json.error);
+        return {anomalies: []};
+      }
+      return json;
+    }
+  }
+
+  return {AlertsRequest};
+});
