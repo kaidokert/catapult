eea9038803d3cb7982b027c75f43b2100ed45fb1
Ben Hayden <benjhayden@chromium.org>
2018-10-10 15:06:36 -0700

    Cache report names, test suites, and descriptors in v2spa service worker
    This CL adds caching for 3 API endpoints:
    /api/describe
    /api/report/names
    /api/test_suites.
    Internal-only data is stored separately from externally-available data.
    Followup CLs will add caching for session IDs, timeseries, and report data.
    
    This is split out from the CL to build and register the service worker:
    https://chromium-review.googlesource.com/c/catapult/+/1257826
    
    Parts of this are from sbalana:
    https://chromium-review.googlesource.com/c/catapult/+/1130105
    
    Bug: catapult:#4461
    Change-Id: I58fc05e9392c837566dddcda0d4e75b3099b979f

---
+++ dashboard/dashboard/spa/service-worker.js
@@ -0,0 +1,26 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+import DescribeCacheRequest from './describe-cache-request.js';
+import ReportNamesCacheRequest from './report-names-cache-request.js';
+import TestSuitesCacheRequest from './test-suites-cache-request.js';
+import analytics from './google-analytics.js';
+
+self.addEventListener('activate', activateEvent => {
+  activateEvent.waitUntil(self.clients.claim());
+});
+
+const FETCH_HANDLERS = {
+  '/api/describe': DescribeCacheRequest,
+  '/api/report/names': ReportNamesCacheRequest,
+  '/api/test_suites': TestSuitesCacheRequest,
+};
+
+self.addEventListener('fetch', fetchEvent => {
+  const cls = FETCH_HANDLERS[new URL(fetchEvent.request.url).pathname];
+  if (!cls) return;
+  new cls(fetchEvent).respond();
+});
