7532a3f6e026a9b553d801a859f36e4c44cd173d
Ben Hayden <benjhayden@chromium.org>
2018-10-11 13:04:20 -0700

    Cache report names, test suites, and descriptors in v2spa service worker
    This CL adds caching for 3 API endpoints:
    /api/describe
    /api/report/names
    /api/test_suites.
    Internal-only data is stored separately from externally-available data.
    Followup CLs will add caching for session IDs, timeseries, and report data.
    
    This is split out from the CL to build and register the service worker:
    https://chromium-review.googlesource.com/c/catapult/+/1257826
    
    Parts of this are from sbalana:
    https://chromium-review.googlesource.com/c/catapult/+/1130105
    
    Bug: catapult:#4461
    Change-Id: I58fc05e9392c837566dddcda0d4e75b3099b979f

---
+++ dashboard/dashboard/spa/task-queue.js
@@ -0,0 +1,49 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+const PAUSED = 'paused';
+const RUNNING = 'running';
+const PAUSING = 'pausing';
+
+let STATE = PAUSED;
+let TIMEOUT_ID;
+const QUEUE = [];
+
+export default {
+  schedule(task) {
+    QUEUE.push(task);
+  },
+
+  scheduleFlush(delayMs = 1000) {
+    TIMEOUT_ID = setTimeout(flush, delayMs);
+  },
+
+  cancelFlush() {
+    if (TIMEOUT_ID) clearTimeout(TIMEOUT_ID);
+    if (STATE === RUNNING) STATE = PAUSING;
+  },
+
+  async flush() {
+    // This async method could potentially take several seconds, during which
+    // time the caller could schedule another task. In order to prevent
+    // deadlocks, there should only be a single flush() running at once.
+    if (STATE !== PAUSED) return;
+    STATE = RUNNING;
+
+    // eslint-disable-next-line no-unmodified-loop-condition
+    while (QUEUE.length && (STATE === RUNNING)) {
+      const task = QUEUE.shift();
+      try {
+        await task();
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
+    }
+
+    STATE = PAUSED;
+  },
+};
