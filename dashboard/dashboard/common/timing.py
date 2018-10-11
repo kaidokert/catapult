d688d815ece3cd85b091eb0073e3360fe6dc27f0
benshayden <benjhayden@chromium.org>
2018-10-11 13:31:19 -0700

    Add ts_mon metrics to common/timing.py
    The chromeperf dashboard backend uses this utility to record CPU usage
    and wall time durations of various operations.
    
    Bug: chromium:867060
    Change-Id: I3ebbf85a6e7f0b4d9d76df753e3ced9b064c2bc5

--- dashboard/dashboard/common/timing.py
+++ dashboard/dashboard/common/timing.py
@@ -2,15 +2,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import gae_ts_mon
 import logging
 import time
 
 
 class WallTimeLogger(object):
-  def __init__(self, label):
+  def __init__(self, label, ts_mon=False):
     self._label = label
     self._start = None
     self.seconds = 0
+    if ts_mon:
+      self._metric = gae_ts_mon.FloatMetric(
+          self._Suffix(), '', [gae_ts_mon.StringField], 's')
+    else:
+      self._metric = None
 
   def _Now(self):
     return time.time()
@@ -24,6 +30,8 @@ class WallTimeLogger(object):
   def __exit__(self, *unused_args):
     self.seconds = self._Now() - self._start
     logging.info('%s:%s=%f', self._label, self._Suffix(), self.seconds)
+    if self._metric:
+      self._metric.set(self.seconds, [self._label])
 
 
 class CpuTimeLogger(WallTimeLogger):
