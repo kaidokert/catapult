7c5ac2264c92c185388f5d49eda4b46f4bcc358b
Rakib M. Hasan <rmhasan@google.com>
2018-12-19 21:43:44 -0800

    Adds ability to pass tags to Typ from browser test suites
    This CL will allow browser test suites to generate tags and
    then pass them to Typ. These tags will be used to get
    expectations for a test. This CL also adds a test case
    browser_test_runner_unittest.py which passes an expectation
    file to Typ through browser_runner_test.Run(). This test also
    checks that generated tags can be passed from a browser test
    class.
    
    R=nednguyen@google.com
    
    Bug: chromium:835690
    Change-Id: I2efe4c7561a327fbf5839084ccedf6c42a7c8f9f

---
+++ telemetry/examples/browser_tests/create_browser_test.py
@@ -0,0 +1,32 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+from telemetry.testing import serially_executed_browser_test_case
+
+
+class CreateBrowserTest(
+    serially_executed_browser_test_case.SeriallyExecutedBrowserTestCase):
+
+  @classmethod
+  def GenerateTags(cls, finder_options, possible_browser):
+    browser_options = finder_options.browser_options
+    with possible_browser.BrowserSession(browser_options) as browser:
+      if browser:
+        del browser
+        return ['foo', 'bar']
+
+  @classmethod
+  def GenerateTestCases__RunPassTest(cls, options):
+    del options
+    yield 'PassTest', ()
+
+  def _RunPassTest(self):
+    pass
+
+def load_tests(loader, tests, pattern):
+  del loader, tests, pattern
+  return serially_executed_browser_test_case.LoadAllTestsInModule(
+      sys.modules[__name__])
