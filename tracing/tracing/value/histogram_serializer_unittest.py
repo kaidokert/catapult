d0ba69d29211d7ba9e151671e6e024c704875108
Ben Hayden <benjhayden@chromium.org>
2018-12-30 14:39:43 -0800

    Serialize a new HistogramSet JSON format.
    Deserializer:
    https://chromium-review.googlesource.com/c/catapult/+/1390694
    
    Design doc:
    https://docs.google.com/document/d/1agEZc6CmYRgMGY_IVjUc2b56Hsq5HZPbsPZ4n3KwLJc
    
    Bug: chromium:918208
    Change-Id: I179dc102647dfa6c5b9713f87eb6f595430410ed

---
+++ tracing/tracing/value/histogram_serializer_unittest.py
@@ -0,0 +1,12 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from tracing.value import histogram_serializer
+
+
+class HistogramSerializerUnittest(unittest.TestCase):
+  def testSerialize(self):
+    pass
