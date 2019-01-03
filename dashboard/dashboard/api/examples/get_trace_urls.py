f8ab86fa4ff7684eb5cdf531c859768c54d5fa4f
behdad <behdadb@chromium.org>
2019-01-03 17:14:20 -0500

    Script returns all trace urls of past N days
    Bug: catapult:#
    Change-Id: I672721bc61c7d4f8cc99521792335639a0a7f454

---
+++ dashboard/dashboard/api/examples/get_trace_urls.py
@@ -0,0 +1,98 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+An example of using perf dashboard API with your own user account.
+This example returns the traceURLs
+required parameters:
+  test_suite (e.g. "rendering.desktop")
+  bot (e.g. "ChromiumPerf:Win 7 Nvidia GPU Perf")
+  measurement (e.g. frame_times)
+Optional parameter:
+  days (specifying the number of days that we want the trace urls for)
+"""
+
+import argparse
+import json
+import sys
+from sets import Set
+from datetime import datetime, timedelta
+import httplib2
+from oauth2client import client
+
+OAUTH_CLIENT_ID = (
+    '62121018386-h08uiaftreu4dr3c4alh3l7mogskvb7i.apps.googleusercontent.com')
+OAUTH_CLIENT_SECRET = 'vc1fZfV1cZC6mgDSHV-KSPOz'
+SCOPES = ['https://www.googleapis.com/auth/userinfo.email']
+
+def Main(argv):
+  parser = argparse.ArgumentParser(description='Return the trace urls')
+  parser.add_argument('test_suite',
+                      help='Name of test_suit (example: "rendering.desktop")')
+  parser.add_argument('bot',
+                      help='Name of bot (example: "ChromiumPerf:Win 7 Nvidia\
+GPU Perf")')
+  parser.add_argument('measurement',
+                      help='Name of metric (example: "frame_times")')
+  parser.add_argument('--days', default=180, type=int,
+                      help='Number of days ago from now that traces are needed\
+for')
+  parser.add_argument('--output', default='/tmp/allUrls.txt', type=str,
+                      help='Path for output file')
+
+  args = parser.parse_args(argv[1:])
+
+  test_suite = args.test_suite.replace(' ', '+')
+  bot = args.bot.replace(' ', '+')
+  measurement = args.measurement.replace(' ', '+')
+  min_timestamp = (datetime.now() - timedelta(days=args.days)).isoformat()
+
+  response, content = MakeApiRequest(test_suite, bot, measurement,
+                                     min_timestamp)
+  # Check response and do stuff with content!
+  if response['status'] != '200':
+    print response
+  else:
+    url_set = Set([])
+    j = json.loads(content)
+    for e in j['data']:
+      for i in e:
+        for url in i['diagnostics']['traceUrls']['values']:
+          url_set.add(url)
+    with open(args.output, 'w') as output_file:
+      for url in url_set:
+        output_file.write(url+'\n')
+
+def MakeApiRequest(test_suite, bot, measurement, min_timestamp):
+  flow = client.OAuth2WebServerFlow(
+      OAUTH_CLIENT_ID,
+      OAUTH_CLIENT_SECRET,
+      SCOPES,
+      approval_prompt='force')
+  flow.redirect_uri = client.OOB_CALLBACK_URN
+  authorize_url = flow.step1_get_authorize_url()
+  print(
+      'Go to the following link in your browser:\n\n'
+      '    %s\n' % authorize_url)
+  code = raw_input('Enter verification code: ').strip()
+  try:
+    creds = flow.step2_exchange(code)
+  except client.FlowExchangeError as e:
+    print 'Authentication has failed: %s' % e
+    return None, None
+
+  http_auth = creds.authorize(httplib2.Http())
+
+  url = "https://chromeperf.appspot.com/api/timeseries2?bot=%s&measurement=%s&\
+test_suite=%s&min_timestamp=%s&columns=histogram\
+" % (bot, measurement, test_suite, min_timestamp)
+
+  resp, content = http_auth.request(
+      url,
+      method="POST",
+  )
+  return (resp, content)
+
+if __name__ == '__main__':
+  sys.exit(Main(sys.argv))
