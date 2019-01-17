639c71822d0317b8fb00cb921a843ad7027e71e3
Rakib M. Hasan <rmhasan@google.com>
2019-01-17 10:46:38 -0800

    Add Flaky test handling to Typ
    This CL allows users to specify the flaky tag as a test expectation. Users
    can pass in a command line argument specifying the number of retries for flaky
    tests. Each flaky test will repeatedly run until the flaky retries limit or
    until the first pass result
    
    R=nednguyen@google.com
    
    Change-Id: I483f5f7d3af763d0ef7daa8245256931c30bc120

--- telemetry/telemetry/testing/serially_executed_browser_test_case.py
+++ telemetry/telemetry/testing/serially_executed_browser_test_case.py
@@ -19,9 +19,10 @@ DEFAULT_LOG_FORMAT = (
 
 
 class SeriallyExecutedBrowserTestCase(unittest.TestCase):
-  def __init__(self, methodName):
+  def __init__(self, methodName, method_args=None):
     super(SeriallyExecutedBrowserTestCase, self).__init__(methodName)
     self._private_methodname = methodName
+    self._private_method_args = method_args
 
   def shortName(self):
     """Returns the method name this test runs, without the package prefix."""
@@ -182,6 +183,16 @@ class SeriallyExecutedBrowserTestCase(unittest.TestCase):
     del finder_options, possible_browser
     return []
 
+  def SetFlakyRetryCount(self, retry_count):
+    pass
+
+  def GetFlakyRetryCount(self):
+    return -1
+
+  @classmethod
+  def ExpectationsFiles(cls):
+    return []
+
 def LoadAllTestsInModule(module):
   """ Load all tests & generated browser tests in a given module.
 
@@ -269,5 +280,5 @@ def GenerateTestCases(test_class, finder_options):
         _ValidateTestMethodname(generated_test_name)
         setattr(test_class, generated_test_name, _GenerateTestMethod(
             based_method, args))
-        test_cases.append(test_class(generated_test_name))
+        test_cases.append(test_class(generated_test_name, args))
   return test_cases
