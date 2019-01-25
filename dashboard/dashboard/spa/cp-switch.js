2e5e00dbf1a08cc8df49d22282397859e80aaf1f
benshayden <benjhayden@chromium.org>
2019-01-25 15:52:07 -0800

    v2spa [DO NOT COMMIT]
    It's easier to rebase a CL than a branch.
    
    Bug: chromium:918193
    Change-Id: I120652303ab92297368e724170aa9dfe41816f4d

--- dashboard/dashboard/spa/cp-switch.js
+++ dashboard/dashboard/spa/cp-switch.js
@@ -7,10 +7,6 @@
   class CpSwitch extends Polymer.Element {
     static get is() { return 'cp-switch'; }
 
-    click() {
-      this.$.native.click();
-    }
-
     onChange_(event) {
       this.dispatchEvent(new CustomEvent('change', {
         bubbles: true,
