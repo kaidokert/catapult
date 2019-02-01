33ddb315abbb649cfd6fe1ca6a69677886b32dda
lannadorai <lakshmana@google.com>
2019-02-01 11:00:42 -0800

    Add a browser interval profiling period for whole story run.
    This period will be available to all the platforms. This is especially required
    on ChromeOS to collect system-wide CPU profile of an entire story run.
    
    Bug: b/121150250
    Test: tested with:
            `./run_tests --browser=cros-chrome --remote=$CROS_REMOTE BrowserIntervalProfilingControllerTest`
            `/usr/local/telemetry/src/third_party/catapult/telemetry/bin/run_tests BrowserIntervalProfilingControllerTest`
    
    Change-Id: I6e6a8d4759784df25f3df6c70883c8a6ebb197b2

--- telemetry/telemetry/internal/browser/browser_options_unittest.py
+++ telemetry/telemetry/internal/browser/browser_options_unittest.py
@@ -91,6 +91,16 @@ class BrowserOptionsTest(unittest.TestCase):
 
     self.assertTrue(options.enable_systrace)
 
+  def testIncompatibleIntervalProfilingPeriods(self):
+    options = browser_options.BrowserFinderOptions()
+    parser = options.CreateParser()
+
+    with self.assertRaises(Exception) as context:
+      parser.parse_args(['--interval-profiling-period=story_run',
+                         '--interval-profiling-period=navigation'])
+    self.assertTrue('Cannot provide other periods along with the story_run'
+                    ' period.' in context.exception)
+
   def testMergeDefaultValues(self):
     options = browser_options.BrowserFinderOptions()
     options.already_true = True
