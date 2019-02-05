b2fdfc457f75962f7d4899eaa6dfcb03a1395875
benshayden <benjhayden@chromium.org>
2019-02-04 22:27:45 -0800

    v2spa [DO NOT COMMIT]
    It's easier to rebase a CL than a branch.
    
    TODO:
    merge chart-base
    pre-collect report data in a cron
    use mouse-move events for chart tooltip instead of dots
    report-section-test
    report-table-test
    report-template-test
    rewrite chart-timeseries to use BatchIterator better
    finish cleaning up report-section, report-template, report-table
    make service-worker remember timeseries2 404s for 20h
    fix sparkline y-axes
    calendrical x-axes
    display recovered alerts differently
    menu-input arrow keys
    debug clicking legend items
    debug everything
    split warmup CL
    split HistogramSet.CreateHistogram
    
    Bug: chromium:918193
    Change-Id: I120652303ab92297368e724170aa9dfe41816f4d

--- dashboard/dashboard/spa/report-request.js
+++ dashboard/dashboard/spa/report-request.js
@@ -13,12 +13,90 @@ tr.exportTo('cp', () => {
       this.body_.set('id', options.id);
       this.body_.set('modified', options.modified.getTime());
       this.body_.set('revisions', options.revisions);
+      this.revisions_ = options.revisions;
     }
 
     get url_() {
       return '/api/report/generate';
     }
+
+    get channelName() {
+      return (location.origin + this.url_ + '?' +
+              new URLSearchParams(this.body_));
+    }
+
+    async localhostResponse_() {
+      const rows = [];
+      const dummyRow = measurement => {
+        const row = {
+          testSuites: ['system_health.common_mobile'],
+          bots: ['master:bot0', 'master:bot1', 'master:bot2'],
+          testCases: [],
+          data: {},
+          measurement,
+        };
+        for (const revision of this.revisions_) {
+          row.data[revision] = {
+            descriptors: [
+              {
+                testSuite: 'system_health.common_mobile',
+                measurement,
+                bot: 'master:bot0',
+                testCase: 'search:portal:google',
+              },
+              {
+                testSuite: 'system_health.common_mobile',
+                measurement,
+                bot: 'master:bot1',
+                testCase: 'search:portal:google',
+              },
+            ],
+            statistics: [
+              10, 0, 0, Math.random() * 1000, 0, 0, Math.random() * 1000],
+            revision,
+          };
+        }
+        return row;
+      };
+
+      for (const group of ['Pixel', 'Android Go']) {
+        rows.push({
+          ...dummyRow('memory:a_size'),
+          label: group + ':Memory',
+          units: 'sizeInBytes_smallerIsBetter',
+        });
+        rows.push({
+          ...dummyRow('loading'),
+          label: group + ':Loading',
+          units: 'ms_smallerIsBetter',
+        });
+        rows.push({
+          ...dummyRow('startup'),
+          label: group + ':Startup',
+          units: 'ms_smallerIsBetter',
+        });
+        rows.push({
+          ...dummyRow('cpu:a'),
+          label: group + ':CPU',
+          units: 'ms_smallerIsBetter',
+        });
+        rows.push({
+          ...dummyRow('power'),
+          label: group + ':Power',
+          units: 'W_smallerIsBetter',
+        });
+      }
+
+      return {
+        name: this.name_,
+        owners: ['benjhayden@chromium.org', 'benjhayden@google.com'],
+        url: window.PRODUCTION_URL,
+        report: {rows, statistics: ['avg', 'std']},
+      };
+    }
   }
 
-  return {ReportRequest};
+  return {
+    ReportRequest,
+  };
 });
