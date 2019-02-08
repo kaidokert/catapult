bdf57db7cdb3e99b795a3fc6c60a6ec31f6754f6
benshayden <benjhayden@chromium.org>
2019-02-08 12:53:57 -0800

    v2spa [DO NOT COMMIT]
    It's easier to rebase a CL than a branch.
    
    Demo: https://dev-benjhayden-f76d60c2-dot-chromeperf.appspot.com/
    
    Steps:
    make service-worker remember timeseries2 404s for 20h
    clicking legend items
    cursorRevision
    display y-axes in getTooltip
    commit this to v2spa branch
    deploy v2spa
    
    Bug: chromium:918193
    Change-Id: I120652303ab92297368e724170aa9dfe41816f4d

--- dashboard/dashboard/spa/utils.js
+++ dashboard/dashboard/spa/utils.js
@@ -292,34 +292,6 @@ tr.exportTo('cp', () => {
     return state;
   }
 
-  /**
-   * Wrap Google Sign-in client library to build the Authorization header, if
-   * one is available. Automatically reloads the token if necessary.
-   */
-  async function authorizationHeaders() {
-    if (window.gapi === undefined) return [];
-    if (gapi.auth2 === undefined) return [];
-
-    const auth = gapi.auth2.getAuthInstance();
-    if (!auth) return [];
-    const user = auth.currentUser.get();
-    let response = user.getAuthResponse();
-
-    if (response.expires_at === undefined) {
-      // The user is not signed in.
-      return [];
-    }
-
-    if (response.expires_at < new Date()) {
-      // The token has expired, so reload it.
-      response = await user.reloadAuthResponse();
-    }
-
-    return [
-      ['Authorization', response.token_type + ' ' + response.access_token],
-    ];
-  }
-
   function normalize(columns, cells) {
     const dict = {};
     for (let i = 0; i < columns.length; ++i) {
@@ -395,6 +367,8 @@ tr.exportTo('cp', () => {
     }
 
     async* [Symbol.asyncIterator]() {
+      if (!this.promises_.size) return;
+
       // Yield the first result immediately in order to allow the user to start
       // to understand it (c.f. First Contentful Paint), and also to measure how
       // long it takes the caller to render the data. Use that measurement as an
@@ -458,17 +432,176 @@ tr.exportTo('cp', () => {
     return pluralSuffix;
   }
 
+  function timeEventListeners(cls) {
+    // Polymer handles the addEventListener() calls, this method just wraps
+    // 'on*_' methods with Timing marks.
+    for (const name of Object.getOwnPropertyNames(cls.prototype)) {
+      if (!name.startsWith('on')) continue;
+      if (!name.endsWith('_')) continue;
+      (() => {
+        const wrapped = cls.prototype[name];
+        const debugName = cls.name + '.' + name;
+
+        cls.prototype[name] = async function eventListenerWrapper(event) {
+          // Measure the time from when the browser receives the event to when
+          // we receive the event.
+          if (event && event.timeStamp) {
+            tr.b.Timing.mark('listener', debugName, event.timeStamp).end();
+          }
+
+          // Measure the first paint latency by starting the event listener
+          // without awaiting it.
+          const firstPaintMark = tr.b.Timing.mark('firstPaint', debugName);
+          const resultPromise = wrapped.call(this, event);
+          (async() => {
+            await cp.afterRender();
+            firstPaintMark.end();
+          })();
+
+          const result = await resultPromise;
+
+          const lastPaintMark = tr.b.Timing.mark('lastPaint', debugName);
+          (async() => {
+            await cp.afterRender();
+            lastPaintMark.end();
+          })();
+
+          return result;
+        };
+      })();
+    }
+  }
+
+  function timeActions(cls) {
+    if (!cls.actions) return;
+    for (const [name, action] of Object.entries(cls.actions)) {
+      const debugName = `${cls.name}.actions.${name}`;
+      const actionReplacement = (...args) => {
+        const thunk = action(...args);
+        Object.defineProperty(thunk, 'name', {value: debugName});
+        const thunkReplacement = async(dispatch, getState) => {
+          const mark = tr.b.Timing.mark('action', debugName);
+          try {
+            return await thunk(dispatch, getState);
+          } finally {
+            mark.end();
+          }
+        };
+        Object.defineProperty(thunkReplacement, 'name', {
+          value: 'timeActions:wrapper',
+        });
+        return thunkReplacement;
+      };
+      actionReplacement.implementation = action;
+      Object.defineProperty(actionReplacement, 'name', {value: debugName});
+      cls.actions[name] = actionReplacement;
+    }
+  }
+
+  /**
+   * Generate pretty colors!
+   * http://basecase.org/env/on-rainbows
+   * https://mycarta.wordpress.com/2012/10/06/the-rainbow-is-deadlong-live-the-rainbow-part-3/
+   *
+   * Set brightnessPct = 0 to always generate black.
+   * Set brightnessPct = 1 to always generate white.
+   * Set brightnessPct = .5 to generate saturated colors.
+   *
+   * @param {number} huePct
+   * @param {number} brightnessPct
+   * @return {!tr.b.Color}
+   */
+  function sinebowColor(huePct, brightnessPct) {
+    // TODO smooth huePct using spline
+    const h = -(huePct + .5);
+    let r = Math.sin(Math.PI * h);
+    let g = Math.sin(Math.PI * (h + 1 / 3));
+    let b = Math.sin(Math.PI * (h + 2 / 3));
+    r *= r;
+    g *= g;
+    b *= b;
+
+    // Roughly correct for human perception.
+    // https://en.wikipedia.org/wiki/Luma_%28video%29
+    // Multiply by 2 to normalize all values to 0.5.
+    // (Halfway between black and white.)
+    const y = 2 * (0.2989 * r + 0.5870 * g + 0.1140 * b);
+    r /= y;
+    g /= y;
+    b /= y;
+
+    if (brightnessPct <= 0.5) {
+      r *= brightnessPct * 2;
+      g *= brightnessPct * 2;
+      b *= brightnessPct * 2;
+    } else {
+      const brightness = tr.b.math.normalize(brightnessPct, .5, 1);
+      r = tr.b.math.lerp(brightness, r, 1);
+      g = tr.b.math.lerp(brightness, g, 1);
+      b = tr.b.math.lerp(brightness, b, 1);
+    }
+    r *= 256;
+    g *= 256;
+    b *= 256;
+    r = Math.round(r);
+    g = Math.round(g);
+    b = Math.round(b);
+    return new tr.b.Color(r, g, b);
+  }
+
+  /**
+   * Compute a given number of colors by evenly spreading them around the
+   * sinebow hue circle, or, if a Range of brightnesses is given, the hue x
+   * brightness cylinder.
+   *
+   * @param {Number} numColors
+   * @param {!Range} opt_options.brightnessRange
+   * @param {Number} opt_options.brightnessPct
+   * @param {Number} opt_options.hueOffset
+   * @return {!Array.<!tr.b.Color>}
+   */
+  function generateColors(numColors, opt_options) {
+    const options = opt_options || {};
+    const brightnessRange = options.brightnessRange;
+    const hueOffset = options.hueOffset || 0;
+    const colors = [];
+    if (numColors > 15 && brightnessRange) {
+      // Evenly spread numColors around the surface of the hue x brightness
+      // cylinder. Maximize distance between (huePct, brightnessPct) vectors.
+      const numCycles = Math.round(numColors / 15);
+      for (let i = 0; i < numCycles; ++i) {
+        colors.push.apply(colors, generateColors(15, {
+          brightnessPct: brightnessRange.lerp(i / (numCycles - 1)),
+        }));
+      }
+    } else {
+      // Evenly spread numColors throughout the sinebow hue circle.
+      const brightnessPct = (options.brightnessPct === undefined) ? 0.5 :
+        options.brightnessPct;
+      for (let i = 0; i < numColors; ++i) {
+        const huePct = hueOffset + (i / numColors);
+        colors.push(sinebowColor(huePct, brightnessPct));
+      }
+    }
+    return colors;
+  }
+
+  function denormalize(objects, columnNames) {
+    return objects.map(obj => columnNames.map(col => obj[col]));
+  }
+
   return {
     BatchIterator,
     NON_BREAKING_SPACE,
     ZERO_WIDTH_SPACE,
     afterRender,
     animationFrame,
-    authorizationHeaders,
     breakWords,
     buildProperties,
     buildState,
     deepFreeze,
+    denormalize,
+    generateColors,
     getActiveElement,
     idle,
     isElementChildOf,
@@ -481,6 +614,8 @@ tr.exportTo('cp', () => {
     plural,
     setImmutable,
     sha,
+    timeActions,
+    timeEventListeners,
     timeout,
   };
 });
