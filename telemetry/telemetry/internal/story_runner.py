9393711aba3a256086c7fc438ca1a0e0eab4e33d
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-02-15 05:11:16 -0500

    speed: Reduce how often network replay is set up.
    Network-replay set up can take 5+ seconds for each page. However, the
    replay does not need to be re-setup if the archive for the current story
    has not changed since the previous story. So to minimize the number of
    times network-replay needs to be set-up, sort the stories based on the
    archive they are in. This saves ~5-10 minutes for a complete
    rendering.desktop run, which is approx. 5-10% of total runtime.
    
    Bug: chromium:932409
    Change-Id: I8a1606fc4656a69002e2cfde38a61346e1451f3a

--- telemetry/telemetry/internal/story_runner.py
+++ telemetry/telemetry/internal/story_runner.py
@@ -200,6 +200,9 @@ def Run(test, story_set, finder_options, results, max_failures=None,
 
   # Filter page set based on options.
   stories = story_module.StoryFilter.FilterStorySet(story_set)
+  wpr_archive_info = story_set.wpr_archive_info
+  if wpr_archive_info:
+    stories = sorted(stories, key=wpr_archive_info.WprFilePathForStory)
 
   if finder_options.print_only:
     if finder_options.print_only == 'tags':
@@ -229,7 +232,7 @@ def Run(test, story_set, finder_options, results, max_failures=None,
         cloud_storage.GetFilesInDirectoryIfChanged(directory,
                                                    story_set.bucket)
     if story_set.archive_data_file and not _UpdateAndCheckArchives(
-        story_set.archive_data_file, story_set.wpr_archive_info, stories):
+        story_set.archive_data_file, wpr_archive_info, stories):
       return
 
   if not stories:
