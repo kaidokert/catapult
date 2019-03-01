3da060ecf91f087d644399587d72a65500f0faf3
Ben Hayden <benjhayden@chromium.org>
2019-02-28 21:08:25 -0800

    Switch results.html to the new histogramset json format.
    Design doc:
    https://docs.google.com/document/d/1agEZc6CmYRgMGY_IVjUc2b56Hsq5HZPbsPZ4n3KwLJc
    
    Bug: chromium:918208
    Change-Id: Ia267e7156690398db38d90431322ccd76d81c5a3

--- tracing/tracing/value/histogram_set.py
+++ tracing/tracing/value/histogram_set.py
@@ -6,6 +6,7 @@ import collections
 
 from tracing.value import histogram as histogram
 from tracing.value import histogram_deserializer
+from tracing.value import histogram_serializer
 from tracing.value.diagnostics import all_diagnostics
 from tracing.value.diagnostics import diagnostic
 from tracing.value.diagnostics import diagnostic_ref
@@ -113,12 +114,7 @@ class HistogramSet(object):
       self.AddHistogram(hist)
 
   def AsDicts(self):
-    dcts = []
-    for d in self._shared_diagnostics_by_guid.values():
-      dcts.append(d.AsDict())
-    for h in self:
-      dcts.append(h.AsDict())
-    return dcts
+    return histogram_serializer.Serialize(list(self))
 
   def ReplaceSharedDiagnostic(self, old_guid, new_diagnostic):
     if not isinstance(new_diagnostic, diagnostic_ref.DiagnosticRef):
