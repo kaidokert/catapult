e78d222250ba2b2fc16f37b71e8b99972fd676f1
benshayden <benjhayden@chromium.org>
2019-03-05 13:05:08 -0800

    v2spa [DO NOT COMMIT]
    It's easier to rebase a CL than a branch.
    
    Demo: https://dev-benjhayden-f76d60c2-dot-chromeperf.appspot.com/
    
    TODO
    chart-compound-test
    sparkline-compound-test
    chart-section-test
    
    timeseries-request-cache 404s
    load untriaged alerts then triaged alerts +min_start_revision
    don't set maxRev in url when 100%
    AlertsCacheRequest always goes to network, writes to timeseries/
    X Axis Coordinate: revision / timestamp / r_...
    
    Bug: chromium:918193
    Change-Id: I2f532321655b5ff28bc0064f41e812b6a2c8037f

--- dashboard/dashboard/spa/request-base.js
+++ dashboard/dashboard/spa/request-base.js
@@ -5,11 +5,21 @@
 'use strict';
 tr.exportTo('cp', () => {
   class RequestBase {
-    constructor(options) {
+    constructor(options = {}) {
       this.responsePromise_ = undefined;
+
       this.method_ = 'GET';
       this.headers_ = new Headers(options.headers);
       this.body_ = undefined;
+
+      this.abortController_ = options.abortController;
+      if (!this.abortController_ && window.AbortController) {
+        this.abortController_ = new window.AbortController();
+      }
+      this.signal_ = undefined;
+      if (this.abortController_) {
+        this.signal_ = this.abortController_.signal;
+      }
     }
 
     get url_() {
@@ -58,6 +68,7 @@ tr.exportTo('cp', () => {
         body: this.body_,
         headers: this.headers_,
         method: this.method_,
+        signal: this.signal_,
       });
       mark.end();
       return this.postProcess_(await response.json());
@@ -65,6 +76,11 @@ tr.exportTo('cp', () => {
 
     postProcess_(response, isFromChannel = false) {
       return response;
+    }
+
+    abort() {
+      if (!this.abortController_) return;
+      this.abortController_.abort();
     }
   }
 
