4a5be6eb35fef48fd85782f7662ea17c0aeff36f
benshayden <benjhayden@chromium.org>
2019-03-21 13:45:14 -0700

    v2spa [DO NOT COMMIT]
    It's easier to rebase a CL than a branch.
    
    Demo: https://dev-benjhayden-f76d60c2-dot-chromeperf.appspot.com/
    
    recent bugs
    session id cache request
    team name
    timeseries-request-cache 404s
    on mouse enter alerts-table row, boldLine in preview
    load untriaged alerts then triaged alerts +min_start_revision
    AlertsCacheRequest always goes to network, writes to timeseries/
    X Axis Coordinate: revision / timestamp / r_...
    
    Bug: chromium:918193
    Change-Id: I2f532321655b5ff28bc0064f41e812b6a2c8037f

--- dashboard/dashboard/spa/request-base.js
+++ dashboard/dashboard/spa/request-base.js
@@ -5,11 +5,21 @@
 'use strict';
 tr.exportTo('cp', () => {
   class RequestBase {
-    constructor(options) {
+    constructor(options = {}) {
       this.responsePromise_ = undefined;
+
       this.method_ = 'GET';
       this.headers_ = new Headers(options.headers);
       this.body_ = undefined;
+
+      this.abortController_ = options.abortController;
+      if (!this.abortController_ && window.AbortController) {
+        this.abortController_ = new window.AbortController();
+      }
+      this.signal_ = undefined;
+      if (this.abortController_) {
+        this.signal_ = this.abortController_.signal;
+      }
     }
 
     get url_() {
@@ -43,8 +53,7 @@ tr.exportTo('cp', () => {
     }
 
     async addAuthorizationHeaders_() {
-      if (!window.IS_PRODUCTION && !window.mocha) return;
-      if (!window.getAuthorizationHeaders) return;
+      if (!window.AUTH_CLIENT_ID) return;
       const headers = await window.getAuthorizationHeaders();
       for (const [name, value] of Object.entries(headers)) {
         this.headers_.set(name, value);
@@ -58,6 +67,7 @@ tr.exportTo('cp', () => {
         body: this.body_,
         headers: this.headers_,
         method: this.method_,
+        signal: this.signal_,
       });
       mark.end();
       return this.postProcess_(await response.json());
@@ -65,6 +75,11 @@ tr.exportTo('cp', () => {
 
     postProcess_(response, isFromChannel = false) {
       return response;
+    }
+
+    abort() {
+      if (!this.abortController_) return;
+      this.abortController_.abort();
     }
   }
 
