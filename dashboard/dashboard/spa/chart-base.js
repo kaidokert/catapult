a3e8ed30ec273d79be49469410a84219e9b6f3d6
benshayden <benjhayden@chromium.org>
2019-03-22 09:46:36 -0700

    v2spa [DO NOT COMMIT]
    It's easier to rebase a CL than a branch.
    
    Demo: https://dev-benjhayden-f76d60c2-dot-chromeperf.appspot.com/
    
    TeamFilter
    timeseries-request-cache 404s
    on mouse enter alerts-table row, boldLine in preview
    load untriaged alerts then triaged alerts +min_start_revision
    AlertsCacheRequest always goes to network, writes to timeseries/
    X Axis Coordinate: revision / timestamp / r_...
    
    Bug: chromium:918193
    Change-Id: I2f532321655b5ff28bc0064f41e812b6a2c8037f

--- dashboard/dashboard/spa/chart-base.js
+++ dashboard/dashboard/spa/chart-base.js
@@ -20,6 +20,26 @@ tr.exportTo('cp', () => {
       return ChartBase.antiBrushes(brushes);
     }
 
+    pct_(x) {
+      return x + '%';
+    }
+
+    brushPointSize_(brushSize) {
+      if (Number.isNaN(brushSize)) return 0;
+      return brushSize * 1.5;
+    }
+
+    tickAnchor_(tick) {
+      return tick.anchor || 'middle';
+    }
+
+    onMainClick_(event) {
+      this.dispatchEvent(new CustomEvent('chart-click', {
+        bubbles: true,
+        composed: true,
+      }));
+    }
+
     tooltipHidden_(tooltip) {
       return !tooltip || !tooltip.isVisible || this.isEmpty_(tooltip.rows);
     }
@@ -29,9 +49,17 @@ tr.exportTo('cp', () => {
           '--mouse').includes('inside');
     }
 
-    maybePollMouseLeaveMain_() {
+    async maybePollMouseLeaveMain_() {
       if (this.mouseLeaveMainPoller_) return;
+
       this.mouseLeaveMainPoller_ = this.pollMouseLeaveMain_();
+      await this.mouseLeaveMainPoller_;
+      this.mouseLeaveMainPoller_ = undefined;
+      this.previousNearestPoint = undefined;
+      this.dispatchEvent(new CustomEvent('mouse-leave-main', {
+        bubbles: true,
+        composed: true,
+      }));
     }
 
     async pollMouseLeaveMain_() {
@@ -40,26 +68,10 @@ tr.exportTo('cp', () => {
       // area. However, mouseleave and mouseout are flaky in obnoxious ways. CSS
       // :hover seems to be robust, so a :hover selector sets the property
       // --mouse: inside. mouseLeaveMain_ polls this property.
+      await cp.animationFrame();
       while (this.isMouseOverMain) {
         await cp.animationFrame();
       }
-      this.dispatchEvent(new CustomEvent('mouse-leave-main', {
-        bubbles: true,
-        composed: true,
-      }));
-      this.mouseLeaveMainPoller_ = undefined;
-    }
-
-    brushPointSize_(brushSize) {
-      if (Number.isNaN(brushSize)) return 0;
-      return brushSize * 1.5;
-    }
-
-    onMainClick_(event) {
-      this.dispatchEvent(new CustomEvent('chart-click', {
-        bubbles: true,
-        composed: true,
-      }));
     }
 
     async onTrackBrushHandle_(event) {
@@ -197,6 +209,7 @@ tr.exportTo('cp', () => {
   };
 
   ChartBase.getNearestPoint = (pt, rect, lines) => {
+    if (!lines) return {};
     const xPct = tr.b.math.normalize(pt.x, rect.left, rect.right) * 100;
     const yPct = tr.b.math.normalize(pt.y, rect.top, rect.bottom) * 100;
     let nearestPoint;
