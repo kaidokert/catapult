43a6b1c569c6a6502010367032da33367191f0ce
Simon <simonhatch@chromium.org>
2019-03-29 17:15:34 -0400

    Pinpoint - Look for performance_browser_tests in browser_tests
    The output from performance_browser_tests is currently being output
    in browser_tests, and so every pinpoint job fails thinking there was
    no output. Until this is fixed properly, if retrieving the output
    fails, look in the other name. Eventually this should just work
    properly and we can remove the hack.
    
    Bug: chromium:947501
    Change-Id: I7c2b55418fe11a719116ea5806523c31659edc00

--- dashboard/dashboard/pinpoint/models/results2.py
+++ dashboard/dashboard/pinpoint/models/results2.py
@@ -154,5 +154,16 @@ def _JsonFromExecution(execution):
     results_filename = execution._results_filename
   else:
     results_filename = 'chartjson-output.json'
-  return read_value._RetrieveOutputJson(
-      isolate_server, isolate_hash, results_filename)
+
+  try:
+    return read_value._RetrieveOutputJson(
+        isolate_server, isolate_hash, results_filename)
+  except read_value.ReadValueError as e:
+    if 'performance_browser_tests' not in results_filename:
+      raise e
+
+    # TODO(simonhatch): Remove this once crbug.com/947501 is resolved.
+    modified_name = results_filename.replace(
+        'performance_browser_tests', 'browser_tests')
+    return read_value._RetrieveOutputJson(
+        isolate_server, isolate_hash, modified_name)
