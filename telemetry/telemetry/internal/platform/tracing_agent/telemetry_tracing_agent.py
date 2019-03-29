e83ad80eba77879b3a3eb74db65342e151509cf2
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-03-29 16:21:29 +0000

    [Telemetry] Make Telemetry tracing agent write only
    Telemetry used to read the trace data from py_trace_event, to then
    add it back as a json trace data on the trace builder.
    
    In this CL:
    
    - We make it so that Telemetry only ever writes to the file; and
    
    - Pass the file itself to the trace builder, which will then own and
      manage the lifetime of the file from then on.
    
    Follow up work will ensure that all trace data writing is done by
    py_trace_event.
    
    Bug: chromium:928278
    Change-Id: I0533d675ccb97fb1f55562bd800d89995f7570f0

--- telemetry/telemetry/internal/platform/tracing_agent/telemetry_tracing_agent.py
+++ telemetry/telemetry/internal/platform/tracing_agent/telemetry_tracing_agent.py
@@ -2,8 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import json
-import os
 import tempfile
 
 from telemetry.internal.platform import tracing_agent
@@ -25,8 +23,8 @@ class TelemetryTracingAgent(tracing_agent.TracingAgent):
   """
   def __init__(self, platform_backend):
     super(TelemetryTracingAgent, self).__init__(platform_backend)
-    self._trace_log = None
     self._telemetry_info = None
+    self._trace_file = None
 
   @classmethod
   def IsSupported(cls, platform_backend):
@@ -46,20 +44,20 @@ class TelemetryTracingAgent(tracing_agent.TracingAgent):
     assert not self.is_tracing, 'Telemetry tracing already running'
 
     # Create a temporary file and pass the opened file-like object to
-    # trace_event.trace_enable(); the file will be closed on trace_disable().
-    # We keep the name of the file on self._trace_log to read the contents
-    # later in CollectAgentTraceData().
-    # In contrast with tempfile.NamedTemporaryFile, this avoids an issue on
-    # Windows where the file remained in use "by another process" and raised
-    # an error when trying to clean up the file.
-    fd, self._trace_log = tempfile.mkstemp()
-    trace_event.trace_enable(os.fdopen(fd, 'wb'))
+    # trace_event.trace_enable(), which will serialize all trace data.
+    # Later the file is collected and passed to the trace builder during
+    # CollectAgentTraceData.
+    self._trace_file = tempfile.NamedTemporaryFile(delete=False)
+    trace_event.trace_enable(
+        self._trace_file, format=trace_event.JSON_WITH_METADATA)
 
     assert self.is_tracing, 'Failed to start Telemetry tracing'
     return True
 
   def StopAgentTracing(self):
     assert self.is_tracing, 'Telemetry tracing is not running'
+    # This would stop tracing, but we should be still be able to later write
+    # the metadata. Probably should keep the file open?
     trace_event.trace_disable()
     assert not self.is_tracing, 'Failed to stop Telemetry tracing'
 
@@ -74,39 +72,31 @@ class TelemetryTracingAgent(tracing_agent.TracingAgent):
 
   def CollectAgentTraceData(self, trace_data_builder, timeout=None):
     assert not self.is_tracing, 'Must stop tracing before collection'
-    try:
-      with open(self._trace_log) as f:
-        # Currently `py_trace_event` stores it's trace data as a json list of
-        # dicts (one for each event). But, since many processes may be writing
-        # events to the file, it doesn't know when it's "done" to close the
-        # list. Therefore, we have to add the closing ']' here.
-        data = json.loads(f.read() + ']')
-    finally:
-      os.remove(self._trace_log)
-      self._trace_log = None
 
-    trace_data_builder.AddTraceFor(
-        trace_data.TELEMETRY_PART,
-        {
-            'traceEvents': data,
-            'metadata': {
-                # TODO(charliea): For right now, we use "TELEMETRY" as the clock
-                # domain to guarantee that Telemetry is given its own clock
-                # domain. Telemetry isn't really a clock domain, though: it's a
-                # system that USES a clock domain like LINUX_CLOCK_MONOTONIC or
-                # WIN_QPC. However, there's a chance that a Telemetry controller
-                # running on Linux (using LINUX_CLOCK_MONOTONIC) is interacting
-                # with an Android phone (also using LINUX_CLOCK_MONOTONIC, but
-                # on a different machine). The current logic collapses clock
-                # domains based solely on the clock domain string, but we really
-                # should to collapse based on some (device ID, clock domain ID)
-                # tuple. Giving Telemetry its own clock domain is a work-around
-                # for this.
-                'clock-domain':
-                    'TELEMETRY',
-                'telemetry': self._LoadTelemetryInfo(),
-            }
-        })
+    # I suppose this should write the metadata and close the file.
+    trace_event.add_metadata({
+        # TODO(charliea): For right now, we use "TELEMETRY" as the clock
+        # domain to guarantee that Telemetry is given its own clock
+        # domain. Telemetry isn't really a clock domain, though: it's a
+        # system that USES a clock domain like LINUX_CLOCK_MONOTONIC or
+        # WIN_QPC. However, there's a chance that a Telemetry controller
+        # running on Linux (using LINUX_CLOCK_MONOTONIC) is interacting
+        # with an Android phone (also using LINUX_CLOCK_MONOTONIC, but
+        # on a different machine). The current logic collapses clock
+        # domains based solely on the clock domain string, but we really
+        # should to collapse based on some (device ID, clock domain ID)
+        # tuple. Giving Telemetry its own clock domain is a work-around
+        # for this.
+        'clock-domain':
+            'TELEMETRY',
+        'telemetry': self._LoadTelemetryInfo(),
+    })
+
+    # The trace file will now be owned by the trace data builder.
+    trace_data_builder.AddTraceFileFor(
+        trace_data.TELEMETRY_PART, self._trace_file.name)
+    self._trace_file = None
+
 
   @staticmethod
   def RecordIssuerClockSyncMarker(sync_id, issue_ts):
