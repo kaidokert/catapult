c617c0df817f64f0790adaa81e95451352f328e5
benshayden <benjhayden@chromium.org>
2019-04-01 11:22:05 -0700

    Add details-table for spa.
    chart-compound displays this below charts.
    It contains summary statistics, links to further resources, and alert-details
    for all lineDescriptors and for all revision ranges brushed in the main chart.
    
    v1:
    <details-table chart-state-path=chartLayout state-path=details>
    chartLayout.receivedTimestamp
    details-table observes receivedTimestamp, brushedRevisionRanges
    details-table displays data from lines
    
    v2:
    When the user clicks on the main chart,
    1. chart-compound sets chartLayout.brushRevisions, xAxis.brushes, and
       details.revisionRanges.
    2. details-table observes lineDescriptors and revisionRanges, debounces, and
       dispatches load.
    3. DetailsTable.actions.load fetches the XY levelOfDetail for all
       fetchDescriptors in a BatchIterator.
    4. When a response for a XY request returns, the revision ranges are
       refined for that fetchDescriptor down to 1 point before each brushed
       revision range up to the last point in the brushed range (or 2 points before
       the brushed range if there are no points within the brushed range), and a
       request for the HISTOGRAMS levelOfDetail for each brushed range is added to
       the BatchIterator. Responses for HISTOGRAMS requests are collated similar to
       what ChartTimeseries does. Data points within a range are merged together.
       The TimeseriesCacheRequest in the service worker is relied upon to quickly
       return data that was recently requested for the main chart.
    5. When a response for a HISTOGRAMS levelOfDetail request returns,
       DetailsTable.reducers.receiveData merges across fetchDescriptors within each
       lineDescriptor.
    6. DetailsTable.reducers.receiveData computes column headers, scalarRows and
       linkRows for each lineDescriptor.
       When computing linkRows for r_ annotations, linked revision ranges begin at
       the reference data point just before the brushed range (+1 if the revision is
       numeric like a commit position).
       If a linkRow is the same for all available lineDescriptors, it is displayed
       once via commonLinkRows.
    
    In ChartCompound.actions.load, if brushRevisions is already set, lineDescriptors
    are synced to details plus min/maxRevision from chartLayout.
    
    Bug: chromium:865515
    Change-Id: Ic3de64d9dc6cd75f9f4ff9c06239b8f2213cb604

---
+++ dashboard/dashboard/spa/details-table.js
@@ -0,0 +1,287 @@
+/* Copyright 2019 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class DetailsTable extends cp.ElementBase {
+    observeConfig_() {
+      if (!this.revisionRanges || this.revisionRanges.length === 0) return;
+
+      this.debounce('load', () => {
+        this.dispatch('load', this.statePath);
+      }, Polymer.Async.microTask);
+    }
+  }
+
+  DetailsTable.State = {
+    isLoading: options => false,
+    lineDescriptors: options => options.lineDescriptors || [],
+    minRevision: options => options.minRevision || 0,
+    maxRevision: options => options.maxRevision || Number.MAX_SAFE_INTEGER,
+    revisionRanges: options => options.revisionRanges || [],
+    columns: options => [],
+    commonLinkRows: options => [],
+    bodies: options => [],
+  };
+
+  DetailsTable.properties = cp.buildProperties(
+      'state', DetailsTable.State);
+  DetailsTable.buildState = options => cp.buildState(
+      DetailsTable.State, options);
+  DetailsTable.observers = [
+    'observeConfig_(lineDescriptors, revisionRanges)',
+  ];
+
+  // Remove empty elements.
+  function filterTimeseriesesByLine(timeseriesesByLine) {
+    const result = [];
+    for (const {lineDescriptor, timeseriesesByRange} of timeseriesesByLine) {
+      const filteredTimeseriesesByRange = [];
+      for (const {range, timeserieses} of timeseriesesByRange) {
+        const filteredTimeserieses = timeserieses.filter(ts => ts);
+        if (filteredTimeserieses.length === 0) continue;
+        filteredTimeseriesesByRange.push({
+          range,
+          timeserieses: filteredTimeserieses,
+        });
+      }
+      if (filteredTimeseriesesByRange.length === 0) continue;
+      result.push({
+        lineDescriptor,
+        timeseriesesByRange: filteredTimeseriesesByRange,
+      });
+    }
+    return result;
+  }
+
+  async function* wrapTimeseriesReader(
+      batches, fetchDescriptor, revisionRanges, receiveCallback) {
+    // This generator does not yield any results to BatchIterator.
+    // This generator only adds more generators from wrapDetailsReader, which
+    // will yield. However, those generators do not yield results. Instead,
+    // results are collated via receiveCallback.
+    const request = new cp.TimeseriesRequest(fetchDescriptor);
+    let timeseries;
+    for await (timeseries of request.reader()) {
+      // Wait for final data.
+    }
+    console.log(fetchDescriptor, timeseries);
+
+    for (const [rangeIndex, revisionRange] of cp.enumerate(revisionRanges)) {
+      fetchDescriptor = {
+        ...fetchDescriptor,
+        levelOfDetail: cp.LEVEL_OF_DETAIL.DETAILS,
+        ...DetailsTable.matchRange(revisionRange, timeseries),
+      };
+      batches.add(wrapDetailsReader(
+          fetchDescriptor, rangeIndex, receiveCallback));
+    }
+  }
+
+  DetailsTable.matchRange = (range, timeseries) => {
+    // Return {minRevision, maxRevision} to fetch in order to get the data
+    // points within revisionRange (or the previous available point if that's
+    // empty) plus one data point before revisionRange as a reference.
+
+    let maxIndex = tr.b.findLowIndexInSortedArray(
+        timeseries, d => d.revision, range.max);
+    // Now, timeseries[maxIndex].revision >= range.max
+
+    if (maxIndex > 0) {
+      // Get the data point *before* range.max, not after it.
+      maxIndex -= 1;
+    }
+
+    let minIndex = tr.b.findLowIndexInSortedArray(
+        timeseries, d => d.revision, range.min);
+    // Now, timeseries[minIndex].revision >= range.min
+
+    while (minIndex > 0 && maxIndex < minIndex) {
+      // Prevent minRevision > maxRevision.
+      minIndex -= 1;
+    }
+
+    // Get the reference data point.
+    if (minIndex > 0) minIndex -= 1;
+
+    const minRevision = timeseries[minIndex].revision;
+    const maxRevision = timeseries[maxIndex].revision;
+    return {minRevision, maxRevision};
+  };
+
+  async function* wrapDetailsReader(
+      fetchDescriptor, rangeIndex, receiveCallback) {
+    const request = new cp.TimeseriesRequest(fetchDescriptor);
+    for await (const timeseries of request.reader()) {
+      receiveCallback(rangeIndex, timeseries);
+      yield {/* Pump BatchIterator. See timeseriesesByLine. */};
+    }
+  }
+
+  // Each lineDescriptor may require data from one or more fetchDescriptors.
+  // Fetch one or more fetchDescriptors per line, batch the readers, collate the
+  // data.
+  // Yields {errors, timeseriesesByLine: [
+  //   {lineDescriptor, timeseriesesByRange: [{range, timeserieses}]},
+  // ]}.
+  async function* generateTimeseries(
+      lineDescriptors, minRevision, maxRevision, revisionRanges) {
+    const timeseriesesByLine = [];
+    const batches = new cp.BatchIterator([]);
+
+    for (const lineDescriptor of lineDescriptors) {
+      const fetchDescriptors = cp.ChartTimeseries.createFetchDescriptors(
+          lineDescriptor, cp.LEVEL_OF_DETAIL.XY);
+      const timeseriesesByRange = new Array(revisionRanges.length);
+      timeseriesesByLine.push({lineDescriptor, timeseriesesByRange});
+
+      for (const [rangeIndex, range] of cp.enumerate(revisionRanges)) {
+        const timeserieses = new Array(fetchDescriptors.length);
+        timeseriesesByRange[rangeIndex] = {range, timeserieses};
+      }
+
+      for (const [fetchIndex, fetchDescriptor] of cp.enumerate(
+          fetchDescriptors)) {
+        function receive(rangeIndex, timeseries) {
+          timeseriesesByRange[rangeIndex].timeserieses[fetchIndex] = timeseries;
+        }
+
+        fetchDescriptor.minRevision = minRevision;
+        fetchDescriptor.maxRevision = maxRevision;
+        batches.add(wrapTimeseriesReader(
+            batches, fetchDescriptor, revisionRanges, receive));
+      }
+    }
+
+    // Use BatchIterator only to batch result *events*, not the results
+    // themselves. Manually collate results above to keep track of which line
+    // and request go with each timeseries.
+
+    for await (const {results, errors} of batches) {
+      const filtered = filterTimeseriesesByLine(timeseriesesByLine);
+      yield {timeseriesesByLine: filtered, errors};
+    }
+  }
+
+  DetailsTable.actions = {
+    load: statePath => async(dispatch, getState) => {
+      let state = Polymer.Path.get(getState(), statePath);
+      if (!state) return;
+
+      const started = performance.now();
+      dispatch({
+        type: DetailsTable.reducers.startLoading.name,
+        statePath,
+        started,
+      });
+
+      const generator = generateTimeseries(
+          state.lineDescriptors,
+          state.minRevision, state.maxRevision,
+          state.revisionRanges);
+      for await (const {timeseriesesByLine, errors} of generator) {
+        state = Polymer.Path.get(getState(), statePath);
+        if (!state || state.started !== started) return;
+
+        dispatch({
+          type: DetailsTable.reducers.receiveData.name,
+          statePath,
+          timeseriesesByLine,
+        });
+      }
+
+      dispatch({type: DetailsTable.reducers.doneLoading.name, statePath});
+    },
+  };
+
+  DetailsTable.reducers = {
+    startLoading: (state, {started}, rootState) => {
+      return {
+        ...state,
+        isLoading: true,
+        started,
+        columns: [],
+        commonLinkRows: [],
+        bodies: [],
+      };
+    },
+
+    receiveData: (state, {timeseriesesByLine}, rootState) => {
+      const columns = [];
+      const commonLinkRows = [];
+      const bodies = [];
+
+      console.log(timeseriesesByLine);
+
+      /*
+      const cells = state.lines.map(line => state.revisionRanges.map(r => []));
+
+      for (const [rangeIndex, revisionRange] of cp.enumerate(
+          state.revisionRanges)) {
+        const range = tr.b.math.Range.fromExplicitRange(
+            revisionRange.minRevision, revisionRange.maxRevision);
+
+        let actualRange = new tr.b.math.Range();
+        for (const [lineIndex, line] of cp.enumerate(state.lines)) {
+          if (!range.intersectsExplicitRangeInclusive(
+              line.data[0].datum.revision,
+              line.data[line.data.length - 1].datum.revision)) {
+            break;
+          }
+
+          let startIndex = tr.b.findLowIndexInSortedArray(line.data, d =>
+            d.datum.revision, range.min);
+
+          while (startIndex > 0 && line.data[startIndex].datum.revision >=
+            range.min) {
+            --startIndex;
+          }
+
+          let endIndex = startIndex;
+          while (endIndex < line.data.length - 1 &&
+                 line.data[endIndex + 1].datum.revision < range.max) {
+            ++endIndex;
+          }
+
+          cells[lineIndex][rangeIndex] = line.data.slice(
+              startIndex, endIndex + 1);
+
+          actualRange.min = line.data[startIndex - 1].datum.revision + 1;
+          actualRange.max = line.data[endIndex].datum.revision;
+        }
+
+        if (actualRange.isEmpty) actualRange = range;
+        columns.push(`${actualRange.min}-${actualRange.max}`);
+      }
+
+      for (const line of state.lines) {
+        const descriptorParts = [
+          line.descriptor.suites.join('\n'),
+          line.descriptor.measurement,
+          line.descriptor.bots.join('\n'),
+          line.descriptor.cases.join('\n'),
+          line.descriptor.buildType,
+        ];
+        const scalarRows = [];
+        const linkRows = [];
+        bodies.push({
+          color: line.color,
+          descriptorParts,
+          scalarRows,
+          linkRows,
+        });
+      }
+      */
+
+      return {...state, columns, commonLinkRows, bodies};
+    },
+
+    doneLoading: (state, action, rootState) => {
+      return {...state, isLoading: false};
+    },
+  };
+
+  cp.ElementBase.register(DetailsTable);
+  return {DetailsTable};
+});
