aeed89cb28edf76cf924d9e655f416eaa6179e17
Rakib M. Hasan <rmhasan@google.com>
2019-04-03 11:31:57 -0700

    Adds optional expectations_files and tags fields to the JSON results
    These fields can be used to debug regressions due to invalid test
    expectations.
    
    Bug: chromium:698902
    Change-Id: I933cc3a70caecc17f5f50ae1d637528ee98cd9a9

--- third_party/typ/typ/json_results.py
+++ third_party/typ/typ/json_results.py
@@ -16,6 +16,11 @@ from collections import OrderedDict
 
 import json
 
+
+_show_only_in_top_level = set([
+    'build_number', 'builder_name', 'chromium_revision'])
+
+
 class ResultType(object):
     Pass = 'PASS'
     Failure = 'FAIL'
@@ -73,9 +78,13 @@ def make_full_results(metadata, seconds_since_epoch, all_test_names, results):
     full_results['path_delimiter'] = TEST_SEPARATOR
     full_results['seconds_since_epoch'] = seconds_since_epoch
 
-    for md in metadata:
-        key, val = md.split('=', 1)
-        full_results[key] = val
+    for k, v in metadata.items():
+        if k in _show_only_in_top_level:
+            full_results[k] = v
+            del metadata[k]
+
+    if metadata:
+        full_results['metadata'] = metadata
 
     passing_tests = _passing_test_names(results)
     skipped_tests = _skipped_test_names(results)
