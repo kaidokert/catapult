3d28bccc07f00317a3fe71deb85578cffb71e6e8
benshayden <benjhayden@chromium.org>
2019-04-09 22:01:44 -0700

    Migrate spa from Polymer 2 to Polymer 3
    No functional changes.
    
    Bug: chromium:950011
    Change-Id: I01ac3c3d2909f5a17c7265f9f4331e100e187655

--- dashboard/dashboard/spa/cp-toast.js
+++ dashboard/dashboard/spa/cp-toast.js
@@ -3,53 +3,55 @@
    found in the LICENSE file.
 */
 'use strict';
-(() => {
-  class CpToast extends Polymer.Element {
-    static get is() { return 'cp-toast'; }
 
-    static get template() {
-      return Polymer.html`
-        <style>
-          :host {
-            bottom: 0;
-            left: 0;
-            right: 0;
-            position: fixed;
-          }
-          :host(:not([opened])) {
-            visibility: hidden;
-          }
-        </style>
+import {html, PolymerElement} from '/@polymer/polymer/polymer-element.js';
+import {timeout} from './utils.js';
 
-        <iron-collapse opened="[[opened]]">
-          <slot></slot>
-        </iron-collapse>
-      `;
-    }
+export default class CpToast extends PolymerElement {
+  static get is() { return 'cp-toast'; }
 
-    /*
-     * Autocloses after `wait` ms if open() is not called again in the interim.
-     * Does not autoclose if `wait` is false.
-     * `wait` can also be a Promise.
-     */
-    async open(wait = 10000) {
-      this.opened = true;
-      if (!wait) return;
-      const start = this.openId_ = cp.simpleGUID();
-      if (typeof wait === 'number') wait = cp.timeout(wait);
-      await wait;
-      if (this.openId_ !== start) return;
-      this.opened = false;
-    }
+  static get template() {
+    return html`
+      <style>
+        :host {
+          bottom: 0;
+          left: 0;
+          right: 0;
+          position: fixed;
+        }
+        :host(:not([opened])) {
+          visibility: hidden;
+        }
+      </style>
+
+      <iron-collapse opened="[[opened]]">
+        <slot></slot>
+      </iron-collapse>
+    `;
   }
 
-  CpToast.properties = {
-    opened: {
-      type: Boolean,
-      value: false,
-      reflectToAttribute: true,
-    },
-  };
+  /*
+    * Autocloses after `wait` ms if open() is not called again in the interim.
+    * Does not autoclose if `wait` is false.
+    * `wait` can also be a Promise.
+    */
+  async open(wait = 10000) {
+    this.opened = true;
+    if (!wait) return;
+    const start = this.openId_ = tr.b.GUID.allocateSimple();
+    if (typeof wait === 'number') wait = timeout(wait);
+    await wait;
+    if (this.openId_ !== start) return;
+    this.opened = false;
+  }
+}
 
-  customElements.define(CpToast.is, CpToast);
-})();
+CpToast.properties = {
+  opened: {
+    type: Boolean,
+    value: false,
+    reflectToAttribute: true,
+  },
+};
+
+customElements.define(CpToast.is, CpToast);
