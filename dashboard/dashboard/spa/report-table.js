b89cd98155aed195d8fceb74f410438bfe9536bb
benshayden <benjhayden@chromium.org>
2019-04-22 14:59:48 -0700

    Finish migrating spa to es6 modules.
    No functional changes.
    
    This looks big, but it's just as mechanical as the previous migrations.
    
    Tests still need to use importModule for anything that depends on
    Polymer or anything else in dependencies.html. Migrating to Polymer 3.0
    will reduce the need for importModule.
    
    Bug: chromium:950011
    Change-Id: Ie771e8c32bc26ef25c397fb38a2b5c81696db7a7

--- dashboard/dashboard/spa/report-table.js
+++ dashboard/dashboard/spa/report-table.js
@@ -5,8 +5,16 @@
 'use strict';
 
 import './cp-toast.js';
+import ElementBase from './element-base.js';
+import {TOGGLE, UPDATE} from './simple-redux.js';
 
-export default class ReportTable extends cp.ElementBase {
+import {
+  buildProperties,
+  buildState,
+  measureElement,
+} from './utils.js';
+
+export default class ReportTable extends ElementBase {
   static get template() {
     return Polymer.html`
       <style>
@@ -262,7 +270,7 @@ export default class ReportTable extends cp.ElementBase {
   }
 
   async onToggleEditing_(event) {
-    await this.dispatch(Redux.TOGGLE(this.statePath + '.isEditing'));
+    await this.dispatch(TOGGLE(this.statePath + '.isEditing'));
   }
 
   async onOpenChart_(event) {
@@ -339,9 +347,9 @@ export default class ReportTable extends cp.ElementBase {
     }
     if (!tr) return;
     const td = tr.querySelector('scalar-span').parentNode;
-    const tdRect = await cp.measureElement(td);
-    const thisRect = await cp.measureElement(this);
-    await this.dispatch(Redux.UPDATE(this.statePath, {
+    const tdRect = await measureElement(td);
+    const thisRect = await measureElement(this);
+    await this.dispatch(UPDATE(this.statePath, {
       tooltip: {
         rows: event.model.row.actualDescriptors.map(descriptor => [
           descriptor.testSuite, descriptor.bot, descriptor.testCase]),
@@ -370,11 +378,11 @@ ReportTable.State = {
   tooltip: options => {return {};},
 };
 
-ReportTable.buildState = options => cp.buildState(
+ReportTable.buildState = options => buildState(
     ReportTable.State, options);
 
 ReportTable.properties = {
-  ...cp.buildProperties('state', ReportTable.State),
+  ...buildProperties('state', ReportTable.State),
   userEmail: {statePath: 'userEmail'},
 };
 
@@ -412,4 +420,4 @@ ReportTable.placeholderTable = name => {
   };
 };
 
-cp.ElementBase.register(ReportTable);
+ElementBase.register(ReportTable);
