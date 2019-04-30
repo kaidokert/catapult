4b1ba616edbb757a61963a9699b40697936cda4c
benshayden <benjhayden@chromium.org>
2019-04-30 13:26:41 -0700

    Migrate spa from wct to karma
    This allows generating much more useful and reliable coverage reports.
    
    -------|----------|----------|----------|----------|
    File   |  % Stmts | % Branch |  % Funcs |  % Lines |
    -------|----------|----------|----------|----------|
     spa/  |    81.96 |    70.72 |    85.32 |    83.77 |
    -------|----------|----------|----------|----------|
    
    I also ran `npm audit fix` to address 2 high and 14 moderate advisories.
    
    No functional changes.
    
    Bug: chromium:957377
    Change-Id: I9feb853df14e50534ea25140d2e33c6dd11436b4

--- dashboard/dashboard/spa/chromeperf-app.js
+++ dashboard/dashboard/spa/chromeperf-app.js
@@ -510,7 +510,8 @@ ChromeperfApp.actions = {
 
     await timeout(NOTIFICATION_MS);
     const state = get(getState(), statePath);
-    if (!state.closedAlertsIds.includes(sectionId)) {
+    if (!state || !state.closedAlertsIds ||
+        !state.closedAlertsIds.includes(sectionId)) {
       // This alerts section was reopened.
       return;
     }
@@ -644,7 +645,7 @@ ChromeperfApp.actions = {
   updateLocation: statePath => async(dispatch, getState) => {
     const rootState = getState();
     const state = get(rootState, statePath);
-    if (!state.readied) return;
+    if (!state || !state.readied) return;
     const nonEmptyAlerts = state.alertsSectionIds.filter(id =>
       !AlertsSection.isEmpty(state.alertsSectionsById[id]));
     const nonEmptyCharts = state.chartSectionIds.filter(id =>
@@ -726,7 +727,8 @@ ChromeperfApp.actions = {
 
     await timeout(NOTIFICATION_MS);
     const state = get(getState(), statePath);
-    if (!state.closedChartIds.includes(sectionId)) {
+    if (!state || !state.closedChartIds ||
+        !state.closedChartIds.includes(sectionId)) {
       // This chart was reopened.
       return;
     }
