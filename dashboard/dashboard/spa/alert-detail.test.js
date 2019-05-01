b2decac86267075e0a0be898ce1b0543edd9f00f
benshayden <benjhayden@chromium.org>
2019-05-01 10:29:27 -0700

    Add alert-detail for spa
    TODO
    Bisect button
    wire up New Bug
    wire up Untriage
    wire up Existing Bug
    wire up Ignore
    
    This component is displayed in details-table below charts.
    It displays details of individual alerts and allows users to modify them.
    
    Demo: https://dev-benjhayden-1553924-dot-chromeperf.appspot.com#bug=934913
    
    Bug: chromium:865515, chromium:893189
    Change-Id: I1d763a9cfb6f7540a5a49144fd492b50e88e9378

---
+++ dashboard/dashboard/spa/alert-detail.test.js
@@ -0,0 +1,26 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+import AlertDetail from './alert-detail.js';
+import {UPDATE} from './simple-redux.js';
+import {afterRender, animationFrame} from './utils.js';
+import {assert} from 'chai';
+
+suite('alert-detail', function() {
+  async function fixture() {
+    const ad = document.createElement('alert-detail');
+    ad.statePath = 'test';
+    await ad.dispatch(UPDATE('test', {}));
+    document.body.appendChild(ad);
+    await afterRender();
+    return ad;
+  }
+
+  test('', async function() {
+    const ad = await fixture();
+    this.timeout(1e6); await cp.timeout(9e5);
+  });
+});
