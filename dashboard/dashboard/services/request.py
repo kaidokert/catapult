ac18cbb2ff0088712cf911f25a13d0347866cecd
Dean Michael Berris <dberris@chromium.org>
2019-05-01 12:22:57 +1000

    SheriffRedux: Wire up HTTP Tests
    This CL adds the wiring for the validation and service metadata
    implementation of the sheriff configuration service. We add some
    rudimentary service testing using the flask testing recommendations and
    features for ensuring our validation implementation handles the cases we
    care about, for the subscription definition.
    
    Bug: chromium:953181
    Change-Id: I54faa40e8d3cc5ddf0553526ea2c8dc6ce0ee833

--- dashboard/dashboard/services/request.py
+++ dashboard/dashboard/services/request.py
@@ -2,6 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Support python3
+from __future__ import absolute_import
+
 import httplib
 import httplib2
 import json
