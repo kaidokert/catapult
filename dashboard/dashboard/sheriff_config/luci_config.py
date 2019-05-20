92c4c4728dfab2dc964729763530526b16e28755
Dean Michael Berris <dberris@chromium.org>
2019-05-20 19:22:12 +1000

    SheriffRedux: Implement a luci-config client
    This uses python37 environment-safe utilities for making requests to the
    luci-config service, for sheriff configuration files. This change does
    the following as well:
    
    - Define a cron job which will regularly poll the luci-config service.
    
    - Store the configuration data by indexing the individual sheriff
      configuration instances to the project config and revision.
    
    - Exposes an API for finding all the sheriff configurations which match
      a given timeseries identifier.
    
    We're isolating the service which matches the sheriff configurations to
    the timeseries identifiers to allow us to consolidate the implementation
    of the logic which determines the sheriff configurations that match.
    
    R=abennetts@chromium.org, nodir@chromium.org, simonhatch@chromium.org
    
    Bug: chromium:953181
    Change-Id: I87da4679a489da0b34782a98f461954353b389cc

---
+++ dashboard/dashboard/sheriff_config/luci_config.py
@@ -0,0 +1,269 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""API Wrapper for the luci-config service."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from google.cloud import datastore
+from googleapiclient.discovery import build
+import base64
+import fnmatch
+import json
+import re
+import sheriff_pb2
+import validator
+
+# The Root of the luci-config API service.
+API_ROOT = 'https://luci-config.appspot.com/_ah/api'
+
+# The path which we will look for in projects.
+SHERIFF_CONFIG_PATH = 'chromeperf-sheriff.cfg'
+
+
+class Error(Exception):
+  """All errors associated with the luci_config module."""
+
+  def __init__(self, error):
+    self.error = error
+    super(Error, self).__init__()
+
+
+class FetchError(Error):
+
+  def __str__(self):
+    return 'Failed fetching project configs: %s' % (self.error)
+
+
+class ConfigError(Error):
+
+  def __str__(self):
+    return 'Failed discovering supported methods: %s' % (self.error)
+
+
+class InvalidConfigError(Error):
+
+  def __init__(self, config, fields):
+    super(InvalidConfigError, self).__init__(None)
+    self.fields = fields
+    self.config = config
+
+  def __str__(self):
+    return 'Config (%r) missing required fields: %r' % (self.config,
+                                                        self.fields)
+
+
+class InvalidConfigFieldError(Error):
+
+  def __init__(self, error, config):
+    super(InvalidConfigFieldError, self).__init__(error)
+    self.config = config
+
+  def __str__(self):
+    return 'Config (%r) content decoding error: %s' % (self.config, self.error)
+
+
+class PutFailure(Error):
+
+  def __str__(self):
+    return 'Failed call to put: %s' % (self.error)
+
+
+class GetFailure(Error):
+
+  def __str__(self):
+    return 'Failed call to get: %s' % (self.error)
+
+
+class NoMatchingConfigs(Error):
+
+  def __init__(self, request):
+    super(NoMatchingConfigs, self).__init__(None)
+    self.request(request)
+
+  def __str__(self):
+    return 'No matching configs for request: < %s >' % (self.request)
+
+
+def CreateConfigClient(http, api_root=API_ROOT, api='config', version='v1'):
+  """Factory function for a creating a config client.
+
+  This uses the discovery API to generate a service object corresponding to the
+  API we're using from luci-config.
+
+  Args:
+    http: a fully configured HTTP client/request
+    api_root: the URL through which the API will be configured (default:
+      API_ROOT)
+    api: the service name (default: 'config')
+    version: the version of the API (default: 'v1')
+  """
+  discovery_url = '%s/discovery/v1/apis/%s/%s/rest' % (api_root, api, version)
+  try:
+    service = build(api, version, discoveryServiceUrl=discovery_url, http=http)
+  except Exception as e:
+    raise ConfigError(e)
+  return service
+
+
+def FindAllSheriffConfigs(service):
+  """Finds all the project configs for chromeperf sheriffs."""
+  try:
+    configs = service.get_project_configs(path=SHERIFF_CONFIG_PATH).execute()
+  except Exception as e:
+    raise FetchError(e)
+  return configs
+
+
+def StoreConfigs(client, configs):
+  """Takes configuration dictionaries and persists them in the datastore.
+
+  Args:
+    client: The client for accessing the storage API.
+    configs: The config objects we'd like to store.
+
+  Raises:
+    InvalidConfigError: raised when config has missing required fields.
+    InvalidConfigFieldError: raised when the content is not a decodable base64
+      encoded JSON document.
+
+  Returns:
+    None
+  """
+  if not configs or len(configs) == 0:
+    raise ValueError('Configs must not be empty nor None.')
+
+  required_fields = set(
+      ('config_set', 'content', 'revision', 'url', 'content_hash'))
+
+  # We group the Subscription instances along a SubscriptionIndex entity. A
+  # SubscriptionIndex will always have the latest version of the subscription
+  # configurations, for easy lookup, as a list. We will only update the
+  # SubscriptionIndex if there were any new revisions in the configuration sets
+  # that we've gotten.
+  subscription_index_key = client.key('SubscriptionIndex', 'global')
+
+  def Transform(config):
+    missing_fields = required_fields.difference(set(config.keys()))
+    if len(missing_fields):
+      raise InvalidConfigError(config, missing_fields)
+
+    # We use a combination of the config set, revision, and content_hash to
+    # identify this particular configuration.
+    key = client.key(
+        'Subscription',
+        ':'.join(
+            (config['config_set'], config['revision'], config['content_hash'])),
+        parent=subscription_index_key)
+    try:
+      sheriff_config = validator.Validate(
+          base64.standard_b64decode(config['content']))
+    except json.JSONDecodeError as error:
+      raise InvalidConfigFieldError(config, error)
+
+    entity = datastore.Entity(
+        key=key, exclude_from_indexes=['sheriff_config', 'url'])
+    entity.update({
+        'config_set': config['config_set'],
+        'revision': config['revision'],
+        'content_hash': config['content_hash'],
+        'url': config['url'],
+        'sheriff_config': sheriff_config.SerializeToString()
+    })
+    return (key, entity)
+
+  entities = dict(Transform(config) for config in configs)
+  with client.transaction():
+    # First, lookup the keys for each of the entities to see whether all of them
+    # have already been stored. This allows us to avoid performing write
+    # operations that end up not doing any updates.
+    missing_entities = []
+    try:
+      if len(entities) > 1:
+        found_entities = client.get_multi(
+            entities.keys(), missing=missing_entities)
+      else:
+        for key in entities.keys():
+          found_entities = [client.get(key, missing=missing_entities)]
+    except ValueError as error:
+      raise GetFailure(error)
+
+    # Short circuit when we find no missing keys.
+    if len(missing_entities) == 0:
+      return None
+
+    # Next, we only put the entities that were missing from our earlier get.
+    try:
+      for found_entity in found_entities:
+        del entities[found_entity.key]
+
+      if len(entities) > 1:
+        client.put_multi(entities.values())
+      elif len(entities) == 1:
+        for entity in entities.values():
+          client.put(entity)
+      else:
+        raise ValueError('Failed to write data: %r' % (entities))
+    except ValueError as error:
+      raise PutFailure(error)
+
+    # From here we'll update an entity in the datastore which lists the grouped
+    # keys in the datastore. This will serve as an index on the latest set of
+    # configurations.
+    subscription_index = datastore.Entity(
+        key=subscription_index_key, exclude_from_indexes=['config_sets'])
+    subscription_index.update({
+        'config_sets': [
+            ':'.join((config['config_set'], config['revision'],
+                      config['content_hash'])) for config in configs
+        ]
+    })
+    client.put(subscription_index)
+
+
+def FindMatchingConfigs(client, request):
+  """Yield tuples of information for """
+  with client.transaction():
+    # First look up the global index.
+    subscription_index_key = client.key('SubscriptionIndex', 'global')
+    subscription_index = client.get(subscription_index_key)
+    if subscription_index is None:
+      raise NoMatchingConfigs(request)
+
+    # Then for each instance in the 'config_sets', create a key based on the
+    # subscription_index_key as a parent, and look those up in one go.
+    config_set_key_ids = subscription_index['config_sets']
+    if len(config_set_key_ids) > 1:
+      config_set_keys = [
+          client.key('Subscription', key, parent=subscription_index_key)
+          for key in config_set_key_ids
+      ]
+      config_sets = client.get_multi(config_set_keys)
+    elif len(config_set_key_ids) == 1:
+      config_sets = [
+          client.get(
+              client.key(
+                  'Subscription',
+                  config_set_key_ids[0],
+                  parent=subscription_index_key))
+      ]
+    else:
+      raise NoMatchingConfigs(request)
+
+  # From there we can then go through each of the subscriptions in the retrieved
+  # configs, and yield the ones that have patterns applying to the request.
+  for config in config_sets:
+    sheriff_config = sheriff_pb2.SheriffConfig()
+    sheriff_config.ParseFromString(config['sheriff_config'])
+    for subscription in sheriff_config.subscriptions:
+      for pattern in subscription.patterns:
+        if pattern.HasField('glob'):
+          matcher = re.compile(fnmatch.translate(pattern.glob))
+        elif pattern.HasField('regex'):
+          matcher = re.compile(pattern.regex)
+        result = matcher.match(request.path)
+        if result is not None:
+          yield (config['config_set'], config['revision'], subscription)
+          break
--- dashboard/dashboard/sheriff_config/tests/sheriff_config_test.py
+++ dashboard/dashboard/sheriff_config/tests/sheriff_config_test.py
@@ -10,29 +10,31 @@ in the service module.
 # Support python3
 from __future__ import absolute_import
 
-import unittest
+import base64
 import json
 import service
-
-# Set up the app under test as a global.
-APP = service.CreateApp({
-    'environ': {
-        'GAE_APPLICATION': 'chromeperf',
-        'GAE_SERVICE': 'sheriff-config'
-    }
-})
+import unittest
+from apiclient.http import HttpMockSequence
 
 
-class ServiceTest(unittest.TestCase):
+class ValidationTest(unittest.TestCase):
 
   def setUp(self):
-    self.client = APP.test_client()
+    with open('tests/config-discovery.json') as discovery_file:
+      self.app = service.CreateApp({
+          'environ': {
+              'GAE_APPLICATION': 'chromeperf',
+              'GAE_SERVICE': 'sheriff-config'
+          },
+          'http': HttpMockSequence([({
+              'status': '200'
+          }, discovery_file.read())])
+      })
+    self.client = self.app.test_client()
 
   def tearDown(self):
     self.client = None
-
-
-class ValidationTest(ServiceTest):
+    self.app = None
 
   def testServiceMetadata(self):
     # We want to ensure that we can get a valid service metadata description
@@ -55,11 +57,14 @@ class ValidationTest(ServiceTest):
     response = self.client.post(
         '/validate',
         json={
-            'config_set': 'project:some-project',
-            'path': 'infra/config/chromeperf-sheriff.cfg',
-            'content': json.dumps({'subscriptions': [{
-                'name': 'Name'
-            }]})
+            'config_set':
+                'project:some-project',
+            'path':
+                'infra/config/chromeperf-sheriff.cfg',
+            'content':
+                base64.standard_b64encode(
+                    bytearray('subscriptions: [{name: "something"}]',
+                              'utf-8')).decode('utf-8')
         })
 
     self.assertEquals(response.status_code, 200)
@@ -78,33 +83,27 @@ class ValidationTest(ServiceTest):
             'path':
                 'infra/config/chromeperf-sheriff.cfg',
             'content':
-                json.dumps({
-                    'subscriptions': [{
-                        'name': 'Release Team',
-                        'notification_email': 'release-team@example.com',
-                        'bug_labels': ['release-blocker'],
-                        'bug_components': ['Sample>Component'],
-                        'patterns': [{
-                            'glob': 'project/**'
-                        }]
-                    }, {
-                        'name':
-                            'Memory Team',
-                        'notification_email':
-                            'memory-team@example.com',
-                        'bug_labels': ['memory-regressions'],
-                        'patterns': [{
-                            'regex': '^project/.*memory_.*$'
-                        }],
-                        'anomaly_configs': [{
-                            'min_relative_change':
-                                0.01,
-                            'patterns': [{
-                                'regex': '^project/platform/.*/memory_peak$'
+                base64.standard_b64encode(
+                    bytearray(
+                        """
+                        subscriptions: [{
+                            name: 'Release Team',
+                            notification_email: 'release-team@example.com',
+                            bug_labels: ['release-blocker'],
+                            bug_components: ['Sample>Component'],
+                            patterns: [{ glob: 'project/**' }]
+                        }, {
+                            name: 'Memory Team',
+                            notification_email: 'memory-team@example.com',
+                            bug_labels: ['memory-regressions'],
+                            patterns: [{ regex: '^project/.*memory_.*$' }],
+                            anomaly_configs: [{
+                                min_relative_change: 0.01,
+                                patterns: [{
+                                    regex: '^project/platform/.*/memory_peak$'
+                                }]
                             }]
-                        }]
-                    }]
-                })
+                        }]""", 'utf-8')).decode('utf-8')
         })
     self.assertEquals(response.status_code, 200)
     response_proto = response.get_json()
