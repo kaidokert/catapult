5e436057aabc323208d7dd397742eb2a354bc53b
Rakib M. Hasan <rmhasan@google.com>
2019-06-03 15:23:48 -0700

    Reland "[Typ] Use a file to store sub process output"
    In crrev.com/c/1611755, we duplicate the original stream's file descriptor and then
    open a new file object using the duplicated file descriptor. We set the file mode to
    'a' for append. However this fails for Chrome OS autotest telemetry tests because they
    monkey patch sys.stdout and sys.stderr with streams where the underlying file descriptors
    were open as read only.
    
    Bug: chromium:960833
    Bug: chromium:960606
    Bug: chromium:952105
    Bug: chromium:967922
    Change-Id: If525b64867ab240e80196ef0c85543fff83224e2

--- telemetry/telemetry/testing/run_chromeos_tests.py
+++ telemetry/telemetry/testing/run_chromeos_tests.py
@@ -2,9 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 import logging
+import sys
 
 from telemetry.testing import run_tests
-
 
 def RunChromeOSTests(browser_type, tests_to_run):
   """ Run ChromeOS tests.
@@ -14,31 +14,34 @@ def RunChromeOSTests(browser_type, tests_to_run):
       |top_level_dir| specifies the top level directory for running tests, and
       |unit_tests| is a list of string test names to run.
   """
-  stream = _LoggingOutputStream()
+  std_out, std_err = (
+      _LoggingOutputStream(sys.stdout), _LoggingOutputStream(sys.stderr))
   error_string = ''
 
   for (top_level_dir, unit_tests) in tests_to_run:
     logging.info('Running unit tests in %s with browser_type "%s".' %
                  (top_level_dir, browser_type))
 
-    ret = _RunOneSetOfTests(browser_type, top_level_dir, unit_tests, stream)
+    ret = _RunOneSetOfTests(
+        browser_type, top_level_dir, unit_tests, std_out, std_err)
     if ret:
       error_string += 'The unit tests of %s failed.\n' % top_level_dir
   return error_string
 
 
-def _RunOneSetOfTests(browser_type, top_level_dir, tests, stream):
+def _RunOneSetOfTests(browser_type, top_level_dir, tests, std_out, std_err):
   args = ['--browser', browser_type,
           '--top-level-dir', top_level_dir,
           '--jobs', '1',
           '--disable-logging-config'] + tests
-  return run_tests.RunTestsCommand.main(args, stream=stream)
+  return run_tests.RunTestsCommand.main(args, std_out=std_out, std_err=std_err)
 
 
 class _LoggingOutputStream(object):
 
-  def __init__(self):
+  def __init__(self, stream):
     self._buffer = []
+    self._stream = stream
 
   def write(self, s):
     """Buffer a string write. Log it when we encounter a newline."""
@@ -58,3 +61,11 @@ class _LoggingOutputStream(object):
 
   def flush(self):
     pass
+
+  def fileno(self):
+    return self._stream.fileno()
+
+  @property
+  def mode(self):
+    return 'r'
+
