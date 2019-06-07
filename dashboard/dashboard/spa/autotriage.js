2856fe3320102e9bdcc8a64a011ed02ff84f343a
benshayden <benjhayden@chromium.org>
2019-06-07 11:12:34 -0700

    Autotriage alerts in spa.
    Change-Id: I79fc0a769f9f9fbe574804bb2467ed374df0b7d4

---
+++ dashboard/dashboard/spa/autotriage.js
@@ -0,0 +1,72 @@
+/* Copyright 2019 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+import {html} from 'lit-element';
+import {plural} from './utils.js';
+import ExistingBugRequest from './existing-bug-request.js';
+
+function analyzeTriaged(triagedAlerts) {
+  const alertCountByBugId = new Map();
+  for (const alert of triagedAlerts) {
+    alertCountByBugId.set(alert.bugId,
+        1 + (alertCountByBugId.get(alert.bugId) || 0));
+  }
+  let bugId = 0;
+  let maxAlertCount = 0;
+  for (const [triagedBugId, alertCount] of alertCountByBugId) {
+    if (alertCount > maxAlertCount) {
+      maxAlertCount = alertCount;
+      bugId = triagedBugId;
+    }
+  }
+
+  return {bugId, maxAlertCount};
+}
+
+function analyzeAlerts(alerts) {
+  let maxPctDelta = 0;
+  for (const alert of alerts) {
+    maxPctDelta = Math.max(maxPctDelta, Math.abs(alert.percentDeltaValue));
+  }
+  return {maxPctDelta};
+}
+
+export const MIN_PCT_DELTA = 0.02;
+
+const PCT_UNIT = tr.b.Unit.byName.normalizedPercentage;
+
+export function autotriage(alerts, triagedAlerts) {
+  const {maxPctDelta} = analyzeAlerts(alerts);
+  const {bugId, maxAlertCount} = analyzeTriaged(triagedAlerts);
+
+  if (maxPctDelta < MIN_PCT_DELTA) {
+    const explanation = html`
+      The largest regression is smaller than ${PCT_UNIT.format(MIN_PCT_DELTA)}.
+    `;
+    return {bugId: ExistingBugRequest.IGNORE_BUG_ID, explanation};
+  }
+
+  if (bugId < 0) {
+    const explanation = html`
+      ${maxAlertCount} similar alert${plural(maxAlertCount)} were ignored.
+    `;
+    return {bugId, explanation};
+  }
+
+  if (bugId) {
+    const explanation = html`
+      ${maxAlertCount} similar alert${plural(maxAlertCount)} were assigned to
+      ${bugId}.
+    `;
+    return {bugId, explanation};
+  }
+
+  const explanation = html`
+    A ${PCT_UNIT.format(maxPctDelta)} regression is significant with no similar
+    triaged alerts.
+  `;
+  return {bugId, explanation};
+}
