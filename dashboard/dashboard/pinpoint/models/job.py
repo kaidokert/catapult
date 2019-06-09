fc19b81089a9b740db3fb2f2df3a9fedc0645225
Oleg Tikhonov <tikhonov@google.com>
2019-06-09 09:57:04 +0000

    Assign bugs to authors of biggest delta.
    If multiple CLs were found, assign regression bug to the author of a change
    which introduced the largest signed difference.
    
    Change-Id: Ib185f0b34926b8c920653e58291ac8cb9ba1a02b

--- dashboard/dashboard/pinpoint/models/job.py
+++ dashboard/dashboard/pinpoint/models/job.py
@@ -261,6 +261,7 @@ class Job(ndb.Model):
 
     difference_details = []
     commit_infos = []
+    authors_deltas = dict()
     for change_a, change_b in differences:
       if change_b.patch:
         commit_info = change_b.patch.AsDict()
@@ -273,11 +274,17 @@ class Job(ndb.Model):
                                            self.state.metric)
       difference_details.append(difference)
       commit_infos.append(commit_info)
+      if values_a and values_b:
+        authors_deltas[commit_info['author']] = job_state.Mean(
+            values_b) - job_state.Mean(values_a)
+      else:
+        authors_deltas[commit_info['author']] = None
+
 
     deferred.defer(
         _UpdatePostAndMergeDeferred,
-        difference_details, commit_infos, self.bug_id, self.tags, self.url,
-        _retry_options=RETRY_OPTIONS)
+        difference_details, commit_infos, authors_deltas, self.bug_id,
+        self.tags, self.url, _retry_options=RETRY_OPTIONS)
 
   def _UpdateGerritIfNeeded(self):
     if self.gerrit_server and self.gerrit_change_id:
@@ -459,13 +466,15 @@ def _GenerateCommitCacheKey(commit_infos):
   return commit_cache_key
 
 
-def _ComputePostOwnerSheriffCCList(commit_infos):
+def _ComputePostOwnerSheriffCCList(commit_infos, authors_deltas):
   owner = None
   sheriff = None
   cc_list = set()
+
+  if authors_deltas:
+    owner = max(authors_deltas, authors_deltas.get)
+
   for cur_commit in commit_infos:
-    # TODO: Assign the largest difference, not the last one.
-    owner = cur_commit['author']
     sheriff = utils.GetSheriffForAutorollCommit(owner, cur_commit['message'])
     cc_list.add(cur_commit['author'])
     if sheriff:
@@ -474,14 +483,15 @@ def _ComputePostOwnerSheriffCCList(commit_infos):
 
 
 def _UpdatePostAndMergeDeferred(
-    difference_details, commit_infos, bug_id, tags, url):
+    difference_details, commit_infos, authors_deltas, bug_id, tags, url):
   if not bug_id:
     return
 
   commit_cache_key = _GenerateCommitCacheKey(commit_infos)
 
   # Bring it all together.
-  owner, sheriff, cc_list = _ComputePostOwnerSheriffCCList(commit_infos)
+  owner, sheriff, cc_list = _ComputePostOwnerSheriffCCList(commit_infos,
+                                                           authors_deltas)
   comment = _FormatComment(difference_details, commit_infos, sheriff, tags, url)
 
   issue_tracker = issue_tracker_service.IssueTrackerService(
