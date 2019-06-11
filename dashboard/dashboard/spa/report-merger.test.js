7db042fa2c8646f6c12372e5fce14c4c3c0b9c3b
benshayden <benjhayden@chromium.org>
2019-06-11 15:10:32 -0700

    Generate reports in the spa frontend.
    TODO:
    placeholder
    display revision ranges in col headers
    descriptors tooltip
    non-cr-commit-pos revisions
    tests
    
    It is surprisingly faster to fetch and process more data in the client
    than to process less data in the server.
    
    Bug: chromium:971655
    
    Change-Id: Idfb86fe48a6f72756ad2bdf6496da21bbcaad6bf

---
+++ dashboard/dashboard/spa/report-merger.test.js
@@ -0,0 +1,13 @@
+/* Copyright 2019 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+import {assert} from 'chai';
+import {ReportMerger} from './report-merger.js';
+
+suite('ReportMerger', function() {
+  test('merge', function() {
+  });
+});
