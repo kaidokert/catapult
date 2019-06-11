9a3e5b4d59b3f0992c7984188e5cdb6f3939bcf6
Mikhail Khokhlov <khokhlov@google.com>
2019-06-11 16:34:47 +0100

    Make StoryRun manage its own artifacts
    Remove ArtifactsResult class that was responsible for keeping track
    of all the artifacts. Make each StoryRun keep track of its artifacts
    instead.
    
    Bug: chromium:972025
    Change-Id: Ieb5072fc29ee804a9034e3ae46fa2d7d75269c81

--- telemetry/telemetry/internal/results/json_3_output_formatter.py
+++ telemetry/telemetry/internal/results/json_3_output_formatter.py
@@ -76,9 +76,8 @@ def ResultsAsDict(page_test_results):
     else:
       test['times'].append(run.duration)
 
-    story_artifacts = page_test_results.GetTestArtifacts(run.story.name)
-    if story_artifacts:
-      test['artifacts'] = dict(story_artifacts)
+    for name, path in run.IterateArtifacts():
+      test.setdefault('artifacts', {}).setdefault(name, []).append(path)
 
     # Shard index is really only useful for failed tests. See crbug.com/960951
     # for details.
