79aea3f258731d93d9d77062a8f38ad9fd3f3240
Brian Sheedy <bsheedy@chromium.org>
2019-06-24 10:47:35 -0700

    [typ] Add artifact implementation to typ
    Adds an artifact implementation to typ, which is exposed to any tests that
    inherit from typ's TestCase class. Any artifacts that a test adds will be
    automatically included in the JSON test results that typ generates, along
    with the suite-wide artifact:mime type map.
    
    Bug: chromium:968276
    Change-Id: I98823d33bc6eaf3df81126d85e49c1177f19c410

--- third_party/typ/typ/tests/json_results_test.py
+++ third_party/typ/typ/tests/json_results_test.py
@@ -14,6 +14,7 @@
 
 import unittest
 
+from typ import artifact_results
 from typ import json_results
 
 
@@ -62,6 +63,19 @@ class TestMakeUploadRequest(unittest.TestCase):
 
 class TestMakeFullResults(unittest.TestCase):
     maxDiff = 2048
+
+    class FakeArtifactResults(object):
+        def __init__(self):
+            self.artifact_type_map = {}
+            self.artifact_map = {}
+        def HasArtifacts(self):
+            return len(self.artifact_map) > 0
+        def IterArtifacts(self):
+            for artifact_type, files in self.artifact_map.iteritems():
+                for f in files:
+                    yield artifact_type, f
+        def IterArtifactTypes(self):
+            return self.artifact_type_map.iteritems()
 
     def test_basic(self):
         test_names = ['foo_test.FooTest.test_fail',
@@ -118,3 +132,92 @@ class TestMakeFullResults(unittest.TestCase):
                         }}}},
             'version': 3}
         self.assertEqual(full_results, expected_full_results)
+
+    def test_artifacts_and_types_added(self):
+        ar = TestMakeFullResults.FakeArtifactResults()
+        ar.artifact_type_map = {'artifact_type': 'mime_type'}
+        ar.artifact_map = {'artifact_type': ['a/b/c.txt']}
+
+        test_names = [ 'foo_test.FooTest.foobar' ]
+
+        result_set = json_results.ResultSet()
+        result_set.add(json_results.Result(
+                'foo_test.FooTest.foobar', json_results.ResultType.Pass,
+                0, 0.2, 0, artifact_results=ar))
+
+        full_results = json_results.make_full_results(
+                {'foo': 'bar'}, 0, test_names, result_set)
+
+        self.assertIn('artifact_types', full_results)
+        self.assertEqual(
+                full_results['artifact_types'], {'artifact_type': 'mime_type'})
+        tests = full_results['tests']['foo_test']['FooTest']
+        self.assertIn('artifacts', tests['foobar'])
+        self.assertEqual(tests['foobar']['artifacts'],
+                         {'artifact_type': ['a/b/c.txt']})
+
+    def test_artifacts_and_types_merged(self):
+        test_names = [ 'foo_test.FooTest.foobar' ]
+        result_set = json_results.ResultSet()
+
+        ar = TestMakeFullResults.FakeArtifactResults()
+        ar.artifact_type_map = {'artifact_type': 'mime_type'}
+        ar.artifact_map = {'artifact_type': ['a/b/c.txt']}
+        result_set.add(json_results.Result(
+                'foo_test.FooTest.foobar', json_results.ResultType.Failure,
+                0, 0.2, 0, artifact_results=ar))
+
+        ar2 = TestMakeFullResults.FakeArtifactResults()
+        ar2.artifact_type_map = {'artifact_type': 'mime_type'}
+        ar2.artifact_map = {'artifact_type': ['d/e/f.txt']}
+        result_set.add(json_results.Result(
+                'foo_test.FooTest.foobar', json_results.ResultType.Failure,
+                0, 0.2, 0, artifact_results=ar2))
+
+        ar3 = TestMakeFullResults.FakeArtifactResults()
+        ar3.artifact_type_map = {'another_type': 'mime_type'}
+        ar3.artifact_map = {'another_type': ['d/e/f.txt']}
+        result_set.add(json_results.Result(
+                'foo_test.FooTest.foobar', json_results.ResultType.Pass,
+                0, 0.2, 0, artifact_results=ar3))
+
+        full_results = json_results.make_full_results(
+                {'foo': 'bar'}, 0, test_names, result_set)
+
+        self.assertIn('artifact_types', full_results)
+        self.assertEqual(
+                full_results['artifact_types'],
+                {'artifact_type': 'mime_type', 'another_type': 'mime_type'})
+        tests = full_results['tests']['foo_test']['FooTest']
+        self.assertIn('artifacts', tests['foobar'])
+        # Order is not guaranteed for the list of paths, so don't use
+        # assertEqual if there are multiple entries
+        artifacts = tests['foobar']['artifacts']
+        self.assertIn('artifact_type', artifacts)
+        self.assertEqual(len(artifacts['artifact_type']), 2)
+        self.assertIn('a/b/c.txt', artifacts['artifact_type'])
+        self.assertIn('d/e/f.txt', artifacts['artifact_type'])
+        self.assertIn('another_type', artifacts)
+        self.assertEqual(artifacts['another_type'], ['d/e/f.txt'])
+
+    def test_artifact_type_conflict(self):
+        test_names = [ 'foo_test.FooTest.foobar' ]
+        result_set = json_results.ResultSet()
+
+        ar = TestMakeFullResults.FakeArtifactResults()
+        ar.artifact_type_map = {'artifact_type': 'mime_type'}
+        ar.artifact_map = {'artifact_type': ['a/b/c.txt']}
+        result_set.add(json_results.Result(
+                'foo_test.FooTest.foobar', json_results.ResultType.Failure,
+                0, 0.2, 0, artifact_results=ar))
+
+        ar2 = TestMakeFullResults.FakeArtifactResults()
+        ar2.artifact_type_map = {'artifact_type': 'another_mime'}
+        ar2.artifact_map = {'artifact_type': ['d/e/f.txt']}
+        result_set.add(json_results.Result(
+                'foo_test.FooTest.foobar', json_results.ResultType.Pass,
+                0, 0.2, 0, artifact_results=ar2))
+
+        with self.assertRaises(artifact_results.ArtifactTypeConflictError):
+            json_results.make_full_results(
+                {'foo': 'bar'}, 0, test_names, result_set)
