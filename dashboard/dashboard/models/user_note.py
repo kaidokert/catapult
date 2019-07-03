2b97fd9d42e81c43f98eba3cb18ce65b28453e6b
benshayden <benjhayden@chromium.org>
2019-07-03 13:57:20 -0700

    Support user notes in spa.
    This feature allows users to save markdown notes for ranges of groups of
    timeserieses.
    
    Demo: https://v2spa-dot-chromeperf.appspot.com/
    
    Bug: chromium:973575
    Change-Id: I6e40d089af813682c97db3307c5ea8e02fe04163

---
+++ dashboard/dashboard/models/user_note.py
@@ -0,0 +1,20 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+from google.appengine.ext import ndb
+
+class UserNote(ndb.Model):
+  author = ndb.StringProperty()
+  updated = ndb.DateTimeProperty(auto_now=True)
+  suite = ndb.StringProperty()
+  measurement = ndb.StringProperty()
+  bot = ndb.StringProperty()
+  case = ndb.StringProperty()
+  text = ndb.TextProperty()
+  min_revision = ndb.IntegerProperty()
+  max_revision = ndb.IntegerProperty()
