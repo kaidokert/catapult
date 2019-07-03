63db195ff4bb5cad0c756c0c3cdc39e6dc0bee92
Caleb Rouleau <crouleau@chromium.org>
2019-07-03 11:25:28 -0700

    [Telemetry] Allow story sets to specify a tag that filters them to a minimal state
    Bug: chromium:965158
    Change-Id: I744aa28905b179591a7589ba77ed9af54e3cb29a

--- telemetry/telemetry/story/story_set.py
+++ telemetry/telemetry/story/story_set.py
@@ -157,6 +157,17 @@ class StorySet(object):
     return self.wpr_archive_info.WprFilePathForStory(
         story, target_platform=target_platform)
 
+  def GetAbridgedStorySetTagFilter(self):
+    """Override this to method to shorten your story set.
+
+    Returns a story tag string that marks the stories that are
+    part of the abridged story set.
+
+    Abridging your story set is useful for large benchmarks so that they can
+    be run quickly when needed.
+    """
+    return None
+
   def __iter__(self):
     return self.stories.__iter__()
 
