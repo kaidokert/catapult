602bcbfe6313787a42aa4bd3742d3b1ad9ecdc2f
Rakib M. Hasan <rmhasan@google.com>
2019-07-12 14:26:58 -0700

    Reland "Reland "[Telemetry] Integrate new test expectations format into telemetry benchmark runner""
    Reason: The previous CL's did not consider that the possible browser
    instance may be a None type before calling the GetTypExpectationsTag
    member function. This CL will add that check.
    
    Original change's description:
    > Reland "[Telemetry] Integrate new test expectations format into telemetry benchmark runner"
    >
    > Reason: The original CL renamed a function that was used in the telemetry_perf_unittests step.
    > This CL will revert the name change. The failing test log can be seen at the following link,
    > https://chromium-swarm.appspot.com/task?id=45fbd593d30af710
    >
    > Original change's description:
    > > [Telemetry] Integrate new test expectations format into telemetry benchmark runner
    > >
    > > We are attempting to standardize the test expectations format. Telemetry benchmark
    > > runner currently uses a format that is different than the standard format. This CL
    > > will make it support the new format as well as  the old format. After we make
    > > modifications to the expectations file for chrome performance benchmarks so that
    > > it uses the new format, we will remove support for the old format. Googlers can view
    > > the design document for this change at go/change-telemetry-benchmarks-expectations-format.
    > >
    > > Bug: chromium:973936
    > > Change-Id: Ic64239cd8a275c42f51742b6b45dc15e100865c1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1677410
    > > Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    > > Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    > > Commit-Queue: Rakib Hasan <rmhasan@google.com>
    >
    > This reverts commit 6a2b028122737961609ae84353eb47a1328b15bc.
    >
    > Bug: chromium:97396
    > Change-Id: I37a37d45774f343a85b29f433c2882f5ab998078
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1699065
    > Commit-Queue: Rakib Hasan <rmhasan@google.com>
    > Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    
    Bug: chromium:97396
    
    This reverts commit 4d94b5d9ba73b44bed1d79444b59fd0095375f69.
    
    Change-Id: Ib6223b001282bd74f75204d934034e1f9b7e8d7c

--- telemetry/telemetry/internal/story_runner.py
+++ telemetry/telemetry/internal/story_runner.py
@@ -427,6 +427,10 @@ def RunBenchmark(benchmark, finder_options):
   """
   benchmark.CustomizeOptions(finder_options)
   possible_browser = browser_finder.FindBrowser(finder_options)
+  if possible_browser:
+    benchmark.expectations.SetTags(
+        possible_browser.GetTypExpectationsTags())
+
   if not _ShouldRunBenchmark(benchmark, possible_browser, finder_options):
     return -1
 
