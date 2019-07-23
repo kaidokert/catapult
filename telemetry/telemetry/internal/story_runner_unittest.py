aecb9c1ef42286260fd299c8a4dffd8a06440a0c
Caleb Rouleau <crouleau@chromium.org>
2019-07-23 02:34:33 +0000

    Revert "[Telemetry] Upload detailed os version string as histogram diagnostic."
    This reverts commit 15ad0b0b623e650887828a303b93b4b67eed170a.
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=985789
    
    Original change's description:
    > [Telemetry] Upload detailed os version string as histogram diagnostic.
    > 
    > Currently we upload something basic like 'win10'. Instead
    > this changelist makes us upload something like '10.0.16299'.
    > For linux, we will still upload 'ubuntu' since the linux platform
    > backend does not support getting a detailed os version string.
    > 
    > Bug: chromium:980256
    > Change-Id: I5b68843033bfa8ae883cd195c4f5170b044d218d
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1684225
    > Auto-Submit: Caleb Rouleau <crouleau@chromium.org>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Reviewed-by: Ben Hayden <benjhayden@chromium.org>
    > Commit-Queue: John Chen <johnchen@chromium.org>
    > Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    
    TBR=benjhayden@chromium.org,crouleau@chromium.org,johnchen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:980256
    Change-Id: I3f75dcad1f1f8ec75f1360de6f97c98b1f0e44d7

--- telemetry/telemetry/internal/story_runner_unittest.py
+++ telemetry/telemetry/internal/story_runner_unittest.py
@@ -70,9 +70,6 @@ class FakePlatform(object):
   def GetOSVersionName(self):
     return 'win10'
 
-  def GetOSVersionDetailString(self):
-    return '10.0.16299'
-
   def GetSystemTotalPhysicalMemory(self):
     return 8 * (1024 ** 3)
 
@@ -724,7 +721,7 @@ class StoryRunnerTest(unittest.TestCase):
         list(diagnostic) for diagnostic in generic_diagnostics]
 
     self.assertGreater(len(generic_diagnostics), 2)
-    self.assertIn(['10.0.16299'], generic_diagnostics_values)
+    self.assertIn(['win10'], generic_diagnostics_values)
     self.assertIn(['win'], generic_diagnostics_values)
     self.assertIn(['amd64'], generic_diagnostics_values)
 
@@ -762,7 +759,7 @@ class StoryRunnerTest(unittest.TestCase):
         list(diagnostic) for diagnostic in generic_diagnostics]
 
     self.assertGreater(len(generic_diagnostics), 2)
-    self.assertIn(['10.0.16299'], generic_diagnostics_values)
+    self.assertIn(['win10'], generic_diagnostics_values)
     self.assertIn(['win'], generic_diagnostics_values)
     self.assertIn(['amd64'], generic_diagnostics_values)
 
@@ -794,7 +791,7 @@ class StoryRunnerTest(unittest.TestCase):
         list(diagnostic) for diagnostic in generic_diagnostics]
 
     self.assertGreater(len(generic_diagnostics), 2)
-    self.assertIn(['10.0.16299'], generic_diagnostics_values)
+    self.assertIn(['win10'], generic_diagnostics_values)
     self.assertIn(['win'], generic_diagnostics_values)
     self.assertIn(['amd64'], generic_diagnostics_values)
 
