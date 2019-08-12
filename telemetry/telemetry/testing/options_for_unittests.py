126380992f16687faf1f007b0927051e9c621728
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-08-12 10:39:14 +0100

    [Telemetry] Remove result options defining/processing in Telemetry
    These are now handled by the external results processor in tools/perf.
    
    Bug: chromium:928275
    Change-Id: Id59eb01e2cb809573c22e1b5765b0416bea47789

--- telemetry/telemetry/testing/options_for_unittests.py
+++ telemetry/telemetry/testing/options_for_unittests.py
@@ -78,9 +78,13 @@ def GetRunOptions(output_dir=None, fake_browser=False, benchmark_cls=None,
   if benchmark_cls is not None:
     benchmark_cls.ProcessCommandLineArgs(parser, options)
   story_runner.ProcessCommandLineArgs(parser, options)
-  options.output_dir = output_dir
-  options.output_formats = ['none']
   options.suppress_gtest_report = True
-  options.upload_bucket = None
+  options.output_dir = output_dir
+  # TODO(crbug.com/928275): Remove these when Telemetry tests no longer
+  # depend on any result processing options.
+  options.output_formats = ['none']
+  options.reset_results = True
+  options.results_label = 'test_run'
   options.upload_results = False
+  options.upload_bucket = None
   return options
