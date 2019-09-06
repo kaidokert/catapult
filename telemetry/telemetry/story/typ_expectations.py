e7a3e15d5ee9f68d3a06d47ce9456face94c0295
Rakib M. Hasan <rmhasan@google.com>
2019-09-06 10:19:06 -0700

    [Telemetry] Add system condition tags frozen set to typ expectations module
    This set can be used to keep track of the tags added to the tag declarartion
    lists of tools/perf/expectations.config.
    
    Bug: chromium:999335
    Change-Id: I979cfbebf3f4f1d99617a67e6245156e5e6541ed

--- telemetry/telemetry/story/typ_expectations.py
+++ telemetry/telemetry/story/typ_expectations.py
@@ -19,6 +19,14 @@ from typ import json_results
 ResultType = json_results.ResultType
 
 
+SYSTEM_CONDITION_TAGS = frozenset([
+    'android', 'android-go', 'android-low-end', 'android-nexus-5',
+    'android-nexus-5x', 'android-nexus-6', 'android-pixel-2',
+    'chromeos', 'desktop', 'linux', 'mac', 'mac-10.12', 'win',
+    'win10', 'win7', 'android-not-webview', 'android-webview'
+])
+
+
 class StoryExpectations(object):
 
   def __init__(self, benchmark_name):
