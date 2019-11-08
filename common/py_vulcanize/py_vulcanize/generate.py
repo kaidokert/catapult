07654e1c4c78719344edaf7904430b098077051a
Deepanjan Roy <dproy@chromium.org>
2019-11-08 16:27:10 -0500

    Add origin trial token for vulcanizer
    Adds WebComponent V0 origin trial token to the vulcanizer for
    googleusercontent.com + subdomains, which is what we use to serve files
    from cloud storage. This should work for both uploaded results.html and
    html trace files.
    
    Bug: chromium:1021137
    Change-Id: I861207e377684ea82135f5c2eb42a1785620fee4

--- common/py_vulcanize/py_vulcanize/generate.py
+++ common/py_vulcanize/py_vulcanize/generate.py
@@ -52,6 +52,13 @@ css_warning_message = """
  */
 """
 
+# Origin trial tokens for WebComponents V0 deprecation.
+origin_trial_tokens = [
+  # Token for googleusercontent.com + subdomains. This is the domain traces
+  # in cloud storage are served from. Expires Nov 5, 2020.
+  "AnYuQDtUf6OrWCmR9Okd67JhWVTbmnRedvPi1TEvAxac8+1p6o9q08FoDO6oCbLD0xEqev+SkZFiIhFSzlY9HgUAAABxeyJvcmlnaW4iOiJodHRwczovL2dvb2dsZXVzZXJjb250ZW50LmNvbTo0NDMiLCJmZWF0dXJlIjoiV2ViQ29tcG9uZW50c1YwIiwiZXhwaXJ5IjoxNjA0NjE0NTM4LCJpc1N1YmRvbWFpbiI6dHJ1ZX0="
+  # Add more tokens here if traces are served from other domains.
+]
 
 def _AssertIsUTF8(f):
   if isinstance(f, StringIO):
@@ -206,6 +213,11 @@ def GenerateStandaloneHTMLAsString(*args, **kwargs):
   GenerateStandaloneHTMLToFile(f, *args, **kwargs)
   return f.getvalue()
 
+def _WriteOriginTrialTokens(output_file):
+  for token in origin_trial_tokens:
+    output_file.write('  <meta http-equiv="origin-trial" content="')
+    output_file.write(token)
+    output_file.write('">')
 
 def GenerateStandaloneHTMLToFile(output_file,
                                  load_sequence,
@@ -231,6 +243,8 @@ def GenerateStandaloneHTMLToFile(output_file,
         '  <head i18n-values="dir:textdirection;">\n'
         '  <meta http-equiv="Content-Type" content="text/html;'
         'charset=utf-8">\n')
+    # See https://crbug.com/1021137
+    _WriteOriginTrialTokens(output_file)
     if title:
       output_file.write('  <title>%s</title>\n  ' % title)
   else:
