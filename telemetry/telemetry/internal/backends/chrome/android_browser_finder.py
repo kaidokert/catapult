73c3476314feddcad5195b5a5f5eab20818a0078
Mikhail Khokhlov <khokhlov@google.com>
2019-11-20 14:42:03 +0000

    [Telemetry] Use chromium_output_dir from external parser
    This CL introduces several changes w.r.t. how the chromium_output_dir
    option is processed.
    1) It expects the value of the option to come from an external command
    line parser (Results Processor).
    2) This option is passed to methods that rely on it explicitly, instead of
    via environment variable.
    2) It no longer tries to iterate through several possible output directory
    names when searching for browser executables/apks, but uses only the given
    directory. Note that this changes the behavior of e.g. "--browser list"
    command.
    
    Bug: chromium:1026296
    Change-Id: Id66a880b7f064200c38f0650d744dae481290826

--- telemetry/telemetry/internal/backends/chrome/android_browser_finder.py
+++ telemetry/telemetry/internal/backends/chrome/android_browser_finder.py
@@ -86,6 +86,7 @@ class PossibleAndroidBrowser(possible_browser.PossibleBrowser):
     self._local_apk = local_apk
     self._flag_changer = None
     self._modules_to_install = None
+    self._build_path = finder_options.chromium_output_dir
 
     if self._local_apk is None and finder_options.chrome_root is not None:
       self._local_apk = self._backend_settings.FindLocalApk(
@@ -241,7 +242,7 @@ class PossibleAndroidBrowser(possible_browser.PossibleBrowser):
     browser_backend = android_browser_backend.AndroidBrowserBackend(
         self._platform_backend, self._browser_options,
         self.browser_directory, self.profile_directory,
-        self._backend_settings)
+        self._backend_settings, self._build_path)
     try:
       return browser.Browser(
           browser_backend, self._platform_backend, startup_args=(),
