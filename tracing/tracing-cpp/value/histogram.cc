c12cb48d8a28ecefec08cea38450963a296135a6
Patrik Höglund <phoglund@google.com>
2019-11-28 14:17:28 +0100

    Prototype with protobufs and JSON conversion.
    I don't think this will work, because it outputs
    
    $ out/Default/histogram
    {"name":"name!","unit":"ms","binBoundaries":[{"binBoundary":17},{"binSpec":{"spec":[1,2,3]}}]}
    
    It should output
    {"name":"name!","unit":"ms","binBoundaries":[17, [1,2,3]]}
    
    I don't think I have sufficient control over the written JSON to make it follow the JSON spec.
    
    DO_NOT_SUBMIT=1
    
    Change-Id: Ic251e78c9ba23184195a796dd194f76e84a614c0
    Bug: chromium::11084

---
+++ tracing/tracing-cpp/value/histogram.cc
@@ -0,0 +1,34 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cmath>
+
+#include "third_party/protobuf/src/google/protobuf/util/json_util.h"
+#include "tracing/tracing-cpp/value/histogram.pb.h"
+
+using namespace catapult;
+
+int main(int argc, char** argv) {
+  Histogram histogram;
+  histogram.set_name("name!");
+  histogram.set_unit(ms);
+  BinBoundary* bin_boundary = histogram.add_bin_boundaries();
+  bin_boundary->set_bin_boundary(17);
+  bin_boundary = histogram.add_bin_boundaries();
+  BinBoundarySpec* spec = bin_boundary->mutable_bin_spec();
+  spec->add_spec(1);
+  spec->add_spec(2);
+  spec->add_spec(3);
+
+  std::string output;
+  google::protobuf::util::Status status =
+      google::protobuf::util::MessageToJsonString(histogram, &output);
+  if (!status.ok()) {
+    printf("Failed: %.*s", static_cast<int>(status.error_message().size()),
+           status.error_message().data());
+    return 1;
+  }
+  printf("%s\n", output.c_str());
+  return 0;
+}
