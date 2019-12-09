09c393bbfa8990779711f5cd18089b195d9cee80
Dean Michael Berris <dberris@chromium.org>
2019-12-09 16:56:26 +1100

    Execution-Engine: Fix the task update handler
    This change adds test coverage for the task update handler to simulate
    what Pinpoint would encounter when executing jobs under the execution
    engine.
    
    R=abennetts@google.com, fancl@chromium.org
    
    Bug: chromium:1031939
    Change-Id: I5851ad907c3647b3ac735bbd051b55fda047fc18

--- dashboard/dashboard/pinpoint/handlers/task_updates_test.py
+++ dashboard/dashboard/pinpoint/handlers/task_updates_test.py
@@ -8,27 +8,19 @@ from __future__ import absolute_import
 
 import base64
 import json
-import logging
-import sys
+import mock
 
-from dashboard.pinpoint import test
+from dashboard.pinpoint.models import job as job_module
+from dashboard.pinpoint.models.tasks import bisection_test_util
+from dashboard.pinpoint.handlers import task_updates
 
 
-class TaskUpdatesTest(test.TestCase):
+class TaskUpdateHandlerTest(bisection_test_util.BisectionTestBase):
 
-  def setUp(self):
-    # Intercept the logging messages, so that we can see them when we have test
-    # output in failures.
-    self.logger = logging.getLogger()
-    self.logger.level = logging.DEBUG
-    self.stream_handler = logging.StreamHandler(sys.stdout)
-    self.logger.addHandler(self.stream_handler)
-    self.addCleanup(self.logger.removeHandler, self.stream_handler)
-    super(TaskUpdatesTest, self).setUp()
-
-  def testPostWorks(self):
-    self.Post(
-        '/_ah/push-handlers/task-updates',
+  def testHandlerGoodCase(self):
+    job = job_module.Job.New((), (), use_execution_engine=True)
+    self.PopulateSimpleBisectionGraph(job)
+    task_updates.HandleTaskUpdate(
         json.dumps({
             'message': {
                 'attributes': {
@@ -41,7 +33,7 @@ class TaskUpdatesTest(test.TestCase):
                                 'some_id',
                             'userdata':
                                 json.dumps({
-                                    'job_id': 'cafef00d',
+                                    'job_id': job.job_id,
                                     'task': {
                                         'id': 1,
                                         'type': 'build',
@@ -49,29 +41,84 @@ class TaskUpdatesTest(test.TestCase):
                                 }),
                         }))
             }
-        }),
-        status=204)
+        }))
 
   def testPostInvalidData(self):
+    with self.assertRaisesRegexp(ValueError, 'Failed decoding `data`'):
+      task_updates.HandleTaskUpdate(
+          json.dumps({
+              'message': {
+                  'attributes': {
+                      'nothing': 'important'
+                  },
+                  'data': '{"not": "base64-encoded"}',
+              },
+          }))
+    with self.assertRaisesRegexp(ValueError, 'Failed JSON parsing `data`'):
+      task_updates.HandleTaskUpdate(
+          json.dumps({
+              'message': {
+                  'attributes': {
+                      'nothing': 'important'
+                  },
+                  'data': base64.urlsafe_b64encode('not json formatted'),
+              },
+          }))
+
+
+@mock.patch('dashboard.common.utils.ServiceAccountHttp', mock.MagicMock())
+@mock.patch('dashboard.services.buildbucket_service.Put')
+@mock.patch('dashboard.services.buildbucket_service.GetJobStatus')
+class ExecutionEngineTaskUpdatesTest(bisection_test_util.BisectionTestBase):
+
+  def testExecutionEngineJobUpdates(self, buildbucket_put,
+                                    buildbucket_getjobstatus):
+    buildbucket_put.return_value = {'build': {'id': '92384098123'}}
+    buildbucket_getjobstatus.return_value = {
+        'build': {
+            'status':
+                'COMPLETED',
+            'result':
+                'SUCCESS',
+            'result_details_json':
+                """
+            {
+              "properties": {
+                "got_revision_cp": "refs/heads/master@commit_0",
+                "isolate_server": "https://isolate.server",
+                "swarm_hashes_refs/heads/master(at)commit_0_without_patch":
+                  {"performance_telemetry_test": "1283497aaf223e0093"}
+              }
+            }
+            """
+        }
+    }
+
+    job = job_module.Job.New((), (), use_execution_engine=True)
+    self.PopulateSimpleBisectionGraph(job)
+    self.assertTrue(job.use_execution_engine)
+    job.Start()
     self.Post(
         '/_ah/push-handlers/task-updates',
         json.dumps({
             'message': {
                 'attributes': {
-                    'nothing': 'important'
+                    'nothing': 'important',
                 },
-                'data': '{"not": "base64-encoded"}',
-            },
-        }),
-        status=204)
-    self.Post(
-        '/_ah/push-handlers/task-updates',
-        json.dumps({
-            'message': {
-                'attributes': {
-                    'nothing': 'important'
-                },
-                'data': base64.urlsafe_b64encode('not json formatted'),
-            },
+                'data':
+                    base64.urlsafe_b64encode(
+                        json.dumps({
+                            'task_id':
+                                'some_task_id',
+                            'userdata':
+                                json.dumps({
+                                    'job_id': job.job_id,
+                                    'task': {
+                                        'type': 'build',
+                                        'id': 'find_isolate@commit_0'
+                                    }
+                                })
+                        }))
+            }
         }),
         status=204)
