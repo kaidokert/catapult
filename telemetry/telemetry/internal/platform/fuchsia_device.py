9fb03cfbabbdbf70a95827b11db0a352d5a927e1
Chong Gu <chonggu@google.com>
2019-12-10 14:02:28 -0800

    [Telemetry] Add Fuchsia device support.
    Add fuchsia_device, stores info for creating fuchsia_platform_backend.
    Download the Fuchsia sdk if not in Chromium repo.
    Use dev_finder in the sdk to search for Fuchsia Devices
    Moved command line helper functions in cros_interface.py to cmd_util.py
    
    Bug: chromium:1014642
    Change-Id: I72f55f0875842916f8925da62a49358771adc4ce

---
+++ telemetry/telemetry/internal/platform/fuchsia_device.py
@@ -0,0 +1,130 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A Fuchsia device instance"""
+
+import logging
+import os
+import platform
+import tarfile
+
+from telemetry.core import fuchsia_interface
+from telemetry.core import util
+from telemetry.internal.platform import device
+from telemetry.util import cmd_util
+
+_LIST_DEVICES_TIMEOUT_MILLISECS = 5000
+__SDK_SHA1 = '8894838554076535504'
+_SDK_ROOT_IN_CATAPULT = os.path.join(util.GetCatapultDir(), 'third_party',
+                                     'fuchsia-sdk', 'sdk')
+_SDK_ROOT_IN_CHROMIUM = os.path.join(util.GetCatapultDir(), '..',
+                                     'fuchsia-sdk', 'sdk')
+
+
+class FuchsiaDevice(device.Device):
+  def __init__(self, target_name, host, output_dir,
+               system_log_file, port):
+    super(FuchsiaDevice, self).__init__(
+        name='Fuchsia with host localhost',
+        guid='fuchsia:%s' % target_name)
+    self._target_name = target_name
+    self._output_dir = output_dir
+    self._system_log_file = system_log_file
+    self._host = host
+    self._port = port
+
+  @classmethod
+  def GetAllConnectedDevices(cls, blacklist):
+    return []
+
+  @property
+  def target_name(self):
+    return self._target_name
+
+  @property
+  def host(self):
+    return self._host
+
+  @property
+  def output_dir(self):
+    return self._output_dir
+
+  @property
+  def system_log_file(self):
+    return self._system_log_file
+
+  @property
+  def port(self):
+    return self._port
+
+
+def _GetFuchsiaSDK(tar_file):
+  os.makedirs(_SDK_ROOT_IN_CATAPULT)
+  gsutil_path = os.path.join(util.GetCatapultDir(), 'third_party', 'gsutil',
+                             'gsutil')
+  sdk_pkg = 'gs://fuchsia/sdk/core/linux-amd64/' + __SDK_SHA1
+  download_cmd = [gsutil_path, 'cp', sdk_pkg, tar_file]
+  cmd_util.RunCmd(download_cmd)
+  files_in_tar = ['tools/dev_finder', 'tools/symbolize']
+
+  with tarfile.open(tar_file, 'r') as tar:
+    for f in files_in_tar:
+      tar.extract(f, _SDK_ROOT_IN_CATAPULT)
+  os.remove(tar_file)
+
+
+def _FindFuchsiaDevice(is_emulator):
+  dev_finder_path = os.path.join(_SDK_ROOT_IN_CATAPULT, 'tools', 'dev_finder')
+  if is_emulator:
+    logging.warning('Fuchsia emulators not supported at this time.')
+    return None
+  finder_cmd = [dev_finder_path, 'list', '-full',
+                '-timeout', str(_LIST_DEVICES_TIMEOUT_MILLISECS)]
+  device_list, _ = cmd_util.GetAllCmdOutput(finder_cmd)
+  if not device_list:
+    logging.warning('No Fuchsia device found.')
+  return device_list
+
+
+def _DownloadFuchsiaSDKIfNecessary():
+  if os.path.exists(_SDK_ROOT_IN_CHROMIUM):
+    if not os.path.exists(_SDK_ROOT_IN_CATAPULT):
+      tar_file = os.path.join(_SDK_ROOT_IN_CATAPULT,
+                              'fuchsia-sdk-%s.tar' % __SDK_SHA1)
+      _GetFuchsiaSDK(tar_file)
+
+
+def FindAllAvailableDevices(options):
+  """Returns a list of available device types."""
+
+  # Will not find Fuchsia devices if Fuchsia browser is not specified
+  # This means that unless specifying browser=web-engine-shell, the user
+  # will not see web-engine-shell as an available browser.
+  if options.browser_type not in fuchsia_interface.FUCHSIA_BROWSERS:
+    return []
+
+  if platform.system() != 'Linux' or platform.machine() != 'x86_64':
+    logging.warning('Fuchsia in Telemetry only supports Linux x64 hosts.')
+    return []
+
+  # Download the Fuchsia sdk if it doesn't exist
+  # TODO(https://crbug.com/1031763): Figure out how to use the dependency
+  # manager
+  _DownloadFuchsiaSDKIfNecessary()
+
+  device_list = _FindFuchsiaDevice(False)
+  if not device_list:
+    return []
+  # Expected output will look something like
+  # 'host0 target0\nhost1 target1\nhost2 target2'
+  first_device = device_list.splitlines()[0]
+  host, target_name = first_device.split(' ')
+
+  logging.info('Using Fuchsia device with address %s and name %s'
+               % (host, target_name))
+  return [FuchsiaDevice(target_name=target_name,
+                        host=host,
+                        system_log_file=None,
+                        output_dir=options.fuchsia_output_dir,
+                        port=22)]
