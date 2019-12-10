927647e5aa176f777d27114f8c20846a7e04973c
Rakib M. Hasan <rmhasan@google.com>
2019-12-10 15:54:04 -0800

    [typ] Skip suspect test
    Bug: chromium: 1022736
    Change-Id: I61a9ac7d819956dda51af17a7d37f32ff842cfff

--- third_party/typ/typ/tests/runner_test.py
+++ third_party/typ/typ/tests/runner_test.py
@@ -221,6 +221,7 @@ class TestWinMultiprocessing(TestCase):
             self.assertEqual(out, '0 tests passed, 0 skipped, 0 failures.\n')
             self.assertEqual(err, '')
 
+    @unittest.skipIf(True, 'Test is leaking state to other tests')
     def test_real_unimportable_main(self):
         h = self.make_host()
         tmpdir = None
