85ad4b2ec1d3fe7b26c3b33afc81d3efb45a65f5
Chong Gu <chonggu@google.com>
2019-12-16 16:05:06 -0800

    [Telemetry] Add Fuchsia backend support
    Add FuchsiaPlatformBackend class, handles creation of the command_runner
    that runs commands on Fuchsia device.
    Add FuchsiaBrowserBackend class, uses the command_runner to launch a
    browser in Fuchsia
    
    Bug: chromium:1014642
    Change-Id: Iaf17868d5b874cca7655a918bb4e6af309436c4b

---
+++ telemetry/telemetry/internal/backends/chrome/fuchsia_browser_backend.py
@@ -0,0 +1,78 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+
+from telemetry.core import exceptions
+from telemetry.internal.backends.chrome import chrome_browser_backend
+from telemetry.internal.platform import fuchsia_platform_backend as \
+  fuchsia_platform_backend_module
+
+_DEFAULT_PORT = 1234
+
+class FuchsiaBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
+  def __init__(self, fuchsia_platform_backend, browser_options,
+               browser_directory, profile_directory):
+    assert isinstance(fuchsia_platform_backend,
+                      fuchsia_platform_backend_module.FuchsiaPlatformBackend)
+    super(FuchsiaBrowserBackend, self).__init__(
+        fuchsia_platform_backend,
+        browser_options=browser_options,
+        browser_directory=browser_directory,
+        profile_directory=profile_directory,
+        supports_extensions=False,
+        supports_tab_control=False)
+    self._command_runner = fuchsia_platform_backend.command_runner
+    self._browser_process = None
+
+  @property
+  def log_file_path(self):
+    return None
+
+  def _FindDevToolsPortAndTarget(self):
+    return _DEFAULT_PORT, None
+
+  def Start(self, startup_args):
+    browser_cmd = [
+        'run',
+        'fuchsia-pkg://fuchsia.com/web_engine_shell#meta/web_engine_shell.cmx',
+        '--remote-debugging-port=%s' % _DEFAULT_PORT,
+        'about:blank'
+    ]
+    self._browser_process = self._command_runner.RunCommandPiped(browser_cmd)
+    logging.info('Browser is up!')
+    try:
+      self.BindDevToolsClient()
+    except exceptions.ProcessGoneException:
+      self.Close()
+      raise
+
+  def GetPid(self):
+    return self._browser_process.pid
+
+  def Background(self):
+    raise NotImplementedError
+
+  def Close(self):
+    super(FuchsiaBrowserBackend, self).Close()
+
+    if self._browser_process:
+      logging.info('Shutting down browser process on Fuchsia')
+      self._browser_process.kill()
+    self._browser_process = None
+
+  def IsBrowserRunning(self):
+    return bool(self._browser_process)
+
+  def GetStandardOutput(self):
+    return 'Stdout is not available on Fuchsia Devices.'
+
+  def GetStackTrace(self):
+    return (False, 'Stack trace is not yet supported on Fuchsia.')
+
+  def SymbolizeMinidump(self, minidump_path):
+    logging.warning('Symbolizing Minidump not supported on Fuchsia.')
+    return None
+
+
