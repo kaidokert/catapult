b1515a0aa128bf6da60764d82a72e4cc192d65e3
Patrik Höglund <phoglund@google.com>
2019-12-18 08:57:59 +0100

    WIP: hacking on histogram API
    Change-Id: I91f58e5a889ff07b618ae1b1c072d25cc5eec424

---
+++ tracing/tracing/value/histogram.h
@@ -0,0 +1,45 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "tracing/tracing/proto/histogram.pb.h"
+
+namespace catapult {
+
+class RunningStatistics;
+
+class HistogramBuilder {
+ public:
+  HistogramBuilder(const std::string& name,
+                   tracing::tracing::proto::UnitAndDirection unit);
+  ~HistogramBuilder();
+
+  void set_description(const std::string& description) {
+    description_ = description;
+  }
+
+  void AddSample(float value);
+
+  std::unique_ptr<tracing::tracing::proto::Histogram> toProto() const;
+
+ private:
+  int _GetDefaultMaxNumSampleValues();
+
+ private:
+  class Resampler;
+
+  std::unique_ptr<Resampler> resampler_;
+  std::unique_ptr<RunningStatistics> running_statistics_;
+  int max_num_sample_values_;
+  std::string name_;
+  std::string description_;
+  tracing::tracing::proto::UnitAndDirection unit_;
+  std::vector<float> sample_values_;
+  int num_nans_;
+};
+
+}  // namespace catapult
---
+++ tracing/tracing/value/running_statistics_unittest.cc
@@ -0,0 +1,39 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tracing/tracing/value/running_statistics.h"
+
+#include <sstream>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace catapult {
+
+TEST(RunningStatisticsUnittest, GetsCountRight) {
+  RunningStatistics stats;
+
+  stats.Add(1);
+  stats.Add(1);
+  stats.Add(1);
+  stats.Add(1);
+  stats.Add(1);
+  stats.Add(1);
+
+  ASSERT_EQ(stats.count(), 6);
+}
+
+TEST(RunningStatisticsUnittest, ComputesMean) {
+  RunningStatistics stats;
+
+  stats.Add(1);
+  stats.Add(2);
+  stats.Add(3);
+  stats.Add(4);
+
+  ASSERT_EQ(stats.mean(), 2.5);
+}
+
+// TODO(https://bugs.chromium.org/1029452): more tests!
+
+}  // namespace catapult
