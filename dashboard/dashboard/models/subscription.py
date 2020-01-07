065a34ff5fdffaeb412cd91a65acd0c7cece8e62
Chenlin Fan <fancl@chromium.org>
2020-01-07 16:01:39 +1100

    Add subscriptions to anomaly
    Storing Match reustl into Anomaly model. We can check the result from
    datastore and use it for further migration. It's almost compatible with
    current sheriff field.
    
    Bug: chromium:953181
    Change-Id: I5ae68a358513294deca68971389325c8a1d8ee82

---
+++ dashboard/dashboard/models/subscription.py
@@ -0,0 +1,32 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""The database model Sheriff, for sheriff rotations."""
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import enum
+from google.appengine.ext import ndb
+
+class Visibility(enum.IntEnum):
+  INTERNAL_ONLY = 0
+  PUBLIC = 1
+
+
+class Subscription(ndb.Model):
+  """
+  Configuration options for alerts' subscriber. It's a mappint to the
+  Subscription protobuf and should never be directly stored to datastore.
+  """
+
+  revision = ndb.StringProperty()
+  name = ndb.StringProperty()
+  rotation_url = ndb.StringProperty()
+  notification_email = ndb.StringProperty()
+  bug_labels = ndb.StringProperty(repeated=True)
+  bug_components = ndb.StringProperty(repeated=True)
+  bug_cc_emails = ndb.StringProperty(repeated=True)
+  visibility = ndb.IntegerProperty(default=Visibility.INTERNAL_ONLY)
+
