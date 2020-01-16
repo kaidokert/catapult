c421678f7f8a4f149d061be7ee5fa0b8fb993b48
Patrik Höglund <phoglund@google.com>
2020-01-16 12:41:47 +0100

    Add reserved_infos.h/cc.
    For now I only think the C++ tests will need the STORIES entry from
    reserved_infos. I wrote a small presubmit to remember people to keep
    them in sync if the STORIES name ever changes in the future.
    
    I did try to write an even fancier presubmit first that actually read
    the files, but is brittle, complex and difficult to get right. I
    think this simple presubmit at least does something.
    
    Bug: chromium:1029452
    Change-Id: I2a7fee754764fb623fdb45417f43a7d78738535f

---
+++ tracing/tracing/value/diagnostics/reserved_infos.h
@@ -0,0 +1,12 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tracing/tracing/proto/histogram.pb.h"
+
+// Keep any values in here in sync with reserved_infos.py.
+namespace catapult {
+
+extern const char kStoriesDiagnostic[];
+
+}
