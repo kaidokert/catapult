3c56e1c2eafa35e1022a8f53a3ef489fa86538c4
Rakib M. Hasan <rmhasan@google.com>
2020-01-22 14:55:08 -0800

    [Typ] Add ability get string value of an Expectation instance
    This CL adds the ability to get the string value of an Expectation
    instance. When we get the string value, all asterisks will be escaped
    in non glob expectations. For glob expectations, the last asterisk will
    not be escaped. Also we will keep the raw strings for the tags and
    results from the original string that was parsed for the expectation.
    We will also keep the raw tags in the same order they appeared in the
    original string. This will prevent unintended modifications to
    expectations in test expectations files during WPT imports.
    
    TBR=dpranke@chromium.org
    Bug: chromium:986447
    Change-Id: I8262107bfe2ec0de2518874af93002dd2b2a63ac

--- third_party/typ/typ/tests/expectations_parser_test.py
+++ third_party/typ/typ/tests/expectations_parser_test.py
@@ -718,3 +718,24 @@ crbug.com/12345 [ tag3 tag4 ] b1/s1 [ Skip ]
         self.assertFalse(msg)
         exp = expectations.expectations_for('test')
         self.assertEqual(exp.reason, 'skbug.com/111 crbug.com/lpz/222 skbug.com/hello/333 crbug.com/444')
+
+    def testExpectationToString(self):
+        exp = Expectation(reason='crbug.com/123', test='test.html?*', tags=['intel'],
+                          results={ResultType.Pass, ResultType.Failure}, is_slow_test=True,
+                          retry_on_failure=True)
+        self.assertEqual(
+            exp.to_string(), 'crbug.com/123 [ intel ] test.html?\* [ Failure Pass Slow RetryOnFailure ]')
+
+    def testExpectationToStringUsingRawSpecifiers(self):
+        exp = Expectation(reason='crbug.com/123', test='test.html?*', tags=['intel'],
+                          results={ResultType.Pass, ResultType.Failure}, raw_tags=['iNteL'],
+                          raw_results=['PasS', 'FailuRe'])
+        self.assertEqual(exp.to_string(), 'crbug.com/123 [ iNteL ] test.html?\* [ PasS FailuRe ]')
+
+    def testExpectationToStringAfterRenamingTest(self):
+        exp = Expectation(reason='crbug.com/123', test='test.html?*', tags=['intel'],
+                          results={ResultType.Pass, ResultType.Failure}, raw_tags=['iNteL'],
+                          raw_results=['FailuRe', 'PasS'])
+        exp.test = 'a/*/test.html?*'
+        self.assertEqual(exp.to_string(), 'crbug.com/123 [ iNteL ] a/\*/test.html?\* [ FailuRe PasS ]')
+
