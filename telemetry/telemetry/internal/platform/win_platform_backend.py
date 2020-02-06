7902613d3306a9690a68858c40774b1198b62d95
Sadrul Habib Chowdhury <sadrul@chromium.org>
2020-02-05 21:11:19 -0500

    windows: Allow taking screenshots.
    Windows was the only platform that still does not allow taking a
    screenshot on failure. Implement this, so that it is easier to diagnose
    an error when a story starts failing.
    
    Win 7: https://pinpoint-dot-chromeperf.appspot.com/job/1085753da20000
    Win 10: https://pinpoint-dot-chromeperf.appspot.com/job/13725e73a20000
    
    BUG=chromium:369490
    
    Change-Id: Idcc859d31b973fa5d4f665ae2d699982ba45b529

--- telemetry/telemetry/internal/platform/win_platform_backend.py
+++ telemetry/telemetry/internal/platform/win_platform_backend.py
@@ -4,6 +4,7 @@
 
 import contextlib
 import ctypes
+import io
 import logging
 import platform
 import re
@@ -18,11 +19,13 @@ from telemetry.internal.platform import desktop_platform_backend
 try:
   import pywintypes  # pylint: disable=import-error
   import win32api  # pylint: disable=import-error
+  from PIL import Image  # pylint: disable=import-error
   from win32com.shell import shell  # pylint: disable=no-name-in-module
   from win32com.shell import shellcon  # pylint: disable=no-name-in-module
   import win32con  # pylint: disable=import-error
   import win32gui  # pylint: disable=import-error
   import win32process  # pylint: disable=import-error
+  import win32ui  # pylint: disable=import-error
   import winerror  # pylint: disable=import-error
   try:
     import winreg  # pylint: disable=import-error
@@ -30,6 +33,7 @@ try:
     import _winreg as winreg  # pylint: disable=import-error,wrong-import-order
   import win32security  # pylint: disable=import-error
 except ImportError:
+  Image = None
   pywintypes = None
   shell = None
   shellcon = None
@@ -40,6 +44,7 @@ except ImportError:
   win32pipe = None
   win32process = None
   win32security = None
+  win32ui = None
   winerror = None
   winreg = None
 
@@ -136,6 +141,30 @@ class WinPlatformBackend(desktop_platform_backend.DesktopPlatformBackend):
   @decorators.Cache
   def GetOSVersionDetailString(self):
     return platform.uname()[3]
+
+  def CanTakeScreenshot(self):
+    return True
+
+  def TakeScreenshot(self, file_path):
+    width = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
+    height = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)
+    screen_win = win32gui.GetDesktopWindow()
+    win_dc = win32gui.GetWindowDC(screen_win)
+    screen_dc = win32ui.CreateDCFromHandle(win_dc)
+    capture_dc = screen_dc.CreateCompatibleDC()
+    capture_bitmap = win32ui.CreateBitmap()
+    capture_bitmap.CreateCompatibleBitmap(screen_dc, width, height)
+    capture_dc.SelectObject(capture_bitmap)
+    capture_dc.BitBlt(
+        (0, 0), (width, height), screen_dc, (0, 0), win32con.SRCCOPY)
+    pixels = capture_bitmap.GetBitmapBits(False)
+    image = Image.open(io.BytesIO(pixels))
+    image.save(file_path)
+    screen_dc.DeleteDC()
+    capture_dc.DeleteDC()
+    win32gui.ReleaseDC(screen_win, win_dc)
+    win32gui.DeleteObject(capture_bitmap.GetHandle())
+    return True
 
   def CanFlushIndividualFilesFromSystemCache(self):
     return True
