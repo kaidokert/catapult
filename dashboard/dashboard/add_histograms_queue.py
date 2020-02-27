56885a0fa6109eed943530847577101b57d36f17
Chenlin Fan <fancl@chromium.org>
2020-02-27 17:54:22 +1100

    Increase sampling from 0.2 to 0.5
    Sampling 50% traffic to sheriff-config for load testing.
    
    Bug: chromium:1052720
    Change-Id: Icf99f8ad3aeeb4d9982dfd1b3741b7fa93af4586

--- dashboard/dashboard/add_histograms_queue.py
+++ dashboard/dashboard/add_histograms_queue.py
@@ -212,11 +212,12 @@ def _AddRowsFromData(params, revision, parent_test, legacy_parent_tests):
 
   def IsMonitored(client, test):
     reason = []
-    request_sampling_percentage = 0.2
+    request_sampling_percentage = 0.5
     if random.random() < request_sampling_percentage:
-      subscriptions, err_msg = client.Match(test.test_path)
-      logging.info('Sheriff Config Matching: %s err=%s', subscriptions, err_msg)
-    if not test.sheriff:
+      subscriptions, _ = client.Match(test.test_path, check=True)
+      if not subscriptions:
+        reason.append('subscriptions')
+    elif not test.sheriff:
       reason.append('sheriff')
     if not test.has_rows:
       reason.append('has_rows')
