94ae04570ebe6045dea431d2e4cc3ae1229ed9b7
Eric Seckler <eseckler@chromium.org>
2020-03-10 16:45:42 +0000

    trace_data: Raise exceptions only after traces are added to artifacts
    Currently, when any trace data loss occurs, we raise an exception right
    after collecting the trace data. This in turns means that we lose the
    trace making it hard to debug that data loss.
    
    This patch buffers the exception into the TraceDataBuilder and only
    raises it when the builder is cleaned up after traces were added to
    the benchmark artifacts. Thus, we should retain the trace data.
    
    Bug: chromium:1059767
    Change-Id: I2d764f9455b60f6c404c70f30be1d7788b0dadfa

--- tracing/tracing/trace_data/trace_data.py
+++ tracing/tracing/trace_data/trace_data.py
@@ -13,8 +13,10 @@ import os
 import platform
 import shutil
 import subprocess
+import sys
 import tempfile
 import time
+import traceback
 import six
 
 
@@ -96,6 +98,10 @@ _TraceItem = collections.namedtuple(
     '_TraceItem', ['part_name', 'handle'])
 
 
+class TraceDataException(Exception):
+  """Exception raised by TraceDataBuilder via RecordTraceDataException()."""
+
+
 class TraceDataBuilder(object):
   """TraceDataBuilder helps build up a trace from multiple trace agents.
 
@@ -112,6 +118,7 @@ class TraceDataBuilder(object):
     self._traces = []
     self._frozen = False
     self._temp_dir = tempfile.mkdtemp()
+    self._exceptions = []
 
   def __enter__(self):
     return self
@@ -192,7 +199,11 @@ class TraceDataBuilder(object):
     return self
 
   def CleanUpTraceData(self):
-    """Clean up resources used by the data builder."""
+    """Clean up resources used by the data builder.
+
+    Will also re-raise any exceptions previously added by
+    RecordTraceCollectionException().
+    """
     if self._traces is None:
       return  # Already cleaned up.
     self.Freeze()
@@ -203,6 +214,11 @@ class TraceDataBuilder(object):
     shutil.rmtree(self._temp_dir)
     self._temp_dir = None
     self._traces = None
+
+    if self._exceptions:
+      raise TraceDataException(
+          'Exceptions raised during trace data collection:\n' +
+          '\n'.join(self._exceptions))
 
   def Serialize(self, file_path, trace_title=None):
     """Serialize the trace data to a file in HTML format."""
@@ -249,6 +265,23 @@ class TraceDataBuilder(object):
     """
     for trace in self._traces:
       yield trace.part_name, trace.handle.name
+
+  def RecordTraceDataException(self):
+    """Records the most recent exception to be re-raised during cleanup.
+
+    Exceptions raised during trace data collection can be stored temporarily
+    in the builder temporarily. They will be re-raised when the builder is
+    cleaned up later. This way, any collected trace data can still be retained
+    before the benchmark is aborted.
+
+    This method is intended to be called from within an "except" handler, e.g.:
+      try:
+        # Collect trace data.
+      except Exception: # pylint: disable=broad-except
+        builder.RecordTraceDataException()
+    """
+    self._exceptions.append(
+        ''.join(traceback.format_exception(*sys.exc_info())))
 
 
 def CreateTestTrace(number=1):
