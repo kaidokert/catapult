8fca3559bc30cd4155a3a17c8b7cf4150b3ff2bf
Chenlin Fan <fancl@chromium.org>
2020-04-02 16:27:57 +1100

    Initial implementation of auto-triage
    This is the initial implementation of auto-triage. It implements
    creating and updating AlertGroup while not actually creating any issue.
    
    Bug: chromium:1064955
    Change-Id: I01b8d887694c796e0c1e5ba22cb4359188dc9be3

---
+++ dashboard/dashboard/alert_groups.py
@@ -0,0 +1,55 @@
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import datetime
+
+from dashboard.common import request_handler
+from dashboard.models import alert_group
+from google.appengine.ext import ndb
+
+
+class AlertGroupsHandler(request_handler.RequestHandler):
+  """Create, Update and Delete AlterGroups."""
+
+  def get(self):
+    groups = alert_group.AlertGroup.GetAll()
+    for group in groups:
+      group.Update()
+      if group.status == alert_group.AlertGroup.Status.untriaged:
+        group.TryTriage()
+      elif group.status == alert_group.AlertGroup.Status.triaged:
+        group.TryBisect()
+      else:
+        deadline = group.updated + datetime.timedelta(days=7)
+        past_due = deadline < datetime.datetime.now()
+        closed = (group.status == alert_group.AlertGroup.Status.closed)
+        untriaged = (group.status == alert_group.AlertGroup.Status.untriaged)
+        if past_due and (closed or untriaged):
+          group.Archive()
+    ndb.put_multi(groups)
+
+    def FindGroup(group):
+      for g in groups:
+        if group.revision.IsOverlapping(g.revision):
+          return g.key
+      groups.append(group)
+      return None
+
+    ungrouped_list = alert_group.AlertGroup.Get('Ungrouped', None)
+    if not ungrouped_list:
+      alert_group.AlertGroup(name='Ungrouped', active=True).put()
+      return
+    ungrouped = ungrouped_list[0]
+    ungrouped_anomalies = ndb.get_multi(ungrouped.anomalies)
+    for anomaly_entity in ungrouped_anomalies:
+      anomaly_entity.groups = [
+          FindGroup(g) or g.put()
+          for g in alert_group.AlertGroup.GenerateAllGroupsForAnomaly(
+              anomaly_entity)
+      ]
+    ndb.put_multi(ungrouped_anomalies)
