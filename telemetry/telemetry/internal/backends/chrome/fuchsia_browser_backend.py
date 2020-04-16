cbdd02fb8fbc1cf7e2ea742f898e33095ce076e0
Chong Gu <chonggu@google.com>
2020-04-16 14:47:24 -0700

    Add logging of web_engine_shell to Standard Output when tests fail.
    Gather stdout and stderr from browser process and provide them when tests fail.
    Symbolize the output if possible.
    
    Bug: chromium:1069256
    Change-Id: I40b9aa5ce64da22417aefca3ca675e219bb0ed8f

--- telemetry/telemetry/internal/backends/chrome/fuchsia_browser_backend.py
+++ telemetry/telemetry/internal/backends/chrome/fuchsia_browser_backend.py
@@ -3,10 +3,12 @@
 # found in the LICENSE file.
 
 import logging
+import os
 import re
 import subprocess
 
 from telemetry.core import exceptions
+from telemetry.core import util
 from telemetry.internal.backends.chrome import chrome_browser_backend
 from telemetry.internal.backends.chrome import minidump_finder
 from telemetry.internal.platform import fuchsia_platform_backend as fuchsia_platform_backend_module
@@ -29,10 +31,37 @@ class FuchsiaBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     self._command_runner = fuchsia_platform_backend.command_runner
     self._browser_process = None
     self._devtools_port = None
+    self._symbolizer_proc = None
+    self._config_dir = fuchsia_platform_backend.config_dir
+    self._browser_log = ''
 
   @property
   def log_file_path(self):
     return None
+
+  def _RunSymbolizer(self, input_file, output_file, build_ids_files,
+                     llvm_symbolizer_path):
+    """Starts a symbolizer process.
+
+    input_file: Input file to be symbolized.
+    output_file: Output file for symbolizer stdout and stderr.
+    build_ids_file: Path to the ids.txt file which maps build IDs to
+                    unstripped binaries on the filesystem.
+    llvm_symbolizer_path: Path to the llvm-symbolizer
+
+    Returns a Popen object for the started process."""
+    sdk_root = os.path.join(util.GetCatapultDir(), '..', 'fuchsia-sdk', 'sdk')
+    symbolizer = os.path.join(sdk_root, 'tools', 'x64', 'symbolize')
+    symbolizer_cmd = [symbolizer,
+                      '-ids-rel', '-llvm-symbolizer', llvm_symbolizer_path,
+                      '-build-id-dir', os.path.join(sdk_root, '.build-id')]
+    for build_ids_file in build_ids_files:
+      symbolizer_cmd.extend(['-ids', build_ids_file])
+
+    logging.info('Running "%s".' % ' '.join(symbolizer_cmd))
+    return subprocess.Popen(symbolizer_cmd, stdin=input_file,
+                            stdout=output_file, stderr=subprocess.STDOUT,
+                            close_fds=True)
 
   def _FindDevToolsPortAndTarget(self):
     return self._devtools_port, None
@@ -43,6 +72,7 @@ class FuchsiaBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
         return None
       line = f.readline()
       tokens = re.search(r'Remote debugging port: (\d+)', line)
+      self._browser_log += line
       return int(tokens.group(1)) if tokens else None
     return py_utils.WaitFor(lambda: TryReadingPort(stderr), timeout=60)
 
@@ -54,11 +84,26 @@ class FuchsiaBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
         'about:blank'
     ]
     self._browser_process = self._command_runner.RunCommandPiped(
-        browser_cmd, stderr=subprocess.PIPE)
+        browser_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    # Symbolize output from browser if possible
+    llvm_symbolizer_path = os.path.join(
+        util.GetCatapultDir(), '..', 'llvm-build',
+        'Release+Asserts', 'bin', 'llvm-symbolizer')
+    shell_build_id = os.path.join(self._config_dir, 'gen', 'fuchsia', 'engine',
+                                  'web_engine_shell', 'ids.txt')
+    if os.path.isfile(llvm_symbolizer_path) and os.path.isfile(shell_build_id):
+      self._symbolizer_proc = self._RunSymbolizer(
+          self._browser_process.stdout, subprocess.PIPE,
+          [shell_build_id], llvm_symbolizer_path)
+      self._browser_process.stderr = self._symbolizer_proc.stdout
+    else:
+      logging.info('Symbolization of web_engine_shell is not available. StdOut \
+                    will only contain raw output.')
+
     self._dump_finder = minidump_finder.MinidumpFinder(
         self.browser.platform.GetOSName(), self.browser.platform.GetArchName())
-    self._devtools_port = self._ReadDevToolsPort(
-        self._browser_process.stderr)
+    self._devtools_port = self._ReadDevToolsPort(self._browser_process.stderr)
     try:
       self.BindDevToolsClient()
     except exceptions.ProcessGoneException:
@@ -77,13 +122,17 @@ class FuchsiaBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     if self._browser_process:
       logging.info('Shutting down browser process on Fuchsia')
       self._browser_process.kill()
+    if self._symbolizer_proc:
+      self._symbolizer_proc.kill()
     self._browser_process = None
 
   def IsBrowserRunning(self):
     return bool(self._browser_process)
 
   def GetStandardOutput(self):
-    return 'Stdout is not available on Fuchsia Devices.'
+    if self._browser_process:
+      self._browser_log += self._browser_process.stderr.read()
+    return self._browser_log
 
   def SymbolizeMinidump(self, minidump_path):
     logging.warning('Symbolizing Minidump not supported on Fuchsia.')
