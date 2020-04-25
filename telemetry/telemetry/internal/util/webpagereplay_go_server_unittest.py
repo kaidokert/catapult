dd5e3dd76869505a64ff592059438a10fc592957
Arthur Wang <wuwang@chromium.org>
2020-04-24 22:13:03 -0700

    Refractor out ts_proxy_server and webpagereplay_go_server to py_utils.
    Also change arg order of internal.util.binary_manager FetchPath and LocalPath to be consistent.
    
    R=bsheedy@chromium.org
    
    Bug: chromium:1060399
    Change-Id: Id7553261b4b2bb7e4733b58ee0c3edf11859cdbd

--- telemetry/telemetry/internal/util/webpagereplay_go_server_unittest.py
+++ telemetry/telemetry/internal/util/webpagereplay_go_server_unittest.py
@@ -19,8 +19,8 @@ class WebPageReplayGoServerTest(unittest.TestCase):
   def setUp(self):
     self.archive_path = binary_manager.FetchPath(
         'example_domain_wpr_go_archive',
-        py_utils.GetHostArchName(),
-        py_utils.GetHostOsName())
+        py_utils.GetHostOsName(),
+        py_utils.GetHostArchName())
 
   @decorators.Disabled('all')  # crbug.com/909746
   @decorators.Disabled('chromeos')  # crbug.com/750323
@@ -49,7 +49,7 @@ class WebPageReplayGoServerTest(unittest.TestCase):
   def testKillingWebPageReplayProcessUponStartupFailure(
       self, atexit_with_log_register_patch):
     atexit_with_log_register_patch.side_effect = KeyError('Bang!')
-    with self.assertRaises(webpagereplay_go_server.ReplayNotStartedError):
+    with self.assertRaises(webpagereplay_go_server.ReplayServerNotStartedError):
       server = webpagereplay_go_server.ReplayServer(
           self.archive_path, replay_host='127.0.0.1', http_port=0, https_port=0,
           replay_options=[])
@@ -57,3 +57,4 @@ class WebPageReplayGoServerTest(unittest.TestCase):
 
     # Ensure replay process is probably cleaned up after StartServer crashed.
     self.assertIsNone(server.replay_process)
+
