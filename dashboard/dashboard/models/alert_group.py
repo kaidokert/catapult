05723583faa63cc94cd7c228f8e4fc0eed1e7612
Chenlin Fan <fancl@chromium.org>
2020-05-01 20:30:04 +1000

    Implementing Auto-triage for AlertGroup
    Implementing automatic creation of the issue in monorail after
    AlertGroup is created. Alerts will be related to the issue and
    auto-triaged.
    
    Bug: chromium:1064955
    Change-Id: I909475536ba217880d2131b8c2e454e66f14bf0a

--- dashboard/dashboard/models/alert_group.py
+++ dashboard/dashboard/models/alert_group.py
@@ -7,10 +7,30 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+import collections
+import datetime
+import logging
+import os
 import uuid
 
+import jinja2
+
+from dashboard import sheriff_config_client
+from dashboard.common import utils
 from dashboard.models import anomaly
+from dashboard.services import issue_tracker_service
 from google.appengine.ext import ndb
+
+# Templates used for rendering issue contents
+_TEMPLATE_LOADER = jinja2.FileSystemLoader(
+    searchpath=os.path.dirname(os.path.realpath(__file__)))
+_TEMPLATE_ENV = jinja2.Environment(loader=_TEMPLATE_LOADER)
+_TEMPLATE_ISSUE_TITLE = jinja2.Template(
+    'Chromeperf Alerts: '
+    '{{ regressions|length }} regressions in {{ group.name }}'
+)
+_TEMPLATE_ISSUE_CONTENT = _TEMPLATE_ENV.get_template(
+    'alert_groups_bug_description.j2')
 
 
 class RevisionRange(ndb.Model):
@@ -100,8 +120,11 @@ class AlertGroup(ndb.Model):
     # TODO(fancl): Fetch issue status
 
   def TryTriage(self):
-    # TODO(fancl): File issue
-    pass
+    self.bug = self._FileIssue()
+    if not self.bug:
+      return
+    self.updated = self.updated = datetime.datetime.now()
+    self.status = self.Status.triaged
 
   def TryBisect(self):
     # TODO(fancl): Trigger bisection
@@ -109,3 +132,110 @@ class AlertGroup(ndb.Model):
 
   def Archive(self):
     self.active = False
+
+  @staticmethod
+  def _GetPreproccessedRegressions(anomalies):
+    regressions = [a for a in anomalies if not a.is_improvement]
+    sheriff_config = sheriff_config_client.GetSheriffConfigClient()
+    subscriptions_dict = {}
+    for a in regressions:
+      response, _ = sheriff_config.Match(a.test.string_id(), check=True)
+      subscriptions_dict.update({s.name: s for s in response})
+      a.auto_triage_enable = any(s.auto_triage_enable for s in response)
+    subscriptions = subscriptions_dict.values()
+    return (regressions, subscriptions)
+
+  @staticmethod
+  def _GetComponentsFromRegressions(regressions):
+    components = []
+    for r in regressions:
+      component = r.ownership and r.ownership.get('component')
+      if not component:
+        continue
+      if isinstance(component, list) and component:
+        components.append(component[0])
+      elif component:
+        components.append(component)
+    return set(components)
+
+  @staticmethod
+  def _GetBenchmarksFromRegressions(regressions):
+    benchmark_tuple = collections.namedtuple(
+        '_Benchmark', ['name', 'owners', 'regressions']
+    )
+    benchmarks_dict = dict()
+    for r in regressions:
+      name = r.benchmark_name
+      b = benchmarks_dict.get(name, benchmark_tuple(name, set(), []))
+      if r.ownership:
+        b.owners.update(r.ownership.get('emails', []))
+      b.regressions.append(r)
+      benchmarks_dict[name] = b
+    return benchmarks_dict.values()
+
+
+  def _FileIssue(self):
+    anomalies = ndb.get_multi(self.anomalies)
+    regressions, subscriptions = self._GetPreproccessedRegressions(anomalies)
+    # Only file a issue if there is at least one regression
+    if not regressions or not any(s.auto_triage_enable for s in subscriptions):
+      return None
+
+    # Preparing template arguments used in rendering issue's title and content.
+    regressions.sort(
+        key=lambda x: - x.median_after_anomaly / x.median_before_anomaly)
+    benchmarks = self._GetBenchmarksFromRegressions(regressions)
+    template_args = {
+        # Current AlertGroup used for rendering templates
+        'group': self,
+        # Performance regressions sorted by relative difference
+        'regressions': regressions,
+        # Benchmarks occur in regressions, including names and owners
+        'benchmarks': benchmarks,
+    }
+    # Rendering issue's title and content
+    title = _BugTitle(template_args)
+    description = _BugDetails(template_args)
+
+    # Fetching issue labels, components and cc from subscriptions and owner
+    issue_tracker = _IssueTracker()
+    # TODO(fancl): Fix legacy bug components in labels
+    components = (
+        set(c for s in subscriptions for c in s.bug_components) |
+        self._GetComponentsFromRegressions(regressions)
+    )
+    cc = set(e for s in subscriptions for e in s.bug_cc_emails)
+    labels = (
+        set(l for s in subscriptions for l in s.bug_labels) |
+        set(['Chromeperf-Auto-Triaged'])
+    )
+    response = issue_tracker.NewBug(
+        title, description, labels=labels, components=components, cc=cc)
+    if 'error' in response:
+      logging.warning('AlertGroup file bug failed: %s', response['error'])
+      return None
+
+    # Link the bug to auto-triage enabled alerts.
+    for a in regressions:
+      if not a.bug_id and a.auto_triage_enable:
+        a.bug_id = response['bug_id']
+    ndb.put_multi(regressions)
+    # TODO(fancl): Add bug project in config and anomaly
+    return BugInfo(project='chromium', bug_id=response['bug_id'])
+
+
+def _BugTitle(env):
+  return _TEMPLATE_ISSUE_TITLE.render(env)
+
+
+def _BugDetails(env):
+  return _TEMPLATE_ISSUE_CONTENT.render(env)
+
+
+def _IssueTracker():
+  """Get a cached IssueTracker instance."""
+  # pylint: disable=protected-access
+  if not hasattr(_IssueTracker, '_client'):
+    _IssueTracker._client = issue_tracker_service.IssueTrackerService(
+        utils.ServiceAccountHttp())
+  return _IssueTracker._client
