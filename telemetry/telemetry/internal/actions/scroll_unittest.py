07a2b2a3483ec40822801ebee7dc5090b2ca6a93
Sadrul Habib Chowdhury <sadrul@chromium.org>
2020-05-25 21:11:39 -0400

    [tests] Re-enable some scroll-related unit-tests.
    Some of the bugs related to wheel-scrolls on android and mac have been
    fixed. So re-enable the tests to see if the tests pass on these
    platforms.
    
    BUG=chromium:934649
    
    Change-Id: I74c64aa539a21494945391a6f97d387d538053db

--- telemetry/telemetry/internal/actions/scroll_unittest.py
+++ telemetry/telemetry/internal/actions/scroll_unittest.py
@@ -90,7 +90,6 @@ class ScrollActionTest(tab_test_case.TabTestCase):
     self._RunScrollDistanceTest(
         500000, 200000, page_action.GESTURE_SOURCE_TOUCH, 200)
 
-  @decorators.Disabled('android')  # crbug.com/934649
   def testScrollDistanceFastWheel(self):
     # Wheel scrolling will have a much greater error than touch. There's 2
     # reasons: 1) synthetic wheel gesture accumulate the sent deltas and use
@@ -113,12 +112,10 @@ class ScrollActionTest(tab_test_case.TabTestCase):
     self._RunScrollDistanceTest(
         1000, 300, page_action.GESTURE_SOURCE_TOUCH, 10)
 
-  @decorators.Disabled('android')  # crbug.com/934649
   def testScrollDistanceSlowWheel(self):
     self._RunScrollDistanceTest(
         1000, 300, page_action.GESTURE_SOURCE_MOUSE, 200)
 
-  @decorators.Disabled('android', 'mac')  # crbug.com/934649
   @decorators.Disabled('chromeos', 'linux')  # crbug.com/805523
   @decorators.Disabled('win-reference')  # crbug.com/805523
   def testWheelScrollDistanceWhileZoomed(self):
