9e6e8bdadc9b80f1a6d1ebf777c13084343eea7d
Dean Michael Berris <dberris@chromium.org>
2020-06-01 22:04:27 +1000

    auto-bisect: Skip bisection for single-rev ranges
    This change checks whether the commit range is a single commit or
    whether there's only one difference in the range. In those cases we:
    
    - Find the author of the revision, and assign it directly.
    
    - Add the Chromeperf-Auto-Assigned label to differentiate the changes
      that skip bisection from those that are actually bisected with
      Pinpoint.
    
    We are re-using as much of the current bug-filing mechanism in the
    file_bug module here, to reduce the need to reimplement all this
    functionality.
    
    R=abennetts@google.com, fancl@chromium.org
    
    Bug: chromium:1087803
    Change-Id: I9ceb49152c27f2d509a3b4b30aceb751294ca3fa

--- dashboard/dashboard/file_bug.py
+++ dashboard/dashboard/file_bug.py
@@ -341,7 +341,9 @@ def _GetSingleCLForAnomalies(alerts):
   return revision
 
 
-def _GetCommitInfoForAlert(alert):
+def GetCommitInfoForAlert(alert, crrev=None, gitiles=None):
+  crrev = crrev or crrev_service
+  gitiles = gitiles or gitiles_service
   repository_url = None
   repositories = namespaced_stored_object.Get('repositories')
   test_path = utils.TestPath(alert.test)
@@ -358,7 +360,7 @@ def _GetCommitInfoForAlert(alert):
   if (re.match(r'^[0-9]{5,7}$', rev) and
       repository_url == repositories['chromium']['repository_url']):
     # This is a commit position, need the git hash.
-    result = crrev_service.GetNumbering(
+    result = crrev.GetNumbering(
         number=rev,
         numbering_identifier='refs/heads/master',
         numbering_type='COMMIT_POSITION',
@@ -369,18 +371,26 @@ def _GetCommitInfoForAlert(alert):
     # This still isn't a git hash; can't assign bug.
     return None
 
-  return gitiles_service.CommitInfo(repository_url, rev)
+  return gitiles.CommitInfo(repository_url, rev)
 
-def _AssignBugToCLAuthor(bug_id, commit_info, service):
+
+def AssignBugToCLAuthor(bug_id,
+                        commit_info,
+                        service,
+                        project='chromium',
+                        labels=None):
   """Assigns the bug to the author of the given revision."""
   author = commit_info['author']['email']
   message = commit_info['message']
   service.AddBugComment(
       bug_id,
-      'Assigning to %s because this is the only CL in range:\n%s' % (
-          author, message),
+      'Assigning to %s because this is the only CL in range:\n%s' %
+      (author, message),
       status='Assigned',
-      owner=author)
+      owner=author,
+      labels=labels,
+      project=project)
+
 
 def FileBug(http, owner, cc, summary, description, labels, components,
             urlsafe_keys, needs_bisect=True):
@@ -423,14 +433,14 @@ def FileBug(http, owner, cc, summary, description, labels, components,
     culprit_rev = _GetSingleCLForAnomalies(alerts)
 
     if culprit_rev is not None:
-      commit_info = _GetCommitInfoForAlert(alerts[0])
+      commit_info = GetCommitInfoForAlert(alerts[0])
       if commit_info:
         author = commit_info['author']['email']
         message = commit_info['message']
         if not utils.GetSheriffForAutorollCommit(author, message):
           needs_bisect = False
-          _AssignBugToCLAuthor(
-              bug_id, commit_info, dashboard_issue_tracker_service)
+          AssignBugToCLAuthor(bug_id, commit_info,
+                              dashboard_issue_tracker_service)
 
     if needs_bisect:
       bisect_result = auto_bisect.StartNewBisectForBug(bug_id)
