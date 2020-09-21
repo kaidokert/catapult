6157f0319e5bcd6a92a5ed98c6169806b5813e4f
Ryan Heise <heiserya@google.com>
2020-09-21 13:35:09 -0700

    [not for landing] - hardcode compile APK to true
    Change-Id: I2e44f83a9cec220fddcb15b411b11a2cb160f977

--- telemetry/telemetry/internal/backends/chrome/android_browser_finder.py
+++ telemetry/telemetry/internal/backends/chrome/android_browser_finder.py
@@ -90,7 +90,7 @@ class PossibleAndroidBrowser(possible_browser.PossibleBrowser):
     self._local_apk = local_apk
     self._flag_changer = None
     self._modules_to_install = None
-    self._compile_apk = finder_options.compile_apk
+    self._compile_apk = True
 
     if self._local_apk is None and finder_options.chrome_root is not None:
       self._local_apk = self._backend_settings.FindLocalApk(
