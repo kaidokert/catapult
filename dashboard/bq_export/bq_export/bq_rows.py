49978a83bda0b3c7d3b6f8660c8b800acbbbe84b
Andrew Bennetts <abennetts@google.com>
2020-11-26 16:37:53 +1100

    chromeperf: Add sample_values to bq_rows export.
    This extends the bq_rows Beam script to also export the sample values
    for each Row.  Because the sample values are recorded in a separate
    entity (Histogram) this complicates the Beam pipeline somewhat.  In
    addition to fetching Rows for the given time period, we need to fetch
    Histograms from that same period, and join these collections together.
    We also need to add logic to unpack the sample values out of the
    zlib-compressed JSON.
    
    Also, now that BigQuery supports loading into clustered partitions from
    clustered tables, we can restore clustering (on master, bot, measurment) and
    revert the manual clustering (splitting the results into separate tables for
    each master).
    
    Change-Id: Ic2f6c7c3de905a7ee5bb7fc53b4eefc96a4984d8

--- dashboard/bq_export/bq_export/bq_rows.py
+++ dashboard/bq_export/bq_export/bq_rows.py
@@ -10,7 +10,7 @@ from __future__ import print_function
 import datetime
 import json
 import logging
-import re
+import zlib
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import DebugOptions
@@ -23,9 +23,6 @@ from bq_export.split_by_timestamp import ReadTimestampRangeFromDatastore
 from bq_export.export_options import BqExportOptions
 from bq_export.utils import (FloatHack, PrintCounters,
                              WriteToPartitionedBigQuery)
-
-# BigQuery table names may only have letters, numbers, and underscore.
-_INVALID_BQ_TABLE_NAME_CHARS_RE = re.compile('[^a-zA-Z0-9_]')
 
 
 def main():
@@ -49,7 +46,8 @@ def main():
    bot STRING NOT NULL,
    measurement STRING,
    test STRING NOT NULL,
-   properties STRING)
+   properties STRING,
+   sample_values ARRAY<FLOAT64>)
   PARTITION BY DATE(`timestamp`)
   CLUSTER BY master, bot, measurement;
   """  # pylint: disable=pointless-string-statement
@@ -100,6 +98,11 @@ def main():
               'type': 'STRING',
               'mode': 'NULLABLE'
           },
+          {
+              'name': 'sample_values',
+              'type': 'FLOAT',
+              'mode': 'REPEATED'
+          },
       ]
   }
 
@@ -144,19 +147,120 @@ def main():
           step=datetime.timedelta(minutes=5)))
 
   row_dicts = (
-      row_entities | 'ConvertEntityToRow(Row)' >> FlatMap(RowEntityToRowDict))
+      row_entities | 'ConvertEntityToDict(Row)' >> FlatMap(RowEntityToRowDict))
 
-  def TableNameFn(element):
-    """Write each element to a table based on the table name."""
-    master = _INVALID_BQ_TABLE_NAME_CHARS_RE.sub('_', element['master'])
-    return '{project}:{dataset}.{master}{suffix}'.format(
+  # The sample_values are not found in the Row entity.  So we have to fetch all
+  # the corresponding Histogram entities and join them with our collection of
+  # Rows (by using test + revision as the join key).  We also need to unpack the
+  # sample values arrays out of the zlib-compressed JSON stored in the
+  # Histogram's "data" property.
+  def HistogramEntityToDict(entity):
+    """Returns dicts with keys: 'test', 'revision', 'sample_values'."""
+    entities_read.inc()
+    try:
+      data = entity['data']
+    except KeyError:
+      logging.getLogger().exception('Histogram missing "data" field')
+      failed_entity_transforms.inc()
+      return []
+    try:
+      json_str = zlib.decompress(data)
+    except zlib.error:
+      logging.getLogger().exception('Histogram data not valid zlib: %r', data)
+      failed_entity_transforms.inc()
+      return []
+    try:
+      data_dict = json.loads(json_str)
+    except json.JSONDecodeError:
+      logging.getLogger().exception('Histogram data not valid json.')
+      failed_entity_transforms.inc()
+      return []
+    sample_values = data_dict.get('sampleValues', [])
+    if not isinstance(sample_values, list):
+      logging.getLogger().exception(
+          'Histogram data.sampleValues not valid list.')
+      failed_entity_transforms.inc()
+      return []
+    count = len(sample_values)
+    sample_values = [v for v in sample_values if v is not None]
+    if len(sample_values) != count:
+      logging.getLogger().warn(
+          'Histogram data.sampleValues contains null: %r', entity.key)
+    for v in sample_values:
+      if not isinstance(v, (int, float)):
+        logging.getLogger().exception(
+            'Histogram data.sampleValues contains non-numeric: %r', v)
+        failed_entity_transforms.inc()
+        return []
+    try:
+      return [{
+          'test': entity['test'].name,
+          'revision': entity['revision'],
+          'sample_values': sample_values,
+      }]
+    except KeyError:
+      logging.getLogger().exception(
+          'Histogram missing test or revision field/s')
+      failed_entity_transforms.inc()
+      return []
+
+  histogram_query_params = dict(project=project, kind='Histogram')
+  histogram_entities = (
+      p
+      | 'ReadFromDatastore(Histogram)' >> ReadTimestampRangeFromDatastore(
+          histogram_query_params,
+          time_range_provider=bq_export_options.GetTimeRangeProvider(),
+          step=datetime.timedelta(minutes=5)))
+
+  histogram_dicts = (
+      histogram_entities
+      | 'ConvertEntityToDict(Histogram)' >> FlatMap(HistogramEntityToDict))
+
+  def TestRevision(element):
+    return (element['test'], element['revision'])
+
+  rows_with_key = (
+      row_dicts | 'WithKeys(Row)' >> beam.WithKeys(TestRevision)
+  )
+  histograms_with_key = (
+      histogram_dicts | 'WithKeys(Histogram)' >> beam.WithKeys(TestRevision)
+  )
+
+  def MergeRowAndSampleValues(element):
+    unused_key, join_values = element
+    rows, histograms = join_values
+    if len(rows) != 1:
+      logging.getLogger().exception("conflicting rows")
+      return rows
+    row = rows[0]
+    if len(histograms) > 1:
+      logging.getLogger().exception("conflicting histograms")
+      return [row]
+    if len(histograms) == 0:
+      # No sample values to annotate the row with
+      return [row]
+    histogram = histograms[0]
+    row['sample_values'] = histogram['sample_values']
+    return [row]
+
+  joined_and_annotated = (
+      (rows_with_key, histograms_with_key)
+      | beam.CoGroupByKey()
+      | beam.FlatMap(MergeRowAndSampleValues)
+  )
+
+  def TableNameFn(unused_element):
+    return '{project}:{dataset}.rows{suffix}'.format(
         project=project,
         dataset=bq_export_options.dataset.get(),
-        master=master,
         suffix=bq_export_options.table_suffix)
 
-  _ = row_dicts | 'WriteToBigQuery(rows)' >> WriteToPartitionedBigQuery(
-      TableNameFn, bq_row_schema)
+  _ = (
+      joined_and_annotated
+      | 'WriteToBigQuery(rows)' >> WriteToPartitionedBigQuery(
+          TableNameFn, bq_row_schema, additional_bq_parameters={
+              'clustering': {'fields': ['master', 'bot', 'measurement']}})
+  )
 
   result = p.run()
   result.wait_until_finish()
