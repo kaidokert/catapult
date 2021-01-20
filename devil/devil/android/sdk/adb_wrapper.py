7220887b2eee57e8fea93502e66ead9b1d476037
Sam Maier <smaier@chromium.org>
2021-01-20 09:36:37 -0500

    Timeout for ADB commands increased
    All timeouts have been upped by 50%, and Install (which was causing the
    most trouble), was upped more than 3x.
    
    Bug: chromium:1168280
    Change-Id: I2b71d719a1e7a587bdf9b4e09b8f281e705c3b91

--- devil/devil/android/sdk/adb_wrapper.py
+++ devil/devil/android/sdk/adb_wrapper.py
@@ -34,7 +34,8 @@ logger = logging.getLogger(__name__)
 ADB_KEYS_FILE = '/data/misc/adb/adb_keys'
 ADB_HOST_KEYS_DIR = os.path.join(os.path.expanduser('~'), '.android')
 
-DEFAULT_TIMEOUT = 30
+DEFAULT_TIMEOUT = 45
+DEFAULT_LONG_TIMEOUT = DEFAULT_TIMEOUT * 10
 DEFAULT_RETRIES = 2
 
 _ADB_VERSION_RE = re.compile(r'Android Debug Bridge version (\d+\.\d+\.\d+)')
@@ -476,7 +477,7 @@ class AdbWrapper(object):
            local,
            remote,
            sync=False,
-           timeout=60 * 5,
+           timeout=DEFAULT_LONG_TIMEOUT,
            retries=DEFAULT_RETRIES):
     """Pushes a file from the host to the device.
 
@@ -549,7 +550,11 @@ class AdbWrapper(object):
 
     self._RunDeviceAdbCmd(push_cmd, timeout, retries)
 
-  def Pull(self, remote, local, timeout=60 * 5, retries=DEFAULT_RETRIES):
+  def Pull(self,
+           remote,
+           local,
+           timeout=DEFAULT_LONG_TIMEOUT,
+           retries=DEFAULT_RETRIES):
     """Pulls a file from the device to the host.
 
     Args:
@@ -832,7 +837,7 @@ class AdbWrapper(object):
               reinstall=False,
               sd_card=False,
               streaming=None,
-              timeout=60 * 2,
+              timeout=DEFAULT_LONG_TIMEOUT,
               retries=DEFAULT_RETRIES):
     """Install an apk on the device.
 
@@ -883,7 +888,7 @@ class AdbWrapper(object):
                       allow_downgrade=False,
                       partial=False,
                       streaming=None,
-                      timeout=60 * 2,
+                      timeout=DEFAULT_LONG_TIMEOUT,
                       retries=DEFAULT_RETRIES):
     """Install an apk with splits on the device.
 
@@ -1002,7 +1007,8 @@ class AdbWrapper(object):
     VerifyLocalFileExists(path)
     self._RunDeviceAdbCmd(['restore'] + [path], timeout, retries)
 
-  def WaitForDevice(self, timeout=60 * 5, retries=DEFAULT_RETRIES):
+  def WaitForDevice(self, timeout=DEFAULT_LONG_TIMEOUT,
+                    retries=DEFAULT_RETRIES):
     """Block until the device is online.
 
     Args:
@@ -1047,7 +1053,9 @@ class AdbWrapper(object):
     """Remounts the /system partition on the device read-write."""
     self._RunDeviceAdbCmd(['remount'], timeout, retries)
 
-  def Reboot(self, to_bootloader=False, timeout=60 * 5,
+  def Reboot(self,
+             to_bootloader=False,
+             timeout=DEFAULT_LONG_TIMEOUT,
              retries=DEFAULT_RETRIES):
     """Reboots the device.
 
