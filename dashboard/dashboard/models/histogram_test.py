57b4641b257931f4df09ed4ffd1e7339793517fc
Andrey Logvin <landrey@google.com>
2021-02-08 20:23:55 +0000

    Ignoring invalid diagnostics when inserting out of order
    https://crrev.com/c/2288944 has fixed 'No JSON object could be decoded' when we append diagnostic to the end. While for out of order insertion it wasn't fixed.
    
    Bug: chromium:1146040
    Change-Id: I26a3078e2689304f3d327da9fa5d8c87163c448d

--- dashboard/dashboard/models/histogram_test.py
+++ dashboard/dashboard/models/histogram_test.py
@@ -742,3 +742,21 @@ class SparseDiagnosticTest(testing_common.TestCase):
         (1, 14, [u'm1']),
         (15, sys.maxsize, [u'm3']),
     ])
+
+  def testFindOrInsertDiagnostics_OutOfOrder_Invalid(self):
+    test_key = utils.TestKey('M/B/S')
+
+    self._AddGenericDiagnostic('foo', 'm1', test_key, 1)
+    invalid = self._AddGenericDiagnostic('foo', 'm2', test_key, 5).get()
+    invalid.data = '{'
+    invalid.put()
+
+    e = self._CreateGenericDiagnostic('foo', 'm3', test_key, 3)
+
+    guid_mapping = (
+        histogram.SparseDiagnostic.FindOrInsertDiagnostics(
+            [e], test_key, e.start_revision, e.start_revision).get_result())
+
+    self._CheckExpectations(e, guid_mapping, [
+        (1, sys.maxsize, [u'm1']),
+    ])
