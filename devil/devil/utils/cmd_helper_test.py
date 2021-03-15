49685d24337d1c7de0091f2af43a17af6fee4f96
Weizhong Xia <weizhong@google.com>
2021-03-15 22:34:08 +0000

    Fix StringIO in catapult
    Try a different approach here: 1. Try not change py2 behavior,
    so this will not break any existing test. 2. In py3 treat output
    from subprocess as utf8, and ignore any decoding error
    
    also includes some syntax fixes
    
    Bug: chromium:1183514
    Change-Id: I33187c5b1a013386ad34f5629c9199e9fd0f46b4

--- devil/devil/utils/cmd_helper_test.py
+++ devil/devil/utils/cmd_helper_test.py
@@ -187,6 +187,18 @@ class _MockProcess(object):
       m.__exit__(exc_type, exc_val, exc_tb)
 
 
+class CmdHelperGetCmdStatusAndOutputTest(unittest.TestCase):
+  def testGetCmdStatusAndOutput_success(self):
+    cmd = 'echo "Hello World"'
+    status, output = cmd_helper.GetCmdStatusAndOutput(cmd, shell=True)
+    self.assertEqual(status, 0)
+    self.assertEqual(output.rstrip(), "Hello World")
+
+  def testGetCmdStatusAndOutput_unicode(self):
+    cmd = 'echo "\x80\x31Hello World\n"'
+    cmd_helper.GetCmdStatusAndOutput(cmd, shell=True)
+    # Test no exception for decoding error
+
 class CmdHelperIterCmdOutputLinesTest(unittest.TestCase):
   """Test IterCmdOutputLines with some calls to the unix 'seq' command."""
 
@@ -204,6 +216,16 @@ class CmdHelperIterCmdOutputLinesTest(unittest.TestCase):
       for num, line in enumerate(
           cmd_helper._IterCmdOutputLines(mock_proc, 'mock_proc'), 1):
         self.assertEquals(num, int(line))
+
+  def testIterCmdOutputLines_unicode(self):
+    output_sequence = [
+        _ProcessOutputEvent(read_contents='\x80\x31\nHello\n\xE2\x98\xA0')
+    ]
+    with _MockProcess(
+        output_sequence=output_sequence) as mock_proc:
+      for line in cmd_helper._IterCmdOutputLines(mock_proc, 'mock_proc'):
+        pass
+    # case is passed if there is no exception
 
   def testIterCmdOutputLines_exitStatusFail(self):
     with self.assertRaises(subprocess.CalledProcessError):
