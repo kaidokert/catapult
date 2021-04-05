891448c5d08b6a81c1e5918c5a7c503fb9f0c5d9
Rakib M. Hasan <rmhasan@google.com>
2021-04-05 13:39:18 -0700

    use killall to kill the adb process
    Bug: chromium:1056214
    Change-Id: I8ab56d5beae6315314a9ce58a82e52da73bc47fe

--- devil/devil/android/sdk/adb_wrapper.py
+++ devil/devil/android/sdk/adb_wrapper.py
@@ -143,7 +143,7 @@ def RestartServer():
   def adb_started():
     return AdbWrapper.IsServerOnline()
 
-  AdbWrapper.KillServer()
+  subprocess.call(["killall", "adb"])
   if not timeout_retry.WaitFor(adb_killed, wait_period=1, max_tries=5):
     # TODO(crbug.com/442319): Switch this to raise an exception if we
     # figure out why sometimes not all adb servers on bots get killed.
