f5f847bfd7e12fc1c16be9bb5b97c48727bf9b88
John Chen <johnchen@chromium.org>
2021-05-19 22:37:59 -0700

    [Telemetry] Re-enable several Android unit tests
    Bug: chromium:905359
    Change-Id: I764b308f25431c1d4b5e2727778f0198cf3272e7

--- telemetry/telemetry/internal/backends/chrome/desktop_browser_finder_unittest.py
+++ telemetry/telemetry/internal/backends/chrome/desktop_browser_finder_unittest.py
@@ -8,7 +8,6 @@ import unittest
 
 from pyfakefs import fake_filesystem_unittest
 
-from telemetry import decorators
 from telemetry.core import exceptions
 from telemetry.core import platform
 from telemetry.core import util
@@ -195,7 +194,6 @@ class LinuxFindTest(fake_filesystem_unittest.TestCase):
   def DoFindAllTypes(self):
     return [b.browser_type for b in self.DoFindAll()]
 
-  @decorators.Disabled('android')  # http://crbug.com/905359
   def testFindAllWithCheckout(self):
     # CHROMIUM_OUTPUT_DIR affects the outcome of the tests, so temporarily
     # remove it from the environment.
@@ -218,7 +216,6 @@ class LinuxFindTest(fake_filesystem_unittest.TestCase):
 
     self.assertFalse(self.DoFindAllTypes())
 
-  @decorators.Disabled('android')  # http://crbug.com/905359
   def testFindWithProvidedExecutable(self):
     self.CreateBrowser('/foo/chrome')
     self._finder_options.browser_executable = '/foo/chrome'
@@ -237,7 +234,6 @@ class LinuxFindTest(fake_filesystem_unittest.TestCase):
       self.DoFindAllTypes()
     self.assertIn('does not exist or is not executable', str(cm.exception))
 
-  @decorators.Disabled('android')  # http://crbug.com/905359
   def testFindAllWithInstalled(self):
     official_names = ['chrome', 'chrome-beta', 'chrome-unstable']
 
@@ -246,14 +242,12 @@ class LinuxFindTest(fake_filesystem_unittest.TestCase):
 
     self.assertEquals(set(self.DoFindAllTypes()), {'stable', 'beta', 'dev'})
 
-  @decorators.Disabled('android')  # http://crbug.com/905359
   def testFindAllSystem(self):
     self.CreateBrowser('/opt/google/chrome/chrome')
     os.symlink('/opt/google/chrome/chrome', '/usr/bin/google-chrome')
 
     self.assertEquals(set(self.DoFindAllTypes()), {'system', 'stable'})
 
-  @decorators.Disabled('android')  # http://crbug.com/905359
   def testFindAllSystemIsBeta(self):
     self.CreateBrowser('/opt/google/chrome/chrome')
     self.CreateBrowser('/opt/google/chrome-beta/chrome')
