ab72c5d67daf2e6a986139d6247511e1a4de8bd0
Wenbin Zhang <wenbinzhang@google.com>
2021-07-26 13:12:07 -0700

    [Catapult] Remove google from modules before importing protobuf
    Change-Id: If48cdfe55151a2177270e21892c5549a6ef2732c

--- tracing/tracing/__init__.py
+++ tracing/tracing/__init__.py
@@ -2,5 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import sys
 import tracing_project
 tracing_project.UpdateSysPathIfNeeded()
+
+if 'google' in sys.modules:
+  sys.modules.pop('google')
+from google.protobuf.internal import enum_type_wrapper
+
