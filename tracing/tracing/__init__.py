b9834761d9ead1523d76b7e9a71a0ef60113e6b7
Wenbin Zhang <wenbinzhang@google.com>
2021-07-26 17:12:47 -0700

    [Catapult] Reload 'google' from modules before importing protobuf
    This CL tries to fix the issue that on Catapult try bots, python always try to import protobuf from site-packages/ instead of the one under third-party.
    
    The solution is referring this old code:
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/catapult/third_party/gsutil/third_party/oauth2client/tests/contrib/appengine/conftest.py;l=29
    
    bug: chromium:1233198
    Change-Id: If48cdfe55151a2177270e21892c5549a6ef2732c

--- tracing/tracing/__init__.py
+++ tracing/tracing/__init__.py
@@ -2,5 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import sys
+from six.moves import reload_module
+
 import tracing_project
 tracing_project.UpdateSysPathIfNeeded()
+
+if 'google' in sys.modules:
+  reload_module(sys.modules['google'])
