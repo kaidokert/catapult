10cd31cdde1db91cc66de0fcae8d9a3dc68fee8e
Wenbin Zhang <wenbinzhang@google.com>
2021-08-20 09:29:47 -0700

    [Catapult] Fix devil tests on python3 Android try jobs
    Change-Id: I4ef8ebc27b4c23a11716646bbb5a7a4d3d81ef48

--- devil/devil/android/sdk/adb_wrapper.py
+++ devil/devil/android/sdk/adb_wrapper.py
@@ -218,10 +218,10 @@ class AdbWrapper(object):
 
       Sends an echo command, then waits until it gets a response.
       """
-      self._process.stdin.write('echo\n')
-      output_line = self._process.stdout.readline()
+      self._process.stdin.write('echo\n'.encode('utf-8'))
+      output_line = self._process.stdout.readline().decode('utf-8')
       while output_line.rstrip() != '':
-        output_line = self._process.stdout.readline()
+        output_line = self._process.stdout.readline().decode('utf-8')
 
     def RunCommand(self, command, close=False):
       """Runs an ADB command and returns the output.
@@ -250,9 +250,9 @@ class AdbWrapper(object):
 
         def run_cmd(cmd):
           send_cmd = '( %s ); echo DONE:$?;\n' % cmd.rstrip()
-          self._process.stdin.write(send_cmd)
+          self._process.stdin.write(send_cmd.encode('utf-8'))
           while True:
-            output_line = self._process.stdout.readline().rstrip()
+            output_line = self._process.stdout.readline().rstrip().decode('utf-8')
             if output_line[:5] == 'DONE:':
               yield output_line[5:]
               break
@@ -268,7 +268,7 @@ class AdbWrapper(object):
     def Stop(self):
       """Stops the ADB process if it is still running."""
       if self._process is not None:
-        self._process.stdin.write('exit\n')
+        self._process.stdin.write('exit\n'.encode('utf-8'))
         self._process = None
 
   @classmethod
