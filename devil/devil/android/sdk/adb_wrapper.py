f3020aa64ff4722f0a7fbfb51ad20d62e87b97da
Wenbin Zhang <wenbinzhang@google.com>
2021-08-23 14:22:35 -0700

    devil test
    Change-Id: I542e3a6ede48338ed0fcb9eb336fa2f5d29006ef

--- devil/devil/android/sdk/adb_wrapper.py
+++ devil/devil/android/sdk/adb_wrapper.py
@@ -218,10 +218,10 @@ class AdbWrapper(object):
 
       Sends an echo command, then waits until it gets a response.
       """
-      self._process.stdin.write('echo\n')
-      output_line = self._process.stdout.readline()
-      while output_line.rstrip() != '':
-        output_line = self._process.stdout.readline()
+      self._process.stdin.write(six.ensure_binary('echo hi\n'))
+#       output_line = self._process.stdout.readline()
+#       while output_line.rstrip() != '':
+#         output_line = self._process.stdout.readline()
 
     def RunCommand(self, command, close=False):
       """Runs an ADB command and returns the output.
