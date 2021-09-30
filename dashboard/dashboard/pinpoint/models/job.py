3446954a5003772bdbff3b1619b6308592d12158
Leina Sun <sunxiaodi@google.com>
2021-09-30 23:26:16 +0000

    Added a few more log statements to try to debug issue. Looking to upload to main branch, set the build to 100%, and revert CL afterwards.
    Bug: chromium:1254010
    Change-Id: I031730cb3b2b22d075954b0537a01a17679c0933

--- dashboard/dashboard/pinpoint/models/job.py
+++ dashboard/dashboard/pinpoint/models/job.py
@@ -688,6 +688,10 @@ class Job(ndb.Model):
     self.retry_count += 1
 
     # Back off exponentially
+    try:
+      logging.debug('crbug/1254010 - reschedule - retry count - %s', self.retry_count)
+    except:
+      logging.debug('crbug/1254010 - log message failed')
     self._Schedule(countdown=_TASK_INTERVAL * (2**self.retry_count))
 
     return True
@@ -724,14 +728,23 @@ class Job(ndb.Model):
         return
 
       if not self._IsTryJob():
+        try: 
+          logging.debug('crbug/1254010 - job.run() - Explore - num changes - %s', len(self.state._changes))
+        except:
+          logging.debug('crbug/1254010 - log messaged failed')
         self.state.Explore()
       work_left = self.state.ScheduleWork()
 
       # Schedule moar task.
       if work_left:
+        try:
+          logging.debug('crbug/1254010 - job.run() - schedule - num changes - %s', len(self.state._changes))
+        except:
+          logging.debug('crbug/1254010 - log messaged failed')
         self._Schedule()
       else:
-        self._Complete()
+        logging.debug('crbug/1254010 - job.run() - job marked complete')
+        self._Complete() # not sure how it's possible for this line to get executed
 
       self.retry_count = 0
     except errors.RecoverableError as e:
