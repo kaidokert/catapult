3b2ad11b0ef8789a447cc7a8363be4d173e7c4c8
seanmccullough <seanmccullough@google.com>
2022-02-13 15:56:06 -0800

    [chromeperf] use buildbucket v2 instead of v1
    Bug: chromium:1264738
    Change-Id: I03ab5171c1ad1b41fc4f8eab65a75a912b3171be

--- dashboard/dashboard/services/buildbucket_service.py
+++ dashboard/dashboard/services/buildbucket_service.py
@@ -10,7 +10,9 @@ import json
 
 from dashboard.services import request
 
-API_BASE_URL = 'https://cr-buildbucket.appspot.com/api/buildbucket/v1/'
+#API_BASE_URL = 'https://cr-buildbucket.apapspot.com/api/buildbucket/v1/'
+
+API_BASE_URL = 'https://cr-buildbucket.appspot.com/prpc/buildbucket.v2.Builds/'
 
 # Default Buildbucket bucket name.
 _BUCKET_NAME = 'master.tryserver.chromium.perf'
@@ -18,28 +20,31 @@ _BUCKET_NAME = 'master.tryserver.chromium.perf'
 
 def Put(bucket, tags, parameters, pubsub_callback=None):
   body = {
-      'bucket': bucket,
-      'tags': tags,
-      'parameters_json': json.dumps(parameters, separators=(',', ':')),
+      'builder': {
+          'project': '',
+          'bucket': bucket,
+          'builder': ''
+      },
+      # Make sure 'tags' gets formatted like StringPair expects:
+      # [{'key': key, 'value'; value}]
+      'tags':  [{'key': v[0], 'value': v[1]} for v in [
+          e.split(':') for e in tags]],
+      'properties': json.dumps(parameters, separators=(',', ':')),
   }
   if pubsub_callback:
     body['pubsub_callback'] = pubsub_callback
-  return request.RequestJson(API_BASE_URL + 'builds', method='PUT', body=body)
-
-
-# TODO: Deprecated. Use Put() instead.
-def PutJob(job, bucket=_BUCKET_NAME):
-  """Creates a job via buildbucket's API."""
-  parameters = job.GetBuildParameters()
-  response_content = Put(bucket, [], parameters)
-  job.response_fields = response_content.get('build')
-  return job.response_fields.get('id')
+  return request.RequestJson(API_BASE_URL + 'ScheduleBuild', method='POST',
+                             body=body)
 
 
 # TODO: Rename to Get().
 def GetJobStatus(job_id):
   """Gets the details of a job via buildbucket's API."""
-  return request.RequestJson(API_BASE_URL + 'builds/%s' % (job_id))
+  body = json.dumps({
+      'id': job_id
+  })
+  return request.RequestJson(API_BASE_URL + 'GetBuild', method='POST',
+                             body=body)
 
 
 # TODO(robertocn): Implement CancelJobByID
