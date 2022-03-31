bd604bed6afc7fa20ba3a78e23c6e4093b4704e6
Eduardo Yap <eduardoyap@google.com>
2022-03-31 22:38:28 +0000

    Testing xattr
    Change-Id: I19d24cd6cbb8943683d69a209a529e6e825582db

--- tracing/tracing/proto/histogram_proto.py
+++ tracing/tracing/proto/histogram_proto.py
@@ -1,8 +1,8 @@
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 from __future__ import absolute_import
+
 try:
   # Note: from tracing.proto import histogram_pb2 would make more sense here,
   # but unfortunately protoc does not generate __init__.py files if you specify
@@ -19,7 +19,8 @@ except ImportError as e:
     # TODO(wenbinzhang): Clean up import paths to work consistently.
     from . import histogram_pb2
     HAS_PROTO = True
-  except ImportError:
+  except ImportError as e:
+    from . import histogram_pb2
     HAS_PROTO = False
 
 
