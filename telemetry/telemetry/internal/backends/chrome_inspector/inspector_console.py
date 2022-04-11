fe62f8e7d219a6de41555d7f820b8f50439d2bfc
John Chen <johnchen@chromium.org>
2022-04-11 08:48:52 -0700

    Handle Console.messageAdded with missing properties
    Change-Id: Ib0888213e6b1ef3f9687b5379e1484e205d048a0

--- telemetry/telemetry/internal/backends/chrome_inspector/inspector_console.py
+++ telemetry/telemetry/internal/backends/chrome_inspector/inspector_console.py
@@ -21,13 +21,13 @@ class InspectorConsole(object):
   def _OnNotification(self, msg):
     if msg['method'] == 'Console.messageAdded':
       assert self._message_output_stream
-      if msg['params']['message']['url'] == 'chrome://newtab/':
+      if msg['params']['message'].get('url', '') == 'chrome://newtab/':
         return
       self._last_message = '(%s) %s:%i: %s' % (
-          msg['params']['message']['level'],
-          msg['params']['message']['url'],
-          msg['params']['message']['line'],
-          msg['params']['message']['text'])
+          msg['params']['message'].get('level', ''),
+          msg['params']['message'].get('url', ''),
+          msg['params']['message'].get('line', ''),
+          msg['params']['message'].get('text', ''))
       self._message_output_stream.write(
           '%s\n' % self._last_message)
 
