bb949ddb7a6e2c311575969d0d0cfef44b3f5c52
Ben Joyce <bjoyce@google.com>
2022-05-11 17:21:17 -0700

    Check persistentShell.
    Bug: chromium:1021686
    Change-Id: Ifd41407cb910ba395292777ae3c80dfa3897a5c5

--- devil/devil/android/sdk/adb_compatibility_devicetest.py
+++ devil/devil/android/sdk/adb_compatibility_devicetest.py
@@ -25,6 +25,8 @@ from devil.utils import timeout_retry
 
 _TEST_DATA_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), 'test', 'data'))
+# Parameters for for if the adb shell should be persistent or not.
+_ADB_SHELL_PARAM_LIST = [True, False]
 
 
 def _hostAdbPids():
@@ -94,26 +96,33 @@ class AdbCompatibilityTest(device_test_case.DeviceTestCase):
     devices = adb_wrapper.AdbWrapper.Devices()
     self.assertNotEqual(0, len(devices), 'No devices found.')
 
-  def getTestInstance(self):
+  def getTestInstance(self, persistent_shell=False):
     """Creates a real AdbWrapper instance for testing."""
-    return adb_wrapper.AdbWrapper(self.serial)
+    return adb_wrapper.AdbWrapper(self.serial,
+                                  persistent_shell=persistent_shell)
 
   def testShell(self):
-    under_test = self.getTestInstance()
-    shell_ls_result = under_test.Shell('ls')
-    self.assertIsInstance(shell_ls_result, str)
-    self.assertTrue(bool(shell_ls_result))
+    for par in _ADB_SHELL_PARAM_LIST:
+      under_test = self.getTestInstance(par)
+      shell_ls_result = under_test.Shell('ls')
+      self.assertIsInstance(shell_ls_result, str)
+      self.assertTrue(bool(shell_ls_result))
+      under_test.KillPersistentAdbs()
 
   def testShell_failed(self):
-    under_test = self.getTestInstance()
-    with self.assertRaises(device_errors.AdbShellCommandFailedError):
-      under_test.Shell('ls /foo/bar/baz')
+    for par in _ADB_SHELL_PARAM_LIST:
+      under_test = self.getTestInstance(par)
+      with self.assertRaises(device_errors.AdbShellCommandFailedError):
+        under_test.Shell('ls /foo/bar/baz')
+      under_test.KillPersistentAdbs()
 
   def testShell_externalStorageDefined(self):
-    under_test = self.getTestInstance()
-    external_storage = under_test.Shell('echo $EXTERNAL_STORAGE')
-    self.assertIsInstance(external_storage, str)
-    self.assertTrue(posixpath.isabs(external_storage))
+    for par in _ADB_SHELL_PARAM_LIST:
+      under_test = self.getTestInstance(par)
+      external_storage = under_test.Shell('echo $EXTERNAL_STORAGE')
+      self.assertIsInstance(external_storage, str)
+      self.assertTrue(posixpath.isabs(external_storage))
+      under_test.KillPersistentAdbs()
 
   @contextlib.contextmanager
   def getTestPushDestination(self, under_test):
@@ -136,51 +145,60 @@ class AdbCompatibilityTest(device_test_case.DeviceTestCase):
       under_test.Shell('rm -rf %s' % path)
 
   def testPush_fileToFile(self):
-    under_test = self.getTestInstance()
-    with self.getTestPushDestination(under_test) as push_target_directory:
-      src = os.path.join(_TEST_DATA_DIR, 'push_file.txt')
-      dest = posixpath.join(push_target_directory, 'push_file.txt')
-      with self.assertRaises(device_errors.AdbShellCommandFailedError):
-        under_test.Shell('ls %s' % dest)
-      under_test.Push(src, dest)
-      self.assertEqual(dest, under_test.Shell('ls %s' % dest).strip())
+    for par in _ADB_SHELL_PARAM_LIST:
+      under_test = self.getTestInstance(par)
+      with self.getTestPushDestination(under_test) as push_target_directory:
+        src = os.path.join(_TEST_DATA_DIR, 'push_file.txt')
+        dest = posixpath.join(push_target_directory, 'push_file.txt')
+        with self.assertRaises(device_errors.AdbShellCommandFailedError):
+          under_test.Shell('ls %s' % dest)
+        under_test.Push(src, dest)
+        self.assertEqual(dest, under_test.Shell('ls %s' % dest).strip())
+      under_test.KillPersistentAdbs()
 
   def testPush_fileToDirectory(self):
-    under_test = self.getTestInstance()
-    with self.getTestPushDestination(under_test) as push_target_directory:
-      src = os.path.join(_TEST_DATA_DIR, 'push_file.txt')
-      dest = push_target_directory
-      resulting_file = posixpath.join(dest, 'push_file.txt')
-      with self.assertRaises(device_errors.AdbShellCommandFailedError):
-        under_test.Shell('ls %s' % resulting_file)
-      under_test.Push(src, dest)
-      self.assertEqual(resulting_file,
-                       under_test.Shell('ls %s' % resulting_file).strip())
+    for par in _ADB_SHELL_PARAM_LIST:
+      under_test = self.getTestInstance(par)
+      with self.getTestPushDestination(under_test) as push_target_directory:
+        src = os.path.join(_TEST_DATA_DIR, 'push_file.txt')
+        dest = push_target_directory
+        resulting_file = posixpath.join(dest, 'push_file.txt')
+        with self.assertRaises(device_errors.AdbShellCommandFailedError):
+          under_test.Shell('ls %s' % resulting_file)
+        under_test.Push(src, dest)
+        self.assertEqual(resulting_file,
+                         under_test.Shell('ls %s' % resulting_file).strip())
+      under_test.KillPersistentAdbs()
 
   def testPush_directoryToDirectory(self):
-    under_test = self.getTestInstance()
-    with self.getTestPushDestination(under_test) as push_target_directory:
-      src = os.path.join(_TEST_DATA_DIR, 'push_directory')
-      dest = posixpath.join(push_target_directory, 'push_directory')
-      with self.assertRaises(device_errors.AdbShellCommandFailedError):
-        under_test.Shell('ls %s' % dest)
-      under_test.Push(src, dest)
-      self.assertEqual(sorted(os.listdir(src)),
-                       sorted(under_test.Shell('ls %s' % dest).strip().split()))
+    for par in _ADB_SHELL_PARAM_LIST:
+      under_test = self.getTestInstance(par)
+      with self.getTestPushDestination(under_test) as push_target_directory:
+        src = os.path.join(_TEST_DATA_DIR, 'push_directory')
+        dest = posixpath.join(push_target_directory, 'push_directory')
+        with self.assertRaises(device_errors.AdbShellCommandFailedError):
+          under_test.Shell('ls %s' % dest)
+        under_test.Push(src, dest)
+        self.assertEqual(
+            sorted(os.listdir(src)),
+            sorted(under_test.Shell('ls %s' % dest).strip().split()))
+      under_test.KillPersistentAdbs()
 
   def testPush_directoryToExistingDirectory(self):
-    under_test = self.getTestInstance()
-    with self.getTestPushDestination(under_test) as push_target_directory:
-      src = os.path.join(_TEST_DATA_DIR, 'push_directory')
-      dest = push_target_directory
-      resulting_directory = posixpath.join(dest, 'push_directory')
-      with self.assertRaises(device_errors.AdbShellCommandFailedError):
-        under_test.Shell('ls %s' % resulting_directory)
-      under_test.Shell('mkdir %s' % resulting_directory)
-      under_test.Push(src, dest)
-      self.assertEqual(
-          sorted(os.listdir(src)),
-          sorted(under_test.Shell('ls %s' % resulting_directory).split()))
+    for par in _ADB_SHELL_PARAM_LIST:
+      under_test = self.getTestInstance(par)
+      with self.getTestPushDestination(under_test) as push_target_directory:
+        src = os.path.join(_TEST_DATA_DIR, 'push_directory')
+        dest = push_target_directory
+        resulting_directory = posixpath.join(dest, 'push_directory')
+        with self.assertRaises(device_errors.AdbShellCommandFailedError):
+          under_test.Shell('ls %s' % resulting_directory)
+        under_test.Shell('mkdir %s' % resulting_directory)
+        under_test.Push(src, dest)
+        self.assertEqual(
+            sorted(os.listdir(src)),
+            sorted(under_test.Shell('ls %s' % resulting_directory).split()))
+      under_test.KillPersistentAdbs()
 
   # TODO(jbudorick): Implement tests for the following:
   # taskset -c
