0f8da34e57d6d18d8a47a5c997a569aea76a91eb
Michael Bas <starhunter17017@gmail.com>
2022-05-12 05:41:15 +0000

    ./mouse_click
    Change-Id: I8370852ed1b66a1d53e3f4a700e4be38110be66b

--- telemetry/telemetry/internal/actions/mouse_click.py
+++ telemetry/telemetry/internal/actions/mouse_click.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python3
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 from __future__ import absolute_import
-from telemetry.internal.actions import page_action
-from telemetry.internal.actions import utils
+from __future__ import division
+from __future__ import print_function
 
 
-class MouseClickAction(page_action.ElementPageAction):
+class MouseClickAction(object):
 
   def WillRunAction(self, tab):
     """Load the mouse click JS code prior to running the action."""
