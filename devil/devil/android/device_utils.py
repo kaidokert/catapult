e917678ee34ef17486fcb6b2c9635965f67bec67
Ben Joyce <bjoyce@google.com>
2022-07-26 16:53:58 -0700

    [Catapult]: Use PersistentShell for device_util.RunShellCommand
    This is a continuation of:
    https://chromium-review.googlesource.com/c/catapult/+/1947706
    
    Due to permissiosn issues I cannot upload to a CL review that
    is a WIP. ( ! [remote rejected]     437c997eb24b0bfb0e03a310ffc3b696f74a2e3f
    -> refs/for/refs/heads/main%wip,m=Initial_upload,cc=catapult-reviews@chromium.org
    (only users with Toggle-Wip-State permission can modify Work-in-Progress)
    )
    
    "adb shell" is oddly expensive. It takes about 30ms on an emulator
    and 100ms on a real device just to spin-up to do an echo.
    
    Chaining commands from the test runner via multiple invocations
    gets expensive fast. Currently chrome_public_test_apk makes 10
    adb shell invocations per test which adds at least 300ms to each
    test.
    
    This CL changes device_util.RunShellCommand to spin up a
    PersistentShell object which can be reused over the lifetime of
    the script.
    
    It also makes a fix to the PersistentShell for the "non-close"
    mode to avoid a deadlock prone coding pattern that can occur if
    simultaneously reading and writing from pipes to a popen()ed
    subprocess.
    
    Without any further changes, this takes test-to-test overhead
    from ~650ms down to 250ms.
    
    Bug: chromium:1021686
    Change-Id: I0eaef44be9ce5475c5fe8aab11ddf5f7bcb91eab

--- devil/devil/android/device_utils.py
+++ devil/devil/android/device_utils.py
@@ -357,10 +357,11 @@ def _JoinLines(lines):
   return ''.join(s for line in lines for s in (line, '\n'))
 
 
-def _CreateAdbWrapper(device):
+def _CreateAdbWrapper(device, **kwargs):
   if isinstance(device, adb_wrapper.AdbWrapper):
     return device
-  return adb_wrapper.AdbWrapper(device)
+
+  return adb_wrapper.AdbWrapper(device, **kwargs)
 
 
 def _FormatPartialOutputError(output):
@@ -444,7 +445,8 @@ class DeviceUtils(object):
                device,
                enable_device_files_cache=False,
                default_timeout=_DEFAULT_TIMEOUT,
-               default_retries=_DEFAULT_RETRIES):
+               default_retries=_DEFAULT_RETRIES,
+               persistent_shell=False):
     """DeviceUtils constructor.
 
     Args:
@@ -458,8 +460,10 @@ class DeviceUtils(object):
         operation should be retried on failure if no explicit value is provided.
     """
     self.adb = None
+    # persistent_shell = True
     if isinstance(device, six.string_types):
-      self.adb = _CreateAdbWrapper(device)
+      self.adb = _CreateAdbWrapper(device, persistent_shell=persistent_shell)
+
     elif isinstance(device, adb_wrapper.AdbWrapper):
       self.adb = device
     else:
@@ -3763,6 +3767,7 @@ class DeviceUtils(object):
                      retries=1,
                      enable_usb_resets=False,
                      abis=None,
+                     persistent_shell=False,
                      **kwargs):
     """Returns a list of DeviceUtils instances.
 
@@ -3793,6 +3798,9 @@ class DeviceUtils(object):
           those that appear to be android devices.
       abis: A list of ABIs for which the device needs to support at least one of
           (optional). See devil.android.ndk.abis for valid values.
+      persistent_shell: Makes AdbWrapper pipe commands through a single
+          "adb shell" instead of spawning a new shell each invocation. Can
+          save a lot of time as adb shell startup can be nearly 100ms.
       A device serial, or a list of device serials (optional).
 
     Returns:
@@ -3838,10 +3846,14 @@ class DeviceUtils(object):
         devices = [cls(x, **kwargs) for x in device_arg if not denylisted(x)]
       else:
         devices = []
-        for adb in adb_wrapper.AdbWrapper.Devices():
+        persistent_shell = True
+        for adb in adb_wrapper.AdbWrapper.Devices(
+            persistent_shell=persistent_shell):
           serial = adb.GetDeviceSerial()
           if not denylisted(serial):
-            device = cls(_CreateAdbWrapper(adb), **kwargs)
+            device = cls(
+                _CreateAdbWrapper(adb, persistent_shell=persistent_shell),
+                **kwargs)
             supported_abis = device.GetSupportedABIs()
             if not supported_abis:
               supported_abis = [device.GetABI()]
@@ -3899,6 +3911,8 @@ class DeviceUtils(object):
     self.RunShellCommand(['setprop', 'ctl.restart', 'adbd'],
                          check_return=False,
                          as_root=True)
+    # Need to kill persistent shells as the restart kills the connection.
+    self.adb.KillPersistentAdbs()
     self.adb.WaitForDevice()
 
   @decorators.WithTimeoutAndRetriesFromInstance()
