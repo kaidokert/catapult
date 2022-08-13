80908708c6f5003710beea906333425274cf24ae
Leina Sun <sunxiaodi@google.com>
2022-08-13 00:02:41 +0000

    [Chromeperf dash] migrate chart_chandler to Flask
    Bug: chromium:1298177
    Change-Id: I61a70a9a4fc44c34972d3a22b3518547ce3f6478

--- dashboard/dashboard/chart_handler.py
+++ dashboard/dashboard/chart_handler.py
@@ -9,27 +9,51 @@ from __future__ import absolute_import
 import json
 
 from dashboard.common import request_handler
+from dashboard.common import utils
 from dashboard import revision_info_client
 
-
-class ChartHandler(request_handler.RequestHandler):
+if utils.IsRunningFlask():
   """Base class for requests which display a chart."""
 
-  def RenderHtml(self, template_file, template_values, status=200):
+  def ChartHandlerRenderHtml(template_file, template_values, status=200):
     """Fills in template values for pages that show charts."""
-    template_values.update(self._GetChartValues())
+    template_values.update(_ChartHandlerGetChartValues())
     template_values['revision_info'] = json.dumps(
         template_values['revision_info'])
-    # TODO(https://crbug.com/1262292): Change to super() after Python2 trybots retire.
-    # pylint: disable=super-with-arguments
-    return super(ChartHandler, self).RenderHtml(template_file, template_values,
-                                                status)
+    return request_handler.RequestHandlerRenderHtml(template_file,
+                                                    template_values, status)
 
-  def GetDynamicVariables(self, template_values, request_path=None):
-    template_values.update(self._GetChartValues())
-    # TODO(https://crbug.com/1262292): Change to super() after Python2 trybots retire.
-    # pylint: disable=super-with-arguments
-    super(ChartHandler, self).GetDynamicVariables(template_values, request_path)
+  def ChartHandlerGetDynamicVariables(self, template_values, request_path=None):
+    template_values.update(_ChartHandlerGetChartValues)
+    return request_handler.RequestHandlerGetDynamicVariable(
+        template_values, request_path)
 
-  def _GetChartValues(self):
+  def _ChartHandlerGetChartValues():
     return {'revision_info': revision_info_client.GetRevisionInfoConfig()}
+
+else:
+
+  pass
+
+  class ChartHandler(request_handler.RequestHandler):
+    """Base class for requests which display a chart."""
+
+    def RenderHtml(self, template_file, template_values, status=200):
+      """Fills in template values for pages that show charts."""
+      template_values.update(self._GetChartValues())
+      template_values['revision_info'] = json.dumps(
+          template_values['revision_info'])
+      # TODO(https://crbug.com/1262292): Change to super() after Python2 trybots retire.
+      # pylint: disable=super-with-arguments
+      return super(ChartHandler, self).RenderHtml(template_file,
+                                                  template_values, status)
+
+    def GetDynamicVariables(self, template_values, request_path=None):
+      template_values.update(self._GetChartValues())
+      # TODO(https://crbug.com/1262292): Change to super() after Python2 trybots retire.
+      # pylint: disable=super-with-arguments
+      super(ChartHandler, self).GetDynamicVariables(template_values,
+                                                    request_path)
+
+    def _GetChartValues(self):
+      return {'revision_info': revision_info_client.GetRevisionInfoConfig()}
