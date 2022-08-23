fdb40da52112faba0a1b8e26a25ef79f2400196c
Leina Sun <sunxiaodi@google.com>
2022-08-23 19:08:44 +0000

    [Chromeperf dashboard] handler migration - alerts
    Bug: chromium:1298177
    Change-Id: I0947f23897d58d33c44a7a4c02b1e5747545296a

--- dashboard/dashboard/api/alerts.py
+++ dashboard/dashboard/api/alerts.py
@@ -14,17 +14,20 @@ from dashboard import alerts
 from dashboard.api import api_request_handler
 from dashboard.api import utils
 from dashboard.common import request_handler
+from dashboard.common import common_utils
 from dashboard.models import anomaly
 from dashboard.models import report_template
 
 
-# pylint: disable=abstract-method
-class AlertsHandler(api_request_handler.ApiRequestHandler):
+if common_utils.isRunningFlask():
+  from flask import request
+  
   """API handler for various alert requests."""
 
   def _CheckUser(self):
     pass
 
+  @api_request_handler.RequestHandlerDecoratorFactory(_CheckUser)
   def Post(self, *args, **kwargs):
     """Returns alert data in response to API requests.
 
@@ -40,42 +43,42 @@ class AlertsHandler(api_request_handler.ApiRequestHandler):
     alert_list = None
     response = {}
     try:
-      is_improvement = utils.ParseBool(self.request.get('is_improvement', None))
-      recovered = utils.ParseBool(self.request.get('recovered', None))
-      start_cursor = self.request.get('cursor', None)
+      is_improvement = utils.ParseBool(request.args.get('is_improvement', None))
+      recovered = utils.ParseBool(request.args.get('recovered', None))
+      start_cursor = request.args.get('cursor', None)
       if start_cursor:
         start_cursor = datastore_query.Cursor(urlsafe=start_cursor)
       min_timestamp = utils.ParseISO8601(
-          self.request.get('min_timestamp', None))
+          request.args.get('min_timestamp', None))
       max_timestamp = utils.ParseISO8601(
-          self.request.get('max_timestamp', None))
+          request.args.get('max_timestamp', None))
 
       test_keys = []
-      for template_id in self.request.get_all('report'):
+      for template_id in request.args.getlist('report'):
         test_keys.extend(report_template.TestKeysForReportTemplate(template_id))
 
       try:
-        sheriff = self.request.get('sheriff', None)
+        sheriff = request.args.get('sheriff', None)
         alert_list, next_cursor, count = anomaly.Anomaly.QueryAsync(
-            bot_name=self.request.get('bot', None),
-            bug_id=self.request.get('bug_id', None),
+            bot_name=request.args.get('bot', None),
+            bug_id=request.args.get('bug_id', None),
             is_improvement=is_improvement,
-            key=self.request.get('key', None),
-            limit=int(self.request.get('limit', 100)),
-            count_limit=int(self.request.get('count_limit', 0)),
-            master_name=self.request.get('master', None),
-            max_end_revision=self.request.get('max_end_revision', None),
-            max_start_revision=self.request.get('max_start_revision', None),
+            key=request.args.get('key', None),
+            limit=int(request.args.get('limit', 100)),
+            count_limit=int(request.args.get('count_limit', 0)),
+            master_name=request.args.get('master', None),
+            max_end_revision=request.args.get('max_end_revision', None),
+            max_start_revision=request.args.get('max_start_revision', None),
             max_timestamp=max_timestamp,
-            min_end_revision=self.request.get('min_end_revision', None),
-            min_start_revision=self.request.get('min_start_revision', None),
+            min_end_revision=request.args.get('min_end_revision', None),
+            min_start_revision=request.args.get('min_start_revision', None),
             min_timestamp=min_timestamp,
             recovered=recovered,
             subscriptions=[sheriff] if sheriff else None,
             start_cursor=start_cursor,
-            test=self.request.get('test', None),
+            test=request.args.get('test', None),
             test_keys=test_keys,
-            test_suite_name=self.request.get('test_suite', None)).get_result()
+            test_suite_name=request.args.get('test_suite', None)).get_result()
         response['count'] = count
       except AssertionError:
         alert_list, next_cursor = [], None
@@ -89,5 +92,80 @@ class AlertsHandler(api_request_handler.ApiRequestHandler):
       six.raise_from(api_request_handler.BadRequestError(str(e)), e)
 
     response['anomalies'] = alerts.AnomalyDicts(
-        alert_list, utils.ParseBool(self.request.get('v2', None)))
+        alert_list, utils.ParseBool(request.args.get('v2', None)))
     return response
+
+else:
+  # pylint: disable=abstract-method
+  class AlertsHandler(api_request_handler.ApiRequestHandler):
+    """API handler for various alert requests."""
+
+    def _CheckUser(self):
+      pass
+
+    def Post(self, *args, **kwargs):
+      """Returns alert data in response to API requests.
+
+      Possible list types:
+        keys: A comma-separated list of urlsafe Anomaly keys.
+        bug_id: A bug number on the Chromium issue tracker.
+        rev: A revision number.
+
+      Outputs:
+        Alerts data; see README.md.
+      """
+      del args, kwargs  # Unused.
+      alert_list = None
+      response = {}
+      try:
+        is_improvement = utils.ParseBool(self.request.get('is_improvement', None))
+        recovered = utils.ParseBool(self.request.get('recovered', None))
+        start_cursor = self.request.get('cursor', None)
+        if start_cursor:
+          start_cursor = datastore_query.Cursor(urlsafe=start_cursor)
+        min_timestamp = utils.ParseISO8601(
+            self.request.get('min_timestamp', None))
+        max_timestamp = utils.ParseISO8601(
+            self.request.get('max_timestamp', None))
+
+        test_keys = []
+        for template_id in self.request.get_all('report'):
+          test_keys.extend(report_template.TestKeysForReportTemplate(template_id))
+
+        try:
+          sheriff = self.request.get('sheriff', None)
+          alert_list, next_cursor, count = anomaly.Anomaly.QueryAsync(
+              bot_name=self.request.get('bot', None),
+              bug_id=self.request.get('bug_id', None),
+              is_improvement=is_improvement,
+              key=self.request.get('key', None),
+              limit=int(self.request.get('limit', 100)),
+              count_limit=int(self.request.get('count_limit', 0)),
+              master_name=self.request.get('master', None),
+              max_end_revision=self.request.get('max_end_revision', None),
+              max_start_revision=self.request.get('max_start_revision', None),
+              max_timestamp=max_timestamp,
+              min_end_revision=self.request.get('min_end_revision', None),
+              min_start_revision=self.request.get('min_start_revision', None),
+              min_timestamp=min_timestamp,
+              recovered=recovered,
+              subscriptions=[sheriff] if sheriff else None,
+              start_cursor=start_cursor,
+              test=self.request.get('test', None),
+              test_keys=test_keys,
+              test_suite_name=self.request.get('test_suite', None)).get_result()
+          response['count'] = count
+        except AssertionError:
+          alert_list, next_cursor = [], None
+        if next_cursor:
+          response['next_cursor'] = next_cursor.urlsafe()
+      except AssertionError as e:
+        # The only known assertion is in InternalOnlyModel._post_get_hook when a
+        # non-internal user requests an internal-only entity.
+        six.raise_from(api_request_handler.BadRequestError('Not found'), e)
+      except request_handler.InvalidInputError as e:
+        six.raise_from(api_request_handler.BadRequestError(str(e)), e)
+
+      response['anomalies'] = alerts.AnomalyDicts(
+          alert_list, utils.ParseBool(self.request.get('v2', None)))
+      return response
