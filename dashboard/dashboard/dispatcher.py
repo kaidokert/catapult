e4c81e5416cb549df292e50aaa9e26c1e18238a7
Leina Sun <sunxiaodi@google.com>
2022-08-23 22:32:13 +0000

    [Chromeperf dash] webapp2 handler migration to flask: edit_site_config
    Bug: chromium:1298177
    Change-Id: Ia8e6b6796103264d5db05f88210d21bb171cdeb6

--- dashboard/dashboard/dispatcher.py
+++ dashboard/dashboard/dispatcher.py
@@ -9,11 +9,18 @@ from __future__ import absolute_import
 from dashboard.common import utils
 
 if utils.IsRunningFlask():
+  from dashboard import edit_site_config
   from dashboard import main
   from dashboard import sheriff_config_poller
 
-  from flask import Flask
+  from flask import Flask, request
   APP = Flask(__name__)
+
+  @APP.route('/edit_site_config', methods=['GET','POST'])
+  def EditSiteConfigHandlerGet():
+    if request.method == 'GET':
+      return edit_site_config.EditSiteConfigHandlerGet()
+    return edit_site_config.EditSiteConfigHandlerPost()
 
   @APP.route('/')
   def MainHandlerGet():
