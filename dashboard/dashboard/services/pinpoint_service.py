4994fb95e89e7c0ea85d1d5e5c56093dea22271a
Wenbin Zhang <wenbinzhang@google.com>
2022-08-31 12:03:01 -0700

    [pinpoint] auto-bisect uses form instead of querystring to create new job
    Change-Id: I47bed151346a5b2d15d2efe87cb0db17a49ebc08

--- dashboard/dashboard/services/pinpoint_service.py
+++ dashboard/dashboard/services/pinpoint_service.py
@@ -26,8 +26,12 @@ def _Request(endpoint, params):
   assert datastore_hooks.IsUnalteredQueryPermitted()
 
   try:
-    return request.RequestJson(
-        endpoint, method='POST', use_cache=False, use_auth=True, **params)
+    if utils.IsRunningFlask():
+      return request.RequestJson(
+          endpoint, method='POST', use_cache=False, use_auth=True, **params)  
+    else:
+      return request.RequestJson(
+          endpoint, method='POST', use_cache=False, use_auth=True, body=params)
   except request.RequestError as e:
     return json.loads(e.content)
 
