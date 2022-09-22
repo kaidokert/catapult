ee3b9ac34fdeff10b52182840b21651cd0110592
Leina Sun <sunxiaodi@google.com>
2022-09-22 22:32:12 +0000

    [Chromeperf dash] short_uri handler migration to flask
    Bug: chromium:1298177
    Change-Id: Ic075beec8b1499df4b8cd72f4022608bc4fde67c

--- dashboard/dashboard/dispatcher.py
+++ dashboard/dashboard/dispatcher.py
@@ -16,6 +16,7 @@ from dashboard import graph_csv
 from dashboard import main
 from dashboard import navbar
 from dashboard import sheriff_config_poller
+from dashboard import short_uri
 from dashboard.api import describe
 
 from flask import Flask
@@ -30,6 +31,21 @@ def AlertsHandlerGet():
 @flask_app.route('/alerts', methods=['POST'])
 def AlertsHandlerPost():
   return alerts.AlertsHandlerPost()
+
+
+@flask_app.route('/add_histograms', methods=['POST'])
+def AddHistogramsPost():
+  return add_histograms.AddHistogramsPost()
+
+
+@flask_app.route('/add_histograms/process', methods=['POST'])
+def AddHistogramsProcessPost():
+  return add_histograms.AddHistogramsProcessPost()
+
+
+@flask_app.route('/api/describe', methods=['POST'])
+def DescribePost():
+  return describe.DescribePost()
 
 
 @flask_app.route('/edit_site_config', methods=['GET'])
@@ -52,6 +68,11 @@ def GraphCSVHandlerPost():
   return graph_csv.GraphCSVPost()
 
 
+@flask_app.route('/list_tests', methods=['POST'])
+def ListTestsHandlerPost():
+  return list_tests.ListTestsHandlerPost()
+
+
 @flask_app.route('/')
 def MainHandlerGet():
   return main.MainHandlerGet()
@@ -67,19 +88,14 @@ def SheriffConfigPollerGet():
   return sheriff_config_poller.SheriffConfigPollerGet()
 
 
-@flask_app.route('/add_histograms', methods=['POST'])
-def AddHistogramsPost():
-  return add_histograms.AddHistogramsPost()
+@flask_app.route('/short_uri', methods=['GET'])
+def ShortUriHandlerGet():
+  return short_uri.ShortUriHandlerGet()
 
 
-@flask_app.route('/add_histograms/process', methods=['POST'])
-def AddHistogramsProcessPost():
-  return add_histograms.AddHistogramsProcessPost()
-
-
-@flask_app.route('/api/describe', methods=['POST'])
-def DescribePost():
-  return describe.DescribePost()
+@flask_app.route('/short_uri', methods=['POST'])
+def ShortUriHandlerPost():
+  return short_uri.ShortUriHandlerPost()
 
 
 if six.PY2:
@@ -223,7 +239,9 @@ _PATHS_HANDLED_BY_FLASK = [
     # '/add_histograms/process',
     '/edit_site_config',
     # '/graph_csv',
+    '/list_tests',
     # '/navbar',
+    '/short_uri',
 ]
 
 
