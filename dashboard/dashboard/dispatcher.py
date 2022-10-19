cf9a91e1469813cc3896443aa95c7585985a09dc
Funing Wang <funing@google.com>
2022-10-19 00:11:04 +0000

    [Chromeperf dash] migrate handlers: /pinpoint/new/perf_try and /pinpoint/new/prefill
    Bug: chromium:1298177
    Change-Id: If5ad1106ace75bcc72ffaf93f152a8536243c051

--- dashboard/dashboard/dispatcher.py
+++ dashboard/dashboard/dispatcher.py
@@ -154,6 +154,16 @@ def PinpointNewBisectPost():
   return pinpoint_request.PinpointNewBisectPost()
 
 
+@flask_app.route('/pinpoint/new/perf_try', methods=['POST'])
+def PinpointNewPerfTryPost():
+  return pinpoint_request.PinpointNewPerfTryPost()
+
+
+@flask_app.route('/pinpoint/new/prefill', methods=['POST'])
+def PinpointNewPrefillPost():
+  return pinpoint_request.PinpointNewPrefillPost()
+
+
 @flask_app.route('/configs/update')
 def SheriffConfigPollerGet():
   return sheriff_config_poller.SheriffConfigPollerGet()
@@ -307,6 +317,8 @@ _PATHS_HANDLED_BY_FLASK = [
     '/list_tests',
     '/navbar',
     '/pinpoint/new/bisect',
+    '/pinpoint/new/perf_try',
+    '/pinpoint/new/prefill',
     '/short_uri',
 ]
 
