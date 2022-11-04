d6c1d0db31c9e34f4a149cb3a657c3cf68930b58
John Abd-El-Malek <jam@chromium.org>
2022-11-04 22:12:20 +0000

    Test
    Change-Id: Ibd8143f502ead8b5e8c59045af170c6ff3521310

--- telemetry/telemetry/core/network_controller.py
+++ telemetry/telemetry/core/network_controller.py
@@ -41,6 +41,8 @@ class NetworkController(
 
   def StartReplay(self, archive_path, make_javascript_deterministic=False,
                   extra_wpr_args=None):
+    self._network_controller_backend.ts_proxy_server.UpdateTrafficSettings(200, 10000, 1000)
+
     extra_wpr_args = tuple(extra_wpr_args) if extra_wpr_args else ()
     self._network_controller_backend.StartReplay(
         archive_path, make_javascript_deterministic, extra_wpr_args)
