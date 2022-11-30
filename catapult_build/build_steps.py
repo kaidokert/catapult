373efc3ddab93b575166d0350e4b66b0820688e0
Wenbin Zhang <wenbinzhang@google.com>
2022-11-29 19:05:58 -0800

    [chromeperf] Update proto files to allow correct absolute imports in python 3
    Change-Id: Ic6490824904d190ebd5d326997e63577c6a7789d

--- catapult_build/build_steps.py
+++ catapult_build/build_steps.py
@@ -232,10 +232,12 @@ def main(args=None):
       for p in ['sheriff.proto', 'sheriff_config.proto']
   ]
 
+  dashboard_protos_path = os.path.join(args.api_path_checkout, 'dashboard')
+
   sheriff_proto_output_path = os.path.join(args.api_path_checkout, 'dashboard',
                                            'dashboard', 'sheriff_config')
   dashboard_proto_output_path = os.path.join(args.api_path_checkout,
-                                             'dashboard', 'dashboard')
+                                             'dashboard', 'dashboard', 'proto')
 
   tracing_protos_path = os.path.join(args.api_path_checkout, 'tracing',
                                      'tracing', 'proto')
@@ -295,6 +297,9 @@ def main(args=None):
           ] + tracing_proto_files,
       },
   ]
+  steps.append(
+    {'name': 'Stay here', 'cmd': ['sleep', '7200']}
+  )
   if args.platform == 'android' and args.run_android_tests:
     # On Android, we need to prepare the devices a bit before using them in
     # tests. These steps are not listed as tests above because they aren't
@@ -391,6 +396,7 @@ def main(args=None):
     if args.use_python3:
       step['always_run'] = True
     steps.append(step)
+
   with open(args.output_json, 'w') as outfile:
     json.dump(steps, outfile)
 
