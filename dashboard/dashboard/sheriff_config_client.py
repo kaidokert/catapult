ca26a23db57035e7adcfa78c72c6e07d833aceea
Wenbin Zhang <wenbinzhang@google.com>
2022-11-29 19:32:08 -0800

    [chromeperf] Update proto files to allow correct absolute imports in python 3
    Change-Id: Ic6490824904d190ebd5d326997e63577c6a7789d

--- dashboard/dashboard/sheriff_config_client.py
+++ dashboard/dashboard/sheriff_config_client.py
@@ -43,8 +43,17 @@ class SheriffConfigClient(object):
     from dashboard.models.subscription import Subscription, AnomalyConfig
     # pylint: disable=import-outside-toplevel
     from google.protobuf import json_format
+
+    import os
+    print(os.listdir('/b/s/w/ir/x/w/catapult/dashboard/'))
+    print(os.listdir('/b/s/w/ir/x/w/catapult/dashboard/dashboard/'))
+    print(os.listdir('/b/s/w/ir/x/w/catapult/dashboard/dashboard/proto/'))
+
     # pylint: disable=import-outside-toplevel
-    from dashboard import sheriff_config_pb2
+    try:
+      from dashboard.proto import sheriff_config_pb2
+    except ImportError:
+      from proto import sheriff_config_pb2
     self._GetEmail = GetEmail  # pylint: disable=invalid-name
     SheriffConfigClient._Subscription = Subscription
     SheriffConfigClient._AnomalyConfig = AnomalyConfig
