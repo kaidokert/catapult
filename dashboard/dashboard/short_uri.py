1c74fb8f7a2cb64cfb22f2072c20bfb393e47316
Leina Sun <sunxiaodi@google.com>
2022-12-12 23:03:36 +0000

    short_uri encode string before generating hash
    CL addresses Internal Service Errors on Chromeperf UI when generating
    report graphs. ISE caused by "Strings must be encoded before hashing"
    
    Bug: chromium:1298177
    Change-Id: I0337b1c6f734a81738416414243bd93eb0f3de35

--- dashboard/dashboard/short_uri.py
+++ dashboard/dashboard/short_uri.py
@@ -119,7 +119,7 @@ def GetOrCreatePageState(state):
 
 def GenerateHash(state_string):
   """Generates a hash for a state string."""
-  return hashlib.sha256(state_string).hexdigest()
+  return hashlib.sha256(state_string.encode('utf-8')).hexdigest()
 
 
 def _UpgradeChart(chart):
