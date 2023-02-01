6715bf44257071367a9a5c1889f4b077bb6f7d65
Eduardo Yap <eduardoyap@google.com>
2023-02-01 02:38:41 +0000

    Add unit tests for email_template.
    Bug: chromium:1410554
    Change-Id: Ic60d143ac763b6655033ece9f384e34fd5f442bc

--- dashboard/dashboard/email_template_test.py
+++ dashboard/dashboard/email_template_test.py
@@ -6,9 +6,12 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+import six
 import unittest
 
 from dashboard import email_template
+from dashboard.common import utils
+from dashboard.models import anomaly
 
 
 class EmailTemplateTest(unittest.TestCase):
@@ -31,6 +34,38 @@ class EmailTemplateTest(unittest.TestCase):
                       'abc-perf-test%2Fpassed%25&checked=passed%25%2C'
                       'passed%25_ref%2Cref&rev=1415919839'),
                      actual_output_no_host)
+
+  def testGetReportPageLink(self):
+    test_path = 'a/b/c/d'
+    link = email_template.GetReportPageLink(
+        test_path, rev=None, add_protocol_and_host=True)
+    self.assertEqual(
+        link,
+        'https://chromeperf.appspot.com/report?masters=a&bots=b&tests=c%2Fd&checked=d%2Cd_ref%2Cref'
+    )
+
+  def testGetReportPageLinkWithRev(self):
+    test_path = 'a/b/c/d'
+    link = email_template.GetReportPageLink(
+        test_path, rev='1234', add_protocol_and_host=False)
+    self.assertEqual(
+        link,
+        '/report?masters=a&bots=b&tests=c%2Fd&checked=d%2Cd_ref%2Cref&rev=1234')
+
+  def testGetGroupReportPageLink(self):
+    a1 = anomaly.Anomaly(id='test_id')
+    a2 = anomaly.Anomaly(test=utils.TestKey('a/b/c/d'))
+
+    link1 = email_template.GetGroupReportPageLink(a1)
+    link2 = email_template.GetGroupReportPageLink(a2)
+
+    self.assertEqual(
+        link1, 'https://chromeperf.appspot.com/group_report?keys=%s' %
+        six.ensure_str(a1.key.urlsafe()))
+    self.assertEqual(
+        link2,
+        'https://chromeperf.appspot.com/report?masters=a&bots=b&tests=c%2Fd&checked=d%2Cd_ref%2Cref'
+    )
 
 
 if __name__ == '__main__':
