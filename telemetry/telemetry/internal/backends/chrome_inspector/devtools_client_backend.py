03c007e7961176bec053fc2ca2d1413b9787bcce
Camillo Bruni <cbruni@chromium.org>
2023-02-01 22:12:36 +0100

    [chrome_inspector] Increase CollectChromeTracingData timeout
    It's unclear whether we're actually encounterin a real timeout when
    loading large trace files or if there is another lingering bug.
    
    This CL temporarily increases the timeout from 120s to 240s.
    
    Bug: chromium:1395482
    Change-Id: Ib40d0e44a09e6cbc95a03d2354e0bc232df4c5d5

--- telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend.py
+++ telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend.py
@@ -436,7 +436,7 @@ class _DevToolsClientBackend():
     """Obtain the inspector backend for the firstly created tab."""
     return next(self._IterInspectorBackends(['page']), None)
 
-  def CollectChromeTracingData(self, trace_data_builder, timeout=120):
+  def CollectChromeTracingData(self, trace_data_builder, timeout=240):
     if not self._tracing_backend:
       return
 
