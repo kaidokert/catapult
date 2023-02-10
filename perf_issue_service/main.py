5e0a65497a359432bf8a072c407af18eea3e58d7
Wenbin Zhang <wenbinzhang@google.com>
2023-02-10 14:42:48 -0800

    [chromeperf] add GET endpoints for perf issue service
    Bug: chromium:1412858
    Change-Id: I60a84a368e32a8f74d937b71d4f47f4a86f5e1f1

--- perf_issue_service/main.py
+++ perf_issue_service/main.py
@@ -3,7 +3,7 @@
 # found in the LICENSE file.
 """Dispatches requests to request handler classes."""
 
-from flask import Flask, request as flask_request, make_response
+from flask import Flask, request, make_response
 import logging
 
 import google.cloud.logging
@@ -13,6 +13,8 @@ try:
 except ImportError:
   pass
 
+import issue_tracker_client
+
 google.cloud.logging.Client().setup_logging(log_level=logging.DEBUG)
 
 APP = Flask(__name__)
@@ -21,6 +23,42 @@ APP = Flask(__name__)
 @APP.route('/')
 def DummyHandler():
   return make_response('welcome')
+
+
+@APP.route('/issues', methods=['GET'])
+def IssuesGetHandler():
+  limit = request.args.get('limit', '2000')
+  age = request.args.get('age', '3')
+  status = request.args.get('status', 'open')
+  labels = request.args.get('labels', '')
+
+  issue_tracker = issue_tracker_client.IssueTrackerService()
+  response = issue_tracker.GetIssuesList(
+      q='opened>today-%s' % age,
+      can=status,
+      label=labels,
+      maxResults=limit,
+      sort='-id')
+  return make_response(response.get('items', []) if response else [])
+
+
+@APP.route('/issues/<issue_id>/project/<project_name>', methods=['GET'])
+def IssuesGetByIdHandler(issue_id, project_name):
+  issue_tracker = issue_tracker_client.IssueTrackerService()
+  response = issue_tracker.GetIssue(
+      issue_id=issue_id,
+      project=project_name)
+  return make_response(response)
+
+
+@APP.route('/issues/<issue_id>/project/<project_name>/comments', methods=['GET'])
+def CommentsHandler(issue_id, project_name):
+  issue_tracker = issue_tracker_client.IssueTrackerService()
+  response = issue_tracker.GetIssueComments(
+      issue_id=bug_id,
+      project=project_name)
+  return make_response(response)
+
 
 if __name__ == '__main__':
   # This is used when running locally only.
