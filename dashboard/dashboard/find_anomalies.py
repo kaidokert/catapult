35f8afffaafed01aab3bd49561e927da6769eb3c
Eduardo Yap <eduardoyap@google.com>
2023-02-28 19:46:28 +0000

    Bug: chromium:1410304
    Change-Id: I9fcfe547a336ab8e9470ff1754fdaced31329166

--- dashboard/dashboard/find_anomalies.py
+++ dashboard/dashboard/find_anomalies.py
@@ -21,7 +21,6 @@ from dashboard import find_change_points
 from dashboard.common import utils
 from dashboard.models import alert_group
 from dashboard.models import anomaly
-from dashboard.models import anomaly_config
 from dashboard.models import graph_data
 from dashboard.models import histogram
 from dashboard.models import subscription
@@ -30,7 +29,7 @@ from tracing.value.diagnostics import reserved_infos
 
 # Number of points to fetch and pass to FindChangePoints. A different number
 # may be used if a test has a "max_window_size" anomaly config parameter.
-DEFAULT_NUM_POINTS = 50
+DEFAULT_NUM_POINTS = 150
 
 
 @ndb.synctasklet
@@ -67,8 +66,7 @@ def _ProcessTest(test_key):
 
   test = yield test_key.get_async()
 
-  config = yield anomaly_config.GetAnomalyConfigDictAsync(test)
-  max_num_rows = config.get('max_window_size', DEFAULT_NUM_POINTS)
+  max_num_rows = DEFAULT_NUM_POINTS
   rows_by_stat = yield GetRowsToAnalyzeAsync(test, max_num_rows)
 
   ref_rows_by_stat = {}
