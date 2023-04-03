654fb0aa8e78d37310054402876756d0507563fb
Jeff Yoon <jeffyoon@google.com>
2023-04-03 11:17:00 -0700

    Add more logging for lacros-chrome launch
    Adding a couple more log statements to ensure that lacros
    is launching.
    
    Bug: chromium:1417071
    Change-Id: I63a0b297cf68c57eeb567dc1e62fb20beb840819

--- telemetry/telemetry/internal/backends/chrome/lacros_browser_backend.py
+++ telemetry/telemetry/internal/backends/chrome/lacros_browser_backend.py
@@ -121,6 +121,8 @@ class LacrosBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     self._ReformatArg(startup_args, 'proxy-bypass-list')
     self._ReformatArg(startup_args, 'user-agent')
 
+    logging.info('Starting Chrome w/ Args: %s', startup_args)
+
     def _Launch():
       # This will block until the launched browser is closed.
       self._RunCommandAndLog(
@@ -169,6 +171,7 @@ class LacrosBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     self._dump_finder = minidump_finder.MinidumpFinder(
         self.browser.platform.GetOSName(), self.browser.platform.GetArchName())
 
+    logging.info('Launching Lacros Chrome')
     self.LaunchLacrosChrome(startup_args)
     if not self._is_browser_running:
       raise EnvironmentError('Unable to launch Lacros. If this is a local '
