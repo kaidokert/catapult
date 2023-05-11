76ec012b9641392ed4656e33b38a5ef2bb37c4d9
Ashwin Verleker <ashwinpv@google.com>
2023-05-11 00:16:12 +0000

    [skia-bridge] Add skia service account to allow list and add cloud metrics
    Change-Id: I5b829d36d5a7921614541613866015898bee8682

--- skia_bridge/application/perf_api/query_anomalies.py
+++ skia_bridge/application/perf_api/query_anomalies.py
@@ -9,6 +9,7 @@ from flask import Blueprint, request, jsonify
 import logging
 import json
 
+from common import cloud_metric
 from application.perf_api import datastore_client, auth_helper
 
 from google.cloud import datastore
@@ -17,8 +18,9 @@ blueprint = Blueprint('query_anomalies', __name__)
 
 
 ALLOWED_CLIENTS = [
-    # TODO: Add the service account used by skia and remove the account below.
-    'ashwinpv@google.com'
+    # TODO: Remove the account below once skia service account is validated.
+    'ashwinpv@google.com',
+    'perf-chrome-public@skia-infra-public.iam.gserviceaccount.com',
 ]
 
 def Serialize(value):
@@ -58,6 +60,7 @@ class AnomalyData:
     return json.dumps(self, default=Serialize)
 
 
+@cloud_metric.APIMetric("skia-bridge", "/anomalies/find")
 @blueprint.route('/find', methods=['POST'])
 def QueryAnomaliesPostHandler():
   try:
