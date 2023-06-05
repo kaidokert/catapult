ba577e70d6c3c7304914ac9fc33359d9d32b2e4d
Alex Sun <sunpeng@google.com>
2023-06-05 10:22:18 -0700

    Enable the min low threshold and high threshold to decrease the bisect job false negative rate
    Change-Id: I05b8ad110559ada6b5cb3441d94eae5d87cd0bf3

--- dashboard/dashboard/pinpoint/models/tasks/performance_bisection_test.py
+++ dashboard/dashboard/pinpoint/models/tasks/performance_bisection_test.py
@@ -104,7 +104,7 @@ class EvaluatorTest(bisection_test_util.BisectionTestBase):
                         }]
                     }): values for commit, values in (
                         ('commit_0', list(range(10))),
-                        ('commit_1', list(range(1, 11))),
+                        ('commit_1', list(range(10))),
                         ('commit_2', list(range(2, 12))),
                         ('commit_3', list(range(3, 13))),
                         ('commit_4', list(range(3, 13))),
