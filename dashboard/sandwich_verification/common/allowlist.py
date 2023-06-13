63cb7baefd6e336b3704ff945537a3bf5b5e3e0c
Leina Sun <sunxiaodi@google.com>
2023-06-13 17:52:43 +0000

    Create benchmark and device allowlist for sandwiching method
    Bug: chromium:1419903
    Change-Id: I0abd69f9093f526d14b8c9ace5a56ca882ac812e

---
+++ dashboard/sandwich_verification/common/allowlist.py
@@ -0,0 +1,17 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+ALLOWABLE_BENCHMARKS = ['jetstream2','speedometer2']
+ALLOWABLE_DEVICES = [
+    'android-pixel2-perf',
+    'android-pixel2_webview-perf'
+    'android-pixel4-perf',
+    'linux-perf',
+    'mac-laptop_high_end-perf',
+    'mac-laptop_low_end-perf',
+    'mac-m1_mini_2020-perf',
+    'win-10_amd_laptop-perf',
+    'win-10_laptop_low_end-perf',
+    'win-10-perf',
+    ]
