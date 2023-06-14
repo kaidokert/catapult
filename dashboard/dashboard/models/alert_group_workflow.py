873576c69f1c002d0375693e35d710a4d1565c32
Leina Sun <sunxiaodi@google.com>
2023-06-14 22:25:29 +0000

    [sandwich] create skeleton for alert_group_workflow.Process to start with regression verification
    Bug: chromium:1454620
    Change-Id: Ibe01f0c960f8db56ddf41d54ba398b94473a3c31

--- dashboard/dashboard/models/alert_group_workflow.py
+++ dashboard/dashboard/models/alert_group_workflow.py
@@ -57,6 +57,8 @@ _TEMPLATE_ISSUE_CONTENT = _TEMPLATE_ENV.get_template(
 _TEMPLATE_ISSUE_COMMENT = _TEMPLATE_ENV.get_template(
     'alert_groups_bug_comment.j2')
 _TEMPLATE_REOPEN_COMMENT = _TEMPLATE_ENV.get_template('reopen_issue_comment.j2')
+_TEMPLATE_AUTO_REGRESSION_VERIFICATION_COMMENT = _TEMPLATE_ENV.get_template(
+    'auto_regression_verification_comment.j2')
 _TEMPLATE_AUTO_BISECT_COMMENT = _TEMPLATE_ENV.get_template(
     'auto_bisect_comment.j2')
 _TEMPLATE_GROUP_WAS_MERGED = _TEMPLATE_ENV.get_template(
@@ -198,8 +200,9 @@ class AlertGroupWorkflow:
 
     # Parity check for canonical group
     try:
-      canonical_group_key = perf_issue_service_client.GetCanonicalGroupByIssue(
+      canonical_group_new = perf_issue_service_client.GetCanonicalGroupByIssue(
           self._group.key.string_id(), merged_into, merged_issue_project)
+      canonical_group_key = canonical_group_new.get('key')
       original_canonical_key = canonical_group.key.string_id()
       if original_canonical_key != canonical_group_key:
         logging.warning('Imparity found for GetCanonicalGroupByIssue. %s, %s',
@@ -362,6 +365,12 @@ class AlertGroupWorkflow:
                    group.created, self._config.triage_delay, update.now,
                    group.status)
       self._TryTriage(update.now, update.anomalies)
+    # TODO(crbug/1454620): Add logic to start sandwich verification when
+    # the regression has not yet been verified and to start bisection if
+    # the bug is verified or there are no regressions in sandwich allowlist
+    # TODO(crbug/1454620): replace with group.Status.verified_regressions
+    # and update
+    # third_party/catapult/dashboard/dashboard/models/alert_group.py;l=48
     elif group.status in {group.Status.triaged}:
       self._TryBisect(update)
     return self._CommitGroup()
@@ -664,6 +673,48 @@ class AlertGroupWorkflow:
         project=self._group.project_id)
     return True
 
+  def _CheckSandwichAllowlist(self, regressions):
+    """Filter list of regressions against the sandwich verification allowlist
+
+    Args:
+      regressions: A list of regressions in the anomaly group.
+
+    Returns:
+      allowed_regressions: A list of sandwich verifiable regressions.
+    """
+    allowed_regressions = regressions.copy()
+    return allowed_regressions
+
+  def _TryVerifyRegression(self, update):
+    """Verify the selected regression using the sandwich verification workflow.
+
+    Args:
+      update: An alert group containing anomalies and potential regressions
+
+    Returns:
+      True or False.
+    """
+    # Do not run sandwiching if anomaly subscription opts out of culprit finding
+    if (update.issue
+        and 'Chromeperf-Auto-BisectOptOut' in update.issue.get('labels')):
+      return False
+
+    # check if any regressions qualify for verification
+    regressions, _ = self._GetRegressions(update.anomalies)
+    verifiable_regressions = self._CheckSandwichAllowlist(regressions)
+    regression = self._SelectAutoBisectRegression(verifiable_regressions)
+
+    if not regression:
+      return False
+
+    self._StartPinpointTryJob(regression)
+
+    # TODO(crbug/1454620): Update the issue associated with this group,
+    # using the same logic in _TryBisect. Use
+    # _TEMPLATE_AUTO_REGRESSION_VERIFICATION_COMMENT as the bug template
+
+    return True
+
   def _TryBisect(self, update):
     if (update.issue
         and 'Chromeperf-Auto-BisectOptOut' in update.issue.get('labels')):
@@ -758,6 +809,17 @@ class AlertGroupWorkflow:
         project=self._group.project_id,
         bug_id=response['issue_id'],
     ), anomalies
+
+  def _StartPinpointTryJob(self, ):
+    """Call sandwich verification workflow to kick off a verification try job
+
+    Args:
+      regression: A regression in a CABE compatible benchmark/workload/device
+
+    Returns:
+      job_id: A string representing the Pinpoint job ID
+    """
+    raise NotImplementedError("crbug/1454620")
 
   def _StartPinpointBisectJob(self, regression):
     try:
