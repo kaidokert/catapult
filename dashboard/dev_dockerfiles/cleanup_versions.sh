3d8224a4b95c032b654d5f430555e59a6e31aec7
Hao Wu <haowoo@google.com>
2023-06-14 14:08:33 -0700

    Chang build retention.
    * This CL adds another argument to specify how many days back to clean up;
    * This CL adds the clean up script to skia-bridge and perf-issue services;
    * This CL decrease the build rentetion to 21 days to save up spaces as the build is failing due to version quota.
    
    Change-Id: I5f9adf4384a60efe8587984cbbec1eeea4117594

--- dashboard/dev_dockerfiles/cleanup_versions.sh
+++ dashboard/dev_dockerfiles/cleanup_versions.sh
@@ -3,11 +3,22 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Usage: days service_name1 service_name2 ...
+# Args:
+#   days: number of days from today to be deleted
+#   service_name: the builds for the service to be deleted
+#
+# This script is to clean up the last x days of builds that don't have
+# any traffic.
+#
+
+days="$1"; shift
 for SERVICE in "$@"; do
   gcloud app versions list \
     --format="table[no-heading](VERSION.ID)" \
     --filter="SERVICE=${SERVICE} AND
               TRAFFIC_SPLIT=0 AND
-              LAST_DEPLOYED.date()<$(date -I --date='-28 day')" \
+              LAST_DEPLOYED.date()<`date -I -v-${days}d`" \
   | xargs --no-run-if-empty gcloud app versions delete -s ${SERVICE}
 done
+
