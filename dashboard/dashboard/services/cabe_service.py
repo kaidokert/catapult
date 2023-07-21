e331f4516a37d83289a575ccd8425f2c3ba459e3
Sean McCullough <seanmccullough@google.com>
2023-07-21 23:32:25 +0000

    [chromeperf dashboard SCRATCH] - break this into separate CLs
    initial intent here was to call CABE from pinpoint, HOWEVER...
    
    pinpoint couldn't import dashboard/sandwich_verification/common/cabe_service.py
    because cabe_service.py imported a sibling package containing the grpc stubs for cabe,
    and cabe_grpc's import path for pinpoint was different than it was for sandwich_verification. So, two python classes in the same repo have different, incompatible demands for what the name of third package, also in the same repo, should be.
    
    python's package import system is garbage.  Just... it's  just utter trash.
    
    So this change allows two different entry points in this same repo (the cloud functions
    flask server located in catapult/dashboard/sandwich_verification/main.py, and the pinpioint
    GAE server application located in cataput/dashboard/dashboard/main.py) to import a shared package which imports another shared package, without breaking python's tiny little import brain.
    
    It also adds some defensive measures to the cloud functions flask request handlers since they rely on flimsy notions of "json" request bodies. With no schema to validate or declare to callers so they can make sure they're passing everything they need to pass, this would otherwise just crash because a `None` value showed up somewhere deep in the bowels of this godforsaken mess, reams of stack frames past where it should have been caught by an automatically-generated validator.
    
    I feel bad for trying to fix any of this instead of just deleting it all.
    
    Change-Id: I4cfbcde3a3cb1406698cda8b1c14c548f0e1244b

--- dashboard/dashboard/services/cabe_service.py
+++ dashboard/dashboard/services/cabe_service.py
@@ -3,11 +3,13 @@
 # found in the LICENSE file.
 from __future__ import absolute_import
 
+import logging
+
 import grpc
 import cabe_grpc.service_pb2_grpc as cabe_grpc
 import cabe_grpc.service_pb2 as cabe_pb
 
-from google.auth import compute_engine
+import google.auth
 from google.auth.transport import grpc as google_auth_transport_grpc
 from google.auth.transport import requests as google_auth_transport_requests
 
@@ -17,11 +19,11 @@ _CABE_USE_PLAINTEXT = False
 def GetAnalysis(job_id):
   "Return a TryJob CABE Analysis as a list."
   request = google_auth_transport_requests.Request()
-
   if _CABE_USE_PLAINTEXT:
     channel = grpc.insecure_channel(_CABE_SERVER_ADDRESS)
   else:
-    credentials = compute_engine.Credentials()
+    credentials, project_id = google.auth.default()
+    logging.info("using default credentials for project_id: %s" % project_id)
     channel = google_auth_transport_grpc.secure_authorized_channel(
         credentials=credentials, request=request, target=_CABE_SERVER_ADDRESS)
 
