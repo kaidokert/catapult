b815b16c754fa3297348d3d108436af4d9e6dad6
Sean McCullough <seanmccullough@google.com>
2023-07-24 16:48:41 +0000

    [pinpoint] attempt to run cabe.GetAnalysis on every completed tryjob
    No functional changes for pinpoint, just log the cabe.GetAnalysis responses/exceptions on the call side in pinpoint.
    
    Bug: chromium:1466686
    Change-Id: I6a8a940ffd173ffbaf83e5e60f3f5b7ac64265b3

---
+++ dashboard/dashboard/services/cabe_service.py
@@ -0,0 +1,39 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from __future__ import absolute_import
+
+import logging
+import sys
+sys.path.append('../../../sandwich_verification')
+
+import grpc
+import sandwich_verification.cabe_grpc.service_pb2_grpc as cabe_grpc_pb
+import sandwich_verification.cabe_grpc.service_pb2 as cabe_pb
+
+import google.auth
+from google.auth.transport import grpc as google_auth_transport_grpc
+from google.auth.transport import requests as google_auth_transport_requests
+
+_CABE_SERVER_ADDRESS = 'cabe.skia.org'
+_CABE_USE_PLAINTEXT = False
+
+def GetAnalysis(job_id):
+  "Return a TryJob CABE Analysis as a list."
+  request = google_auth_transport_requests.Request()
+
+  if _CABE_USE_PLAINTEXT:
+    channel = grpc.insecure_channel(_CABE_SERVER_ADDRESS)
+  else:
+    credentials, project_id = google.auth.default()
+    logging.info("using default credentials for project_id: %s", project_id)
+    channel = google_auth_transport_grpc.secure_authorized_channel(
+        credentials=credentials, request=request, target=_CABE_SERVER_ADDRESS)
+
+  try:
+    stub = cabe_grpc_pb.AnalysisStub(channel)
+    request = cabe_pb.GetAnalysisRequest(pinpoint_job_id=job_id)
+    response = stub.GetAnalysis(request)
+    return response.results
+  finally:
+    channel.close()
