090a714e93335011394cbb75871c24cefedb45a7
Prakhar <pasthana@google.com>
2023-08-09 21:47:53 -0700

    Raise Exception to know code paths
    First commit - same as earlier one
    
    Change-Id: I43fe5baa80df236c3e42b088e64fa8202ed6a762

--- telemetry/telemetry/internal/backends/chrome/desktop_browser_backend.py
+++ telemetry/telemetry/internal/backends/chrome/desktop_browser_backend.py
@@ -167,8 +167,14 @@ class DesktopBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
       ])
 
     cmd = [self._executable]
-    if self.browser.platform.GetOSName() == 'mac':
+    if "START_BROWSER_IN_FOREGROUND" in os.environ and \
+      self.browser.platform.GetOSName() == 'mac':
+      # Start chrome on mac using `open`, when running benchmarks
+      # so that it starts with default priority
+      cmd = ['open', '-n', '-W', '-a', self._executable, '--args']
       cmd.append('--use-mock-keychain')  # crbug.com/865247
+    else:
+      cmd = [self._executable]
     cmd.extend(startup_args)
     cmd.append('about:blank')
     env = os.environ.copy()
