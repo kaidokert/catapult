60364f4b011fe434a43daa1d7b432e76b3762323
John Chen <johnchen@chromium.org>
2023-09-11 23:18:14 -0700

    Merge submodule support into depot_tools
    Copy some code from latest depot_tools into our code, to enable
    parsing DEPS file with submodule support.
    
    Bug: chromium:1479963
    Change-Id: I3a90c5b64f723673359edac35896362c69f63c48

--- third_party/depot_tools/depot_tools/gclient_eval.py
+++ third_party/depot_tools/depot_tools/gclient_eval.py
@@ -23,6 +23,13 @@ else:
   basestring = str
 
 
+# git_dependencies migration states. Used within the DEPS file to indicate
+# the current migration state.
+DEPS = 'DEPS'
+SYNC = 'SYNC'
+SUBMODULES = 'SUBMODULES'
+
+
 class Error(Exception):
   """gclient exception class."""
   def __init__(self, msg, *args, **kwargs):
@@ -164,6 +171,10 @@ _GCLIENT_HOOKS_SCHEMA = [
 
 _GCLIENT_SCHEMA = schema.Schema(
     _NodeDictSchema({
+        # Current state of the git submodule migration.
+        # git_dependencies = [DEPS (default) | SUBMODULES | SYNC]
+        schema.Optional('git_dependencies'): schema.Or(DEPS, SYNC, SUBMODULES),
+
         # List of host names from which dependencies are allowed (allowlist).
         # NOTE: when not present, all hosts are allowed.
         # NOTE: scoped to current DEPS file, not recursive.
