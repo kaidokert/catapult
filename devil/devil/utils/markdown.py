67c3a8f15537ba7ba0b1f003b532159c8c2563fb
Ho Cheung <uioptt24@gmail.com>
2023-10-26 22:07:16 +0800

    [devil] Remove imp usage
    And use its equivalent instead.
    
    Bug: chromium:1487454
    Change-Id: Iba2f2beec729f3a9d03eb825b5798d404b530955

--- devil/devil/utils/markdown.py
+++ devil/devil/utils/markdown.py
@@ -6,7 +6,7 @@
 from __future__ import print_function
 
 import argparse
-import imp
+import importlib.util
 import os
 import re
 import sys
@@ -170,9 +170,11 @@ def load_module_from_path(module_path):
     if module:
       d = module.__path__
       full_module_name += '.'
-    r = imp.find_module(package_name, d)
+    r = importlib.util.spec_from_file_location(
+        full_module_name, os.path.join(d[0], package_name + '.py'))
+    module = importlib.util.module_from_spec(r)
+    r.loader.exec_module(module)
     full_module_name += package_name
-    module = imp.load_module(full_module_name, *r)
   return module
 
 
