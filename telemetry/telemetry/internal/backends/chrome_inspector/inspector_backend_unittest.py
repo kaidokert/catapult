6518e1a43d0794112eb8175fb03cef5a8695950e
Camillia Smith Barnes <cammie@chromium.org>
2024-01-16 21:20:57 -0500

    Shared Storage: Add inspector/tab method to wait for events
    Bug: chromium:1516507
    Change-Id: I0439ee248696b26f4489de9b3be478ef689b6725

--- telemetry/telemetry/internal/backends/chrome_inspector/inspector_backend_unittest.py
+++ telemetry/telemetry/internal/backends/chrome_inspector/inspector_backend_unittest.py
@@ -6,6 +6,9 @@ from __future__ import absolute_import
 from telemetry import decorators
 from telemetry.core import exceptions
 from telemetry.testing import browser_test_case
+from telemetry.testing import tab_test_case
+
+import py_utils
 
 
 class InspectorBackendTest(browser_test_case.BrowserTestCase):
@@ -29,3 +32,105 @@ class InspectorBackendTest(browser_test_case.BrowserTestCase):
     backend = context_map.GetInspectorBackend(context_map.contexts[0]['id'])
     with self.assertRaises(exceptions.EvaluateException):
       backend.WaitForJavaScriptCondition('syntax error!')
+
+
+class InspectorBackendSharedStorageTest(tab_test_case.TabTestCase):
+  @classmethod
+  def CustomizeBrowserOptions(cls, options):
+    options.AppendExtraBrowserArgs([
+      '--enable-features=SharedStorageAPI,'
+      + 'FencedFrames:implementation_type/mparch,FencedFramesDefaultMode,'
+      + 'PrivacySandboxAdsAPIsOverride,DefaultAllowPrivacySandboxAttestations',
+      '--enable-privacy-sandbox-ads-apis'
+    ])
+
+  @property
+  def _devtools_client(self):
+    return self._browser._browser_backend.devtools_client
+
+  def testWaitForSharedStorageEventsStrict_Passes(self):
+    self._tab.Navigate(self.UrlOfUnittestFile('blank.html'))
+    context_map = self._devtools_client.GetUpdatedInspectableContexts()
+    backend = context_map.GetInspectorBackend(context_map.contexts[0]['id'])
+    enabled = backend.EvaluateJavaScript('Boolean(window.sharedStorage)')
+    if not enabled:
+      raise RuntimeError("Shared Storage not enabled")
+    backend.EnableSharedStorageNotifications()
+    backend.EvaluateJavaScript("window.sharedStorage.set('a', 'b')",
+                               promise=True)
+    backend.EvaluateJavaScript("window.sharedStorage.append('c', 'd')",
+                               promise=True)
+    backend.EvaluateJavaScript("window.sharedStorage.delete('a')",
+                               promise=True)
+    expected_events = [{'type': 'documentSet',
+                        'params': {'key': 'a', 'value': 'b'}},
+                       {'type': 'documentAppend',
+                        'params': {'key': 'c', 'value': 'd'}},
+                       {'type': 'documentDelete'}]
+    backend.WaitForSharedStorageEvents(expected_events, mode='strict')
+    backend.DisableSharedStorageNotifications()
+    backend.ClearSharedStorageNotifications()
+
+  def testWaitForSharedStorageEventsStrict_Fails(self):
+    self._tab.Navigate(self.UrlOfUnittestFile('blank.html'))
+    context_map = self._devtools_client.GetUpdatedInspectableContexts()
+    backend = context_map.GetInspectorBackend(context_map.contexts[0]['id'])
+    enabled = backend.EvaluateJavaScript('Boolean(window.sharedStorage)')
+    if not enabled:
+      raise RuntimeError("Shared Storage not enabled")
+    backend.EnableSharedStorageNotifications()
+    backend.EvaluateJavaScript("window.sharedStorage.set('a', 'b')",
+                               promise=True)
+    backend.EvaluateJavaScript("window.sharedStorage.delete('a')",
+                               promise=True)
+    expected_events = [{'type': 'documentDelete'},
+                       {'type': 'documentSet',
+                        'params': {'key': 'a', 'value': 'b'}}]
+    with self.assertRaises(py_utils.TimeoutException):
+      backend.WaitForSharedStorageEvents(expected_events, mode='strict',
+                                         timeout=10)
+    backend.DisableSharedStorageNotifications()
+    backend.ClearSharedStorageNotifications()
+
+  def testWaitForSharedStorageEventsRelaxed_Passes(self):
+    self._tab.Navigate(self.UrlOfUnittestFile('blank.html'))
+    context_map = self._devtools_client.GetUpdatedInspectableContexts()
+    backend = context_map.GetInspectorBackend(context_map.contexts[0]['id'])
+    enabled = backend.EvaluateJavaScript('Boolean(window.sharedStorage)')
+    if not enabled:
+      raise RuntimeError("Shared Storage not enabled")
+    backend.EnableSharedStorageNotifications()
+    backend.EvaluateJavaScript("window.sharedStorage.set('a', 'b')",
+                               promise=True)
+    backend.EvaluateJavaScript("window.sharedStorage.append('c', 'd')",
+                               promise=True)
+    backend.EvaluateJavaScript("window.sharedStorage.delete('a')",
+                               promise=True)
+    expected_events = [{'type': 'documentAppend'},
+                       {'type': 'documentDelete',
+                        'params': {'key': 'a'}}]
+    backend.WaitForSharedStorageEvents(expected_events, mode='relaxed')
+    backend.DisableSharedStorageNotifications()
+    backend.ClearSharedStorageNotifications()
+
+  def testWaitForSharedStorageEventsRelaxed_Fails(self):
+    self._tab.Navigate(self.UrlOfUnittestFile('blank.html'))
+    context_map = self._devtools_client.GetUpdatedInspectableContexts()
+    backend = context_map.GetInspectorBackend(context_map.contexts[0]['id'])
+    enabled = backend.EvaluateJavaScript('Boolean(window.sharedStorage)')
+    if not enabled:
+      raise RuntimeError("Shared Storage not enabled")
+    backend.EnableSharedStorageNotifications()
+    backend.EvaluateJavaScript("window.sharedStorage.set('a', 'b')",
+                               promise=True)
+    backend.EvaluateJavaScript("window.sharedStorage.delete('a')",
+                               promise=True)
+    expected_events = [{'type': 'documentSet',
+                        'params': {'key': 'a', 'value': 'b'}},
+                       {'type': 'documentDelete',
+                        'params': {'key': 'c'}}]
+    with self.assertRaises(py_utils.TimeoutException):
+      backend.WaitForSharedStorageEvents(expected_events, mode='relaxed',
+                                         timeout=10)
+    backend.DisableSharedStorageNotifications()
+    backend.ClearSharedStorageNotifications()
