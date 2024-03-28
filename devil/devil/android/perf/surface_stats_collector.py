f1bcedb629a9c455e978cc8b14c5f14df2fb434f
Peter Wen <wnwen@chromium.org>
2024-03-28 14:36:43 -0400

    Reduce unnecessary empty log lines
    Currently surface_stats_collector.py uses logging.warning to output all
    unexpected lines, even when these lines are empty, leading to entire
    terminals of unnecessary empty log lines.
    
    This CL updates surface_stats_collector.py to only output unexpected
    lines when they are non-empty.
    
    Bug: chromium:325292888
    Change-Id: Ic1b03e96897a4f527b70e5b00639e8c658288182

--- devil/devil/android/perf/surface_stats_collector.py
+++ devil/devil/android/perf/surface_stats_collector.py
@@ -211,12 +211,16 @@ def ParseFrameData(lines, parse_timestamps):
   pending_fence_timestamp = (1 << 63) - 1
 
   for line in results[1:]:
+    # Avoid outputting empty lines and bloating stderr.
+    if not line:
+      continue
+
     fields = line.split()
     if len(fields) != 3:
       logging.warning('Unexpected line: %s', line)
       continue
-    timestamp = to_long_int(fields[1])
 
+    timestamp = to_long_int(fields[1])
     if timestamp == pending_fence_timestamp:
       continue
     timestamp /= nanoseconds_per_millisecond
