e628ad001798612b90083e30543770e38a20eb30
Ashwin Verleker <ashwinpv@google.com>
2024-05-30 19:18:37 +0000

    test
    Change-Id: I9aee235fc48058322c517d8e3e44c0808e7e88ce

--- telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend.py
+++ telemetry/telemetry/internal/backends/chrome_inspector/devtools_client_backend.py
@@ -284,7 +284,15 @@ class _DevToolsClientBackend():
     return 0
 
   def _ListInspectableContexts(self):
-    return self._devtools_http.RequestJson('')
+    returnObj = {}
+    print('Getting inspectable contexts')
+    try:
+      returnObj = self._devtools_http.RequestJson('')
+      print('Got contexts: ' + str(returnObj))
+    except Exception as e:
+      print("Error: " + str(e))
+    return returnObj
+
 
   def RequestNewTab(self, timeout, in_new_window=False, url=None):
     """Creates a new tab, either in new window or current window.
