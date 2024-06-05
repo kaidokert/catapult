134313445b6d5e7a988877ac5a4f6f42fad8e8c8
Ben Joyce <bjoyce@google.com>
2024-06-05 11:27:55 -0700

    Move mock to use innate mock
    Need to remove use of third_party/mock.py which has some deprecated methods. We can use unittest mock which was moved into python after 3.3.
    
    Similar to https://chromium-review.googlesource.com/c/catapult/+/5330148
    
    Bug: chromium:40942322
    Change-Id: Ie27d979fb82776b0fc47ee5577ddbf428bf4be98

--- experimental/bisect_lib/fetch_intervening_revisions_test.py
+++ experimental/bisect_lib/fetch_intervening_revisions_test.py
@@ -13,7 +13,7 @@ _CATAPULT_PATH = os.path.abspath(os.path.join(
 sys.path.insert(0, os.path.join(_CATAPULT_PATH, 'third_party', 'mock'))
 
 # pylint: disable=wrong-import-position
-import mock
+from unittest import mock
 
 from bisect_lib import fetch_intervening_revisions
 # pylint: enable=wrong-import-position
