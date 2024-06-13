cb4ddafba03e929118dd601b6688ac46765d426c
Ashwin Verleker <ashwinpv@google.com>
2024-06-13 19:29:28 +0000

    Remove snap it tests
    Change-Id: I3dfcf1354cbd18bff5b62bf68998d36df6fd8eda

--- catapult_build/build_steps.py
+++ catapult_build/build_steps.py
@@ -98,14 +98,6 @@ _CATAPULT_TESTS = [
         'path': 'systrace/bin/run_tests',
     },
     {
-        'name': 'Snap-it Tests',
-        'path': 'telemetry/bin/run_snap_it_unittest',
-        'additional_args': ['--browser=reference',],
-        'uses_sandbox_env': True,
-        'disabled': ['android'],
-        'python_versions': [3],
-    },
-    {
         'name': 'Telemetry Tests with Stable Browser (Desktop)',
         'path': 'catapult_build/fetch_telemetry_deps_and_run_tests',
         'additional_args': [
